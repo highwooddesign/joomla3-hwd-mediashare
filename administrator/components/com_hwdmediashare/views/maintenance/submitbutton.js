684c0fac791c83c9a3a40c89a730a303b5e32552
Dave Horsfall <dave@horsfall.eu>
2014-03-13 10:43:03 +0000

    Updated submitbutton.js for maintenance to target only the run task, allowing us to submit the form for other tasks, such as the new cancel button.

--- administrator/components/com_hwdmediashare/views/maintenance/submitbutton.js
+++ administrator/components/com_hwdmediashare/views/maintenance/submitbutton.js
@@ -1,60 +1,71 @@
 Joomla.submitbutton = function(task)
-{
-        var tasks = new Array();
-        tasks[0]="cleancategorymap";
-        tasks[1]="cleantagmap";
-        tasks[2]="emptyuploadtokens";
-        tasks[3]="purgeoldprocesses";
+{      
+        if (task == 'maintenance.run')
+	{
+                var tasks = new Array();
+                tasks[0]="cleancategorymap";
+                tasks[1]="cleantagmap";
+                tasks[2]="emptyuploadtokens";
+                tasks[3]="purgeoldprocesses";
 
-        var maintenanceRequest = new Request({
-                url: 'index.php',
-                method: 'post',
-                link: 'chain',
-                async: false,
-                onRequest: function()
-                {
-                        $('ajax-container-' + task).empty().addClass('ajax-loading');
-                },
-                onComplete: function( response )
-                {
-                        try {
-                                var object = JSON.decode(response);
-                        } catch(e) {
+                var maintenanceRequest = new Request({
+                        url: 'index.php',
+                        method: 'post',
+                        link: 'chain',
+                        async: false,
+                        onRequest: function()
+                        {
+                                $('ajax-container-' + task).empty().addClass('ajax-loading');
+                        },
+                        onComplete: function( response )
+                        {
+                                try {
+                                        var object = JSON.decode(response);
+                                } catch(e) {
+                                        $('ajax-container-' + task).empty();
+                                        $('ajax-container-' + task).removeClass('ajax-loading');
+                                        $('ajax-container-' + task).set('html', '<span class="label label-danger">Failed</span>');
+                                }
+
+                                try {
+                                        if (object['success'] == 1)
+                                        {
+                                                $('ajax-container-' + task).empty();
+                                                $('ajax-container-' + task).removeClass('ajax-loading');
+                                                $('ajax-container-' + task).set('html', '<span class="label label-success">Success</span>');                                    
+                                        }
+                                        else
+                                        {
+                                                $('ajax-container-' + task).empty();
+                                                $('ajax-container-' + task).removeClass('ajax-loading');
+                                                $('ajax-container-' + task).set('html', '<span class="label label-danger">Failed</span>'); 
+                                        }
+                                } catch(e) {
+                                        $('ajax-container-' + task).empty();
+                                        $('ajax-container-' + task).removeClass('ajax-loading');
+                                        $('ajax-container-' + task).set('html', '<span class="label label-danger">Failed</span>');
+                                }                                    
+                        },
+                        onFailure: function()
+                        {
                                 $('ajax-container-' + task).empty();
                                 $('ajax-container-' + task).removeClass('ajax-loading');
                                 $('ajax-container-' + task).set('html', '<span class="label label-danger">Failed</span>');
                         }
-                        
-                        try {
-                                if (object['success'] == 1)
-                                {
-                                        $('ajax-container-' + task).empty();
-                                        $('ajax-container-' + task).removeClass('ajax-loading');
-                                        $('ajax-container-' + task).set('html', '<span class="label label-success">Success</span>');                                    
-                                }
-                                else
-                                {
-                                        $('ajax-container-' + task).empty();
-                                        $('ajax-container-' + task).removeClass('ajax-loading');
-                                        $('ajax-container-' + task).set('html', '<span class="label label-danger">Failed</span>'); 
-                                }
-                        } catch(e) {
-                                $('ajax-container-' + task).empty();
-                                $('ajax-container-' + task).removeClass('ajax-loading');
-                                $('ajax-container-' + task).set('html', '<span class="label label-danger">Failed</span>');
-                        }                                    
-                },
-                onFailure: function()
+                });
+
+                for (var i = 0; i < tasks.length; i++)
                 {
-                        $('ajax-container-' + task).empty();
-                        $('ajax-container-' + task).removeClass('ajax-loading');
-                        $('ajax-container-' + task).set('html', '<span class="label label-danger">Failed</span>');
+                        var task = tasks[i];
+                        maintenanceRequest.send('option=com_hwdmediashare&task=maintenance.run&format=raw&maintenance=' + task);
                 }
-        });
-        
-        for (var i = 0; i < tasks.length; i++)
+                
+	}
+        else
         {
-                var task = tasks[i];
-                maintenanceRequest.send('option=com_hwdmediashare&task=maintenance.run&format=raw&maintenance=' + task);
+                Joomla.submitform(task);
+                return true;
         }
+        
+        return false;
 }
