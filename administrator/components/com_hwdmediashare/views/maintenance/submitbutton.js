8b488eeb5d26adcd069eb624765df25a69864c06
Dave Horsfall <dave@horsfall.eu>
2014-03-13 10:18:41 +0000

    Updated administrator maintenance submitbutton.js javascript

--- administrator/components/com_hwdmediashare/views/maintenance/submitbutton.js
+++ administrator/components/com_hwdmediashare/views/maintenance/submitbutton.js
@@ -1,18 +1,10 @@
-/**
- * @version    SVN $Id: submitbutton.js 277 2012-03-28 10:03:31Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2011 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      15-Apr-2011 10:13:15
- */
-
 Joomla.submitbutton = function(task)
 {
         var tasks = new Array();
         tasks[0]="cleancategorymap";
         tasks[1]="cleantagmap";
         tasks[2]="emptyuploadtokens";
+        tasks[3]="purgeoldprocesses";
 
         var maintenanceRequest = new Request({
                 url: 'index.php',
@@ -25,22 +17,38 @@ Joomla.submitbutton = function(task)
                 },
                 onComplete: function( response )
                 {
-                        var object = JSON.decode(response);
-                        if (object['success'] == 1)
-                        {
-                                var html = '<div class="jgrid"><span class="state publish"><span class="text">Success</span></span></div>';
+                        try {
+                                var object = JSON.decode(response);
+                        } catch(e) {
+                                $('ajax-container-' + task).empty();
+                                $('ajax-container-' + task).removeClass('ajax-loading');
+                                $('ajax-container-' + task).set('html', '<span class="label label-danger">Failed</span>');
                         }
-                        else
-                        {
-                                $('ajax-container-' + task).addClass('hasResult').set('title', object['data']['error_msg']);
-                                var html = '<div class="jgrid"><span class="state unpublish"><span class="text">Fail</span></span></div>';
-                                var JTooltips = new Tips($$('.hasResult'));
-                        }                
-                        $('ajax-container-' + object['data']['task']).removeClass('ajax-loading').set('html', html);
+                        
+                        try {
+                                if (object['success'] == 1)
+                                {
+                                        $('ajax-container-' + task).empty();
+                                        $('ajax-container-' + task).removeClass('ajax-loading');
+                                        $('ajax-container-' + task).set('html', '<span class="label label-success">Success</span>');                                    
+                                }
+                                else
+                                {
+                                        $('ajax-container-' + task).empty();
+                                        $('ajax-container-' + task).removeClass('ajax-loading');
+                                        $('ajax-container-' + task).set('html', '<span class="label label-danger">Failed</span>'); 
+                                }
+                        } catch(e) {
+                                $('ajax-container-' + task).empty();
+                                $('ajax-container-' + task).removeClass('ajax-loading');
+                                $('ajax-container-' + task).set('html', '<span class="label label-danger">Failed</span>');
+                        }                                    
                 },
                 onFailure: function()
                 {
-                        var html = '<div class="jgrid"><span class="state unpublish"><span class="text">Fail</span></span></div>';
+                        $('ajax-container-' + task).empty();
+                        $('ajax-container-' + task).removeClass('ajax-loading');
+                        $('ajax-container-' + task).set('html', '<span class="label label-danger">Failed</span>');
                 }
         });
         
