5407369b3e081debfb4e40591e773b66d3381611
Dave Horsfall <dave@horsfall.eu>
2014-03-13 09:50:33 +0000

    Updated administrator user view

--- administrator/components/com_hwdmediashare/views/user/view.html.php
+++ administrator/components/com_hwdmediashare/views/user/view.html.php
@@ -1,115 +1,95 @@
 <?php
 /**
- * @version    SVN $Id: view.html.php 277 2012-03-28 10:03:31Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2011 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      15-Apr-2011 10:13:15
+ * @package     Joomla.administrator
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
 
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
 
-// import Joomla view library
-jimport('joomla.application.component.view');
+class hwdMediaShareViewUser extends JViewLegacy 
+{
+	protected $state;
 
-/**
- * hwdMediaShare View
- */
-class hwdMediaShareViewUser extends JViewLegacy {
+	protected $item;
+
+	protected $form;
+        
 	/**
-	 * display method of Hello view
-	 * @return void
+	 * Display the view
+	 *
+	 * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
+	 *
+	 * @return  void
 	 */
 	public function display($tpl = null)
 	{
-                // get the Data
-		$form = $this->get('Form');
-                $item = $this->get('Item');
-		$script = $this->get('Script');
+                // Get data from the model.
+		$this->state	= $this->get('State');
+		$this->item	= $this->get('Item');
+		$this->form	= $this->get('Form');
 
-		// Check for errors.
-		if (count($errors = $this->get('Errors')))
-		{
-			JError::raiseError(500, implode('<br />', $errors));
-			return false;
-		}
+                // Check for errors.
+                if (count($errors = $this->get('Errors')))
+                {
+                        JError::raiseError(500, implode('<br />', $errors));
+                        return false;
+                }
 
-		// Assign the Data
-		$this->form = $form;
-		$this->item = $item;
-		$this->script = $script;
-
-		// Set the toolbar
-		$this->addToolBar();
+		$this->addToolbar();
+		$this->sidebar = JHtmlSidebar::render();
 
 		// Display the template
 		parent::display($tpl);
-
-		// Set the document
-		$this->setDocument();
 	}
 
 	/**
-	 * Setting the toolbar
+	 * Add the page title and toolbar.
+	 *
+	 * @return  void
 	 */
 	protected function addToolBar()
 	{
-		JRequest::setVar('hidemainmenu', true);
-		$user = JFactory::getUser();
-		$userId = $user->id;
-		$isNew = $this->item->id == 0;
-		$canDo = hwdMediaShareHelper::getActions($this->item->id, 'user');
-		JToolBarHelper::title($isNew ? JText::_('COM_HWDMS_NEW_USER_CHANNEL') : JText::_('COM_HWDMS_EDIT_USER_CHANNEL'), 'hwdmediashare');
-		// Built the actions for new and existing records.
-		if ($isNew)
+		JFactory::getApplication()->input->set('hidemainmenu', true);
+
+		$user		= JFactory::getUser();
+		$isNew		= ($this->item->id == 0);
+		$checkedOut	= !($this->item->checked_out == 0 || $this->item->checked_out == $user->get('id'));
+
+		// Since we don't track these assets at the item level, use the category id.
+		$canDo		= hwdMediaShareHelper::getActions($this->item->id, 'user');
+
+		JToolBarHelper::title($isNew ? JText::_('COM_HWDMS_NEW_USER_CHANNEL') : JText::_('COM_HWDMS_EDIT_USER_CHANNEL'), 'user');
+
+		// If not checked out, can save the item.
+		if (!$checkedOut && ($canDo->get('core.edit')||(count($user->getAuthorisedCategories('com_hwdmediashare', 'core.create')))))
 		{
-			// For new records, check the create permission.
-			if ($canDo->get('core.create'))
-			{
-				JToolBarHelper::apply('user.apply', 'JTOOLBAR_APPLY');
-				JToolBarHelper::save('user.save', 'JTOOLBAR_SAVE');
-				JToolBarHelper::custom('user.save2new', 'save-new.png', 'save-new_f2.png', 'JTOOLBAR_SAVE_AND_NEW', false);
-			}
-			JToolBarHelper::cancel('user.cancel', 'JTOOLBAR_CANCEL');
+			JToolbarHelper::apply('user.apply');
+			JToolbarHelper::save('user.save');
+		}
+		if (!$checkedOut && (count($user->getAuthorisedCategories('com_hwdmediashare', 'core.create'))))
+		{
+			JToolbarHelper::save2new('user.save2new');
+		}
+		// If an existing item, can save to a copy.
+		if (!$isNew && (count($user->getAuthorisedCategories('com_hwdmediashare', 'core.create')) > 0))
+		{
+			JToolbarHelper::save2copy('user.save2copy');
+		}
+		if (empty($this->item->id))
+		{
+			JToolbarHelper::cancel('user.cancel');
 		}
 		else
 		{
-			if ($canDo->get('core.edit'))
-			{
-				// We can save the new record
-				JToolBarHelper::apply('user.apply', 'JTOOLBAR_APPLY');
-				JToolBarHelper::save('user.save', 'JTOOLBAR_SAVE');
-
-				// We can save this record, but check the create permission to see if we can return to make a new one.
-				if ($canDo->get('core.create'))
-				{
-					JToolBarHelper::custom('user.save2new', 'save-new.png', 'save-new_f2.png', 'JTOOLBAR_SAVE_AND_NEW', false);
-				}
-			}
-			if ($canDo->get('core.create'))
-			{
-				JToolBarHelper::custom('user.save2copy', 'save-copy.png', 'save-copy_f2.png', 'JTOOLBAR_SAVE_AS_COPY', false);
-			}
-			JToolBarHelper::cancel('user.cancel', 'JTOOLBAR_CLOSE');
+			JToolbarHelper::cancel('user.cancel', 'JTOOLBAR_CLOSE');
 		}
-                JToolBarHelper::divider();
-                JToolBarHelper::custom('help', 'help.png', 'help.png', 'JHELP', false);
-	}
-	/**
-	 * Method to set up the document properties
-	 *
-	 * @return void
-	 */
-	protected function setDocument()
-	{
-		$isNew = $this->item->id == 0;
-		$document = JFactory::getDocument();
-		$document->setTitle($isNew ? JText::_('COM_HWDMS_HWDMEDIASHARE').' '.JText::_('COM_HWDMS_NEW_USER_CHANNEL') : JText::_('COM_HWDMS_HWDMEDIASHARE').' '.JText::_('COM_HWDMS_EDIT_USER_CHANNEL'));
-		$document->addScript(JURI::root() . $this->script);
-		$document->addScript(JURI::root() . "/administrator/components/com_hwdmediashare/views/".JRequest::getCmd('view')."/submitbutton.js");
-                JText::script('COM_HWDMS_ERROR_UNACCEPTABLE');
+
+		JToolbarHelper::divider();
+		JToolBarHelper::custom('help', 'help.png', 'help.png', 'JHELP', false);  
 	}
 }
 
