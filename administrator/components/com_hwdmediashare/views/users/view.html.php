eccb9d98cf23c7acfd36704fac96cddb21d7c4b3
Dave Horsfall <dave@horsfall.eu>
2014-03-13 09:51:06 +0000

    Updated administrator users view

--- administrator/components/com_hwdmediashare/views/users/view.html.php
+++ administrator/components/com_hwdmediashare/views/users/view.html.php
@@ -1,34 +1,35 @@
 <?php
 /**
- * @version    SVN $Id: view.html.php 493 2012-08-28 13:20:17Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2011 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      15-Apr-2011 10:13:15
+ * @package     Joomla.administrator
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
 
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
 
-// import Joomla view library
-jimport('joomla.application.component.view');
-
-/**
- * hwdMediaShare View
- */
-class hwdMediaShareViewUsers extends JViewLegacy {
+class hwdMediaShareViewUsers extends JViewLegacy
+{
 	var $name = "users";
-        /**
-	 * display method of Hello view
-	 * @return void
+        
+	/**
+	 * Display the view
+	 *
+	 * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
+	 *
+	 * @return  void
 	 */
 	function display($tpl = null)
 	{
-                // Get data from the model
-                $items = $this->get('Items');
-                $pagination = $this->get('Pagination');
-		$state	= $this->get('State');
+                // Get data from the model.
+                $this->items = $this->get('Items');
+                $this->pagination = $this->get('Pagination');
+		$this->state = $this->get('State');
+
+                hwdMediaShareFactory::load('downloads');
+                hwdMediaShareFactory::load('files');
 
                 // Check for errors.
                 if (count($errors = $this->get('Errors')))
@@ -36,29 +37,32 @@ class hwdMediaShareViewUsers extends JViewLegacy {
                         JError::raiseError(500, implode('<br />', $errors));
                         return false;
                 }
-
-                // Assign data to the view
-                $this->items = $items;
-                $this->pagination = $pagination;
-                $this->state = $state;
-
-		// Set the toolbar
-		$this->addToolBar();
-
+                
+		// We don't need toolbar in the modal window.
+		if ($this->getLayout() !== 'modal')
+		{
+			$this->addToolbar();
+			$this->sidebar = JHtmlSidebar::render();
+		}
+                
 		// Display the template
 		parent::display($tpl);
-
-		// Set the document
-		$this->setDocument();
 	}
 
 	/**
-	 * Setting the toolbar
+	 * Add the page title and toolbar.
+	 *
+	 * @return  void
 	 */
 	protected function addToolBar()
 	{
 		$canDo = hwdMediaShareHelper::getActions();
-		JToolBarHelper::title(JText::_('COM_HWDMS_USER_CHANNELS'), 'hwdmediashare');
+		$user  = JFactory::getUser();
+                
+		// Get the toolbar object instance
+		$bar = JToolBar::getInstance('toolbar');
+                                
+		JToolBarHelper::title(JText::_('COM_HWDMS_USER_CHANNELS'), 'user');
 
                 if ($canDo->get('core.create'))
 		{
@@ -92,90 +96,5 @@ class hwdMediaShareViewUsers extends JViewLegacy {
                         JToolBarHelper::divider();
 		}
                 JToolBarHelper::custom('help', 'help.png', 'help.png', 'JHELP', false);
-	}
- 	/**
-	 * Method to set up the document properties
-	 *
-	 * @return void
-	 */
-	protected function setDocument()
-	{
-		$document = JFactory::getDocument();
-                $document->addScript(JURI::root() . "/administrator/components/com_hwdmediashare/views/".JRequest::getCmd('view')."/submitbutton.js");
-		$document->setTitle(JText::_('COM_HWDMS_HWDMEDIASHARE').' '.JText::_('COM_HWDMS_USER_CHANNELS'));
-	}
-	/**
-	 * Method to get the publish status HTML
-	 *
-	 * @param	object	Field object
-	 * @param	string	Type of the field
-	 * @param	string	The ajax task that it should call
-	 * @return	string	HTML source
-	 **/
-	public function getPublish( &$row, $type, $i )
-	{
-                unset($state);
-                unset($func);
-                unset($alt);
-
-                if ($type == "created")
-                {
-                        $state = $row->$type ? 'publish' : 'expired';
-                        $alt  = $row->$type ? JText::_('COM_HWDMS_APPROVED') : JText::_('COM_HWDMS_UNAPPROVED');
-
-                        $image = '<span class="state '.$state.'"><span class="text">'.$alt.'</span></span>';
-
-                        $href = '<div class="jgrid">';
-                        $href .= $image.'</div>';
-
-                        return $href;
-                }
-
-                $state = $row->$type ? 'publish' : 'unpublish';
-
-                if ($row->created)
-                {
-                        if ($type == "status")
-                        {
-                                $func = ($row->$type == 1) ? 'unapprove' : 'approve';
-                                $alt  = $row->$type ? JText::_('COM_HWDMS_APPROVED') : JText::_('COM_HWDMS_UNAPPROVED');
-                                if ($row->$type == 2)
-                                {
-                                        $state = 'pending';
-                                        $func = 'approve';
-                                        $alt = JText::_('COM_HWDMS_PENDING');
-                                }
-                                else if ($row->$type == 3)
-                                {
-                                        $state = 'expired';
-                                        $func = 'approve';
-                                        $alt = JText::_('COM_HWDMS_REPORTED');
-                                }
-                        }
-                        else if ($type == "featured")
-                        {
-                                $func = $row->$type ? 'unfeature' : 'feature';
-                                $alt  = $row->$type ? JText::_('COM_HWDMS_FEATURE') : JText::_('COM_HWDMS_UNFEATURE');
-                        }
-                }
-                else
-                {
-                        $state = 'unpublish';
-                        $alt  = JText::_('COM_HWDMS_UNPUBLISH');
-                        
-                        $image = '<span class="state '.$state.'"><span class="text">'.$alt.'</span></span>';
-
-                        $href = '<div class="jgrid">';
-                        $href .= $image.'</div>';
-
-                        return $href;
-                }
-
-                $image = '<span class="state '.$state.'"><span class="text">'.$alt.'</span></span>';
-
-                $href = '<a class="jgrid" href="javascript:void(0);" onclick="return listItemTask(\'cb'.$i.'\',\'users.'.$func.'\')" title="'.JText::_($alt).'">';
-                $href .= $image.'</a>';
-
-		return $href;
-	}
+	}        
 }
