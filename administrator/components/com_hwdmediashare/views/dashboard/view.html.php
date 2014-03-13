1f02545471228d94e33faf25c5b8479c2a7b5129
Dave Horsfall <dave@horsfall.eu>
2014-03-13 13:11:27 +0000

    Updated administrator dashboard view

--- administrator/components/com_hwdmediashare/views/dashboard/view.html.php
+++ administrator/components/com_hwdmediashare/views/dashboard/view.html.php
@@ -1,75 +1,73 @@
 <?php
 /**
- * @version    SVN $Id: view.html.php 492 2012-08-24 15:11:58Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2011 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      15-Apr-2011 10:13:15
+ * @package     Joomla.administrator
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
 
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
 
-// import Joomla view library
-jimport('joomla.application.component.view');
-
-/**
- * hwdMediaShare View
- */
-class hwdMediaShareViewDashboard extends JViewLegacy {
+class hwdMediaShareViewDashboard extends JViewLegacy
+{
 	/**
-	 * display method of Hello view
-	 * @return void
+	 * Display the view
+	 *
+	 * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
+	 *
+	 * @return  void
 	 */
 	public function display($tpl = null)
 	{
-                $media = $this->get('Media');
-                $version = $this->get('Version');
-                $nummedia = $this->get('CountMedia');
-                $numcategories = $this->get('CountCategories');
-                $numalbums = $this->get('CountAlbums');
-                $numgroups = $this->get('CountGroups');
-                $numchannels = $this->get('CountChannels');
-                $numplaylists = $this->get('CountPlaylists');
-
-                // Check for errors.
-		if (count($errors = $this->get('Errors')))
-		{
-			JError::raiseError(500, implode('<br />', $errors));
-			return false;
-		}
-
-                $this->assign( 'version', $version );
-                $this->assign( 'nummedia', $nummedia );
-                $this->assign( 'numcategories', $numcategories );
-                $this->assign( 'numalbums', $numalbums );
-                $this->assign( 'numgroups', $numgroups );
-                $this->assign( 'numchannels', $numchannels );
-                $this->assign( 'numplaylists', $numplaylists );
+                // Get data from the model
+                $this->media = $this->get('Media');
+                $this->activity = $this->get('Activity');
+                $this->version = $this->get('Version');
+                $this->nummedia = $this->get('CountMedia');
+                $this->numcategories = $this->get('CountCategories');
+                $this->numalbums = $this->get('CountAlbums');
+                $this->numgroups = $this->get('CountGroups');
+                $this->numchannels = $this->get('CountChannels');
+                $this->numplaylists = $this->get('CountPlaylists');
                 
-		jimport( 'joomla.html.html.sliders' );
+                // Load HWD libaries
+                hwdMediaShareFactory::load('activities');
+                
+                // Check for errors
+                if (count($errors = $this->get('Errors')))
+                {
+                        JError::raiseError(500, implode('<br />', $errors));
+                        return false;
+                }
 
-                $this->assignRef( 'media', $media );
-
-		// Set the toolbar
-		$this->addToolBar();
-
+		// We don't need toolbar in the modal window.
+		if ($this->getLayout() !== 'modal')
+		{
+			$this->addToolbar();
+			$this->sidebar = JHtmlSidebar::render();
+		}
+                
 		// Display the template
 		parent::display($tpl);
-
-		// Set the document
-		$this->setDocument();
 	}
 
 	/**
-	 * Setting the toolbar
+	 * Add the page title and toolbar.
+	 *
+	 * @return  void
 	 */
 	protected function addToolBar()
 	{
-                JToolBarHelper::title(JText::_('COM_HWDMS_DASHBOARD'), 'hwdmediashare');
-                JToolBarHelper::custom('help', 'help.png', 'help.png', 'JHELP', false);
+		$canDo = hwdMediaShareHelper::getActions();
+		$user  = JFactory::getUser();
                 
+		// Get the toolbar object instance
+		$bar = JToolBar::getInstance('toolbar');
+                
+		JToolBarHelper::title(JText::_('COM_HWDMS_DASHBOARD'), 'home');
+
                 // Sample data install option
                 if ($this->nummedia == 0 && $this->numcategories == 0 && $this->numalbums == 0 && $this->numgroups == 0 && $this->numchannels == 0 && $this->numplaylists == 0) 
                 {
@@ -77,34 +75,6 @@ class hwdMediaShareViewDashboard extends JViewLegacy {
                         $document->addStyleDeclaration('.icon-32-sample {background-image: url(../media/com_hwdmediashare/assets/images/icons/32/sample-data.png);width:98px!important;}');
                         JToolBarHelper::custom('sample.install', 'sample.png', 'sample_f2.png', JText::_('COM_HWDMS_INSTALL_SAMPLE_DATA'), true);
                 }
-	}
-	/**
-	 * Method to set up the document properties
-	 *
-	 * @return void
-	 */
-	protected function setDocument()
-	{
-		$document = JFactory::getDocument();
-                $document->addScript(JURI::root() . "/administrator/components/com_hwdmediashare/views/dashboard/submitbutton.js");
-		$document->setTitle(JText::_('COM_HWDMS_HWDMEDIASHARE').' '.JText::_('COM_HWDMS_DASHBOARD'));
-        }
-
-	function addIcon( $image, $url, $text, $newWindow = false, $modal = false )
-	{
-		$lang		=& JFactory::getLanguage();
-
-		$newWindow	= ( $newWindow ) ? ' target="_blank"' : '';
-		$modal          = ( $modal ) ? ' class="modal" rel="{handler: \'iframe\', size: {x: 415, y: 250}}" ' : '';
-                ?>
-		<div style="float:<?php echo ($lang->isRTL()) ? 'right' : 'left'; ?>;">
-			<div class="icon">
-				<a href="<?php echo $url; ?>"<?php echo $newWindow; ?><?php echo $modal; ?>>
-					<?php echo JHtml::_('image', 'media/com_hwdmediashare/assets/images/icons/48/' . $image , NULL, NULL ); ?>
-					<span><?php echo $text; ?></span>
-                                </a>
-			</div>
-		</div>
-                <?php
+                JToolbarHelper::help('HWD', false, 'http://hwdmediashare.co.uk/learn/docs');
 	}
 }
