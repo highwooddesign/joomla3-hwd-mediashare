5f58d89f2db40ce721857d739e664c04e53b8a83
Dave Horsfall <dave@horsfall.eu>
2014-03-13 13:10:43 +0000

    Updated administrator configuration view

--- administrator/components/com_hwdmediashare/views/configuration/view.html.php
+++ administrator/components/com_hwdmediashare/views/configuration/view.html.php
@@ -1,106 +1,79 @@
 <?php
 /**
- * @version    SVN $Id: view.html.php 938 2013-01-25 15:52:30Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2011 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      15-Apr-2011 10:13:15
+ * @package     Joomla.administrator
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
 
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
-
-// import Joomla view library
-jimport('joomla.application.component.view');
 
 /**
- * hwdMediaShare View
+ * UNFINISHED
  */
-class hwdMediaShareViewConfiguration extends JViewLegacy {
+defined('_JEXEC') or die;
+
+class hwdMediaShareViewConfiguration extends JViewLegacy 
+{
 	/**
-	 * Method to display the view.
+	 * Display the view
+	 *
+	 * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
+	 *
+	 * @return  void
 	 */
 	public function display($tpl = null)
 	{
-                $hwdms = hwdMediaShareFactory::getInstance();          
-                $config = $hwdms->getConfig();
-
-                // get the Data
-		$form = $this->get('Form');
-		$script = $this->get('Script');
-
-		// Check for errors.
-		if (count($errors = $this->get('Errors')))
-		{
-			JError::raiseError(500, implode('<br />', $errors));
-			return false;
-		}
-
-                // Bind the form to the data.
-		if ($form && $config)
+                // Get data from the model.
+		$this->state	= $this->get('State');
+		$this->item	= $this->get('Item');
+		$this->form	= $this->get('Form');
+                
+                // Check for errors.
+                if (count($errors = $this->get('Errors')))
                 {
-			$form->bind($config);
+                        JError::raiseError(500, implode('<br />', $errors));
+                        return false;
+                }
+
+		// We don't need toolbar in the modal window.
+		if ($this->getLayout() !== 'modal')
+		{
+			$this->addToolbar();
+			$this->sidebar = JHtmlSidebar::render();
 		}
-
-                // Assign the Data
-		$this->form = $form;
-		$this->config = $config;
-		$this->script = $script;
-
-		// Set the toolbar
-		$this->addToolBar();
-		$this->setSubMenu();
-
+                
 		// Display the template
 		parent::display($tpl);
-
-		// Set the document
-		$this->setDocument();
+                
+		$document = JFactory::getDocument();
+		$document->addStyleSheet(JURI::root() . "media/com_hwdmediashare/assets/css/administrator.css"); 
+                $document->addScript(JURI::root() . "/administrator/components/com_hwdmediashare/views/maintenance/submitbutton.js");
 	}
 
 	/**
-	 * Setting the toolbar
+	 * Add the page title and toolbar.
+	 *
+	 * @return  void
 	 */
 	protected function addToolBar()
 	{
-		JToolBarHelper::title(JText::_('COM_HWDMS_CONFIGURATION'), 'hwdmediashare');
+		JFactory::getApplication()->input->set('hidemainmenu', true);
 
-                // We can save the new record
-                JToolBarHelper::apply('configuration.apply');
-                JToolBarHelper::save('configuration.save');
-                JToolBarHelper::divider();
-                JToolBarHelper::cancel('configuration.cancel');
-                JToolBarHelper::divider();
-                JToolBarHelper::help('JHELP_SITE_GLOBAL_CONFIGURATION');
-	}
-        
-	/**
-	 * Setup the SubMenu
-	 *
-	 * @since	1.6
-	 */
-	protected function setSubMenu()
-	{
-                // Load submenu template, using element id 'submenu' as needed by behavior.switcher
-		$contents = $this->loadTemplate('navigation');
-		$document = JFactory::getDocument();
-		$document->setBuffer($contents, 'modules', 'submenu');
-	}        
+		// Since we don't track these assets at the item level, use the category id.
+		$canDo		= hwdMediaShareHelper::getActions();
 
-	/**
-	 * Method to set up the document properties
-	 *
-	 * @return void
-	 */
-	protected function setDocument()
-	{
-		$document = JFactory::getDocument();
-		$document->setTitle(JText::_('COM_HWDMS_HWDMEDIASHARE').' '.JText::_('COM_HWDMS_CONFIGURATION'));
-		$document->addScript(JURI::root() . $this->script);
-		// Prevent user end javascript submission as it generally causes significnat delay
-                //$document->addScript(JURI::root() . "/administrator/components/com_hwdmediashare/views/".JRequest::getCmd('view')."/submitbutton.js");
-		$document->addStyleSheet(JURI::root() . "media/com_hwdmediashare/assets/css/administrator.css");
-                JText::script('COM_HWDMS_ERROR_UNACCEPTABLE');
+		JToolBarHelper::title(JText::_('COM_HWDMS_CONFIGURATION'), 'cog');
+
+		// If not checked out, can save the item.
+		if ($canDo->get('core.edit'))
+		{
+			JToolbarHelper::apply('configuration.apply');
+			JToolbarHelper::save('configuration.save');
+		}
+		JToolbarHelper::cancel('configuration.cancel', 'JTOOLBAR_CLOSE');
+		JToolbarHelper::divider();
+		JToolbarHelper::help('HWD', false, 'http://hwdmediashare.co.uk/learn/docs'); 
 	}
 }
