7621205ebfe82fddae51bb273fdb4673a7be439c
Dave Horsfall <dave@horsfall.eu>
2014-03-14 12:06:28 +0000

    Updated extension view edit template.

--- administrator/components/com_hwdmediashare/views/extension/tmpl/edit.php
+++ administrator/components/com_hwdmediashare/views/extension/tmpl/edit.php
@@ -1,49 +1,89 @@
 <?php
 /**
- * @version    SVN $Id: edit.php 277 2012-03-28 10:03:31Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2011 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      15-Apr-2011 10:13:15
+ * @package     Joomla.administrator
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
 
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
+
+// Include the component HTML helpers.
+JHtml::addIncludePath(JPATH_COMPONENT . '/helpers/html');
+
+JHtml::_('behavior.formvalidation');
+JHtml::_('behavior.keepalive');
+JHtml::_('formbehavior.chosen', 'select');
 JHtml::_('behavior.tooltip');
 JHtml::_('behavior.modal');
-JHtml::_('behavior.formvalidation');
+
+$app = JFactory::getApplication();
+$input = $app->input;
+$isNew = $this->item->id == 0 ? true : false ;
 ?>
-<form action="<?php echo JRoute::_('index.php?option=com_hwdmediashare&layout=edit&id='.(int) $this->item->id); ?>" method="post" name="adminForm" id="adminForm" class="form-validate">
-	<div class="width-60 fltlft">
-		<fieldset class="adminform">
-			<legend><?php echo JText::_( 'COM_HWDMS_FILE_EXTENSION' ); ?></legend>
-			<ul class="adminformlist">
-                                <?php foreach($this->form->getFieldset('details') as $field): ?>
-                                        <li><?php echo $field->label;echo $field->input;?></li>
-                                <?php endforeach; ?>
-			</ul>
-                </fieldset>
+<script type="text/javascript">
+	Joomla.submitbutton = function(task)
+	{
+		if (task == 'extension.cancel' || document.formvalidator.isValid(document.id('item-form')))
+		{
+			Joomla.submitform(task, document.getElementById('item-form'));
+		}
+	}
+</script>
+<form action="<?php echo JRoute::_('index.php?option=com_hwdmediashare&view=extension&layout=edit&id=' . (int) $this->item->id); ?>" method="post" name="adminForm" id="item-form" class="form-validate" enctype="multipart/form-data">
+
+        <div class="form-inline form-inline-header">
+                <div class="control-group">
+                        <div class="control-label">
+                                <?php echo $this->form->getLabel('ext'); ?>
+                        </div>
+                        <div class="controls">
+                                <?php echo $this->form->getInput('ext'); ?>
+                        </div>
+                </div>
         </div>
+    
+	<div class="form-horizontal">
+            
+		<?php echo JHtml::_('bootstrap.startTabSet', 'myTab', array('active' => 'general')); ?>
 
-	<div class="width-40 fltrt">
-		<?php echo JHtml::_('sliders.start', 'hwdmediashare-slider'); ?>
-                <?php echo JHtml::_('sliders.panel', JText::_( 'COM_HWDMS_PUBLISHING' ), 'publishing');?>
-                <fieldset class="adminform" >
-                        <ul class="panelform">
-                            <?php foreach($this->form->getFieldset('publishing') as $field): ?>
-                                <li><?php echo $field->label;echo $field->input;?></li>
-                            <?php endforeach; ?>
-                        </ul>
-                </fieldset>
-                <?php echo JHtml::_('sliders.end'); ?>
-	</div>
+		<?php echo JHtml::_('bootstrap.addTab', 'myTab', 'general', JText::_('COM_HWDMS_FILE_EXTENSION', true)); ?>
+                <div class="control-group">
+                        <div class="control-label">
+                                <?php echo $this->form->getLabel('media_type'); ?>
+                        </div>
+                        <div class="controls">
+                                <?php echo $this->form->getInput('media_type'); ?>
+                        </div>
+                </div>
+                <div class="control-group">
+                        <div class="control-label">
+                                <?php echo $this->form->getLabel('published'); ?>
+                        </div>
+                        <div class="controls">
+                                <?php echo $this->form->getInput('published'); ?>
+                        </div>
+                </div>
+                <div class="control-group">
+                        <div class="control-label">
+                                <?php echo $this->form->getLabel('access'); ?>
+                        </div>
+                        <div class="controls">
+                                <?php echo $this->form->getInput('access'); ?>
+                        </div>
+                </div>
+		<?php echo JHtml::_('bootstrap.endTab'); ?>
 
-        <div class="clr"> </div>
+                <?php echo JHtml::_('bootstrap.addTab', 'myTab', 'publishing', JText::_('COM_HWDMS_PUBLISHING', true)); ?>
+                        <?php echo JLayoutHelper::render('joomla.edit.publishingdata', $this); ?>         
+                <?php echo JHtml::_('bootstrap.endTab'); ?>
 
-        <div>
+		<?php echo JHtml::_('bootstrap.endTabSet'); ?>
+
 		<input type="hidden" name="task" value="" />
+		<input type="hidden" name="return" value="<?php echo $input->getCmd('return'); ?>" />
 		<?php echo JHtml::_('form.token'); ?>
-	</div>
+        </div>
 </form>
-
