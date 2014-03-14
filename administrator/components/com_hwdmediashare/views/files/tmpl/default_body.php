1cf3a30900da31810d789ec56359e3b65d119f14
Dave Horsfall <dave@horsfall.eu>
2014-03-14 10:17:42 +0000

    Updated files view templates

--- administrator/components/com_hwdmediashare/views/files/tmpl/default_body.php
+++ administrator/components/com_hwdmediashare/views/files/tmpl/default_body.php
@@ -1,69 +1,72 @@
 <?php
 /**
- * @version    SVN $Id: default_body.php 425 2012-06-28 07:48:57Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2012 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      15-Mar-2012 21:29:13
+ * @package     Joomla.administrator
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
 
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
 
 JHtml::addIncludePath(JPATH_COMPONENT.'/helpers/html');
-JHtml::_('behavior.tooltip');
-JHtml::_('behavior.modal');
-JHtml::_('script','system/multiselect.js', false, true);
 
 $user		= JFactory::getUser();
 $userId		= $user->get('id');
 $listOrder	= $this->escape($this->state->get('list.ordering'));
 $listDirn	= $this->escape($this->state->get('list.direction'));
+$saveOrder	= $listOrder == 'a.ordering';
 
-$ordering	= ($listOrder == 'a.ordering');
-$canEdit	= $user->authorise('core.edit', 'com_hwdmediashare');
+$archived	= $this->state->get('filter.published') == 2 ? true : false;
+$trashed	= $this->state->get('filter.published') == -2 ? true : false;
+
 ?>
-<?php foreach($this->items as $i => $item):
-        $canCheckin	= $user->authorise('core.manage', 'com_checkin') || $item->checked_out==$user->get('id') || $item->checked_out==0;
-        $canChange	= $user->authorise('core.edit.state', 'com_hwdmediashare') && $canCheckin;
-        ?>
-        <tr class="row<?php echo $i % 2; ?>">
-                <td>
-                        <?php echo JHtml::_('grid.id', $i, $item->id); ?>
-                </td>
-                <td>
-                        <?php if ($canEdit) : ?>
-                                <a href="<?php echo JRoute::_('index.php?option=com_hwdmediashare&task=editmedia.edit&id='.(int) $item->mediaid); ?>">
-                                        <?php echo $this->escape($item->title); ?>
-                                </a>
+<?php foreach ($this->items as $i => $item) :
+$ordering   = ($listOrder == 'a.ordering');
+$canCreate  = $user->authorise('core.create',     'com_hwdmediashare');
+$canEdit    = $user->authorise('core.edit',       'com_hwdmediashare');
+$canCheckin = $user->authorise('core.manage',     'com_hwdmediashare') || $item->checked_out == $userId || $item->checked_out == 0;
+$canEditOwn = $user->authorise('core.edit.own',   'com_hwdmediashare') && $item->created_user_id == $userId;
+$canChange  = $user->authorise('core.edit.state', 'com_hwdmediashare') && $canCheckin;
+?>
+<tr class="row<?php echo $i % 2; ?>">
+        <td class="center hidden-phone">
+                <?php echo JHtml::_('grid.id', $i, $item->id); ?>
+        </td>
+        <td class="nowrap">
+                <div class="pull-left">
+                        <?php if ($canEdit || $canEditOwn) : ?>
+                                <a href="<?php echo JRoute::_('index.php?option=com_hwdmediashare&task=editmedia.edit&id=' . $item->id); ?>" title="<?php echo JText::_('JACTION_EDIT'); ?>">
+                                        <?php echo $this->escape(JHtmlString::truncate($item->title, 20, false, false)); ?></a>
                         <?php else : ?>
-                                        <?php echo $this->escape($item->title); ?>
-                        <?php endif; ?>
-                </td>
-                <td>
-                        <a href="<?php echo 'index.php?option=com_hwdmediashare&task=editmedia.view&id='.$item->element_id.'&file_type='.$item->file_type.'&tmpl=component'; ?>" class="modal" rel="{handler: 'iframe', size: {x: 500, y: 400}}" ><?php echo $this->getFileType($item); ?></a>
-                </td>
-                <td>
-                        <?php echo $this->getPath($item); ?>
-                </td>
-                <td align="center">
-                        <?php echo $this->getExtension($item); ?>
-                </td>
-                <td align="center">
-                        <?php echo JHtml::_('number.bytes', $item->size); ?>
-                </td>
-                <td class="center">
-                        <?php echo $this->escape($item->download_level); ?>
-                </td>
-                <td class="center">
-                        <?php echo JHtml::_('date',$item->created, JText::_('DATE_FORMAT_LC4')); ?>
-                </td>
-                <td class="center">
-                        <?php echo (int) $item->hits; ?>
-                </td>
-                <td class="center">
-                        <?php echo (int) $item->id; ?>
-                </td>
-        </tr>
+                                <span title="<?php echo JText::sprintf('JFIELD_ALIAS_LABEL', $this->escape($item->alias)); ?>"><?php echo $this->escape($item->title); ?></span>
+                        <?php endif; ?>                             
+                </div>
+        </td>  
+        <td class="nowrap has-context">
+                <a href="<?php echo hwdMediaShareDownloads::protectedUrl($item->mediaid, $item->file_type, 1, 1); ?>"><?php echo $this->getPath($item); ?></a>
+        </td>        
+        <td class="small hidden-phone">
+                <?php echo $this->getFileType($item); ?>
+        </td>
+        <td class="small hidden-phone">
+                <?php echo $this->getExtension($item); ?>
+        </td>
+        <td class="nowrap small hidden-phone">
+                <?php echo JHtml::_('number.bytes', $item->size); ?>
+        </td>
+        <td class="small hidden-phone">
+                <?php echo $this->escape($item->download_level); ?>
+        </td>
+        <td class="nowrap small hidden-phone">
+                <?php echo JHtml::_('date', $item->created, JText::_('DATE_FORMAT_LC4')); ?>
+        </td>
+        <td class="center">
+                <?php echo (int) $item->hits; ?>
+        </td>
+        <td class="center hidden-phone">
+                <?php echo (int) $item->id; ?>
+        </td>
+</tr>
 <?php endforeach; ?>
--- administrator/components/com_hwdmediashare/views/files/tmpl/default_head.php
+++ administrator/components/com_hwdmediashare/views/files/tmpl/default_head.php
@@ -1,53 +1,47 @@
 <?php
 /**
- * @version    SVN $Id: default_head.php 953 2013-01-29 13:45:49Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2012 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      15-Mar-2012 21:28:45
+ * @package     Joomla.administrator
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
 
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
 
-$user		= JFactory::getUser();
-$userId		= $user->get('id');
 $listOrder	= $this->escape($this->state->get('list.ordering'));
 $listDirn	= $this->escape($this->state->get('list.direction'));
-$canOrder	= $user->authorise('core.edit.state', 'com_hwdmediashare.group');
-$saveOrder	= $listOrder == 'a.ordering';
 ?>
 <tr>
-        <th width="20">
-                <input type="checkbox" name="toggle" value="" onclick="Joomla.checkAll(this);" />
-        </th>
+        <th width="1%" class="center hidden-phone">
+                <?php echo JHtml::_('grid.checkall'); ?>
+        </th>       
+        <th width="10%" class="nowrap">
+		<?php echo JHtml::_('searchtools.sort', 'COM_HWDMS_MEDIA', 'm.title', $listDirn, $listOrder); ?>
+        </th> 
         <th>
-                <?php echo JHtml::_('grid.sort', JText::_('COM_HWDMS_TITLE'), 'm.title', $listDirn, $listOrder); ?>
+		<?php echo JHtml::_('searchtools.sort', 'COM_HWDMS_FILE_PATH', 'a.basename', $listDirn, $listOrder); ?>
+        </th>         
+        <th width="10%" class="nowrap">
+		<?php echo JHtml::_('searchtools.sort', 'COM_HWDMS_FILE_TYPE', 'a.file_type', $listDirn, $listOrder); ?>
+        </th>
+        <th width="1%" class="nowrap hidden-phone">
+		<?php echo JHtml::_('searchtools.sort', 'COM_HWDMS_EXTENSION', 'a.ext', $listDirn, $listOrder); ?>
+        </th>
+        <th width="7%" class="nowrap hidden-phone">
+		<?php echo JHtml::_('searchtools.sort', 'COM_HWDMS_FILE_SIZE', 'a.size', $listDirn, $listOrder); ?>
+        </th>
+        <th width="10%" class="nowrap hidden-phone">
+		<?php echo JHtml::_('searchtools.sort', 'COM_HWDMS_DOWNLOAD', 'download_level', $listDirn, $listOrder); ?>
+        </th>
+        <th width="10%" class="nowrap hidden-phone">
+		<?php echo JHtml::_('searchtools.sort', 'JDATE', 'a.created', $listDirn, $listOrder); ?>
         </th>
         <th width="10%">
-                <?php echo JHtml::_('grid.sort', JText::_('COM_HWDMS_FILE_TYPE'), 'a.file_type', $listDirn, $listOrder); ?>
+		<?php echo JHtml::_('searchtools.sort', 'JGLOBAL_HITS', 'a.hits', $listDirn, $listOrder); ?>
         </th>
-        <th>
-                <?php echo JHtml::_('grid.sort', JText::_('COM_HWDMS_FILE_PATH'), 'a.basename', $listDirn, $listOrder); ?>
-        </th>
-        <th width="5%">
-                <?php echo JHtml::_('grid.sort', JText::_('COM_HWDMS_EXTENSION'), 'a.ext', $listDirn, $listOrder); ?>
-        </th>
-        <th width="10%">
-                <?php echo JHtml::_('grid.sort', JText::_('COM_HWDMS_FILE_SIZE'), 'a.size', $listDirn, $listOrder, NULL, 'desc'); ?>
-        </th>
-        <th width="10%">
-                <?php echo JHtml::_('grid.sort', JText::_('COM_HWDMS_DOWNLOAD'), 'download_level', $listDirn, $listOrder); ?>
-        </th>
-        <th width="5%">
-                <?php echo JHtml::_('grid.sort', JText::_('JDATE'), 'a.created', $listDirn, $listOrder); ?>
-        </th>
-        <th width="5%">
-                <?php echo JHtml::_('grid.sort', JText::_('JGLOBAL_HITS'), 'a.hits', $listDirn, $listOrder); ?>
-        </th>
-        <th width="1%" class="nowrap">
-                <?php echo JHtml::_('grid.sort', JText::_('JGRID_HEADING_ID'), 'a.id', $listDirn, $listOrder); ?>
+        <th width="1%" class="nowrap hidden-phone">
+		<?php echo JHtml::_('searchtools.sort', 'JGRID_HEADING_ID', 'a.id', $listDirn, $listOrder); ?>
         </th>
 </tr>
-
