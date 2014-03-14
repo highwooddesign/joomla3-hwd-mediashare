c8c60a0ab171214fd72121cc645d2aea9a99fa03
Dave Horsfall <dave@horsfall.eu>
2014-03-14 10:28:30 +0000

    Updated maintenance view template

--- administrator/components/com_hwdmediashare/views/maintenance/tmpl/default.php
+++ administrator/components/com_hwdmediashare/views/maintenance/tmpl/default.php
@@ -1,31 +1,37 @@
 <?php
 /**
- * @version    SVN $Id: default.php 277 2012-03-28 10:03:31Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2011 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      15-Apr-2011 10:13:15
+ * @package     Joomla.administrator
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
 
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
-JHtml::_('behavior.framework', true);
-JHtml::_('behavior.tooltip');
+defined('_JEXEC') or die;
 
+JHtml::_('behavior.framework', true);
 ?>
-<form action="<?php echo JRoute::_('index.php?option=com_hwdmediashare'); ?>" method="post" name="adminForm" id="adminForm">
-        <table class="adminlist">
+<form action="<?php echo JRoute::_('index.php?option=com_hwdmediashare&view=maintenance'); ?>" method="post" name="adminForm" id="adminForm">
+<?php if (!empty( $this->sidebar)) : ?>
+	<div id="j-sidebar-container" class="span2">
+		<?php echo $this->sidebar; ?>
+	</div>
+	<div id="j-main-container" class="span10">
+<?php else : ?>
+	<div id="j-main-container">
+<?php endif;?>
+        <table class="table table-striped" id="maintenanceList">
                 <tbody>
-                        <tr class="row0">
-                                <td>
+                        <tr>
+                                <td width="90%">
                                         <div><?php echo JText::_( 'COM_HWDMS_CLEAN_CATEGORY_MAP' ); ?></div>
                                 </td>
-                                <td>
+                                <td width="10%">
                                         <div id="ajax-container-cleancategorymap" class="fltrt"></div>
                                 </td>
                         </tr>
-                        <tr class="row1">
+                        <tr>
                                 <td>
                                         <div><?php echo JText::_( 'COM_HWDMS_CLEAN_TAG_MAP' ); ?></div>
                                 </td>
@@ -33,7 +39,7 @@ JHtml::_('behavior.tooltip');
                                         <div id="ajax-container-cleantagmap" class="fltrt"></div>
                                 </td>
                         </tr>
-                        <tr class="row0">
+                        <tr>
                                 <td>
                                         <div><?php echo JText::_( 'COM_HWDMS_EMPTY_UPLOAD_TOKENS' ); ?></div>
                                 </td>
@@ -41,9 +47,17 @@ JHtml::_('behavior.tooltip');
                                         <div id="ajax-container-emptyuploadtokens" class="fltrt"></div>
                                 </td>
                         </tr>
+                        <tr>
+                                <td>
+                                        <div><?php echo JText::_( 'COM_HWDMS_PURGE_OLD_PROCESSES' ); ?></div>
+                                </td>
+                                <td>
+                                        <div id="ajax-container-purgeoldprocesses" class="fltrt"></div>
+                                </td>
+                        </tr>
                 </tbody>
         </table>
-	<input type="hidden" name="task" value="" />
-        <input type="hidden" name="view" value="maintenance" />
+        <input type="hidden" name="task" value="" />
         <?php echo JHtml::_('form.token'); ?>
+        </div>
 </form>
