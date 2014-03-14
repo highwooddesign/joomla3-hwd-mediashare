dcf723609b9c13ffc2c26d2c82fb612b8946a5cd
Dave Horsfall <dave@horsfall.eu>
2014-03-14 10:26:04 +0000

    Updated groups view templates

--- administrator/components/com_hwdmediashare/views/groups/tmpl/default_body.php
+++ administrator/components/com_hwdmediashare/views/groups/tmpl/default_body.php
@@ -1,108 +1,125 @@
 <?php
 /**
- * @version    SVN $Id: default_body.php 1265 2013-03-13 13:53:35Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2011 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      15-Apr-2011 10:13:15
+ * @package     Joomla.administrator
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
 
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
 
 JHtml::addIncludePath(JPATH_COMPONENT.'/helpers/html');
-JHtml::_('behavior.tooltip');
-JHtml::_('script','system/multiselect.js', false, true);
 
 $user		= JFactory::getUser();
 $userId		= $user->get('id');
 $listOrder	= $this->escape($this->state->get('list.ordering'));
 $listDirn	= $this->escape($this->state->get('list.direction'));
 $saveOrder	= $listOrder == 'a.ordering';
-$canOrder	= $user->authorise('core.edit.state', 'com_hwdmediashare.category');
 
-$ordering	= ($listOrder == 'a.ordering');
-$canEdit	= $user->authorise('core.edit', 'com_hwdmediashare');
+$archived	= $this->state->get('filter.published') == 2 ? true : false;
+$trashed	= $this->state->get('filter.published') == -2 ? true : false;
+
 ?>
-<?php foreach($this->items as $i => $item):
-        $canCheckin	= $user->authorise('core.manage', 'com_checkin') || $item->checked_out==$userId || $item->checked_out==0;
-        $canChange	= $user->authorise('core.edit.state', 'com_hwdmediashare') && $canCheckin;
-
-        $owner =& JFactory::getUser($item->created_user_id);
-        ?>
-        <tr class="row<?php echo $i % 2; ?>">
-                <td>
-                        <?php echo JHtml::_('grid.id', $i, $item->id); ?>
-                </td>
-                <?php if ($this->state->get('filter.status') == 3) : ?>  
-                <td>
-                        <?php echo (int) $item->report_count; ?>
-                </td>
+<?php foreach ($this->items as $i => $item) :
+$ordering   = ($listOrder == 'a.ordering');
+$canCreate  = $user->authorise('core.create',     'com_hwdmediashare.group');
+$canEdit    = $user->authorise('core.edit',       'com_hwdmediashare.group.'.$item->id);
+$canCheckin = $user->authorise('core.manage',     'com_hwdmediashare') || $item->checked_out == $userId || $item->checked_out == 0;
+$canEditOwn = $user->authorise('core.edit.own',   'com_hwdmediashare.group.'.$item->id) && $item->created_user_id == $userId;
+$canChange  = $user->authorise('core.edit.state', 'com_hwdmediashare.group.'.$item->id) && $canCheckin;
+?>
+<tr class="row<?php echo $i % 2; ?>">
+        <td class="order nowrap center hidden-phone">
+                <?php
+                $iconClass = '';
+                if (!$canChange)
+                {
+                        $iconClass = ' inactive';
+                }
+                elseif (!$saveOrder)
+                {
+                        $iconClass = ' inactive tip-top hasTooltip" title="' . JHtml::tooltipText('JORDERINGDISABLED');
+                }
+                ?>
+                <span class="sortable-handler<?php echo $iconClass ?>">
+                        <i class="icon-menu"></i>
+                </span>
+                <?php if ($canChange && $saveOrder) : ?>
+                        <input type="text" style="display:none" name="order[]" size="5" value="<?php echo $item->ordering; ?>" class="width-20 text-area-order " />
                 <?php endif; ?>
-                <td>
+        </td>
+        <td class="center hidden-phone">
+                <?php echo JHtml::_('grid.id', $i, $item->id); ?>
+        </td>
+        <td class="center">
+                <div class="btn-group">
+                        <?php echo JHtml::_('jgrid.published', $item->published, $i, 'groups.', $canChange, 'cb', $item->publish_up, $item->publish_down); ?>
+                        <?php echo JHtml::_('HwdAdminGroups.featured', $item->featured, $i, $canChange); ?>
+                        <?php echo JHtml::_('HwdAdminGroups.status', $item->status, $i, $canChange); ?> 
+                        <?php
+                        // Create dropdown items
+                        $action = $archived ? 'unarchive' : 'archive';
+                        JHtml::_('actionsdropdown.' . $action, 'cb' . $i, 'groups');
+
+                        $action = $trashed ? 'untrash' : 'trash';
+                        JHtml::_('actionsdropdown.' . $action, 'cb' . $i, 'groups');
+
+                        // Render dropdown list
+                        echo JHtml::_('actionsdropdown.render', $this->escape($item->title));
+                        ?>                    
+                </div>
+        </td>
+        <td class="nowrap has-context">
+                <div class="pull-left">
                         <?php if ($item->checked_out) : ?>
                                 <?php echo JHtml::_('jgrid.checkedout', $i, $item->editor, $item->checked_out_time, 'groups.', $canCheckin); ?>
                         <?php endif; ?>
-                        <span class="editlinktip hasTip" title="<?php echo $this->escape($item->title); ?>::<?php echo $this->escape($item->description); ?>" >
-                        <?php if ($canEdit) : ?>
-                                <a href="<?php echo JRoute::_('index.php?option=com_hwdmediashare&task=group.edit&id='.(int) $item->id); ?>">
+                        <?php if ($item->language == '*'):?>
+                                <?php $language = JText::alt('JALL', 'language'); ?>
+                        <?php else:?>
+                                <?php $language = $item->language_title ? $this->escape($item->language_title) : JText::_('JUNDEFINED'); ?>
+                        <?php endif;?>
+                        <?php if ($canEdit || $canEditOwn) : ?>
+                                <a href="<?php echo JRoute::_('index.php?option=com_hwdmediashare&task=group.edit&id=' . $item->id); ?>" title="<?php echo JText::_('JACTION_EDIT'); ?>">
                                         <?php echo $this->escape($item->title); ?></a>
                         <?php else : ?>
-                                        <?php echo $this->escape($item->title); ?>
+                                <span title="<?php echo JText::sprintf('JFIELD_ALIAS_LABEL', $this->escape($item->alias)); ?>"><?php echo $this->escape($item->title); ?></span>
                         <?php endif; ?>
-                        <p class="smallsub">
+                        <div class="small">
                                 <?php echo JText::sprintf('JGLOBAL_LIST_ALIAS', $this->escape($item->alias));?>
-                        </p>
-                        </span>
-                </td>
-                <td class="center">
-                        <?php echo JHtml::_('jgrid.published', $item->published, $i, 'groups.', $canChange, 'cb', $item->publish_up, $item->publish_down); ?>
-                </td>
-                <td align="center">
-                        <?php echo $this->getPublish($item, 'status', $i); ?>
-                </td>
-                <td align="center">
-                        <?php echo $this->getPublish($item, 'featured', $i); ?>
-                </td>
-                <td class="order">
-                        <?php if ($canChange) : ?>
-                                <?php if ($saveOrder) :?>
-                                        <?php if ($listDirn == 'asc') : ?>
-                                                <span><?php echo $this->pagination->orderUpIcon($i, true, 'group.orderup', 'JLIB_HTML_MOVE_UP', $ordering); ?></span>
-                                                <span><?php echo $this->pagination->orderDownIcon($i, $this->pagination->total, true, 'media.orderdown', 'JLIB_HTML_MOVE_DOWN', $ordering); ?></span>
-                                        <?php elseif ($listDirn == 'desc') : ?>
-                                                <span><?php echo $this->pagination->orderUpIcon($i, true, 'group.orderdown', 'JLIB_HTML_MOVE_UP', $ordering); ?></span>
-                                                <span><?php echo $this->pagination->orderDownIcon($i, $this->pagination->total, true, 'media.orderup', 'JLIB_HTML_MOVE_DOWN', $ordering); ?></span>
-                                        <?php endif; ?>
-                                <?php endif; ?>
-                                <?php $disabled = $saveOrder ?  '' : 'disabled="disabled"'; ?>
-                                <input type="text" name="order[]" size="5" value="<?php echo $item->ordering;?>" <?php echo $disabled ?> class="text-area-order" />
-                        <?php else : ?>
-                                <?php echo $item->ordering; ?>
-                        <?php endif; ?>
-                </td>
-                <td class="center">
-                        <?php echo $this->escape($item->access_level); ?>
-                </td>
-                <td class="center">
-                        <?php echo $this->escape($owner->username); ?>
-                </td>
-                <td class="center nowrap">
-                        <?php echo JHtml::_('date',$item->created, JText::_('DATE_FORMAT_LC4')); ?>
-                </td>
-                <td class="center">
-                        <?php echo (int) $item->hits; ?>
-                </td>
-                <td class="center">
-                        <?php if ($item->language=='*'):?>
-                                <?php echo JText::alt('JALL','language'); ?>
-                        <?php else:?>
-                                <?php echo $item->language_title ? $this->escape($item->language_title) : JText::_('JUNDEFINED'); ?>
-                        <?php endif;?>
-                </td>
-                <td class="center">
-                        <?php echo (int) $item->id; ?>
-                </td>
-        </tr>
+                        </div>                                
+                </div>
+        </td>     
+        <td class="small hidden-phone">
+                <?php echo $this->escape($item->access_level); ?>
+        </td>
+        <td class="small hidden-phone">
+                <?php if ($item->created_user_id_alias) : ?>
+                        <a href="<?php echo JRoute::_('index.php?option=com_users&task=user.edit&id='.(int) $item->created_user_id); ?>" title="<?php echo JText::_('JAUTHOR'); ?>">
+                        <?php echo $this->escape($item->author); ?></a>
+                        <p class="smallsub"> <?php echo JText::sprintf('JGLOBAL_LIST_ALIAS', $this->escape($item->created_user_id_alias)); ?></p>
+                <?php else : ?>
+                        <a href="<?php echo JRoute::_('index.php?option=com_users&task=user.edit&id='.(int) $item->created_user_id); ?>" title="<?php echo JText::_('JAUTHOR'); ?>">
+                        <?php echo $this->escape($item->author); ?></a>
+                <?php endif; ?>
+        </td>
+        <td class="small hidden-phone">
+                <?php if ($item->language == '*'):?>
+                        <?php echo JText::alt('JALL', 'language'); ?>
+                <?php else:?>
+                        <?php echo $item->language_title ? $this->escape($item->language_title) : JText::_('JUNDEFINED'); ?>
+                <?php endif;?>
+        </td>
+        <td class="nowrap small hidden-phone">
+                <?php echo JHtml::_('date', $item->created, JText::_('DATE_FORMAT_LC4')); ?>
+        </td>
+        <td class="center">
+                <?php echo (int) $item->hits; ?>
+        </td>
+        <td class="center hidden-phone">
+                <?php echo (int) $item->id; ?>
+        </td>
+</tr>
 <?php endforeach; ?>
--- administrator/components/com_hwdmediashare/views/groups/tmpl/modal.php
+++ administrator/components/com_hwdmediashare/views/groups/tmpl/modal.php
@@ -1,118 +1,165 @@
 <?php
 /**
- * @version    SVN $Id: modal.php 1141 2013-02-21 11:09:56Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2011 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      06-Dec-2011 10:16:33
+ * @package     Joomla.administrator
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
 
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
 
-// load tooltip behavior
-JHtml::_('behavior.tooltip');
-JHtml::_('script','system/multiselect.js', false, true);
+$app = JFactory::getApplication();
 
-$listOrder	= $this->escape($this->state->get('list.ordering'));
-$listDirn	= $this->escape($this->state->get('list.direction'));
-$function       = JRequest::getCmd('function', 'jSelectGroup_group_id');
+if ($app->isSite())
+{
+	JSession::checkToken('get') or die(JText::_('JINVALID_TOKEN'));
+}
+
+JHtml::addIncludePath(JPATH_COMPONENT.'/helpers/html');
+JHtml::_('bootstrap.tooltip');
+JHtml::_('behavior.framework', true);
+
+$function  = $app->input->getCmd('function', 'jSelectGroup');
+$listOrder = $this->escape($this->state->get('list.ordering'));
+$listDirn  = $this->escape($this->state->get('list.direction'));
 ?>
-<form action="<?php echo JRoute::_('index.php?option=com_hwdmediashare'); ?>" method="post" name="adminForm" id="adminForm">
-	<fieldset id="filter-bar">
-		<div class="filter-search fltlft">
-			<label class="filter-search-lbl" for="filter_search"><?php echo JText::_('JSEARCH_FILTER_LABEL'); ?></label>
-			<input type="text" name="filter_search" id="filter_search" value="<?php echo $this->escape($this->state->get('filter.search')); ?>" title="<?php echo JText::_('COM_HWDMS_SEARCH_IN_TITLE'); ?>" />
-			<button type="submit"><?php echo JText::_('JSEARCH_FILTER_SUBMIT'); ?></button>
-			<button type="button" onclick="document.id('filter_search').value='';this.form.submit();"><?php echo JText::_('JSEARCH_FILTER_CLEAR'); ?></button>
+<form action="<?php echo JRoute::_('index.php?option=com_hwdmediashare&view=groups&layout=modal&tmpl=component&function='.$function.'&'.JSession::getFormToken().'=1');?>" method="post" name="adminForm" id="adminForm" class="form-inline">
+	<fieldset class="filter clearfix">
+		<div class="btn-toolbar">
+			<div class="btn-group pull-left">
+				<label for="filter_search">
+					<?php echo JText::_('JSEARCH_FILTER_LABEL'); ?>
+				</label>
+			</div>
+			<div class="btn-group pull-left">
+				<input type="text" name="filter_search" id="filter_search" value="<?php echo $this->escape($this->state->get('filter.search')); ?>" size="30" title="<?php echo JText::_('COM_CONTENT_FILTER_SEARCH_DESC'); ?>" />
+			</div>
+			<div class="btn-group pull-left">
+				<button type="submit" class="btn hasTooltip" title="<?php echo JHtml::tooltipText('JSEARCH_FILTER_SUBMIT'); ?>" data-placement="bottom">
+					<span class="icon-search"></span><?php echo '&#160;' . JText::_('JSEARCH_FILTER_SUBMIT'); ?></button>
+				<button type="button" class="btn hasTooltip" title="<?php echo JHtml::tooltipText('JSEARCH_FILTER_CLEAR'); ?>" data-placement="bottom" onclick="document.id('filter_search').value='';this.form.submit();">
+					<span class="icon-remove"></span><?php echo '&#160;' . JText::_('JSEARCH_FILTER_CLEAR'); ?></button>
+			</div>
+			<div class="clearfix"></div>
 		</div>
-		<div class="filter-select fltrt">
-			<select name="filter_published" class="inputbox" onchange="this.form.submit()">
-				<option value=""><?php echo JText::_('JOPTION_SELECT_PUBLISHED');?></option>
-				<?php echo JHtml::_('select.options', JHtml::_('jgrid.publishedOptions'), 'value', 'text', $this->state->get('filter.published'), true);?>
-			</select>
-                        <select name="filter_access" class="inputbox" onchange="this.form.submit()">
+		<hr class="hr-condensed" />
+		<div class="filters pull-left">
+			<select name="filter_access" class="input-medium" onchange="this.form.submit()">
 				<option value=""><?php echo JText::_('JOPTION_SELECT_ACCESS');?></option>
 				<?php echo JHtml::_('select.options', JHtml::_('access.assetgroups'), 'value', 'text', $this->state->get('filter.access'));?>
 			</select>
-			<select name="filter_language" class="inputbox" onchange="this.form.submit()">
+
+			<select name="filter_published" class="input-medium" onchange="this.form.submit()">
+				<option value=""><?php echo JText::_('JOPTION_SELECT_PUBLISHED');?></option>
+				<?php echo JHtml::_('select.options', JHtml::_('jgrid.publishedOptions'), 'value', 'text', $this->state->get('filter.published'), true);?>
+			</select>
+
+			<?php if ($this->state->get('filter.forcedLanguage')) : ?>
+			<select name="filter_category_id" class="input-medium" onchange="this.form.submit()">
+				<option value=""><?php echo JText::_('JOPTION_SELECT_CATEGORY');?></option>
+				<?php echo JHtml::_('select.options', JHtml::_('category.options', 'com_content', array('filter.language' => array('*', $this->state->get('filter.forcedLanguage')))), 'value', 'text', $this->state->get('filter.category_id'));?>
+			</select>
+			<input type="hidden" name="forcedLanguage" value="<?php echo $this->escape($this->state->get('filter.forcedLanguage')); ?>" />
+			<input type="hidden" name="filter_language" value="<?php echo $this->escape($this->state->get('filter.language')); ?>" />
+			<?php else : ?>
+			<select name="filter_category_id" class="input-medium" onchange="this.form.submit()">
+				<option value=""><?php echo JText::_('JOPTION_SELECT_CATEGORY');?></option>
+				<?php echo JHtml::_('select.options', JHtml::_('category.options', 'com_content'), 'value', 'text', $this->state->get('filter.category_id'));?>
+			</select>
+			<select name="filter_language" class="input-medium" onchange="this.form.submit()">
 				<option value=""><?php echo JText::_('JOPTION_SELECT_LANGUAGE');?></option>
 				<?php echo JHtml::_('select.options', JHtml::_('contentlanguage.existing', true, true), 'value', 'text', $this->state->get('filter.language'));?>
 			</select>
+			<?php endif; ?>
 		</div>
 	</fieldset>
-	<div class="clr"> </div>
-        <table class="adminlist">
-                <thead>
-                        <tr>
-                                <th>
-                                        <?php echo JHtml::_('grid.sort',  JText::_('COM_HWDMS_TITLE'), 'a.title', $listDirn, $listOrder); ?>
-                                </th>
-                                <th width="10%">
-                                        <?php echo JHtml::_('grid.sort', JText::_('JGRID_HEADING_ACCESS'), 'access_level', $listDirn, $listOrder); ?>
-                                </th>
-                                <th width="10%">
-                                        <?php echo JHtml::_('grid.sort', JText::_('JGRID_HEADING_CREATED_BY'), 'a.created_user_id', $listDirn, $listOrder); ?>
-                                </th>
-                                <th width="5%">
-                                        <?php echo JHtml::_('grid.sort', JText::_('JDATE'), 'a.created', $listDirn, $listOrder); ?>
-                                </th>
-                                <th width="5%">
-                                        <?php echo JHtml::_('grid.sort', JText::_('JGLOBAL_HITS'), 'a.hits', $listDirn, $listOrder); ?>
-                                </th>
-                                <th width="5%">
-                                        <?php echo JHtml::_('grid.sort', JText::_('JGRID_HEADING_LANGUAGE'), 'language', $listDirn, $listOrder); ?>
-                                </th>
-                           </tr>
-                </thead>
-                <tbody>
-                        <?php foreach($this->items as $i => $item):
-                                $owner =& JFactory::getUser($item->created_user_id);
-                                ?>
-                                <tr class="row<?php echo $i % 2; ?>">
-                                        <td>
-                                                <span class="editlinktip hasTip" title="<?php echo $this->escape($item->title); ?>::<?php echo $this->escape($item->description); ?>" >
-                                                <a class="pointer" onclick="if (window.parent) window.parent.<?php echo $this->escape($function);?>('<?php echo $item->id; ?>', '<?php echo $this->escape(addslashes($item->title)); ?>');"><?php echo $this->escape($item->title); ?></a>
-                                                </span>
-                                        </td>
-                                        <td class="center">
-                                                <?php echo $this->escape($item->access_level); ?>
-                                        </td>
-                                        <td class="center">
-                                                <?php echo $this->escape($owner->username); ?>
-                                        </td>
-                                        <td class="center nowrap">
-                                                <?php echo JHtml::_('date',$item->created, JText::_('DATE_FORMAT_LC4')); ?>
-                                        </td>
-                                        <td class="center">
-                                                <?php echo (int) $item->hits; ?>
-                                        </td>
-                                        <td class="center">
-                                                <?php if ($item->language=='*'):?>
-                                                        <?php echo JText::alt('JALL','language'); ?>
-                                                <?php else:?>
-                                                        <?php echo $item->language_title ? $this->escape($item->language_title) : JText::_('JUNDEFINED'); ?>
-                                                <?php endif;?>
-                                        </td>
-                                </tr>
-                        <?php endforeach; ?>
-                </tbody>
-                <tfoot>
-                        <tr>
-                                <td colspan="6"><?php echo $this->pagination->getListFooter(); ?></td>
-                        </tr>
-                </tfoot>
-        </table>
-        <div>
-                <input type="hidden" name="task" value="" />
-                <input type="hidden" name="view" value="groups" />
-                <input type="hidden" name="boxchecked" value="0" />
-                <input type="hidden" name="layout" value="modal" />
-                <input type="hidden" name="tmpl" value="component" />
+	<table class="table table-striped table-condensed">
+		<thead>
+			<tr>
+				<th class="title">
+					<?php echo JHtml::_('grid.sort', 'COM_HWDMS_TITLE', 'a.title', $listDirn, $listOrder); ?>
+				</th>
+				<th width="15%" class="center nowrap">
+					<?php echo JHtml::_('grid.sort', 'JGRID_HEADING_ACCESS', 'access_level', $listDirn, $listOrder); ?>
+				</th>
+                                <th width="15%" class="center nowrap">
+                                        <?php echo JHtml::_('grid.sort', 'JGRID_HEADING_CREATED_BY', 'a.created_user_id', $listDirn, $listOrder); ?>
+                                </th>                                
+				<th width="5%" class="center nowrap">
+					<?php echo JHtml::_('grid.sort', 'JGRID_HEADING_LANGUAGE', 'language', $listDirn, $listOrder); ?>
+				</th>
+				<th width="5%" class="center nowrap">
+					<?php echo JHtml::_('grid.sort', 'JDATE', 'a.created', $listDirn, $listOrder); ?>
+				</th>
+				<th width="1%" class="center nowrap">
+					<?php echo JHtml::_('grid.sort', 'JGRID_HEADING_ID', 'a.id', $listDirn, $listOrder); ?>
+				</th>
+			</tr>
+		</thead>
+		<tfoot>
+			<tr>
+				<td colspan="15">
+					<?php echo $this->pagination->getListFooter(); ?>
+				</td>
+			</tr>
+		</tfoot>
+		<tbody>
+		<?php foreach ($this->items as $i => $item) : ?>
+			<?php if ($item->language && JLanguageMultilang::isEnabled())
+			{
+				$tag = strlen($item->language);
+				if ($tag == 5)
+				{
+					$lang = substr($item->language, 0, 2);
+				}
+				elseif ($tag == 6)
+				{
+					$lang = substr($item->language, 0, 3);
+				}
+				else {
+					$lang = "";
+				}
+			}
+			elseif (!JLanguageMultilang::isEnabled())
+			{
+				$lang = "";
+			}
+			?>
+			<tr class="row<?php echo $i % 2; ?>">
+				<td>
+					<a href="javascript:void(0)" onclick="if (window.parent) window.parent.<?php echo $this->escape($function);?>('<?php echo $item->id; ?>', '<?php echo $this->escape(addslashes($item->title)); ?>', '<?php //echo $this->escape($item->catid); ?>', null, '<?php //echo $this->escape(ContentHelperRoute::getArticleRoute($item->id, $item->catid, $item->language)); ?>', '<?php echo $this->escape($lang); ?>', null);">
+						<?php echo $this->escape($item->title); ?></a>
+				</td>
+				<td class="center">
+					<?php echo $this->escape($item->access_level); ?>
+				</td>
+				<td class="center">
+					<?php echo $this->escape($item->author); ?>
+				</td>
+				<td class="center">
+					<?php if ($item->language == '*'):?>
+						<?php echo JText::alt('JALL', 'language'); ?>
+					<?php else:?>
+						<?php echo $item->language_title ? $this->escape($item->language_title) : JText::_('JUNDEFINED'); ?>
+					<?php endif;?>
+				</td>
+				<td class="center nowrap">
+					<?php echo JHtml::_('date', $item->created, JText::_('DATE_FORMAT_LC4')); ?>
+				</td>
+				<td class="center">
+					<?php echo (int) $item->id; ?>
+				</td>
+			</tr>
+			<?php endforeach; ?>
+		</tbody>
+	</table>
+	<div>
+		<input type="hidden" name="task" value="" />
+		<input type="hidden" name="boxchecked" value="0" />
 		<input type="hidden" name="filter_order" value="<?php echo $listOrder; ?>" />
 		<input type="hidden" name="filter_order_Dir" value="<?php echo $listDirn; ?>" />
-		<input type="hidden" name="function" value="jSelectGroup_assign_group_id" />                                
-                <?php echo JHtml::_('form.token'); ?>
-        </div>
+		<?php echo JHtml::_('form.token'); ?>
+	</div>
 </form>
