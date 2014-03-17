47dd6d005920af72bcf9598e54518b64d580d2f3
Dave Horsfall <dave@horsfall.eu>
2014-03-17 14:23:43 +0000

    Added new JHTML helper in HWDMediaShare (hwdgrid) to render a checkbox similar to the Joomla framework, but give more control over the name of the input, and additional properties. This is required for the linking views which can potentially act over two database tables with separate primary keys.

---
+++ administrator/components/com_hwdmediashare/helpers/html/hwdgrid.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @package     Joomla.administrator
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
+ */
+
+defined('_JEXEC') or die;
+
+abstract class JHtmlHwdGrid
+{
+	/**
+	 * Method to create a checkbox for a grid row.
+	 *
+	 * @param   integer  $rowNum      The row index
+	 * @param   integer  $recId       The record id
+	 * @param   string   $idName      The form element id identifer
+	 * @param   boolean  $checkedOut  True if item is checked out
+	 * @param   string   $name        The name of the form element
+	 * @param   string   $class       The class of the form element
+	 *
+	 * @return  mixed    String of html with a checkbox if item is not checked out, null if checked out.
+	 */
+	public static function id($rowNum, $recId, $idName = 'cb', $checkedOut = false, $name = 'cid', $class = '')
+	{
+		return $checkedOut ? '' : '<input type="checkbox" id="' . $idName . $rowNum . '" name="' . $name . '[]" value="' . $recId
+			. '" onclick="Joomla.isChecked(this.checked);" class="' . $class . '" />';
+	}
+}
