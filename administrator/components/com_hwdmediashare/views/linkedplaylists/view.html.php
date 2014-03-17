5c518958af8f0c615d48269e9c582c1249e4271f
Dave Horsfall <dave@horsfall.eu>
2014-03-17 14:17:12 +0000

    Removed old template files for linkedplaylists view. Updated template files for the linkedplaylists view. Updated linkedplaylists view class.

--- administrator/components/com_hwdmediashare/views/linkedplaylists/view.html.php
+++ administrator/components/com_hwdmediashare/views/linkedplaylists/view.html.php
@@ -25,7 +25,6 @@ class hwdMediaShareViewLinkedPlaylists extends JViewLegacy
                 $this->items = $this->get('Items');
                 $this->pagination = $this->get('Pagination');
 		$this->state = $this->get('State');
-                $this->filterForm = $this->get('FilterForm');
                 $this->mediaId = JFactory::getApplication()->input->get('media_id', '', 'int');
 
                 hwdMediaShareFactory::load('downloads');
@@ -48,19 +47,19 @@ class hwdMediaShareViewLinkedPlaylists extends JViewLegacy
 	 * Display appropriate button to either link or unlink the playlist from the media item.
 	 * @return  void
 	 */
-	public function getConnection($row, $i)
+	public function getButton($row, $i)
 	{
                 $task = $row->connection ? 'unlink' : 'link';
-                $text = $row->connection ? JText::_('COM_HWDMS_UNLINK') : JText::_('COM_HWDMS_LINK');
+                $buttonClass = $row->connection ? 'btn btn-danger' : 'btn';
 
                 // Start output
                 ob_start();
                 ?>
-<div class="btn-wrapper pull-right">
-<a title="" class="btn hasTooltip btn-primary" href="index.php?option=com_hwdmediashare&task=playlistmedia.<?php echo $task; ?>&tmpl=component&playlist_id=28&tmpl=component&playlist_id=28&add=0" data-original-title="Filter the list items">
-<?php echo $text; ?>
-</a>
-</div> 
+                <div class="btn-wrapper pull-right">
+                        <a class="<?php echo $buttonClass; ?>" href="javascript:void(0);" onclick="return listItemTask('mb<?php echo $i; ?>','linkedplaylists.<?php echo $task; ?>')">
+                                <?php echo ($row->connection ? JText::_('COM_HWDMS_BTN_REMOVE_MEDIA_FROM_PLAYLIST') : JText::_('COM_HWDMS_BTN_ADD_MEDIA_TO_PLAYLIST')); ?>
+                        </a>
+                </div>
                 <?php
                 $html = ob_get_contents();
                 ob_end_clean();
