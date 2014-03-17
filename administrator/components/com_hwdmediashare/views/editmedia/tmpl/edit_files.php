68c720e0aa55edfac4afe318b6fc5ce67450e302
Dave Horsfall <dave@horsfall.eu>
2014-03-17 14:53:19 +0000

    Updated the edit_files template of the editmedia view.

--- administrator/components/com_hwdmediashare/views/editmedia/tmpl/edit_files.php
+++ administrator/components/com_hwdmediashare/views/editmedia/tmpl/edit_files.php
@@ -1,66 +1,62 @@
 <?php
 /**
- * @version    SVN $Id: edit_files.php 277 2012-03-28 10:03:31Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2011 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      15-Apr-2011 10:13:15
+ * @package     Joomla.administrator
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
 
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
+
 jimport( 'joomla.html.html.number' );
 ?>
-<thead>
+<table class="table table-striped" id="filesList">
+    <thead>
         <tr>
-                <th width="5">
-                        <?php echo JText::_('COM_HWDMS_ID'); ?>
-                </th>
-                <th>
-                        <?php echo JText::_('COM_HWDMS_FILE_TYPE'); ?>
-                </th>
-                <th>
-                        <?php echo JText::_('COM_HWDMS_FILE_PATH'); ?>
-                </th>
-                <th>
-                        <?php echo JText::_('COM_HWDMS_EXTENSION'); ?>
-                </th>
-                <th>
-                        <?php echo JText::_('COM_HWDMS_FILE_SIZE'); ?>
-                </th>
-                <th>
-                        <?php echo JText::_('COM_HWDMS_DOWNLOAD'); ?>
-                </th>
+            <th width="5">
+                    <?php echo JText::_('COM_HWDMS_ID'); ?>
+            </th>
+            <th>
+                    <?php echo JText::_('COM_HWDMS_FILE_TYPE'); ?>
+            </th>
+            <th>
+                    <?php echo JText::_('COM_HWDMS_FILE_PATH'); ?>
+            </th>
+            <th>
+                    <?php echo JText::_('COM_HWDMS_EXTENSION'); ?>
+            </th>
+            <th>
+                    <?php echo JText::_('COM_HWDMS_FILE_SIZE'); ?>
+            </th>
+            <th>
+                    <?php echo JText::_('COM_HWDMS_DOWNLOAD'); ?>
+            </th>
         </tr>
-</thead>
-<tbody>
-    <?php foreach($this->item->mediaitems as $i => $item):
-            JTable::addIncludePath(JPATH_ADMINISTRATOR.'/components/com_hwdmediashare/tables');
-            $table =& JTable::getInstance('Media', 'hwdMediaShareTable');
-            $table->load( $item->element_id );
-
-            $properties = $table->getProperties(1);
-            $row = JArrayHelper::toObject($properties, 'JObject'); ?>
-            <tr class="row<?php echo $i % 2; ?>">
-                    <td>
-                            <?php echo $item->id; ?>
-                    </td>
-                    <td>
-                            <a href="<?php echo 'index.php?option=com_hwdmediashare&task=editmedia.view&id='.$this->item->id.'&file_type='.$item->file_type.'&tmpl=component'; ?>" class="modal" rel="{handler: 'iframe', size: {x: 500, y: 400}}" ><?php echo $this->getFileType($item); ?></a>
-                    </td>
-                    <td>
-                            <?php echo $this->getPath($item); ?>
-                    </td>
-                    <td>
-                            <?php echo $this->getExtension($item); ?>
-                    </td>
-                    <td>
-                            <?php echo JHtml::_('number.bytes', $item->size); ?>
-                    </td>
-                    <td>
-                            <a href="<?php echo hwdMediaShareDownloads::url($row,$item->file_type); ?>">Download</a>
-                    </td>
-            </tr>
+    </thead>
+    <tbody>
+    <?php foreach($this->item->mediaitems as $i => $item): ?>
+        <tr class="row<?php echo $i % 2; ?>">
+                <td>
+                        <?php echo $item->id; ?>
+                </td>
+                <td>
+                        <?php echo $this->getFileType($item); ?>
+                </td>
+                <td>
+                        <?php echo $this->getPath($item); ?>
+                </td>
+                <td>
+                        <?php echo $this->getExtension($item); ?>
+                </td>
+                <td>
+                        <?php echo JHtml::_('number.bytes', $item->size); ?>
+                </td>
+                <td>
+                        <a class="btn" href="<?php echo hwdMediaShareDownloads::url($this->item, $item->file_type); ?>"><?php echo JText::_('COM_HWDMS_DOWNLOAD'); ?></a>
+                </td>
+        </tr>
     <?php endforeach; ?>
-</tbody>
+    </tbody>
+</table>
