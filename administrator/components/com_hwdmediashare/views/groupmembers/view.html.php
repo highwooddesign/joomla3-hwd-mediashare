4c1fad4678a9937f05b65f59fdc610d57cf8b8d6
Dave Horsfall <dave@horsfall.eu>
2014-03-17 13:55:50 +0000

    Updated groupmember view class.

--- administrator/components/com_hwdmediashare/views/groupmembers/view.html.php
+++ administrator/components/com_hwdmediashare/views/groupmembers/view.html.php
@@ -25,7 +25,6 @@ class hwdMediaShareViewGroupMembers extends JViewLegacy
                 $this->items = $this->get('Items');
                 $this->pagination = $this->get('Pagination');
 		$this->state = $this->get('State');
-                $this->filterForm = $this->get('FilterForm');
                 $this->groupId = JFactory::getApplication()->input->get('group_id', '', 'int');
 
                 hwdMediaShareFactory::load('downloads');
@@ -48,19 +47,19 @@ class hwdMediaShareViewGroupMembers extends JViewLegacy
 	 * Display appropriate button to either link or unlink the member from the group.
 	 * @return  void
 	 */
-	public function getConnection($row, $i)
+	public function getButton($row, $i)
 	{
                 $task = $row->connection ? 'unlink' : 'link';
-                $text = $row->connection ? JText::_('COM_HWDMS_UNLINK') : JText::_('COM_HWDMS_LINK');
+                $buttonClass = $row->connection ? 'btn btn-danger' : 'btn';
 
                 // Start output
                 ob_start();
                 ?>
-<div class="btn-wrapper pull-right">
-<a title="" class="btn hasTooltip btn-primary" href="index.php?option=com_hwdmediashare&task=playlistmedia.<?php echo $task; ?>&tmpl=component&playlist_id=28&tmpl=component&playlist_id=28&add=0" data-original-title="Filter the list items">
-<?php echo $text; ?>
-</a>
-</div> 
+                <div class="btn-wrapper pull-right">
+                        <a class="<?php echo $buttonClass; ?>" href="javascript:void(0);" onclick="return listItemTask('cb<?php echo $i; ?>','groupmembers.<?php echo $task; ?>')">
+                                <?php echo ($row->connection ? JText::_('COM_HWDMS_BTN_REMOVE_FROM_GROUP') : JText::_('COM_HWDMS_BTN_ADD_TO_GROUP')); ?>
+                        </a>
+                </div>
                 <?php
                 $html = ob_get_contents();
                 ob_end_clean();
