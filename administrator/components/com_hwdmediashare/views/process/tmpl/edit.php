4d90e2bab0db88148c88fbd0f12a7ed03e23b7cc
Dave Horsfall <dave@horsfall.eu>
2014-03-17 11:26:46 +0000

    Updated edit template for the process view.

--- administrator/components/com_hwdmediashare/views/process/tmpl/edit.php
+++ administrator/components/com_hwdmediashare/views/process/tmpl/edit.php
@@ -1,53 +1,70 @@
 <?php
 /**
- * @version    SVN $Id: edit.php 425 2012-06-28 07:48:57Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2011 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      09-Nov-2011 09:20:38
+ * @package     Joomla.administrator
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
 
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
 
-// load tooltip behavior
+// Include the component HTML helpers.
+JHtml::addIncludePath(JPATH_COMPONENT . '/helpers/html');
+
+JHtml::_('behavior.formvalidation');
+JHtml::_('behavior.keepalive');
+JHtml::_('formbehavior.chosen', 'select');
 JHtml::_('behavior.tooltip');
+JHtml::_('behavior.modal');
 
+$app = JFactory::getApplication();
+$input = $app->input;
 ?>
-<form action="<?php echo JRoute::_('index.php?option=com_hwdmediashare&layout=edit'); ?>" method="post" name="adminForm" id="adminForm" class="form-validate">
-        <fieldset class="adminform" >
-                <legend><?php echo JText::_('COM_HWDMS_MEDIA_PROCESSOR_LOG'); ?></legend>
-                <div class="clr"></div>
-                <p><?php echo JText::sprintf( 'COM_HWDMS_GENERATE_X_FOR_MEDIA_X', '<strong>'.$this->getProcessType($this->process).'</strong>', '<strong>'.$this->process->title ).'</strong>'; ?></p>
-                <?php if (count($this->items) == 0) : ?>
-                        <p><?php echo JText::_('COM_HWDMS_THE_LOG_IS_EMPTY'); ?></p>
-                <?php else : ?>               
-                <ul class="panelform">
-                         <?php echo JHtml::_('sliders.start', 'hwdmediashare-slider'); ?>
-                                <?php foreach($this->items as $i => $item): ?>
-                                        <?php echo JHtml::_('sliders.panel', JText::sprintf( '%s - %s', JHtml::_('date',$item->created, JText::_('DATE_FORMAT_LC2')), hwdMediaShareProcesses::getStatus($item) ), 'publishing');?>
-                                        <fieldset class="adminform" >
-                                                <ul class="panelform">
-                                                        <li>
-                                                                <label title="" class="hasTip" for="jform_input" id="jform_input-lbl"><?php echo JText::_('COM_HWDMS_INPUT'); ?></label>
-                                                                <textarea style="width: 100%; height: 100px;" rows="0" cols="0" id="jform_input" name="jform[input]"><?php echo $item->input; ?></textarea>
-                                                        </li>
-                                                        <li>
-                                                                <label title="" class="hasTip" for="jform_output" id="jform_output-lbl"><?php echo JText::_('COM_HWDMS_OUTPUT'); ?></label>
-                                                                <textarea style="width: 100%; height: 100px;" rows="0" cols="0" id="jform_output" name="jform[output]"><?php echo $item->output; ?></textarea>
-                                                        </li>
-                                                </ul>
-                                        </fieldset>
-                                <?php endforeach; ?>
-                        <?php echo JHtml::_('sliders.end'); ?>
+<form action="<?php echo JRoute::_('index.php?option=com_hwdmediashare&view=playlist&layout=edit&id=' . (int) $this->item->id); ?>" method="post" name="adminForm" id="adminForm">
 
-                </ul>
-                <?php endif; ?>
-        </fieldset>
-	<div>
-		<input type="hidden" name="task" value="" />
-		<?php echo JHtml::_('form.token'); ?>
-	</div>
+        <h1><?php echo JText::_('COM_HWDMS_MEDIA_PROCESSOR_LOG'); ?></h1>
+        <p><?php echo JText::sprintf( 'COM_HWDMS_GENERATE_X_FOR_MEDIA_X', '<strong>'.$this->getProcessType($this->item).'</strong>', '<a href="">'.$this->item->title.'</a>'); ?></p>
+        
+        <?php if (count($this->items) == 0) : ?>
+        
+                <p><?php echo JText::_('COM_HWDMS_THE_LOG_IS_EMPTY'); ?></p>
+        
+        <?php else : ?>  
+                        
+                <?php echo JHtml::_('bootstrap.startAccordion', 'slide-log', array('active' => 'log-0')); ?>
+  
+                <?php foreach($this->items as $i => $item): ?>
+                        <?php echo JHtml::_('bootstrap.addSlide', 'slide-log', JText::sprintf('%s (%s)', hwdMediaShareProcesses::getStatus($item), JHtml::_('date.relative', $item->created)), 'log-'.$i); ?>
+                        <div class="row-fluid form-horizontal-desktop">
+                                <div class="control-group">
+                                        <div class="control-label">
+                                                <label><?php echo JText::_('COM_HWDMS_INPUT'); ?></label>
+                                                    
+                                        </div>
+                                        <div class="controls">
+                                                <textarea style="width:90%;height:100px;"><?php echo $item->input; ?></textarea>
+                                        </div>
+                                </div> 
+                                <div class="control-group">
+                                        <div class="control-label">
+                                                <label><?php echo JText::_('COM_HWDMS_OUTPUT'); ?></label>
+                                                    
+                                        </div>
+                                        <div class="controls">
+                                                <textarea style="width:90%;height:200px;"><?php echo $item->output; ?></textarea>
+                                        </div>
+                                </div> 
+                        </div> 
+                        <?php echo JHtml::_('bootstrap.endSlide'); ?>
+                <?php endforeach; ?>
+
+                <?php echo JHtml::_('bootstrap.endAccordion'); ?>
+                
+        <?php endif; ?>
+                
+        <input type="hidden" name="task" value="" />
+        <input type="hidden" name="return" value="<?php echo $input->getCmd('return'); ?>" />
+        <?php echo JHtml::_('form.token'); ?>
 </form>
-
