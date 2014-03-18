48829bbd1f2f3d45f81bf714d38548bd3d96a2a4
Dave Horsfall <dave@horsfall.eu>
2014-03-18 10:03:05 +0000

    Bugfix in linkedplaylists view class

--- administrator/components/com_hwdmediashare/views/linkedplaylists/view.html.php
+++ administrator/components/com_hwdmediashare/views/linkedplaylists/view.html.php
@@ -56,7 +56,7 @@ class hwdMediaShareViewLinkedPlaylists extends JViewLegacy
                 ob_start();
                 ?>
                 <div class="btn-wrapper pull-right">
-                        <a class="<?php echo $buttonClass; ?>" href="javascript:void(0);" onclick="return listItemTask('mb<?php echo $i; ?>','linkedplaylists.<?php echo $task; ?>')">
+                        <a class="<?php echo $buttonClass; ?>" href="javascript:void(0);" onclick="return listItemTask('cb<?php echo $i; ?>','linkedplaylists.<?php echo $task; ?>')">
                                 <?php echo ($row->connection ? JText::_('COM_HWDMS_BTN_REMOVE_MEDIA_FROM_PLAYLIST') : JText::_('COM_HWDMS_BTN_ADD_MEDIA_TO_PLAYLIST')); ?>
                         </a>
                 </div>
