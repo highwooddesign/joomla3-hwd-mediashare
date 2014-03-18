76e00cc344dab71c9a9f4dc0e24612e252f1891b
Dave Horsfall <dave@horsfall.eu>
2014-03-18 11:47:29 +0000

    Updated extension model.

--- administrator/components/com_hwdmediashare/models/extension.php
+++ administrator/components/com_hwdmediashare/models/extension.php
@@ -1,113 +1,88 @@
 <?php
 /**
- * @version    SVN $Id: extension.php 493 2012-08-28 13:20:17Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2011 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      15-Apr-2011 10:13:15
+ * @package     Joomla.administrator
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
 
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
 
-// import Joomla modelform library
-jimport('joomla.application.component.modeladmin');
-
-/**
- * hwdMediaShare Model
- */
 class hwdMediaShareModelExtension extends JModelAdmin
 {
 	/**
-	 * Method override to check if you can edit an existing record.
+	 * Method to get a table object, load it if necessary.
 	 *
-	 * @param	array	$data	An array of input data.
-	 * @param	string	$key	The name of the key for the primary key.
+	 * @param   string  $name     The table name. Optional.
+	 * @param   string  $prefix   The class prefix. Optional.
+	 * @param   array   $options  Configuration array for model. Optional.
 	 *
-	 * @return	boolean
-	 * @since	0.1
+	 * @return  JTable  A JTable object
 	 */
-	protected function allowEdit($data = array(), $key = 'id')
+	public function getTable($name = 'FileExtension', $prefix = 'hwdMediaShareTable', $config = array())
 	{
-		// Check specific edit permission then general edit permission.
-		return JFactory::getUser()->authorise('core.edit', 'com_hwdmediashare.extension.'.((int) isset($data[$key]) ? $data[$key] : 0)) or parent::allowEdit($data, $key);
+		return JTable::getInstance($name, $prefix, $config);
 	}
+        
 	/**
-	 * Returns a reference to the a Table object, always creating it.
+	 * Abstract method for getting the form from the model.
 	 *
-	 * @param	type	The table type to instantiate
-	 * @param	string	A prefix for the table class name. Optional.
-	 * @param	array	Configuration array for model. Optional.
-	 * @return	JTable	A database object
-	 * @since	0.1
-	 */
-	public function getTable($type = 'FileExtension', $prefix = 'hwdMediaShareTable', $config = array())
-	{
-		return JTable::getInstance($type, $prefix, $config);
-	}
-	/**
-	 * Method to get the record form.
+	 * @param   array    $data      Data for the form.
+	 * @param   boolean  $loadData  True if the form is to load its own data (default case), false if not.
 	 *
-	 * @param	array	$data		Data for the form.
-	 * @param	boolean	$loadData	True if the form is to load its own data (default case), false if not.
-	 * @return	mixed	A JForm object on success, false on failure
-	 * @since	0.1
+	 * @return  mixed  A JForm object on success, false on failure
 	 */
 	public function getForm($data = array(), $loadData = true)
 	{
 		// Get the form.
 		$form = $this->loadForm('com_hwdmediashare.extension', 'extension', array('control' => 'jform', 'load_data' => $loadData));
-                if (empty($form))
+
+		if (empty($form))
 		{
 			return false;
 		}
+
 		return $form;
 	}
-	/**
-	 * Method to get the script that have to be included on the form
-	 *
-	 * @return string	Script files
-	 */
-	public function getScript()
-	{
-		return 'administrator/components/com_hwdmediashare/models/forms/extension.js';
-	}
+
 	/**
 	 * Method to get the data that should be injected in the form.
 	 *
-	 * @return	mixed	The data for the form.
-	 * @since	0.1
+	 * @return  mixed  The data for the form.
 	 */
 	protected function loadFormData()
 	{
 		// Check the session for previously entered form data.
 		$data = JFactory::getApplication()->getUserState('com_hwdmediashare.edit.extension.data', array());
+
 		if (empty($data))
 		{
 			$data = $this->getItem();
 		}
+
 		return $data;
 	}
+
 	/**
 	 * Method to save the form data.
 	 *
-	 * @param	array	The form data.
+	 * @param	array   $data   The form data.
 	 *
 	 * @return	boolean	True on success.
-	 * @since	0.1
 	 */
 	public function save($data)
 	{
 		// Initialise variables.
-                $date =& JFactory::getDate();
-                $user = & JFactory::getUser();
-                $data = JRequest::getVar('jform', array(), 'post', 'array');
+                $date = JFactory::getDate();
+                $user = JFactory::getUser();
 
-                // Alter the title for save as copy
+                // Set the modified details
                 $data['modified'] = $date->format('Y-m-d H:i:s');
                 $data['modified_user_id'] = $user->id;
-
+                
+                // Populate empty data
                 empty($data['created_user_id']) ? $data['created_user_id'] = $user->id : null;
                 empty($data['created']) ? $data['created'] = $date->format('Y-m-d H:i:s') : null;
                 empty($data['publish_up']) ? $data['publish_up'] = $date->format('Y-m-d H:i:s') : null;
@@ -120,69 +95,38 @@ class hwdMediaShareModelExtension extends JModelAdmin
 
 		return false;
 	}
-
-        /**
-	 * Method to assign user to a single record
-	 *
-	 * @param	integer	The id of the primary key.
-	 *
-	 * @return	mixed	Object on success, false on failure.
-	 */
-	public function batch($pks, $value = array())
-	{
-		// Initialise variables.
-		$user		= JFactory::getUser();
-		$table		= $this->getTable();
-		$pks		= (array) $pks;
-
-		if (!isset($value['user']) || !isset($value['access']))
-                {
-			$this->setError(JText::_('JGLOBAL_ERROR_INSUFFICIENT_BATCH_INFORMATION'));
-                        return false;
-		}
-                
-		// Access checks.
-		foreach ($pks as $i => $id)
-		{
-                        $data = array();
-                        $data['id'] = $id;
-                        !empty($value['user']) ? $data['created_user_id'] = $value['user'] : null;
-                        !empty($value['access']) ? $data['access'] = $value['access'] : null;
-
-                        if (!parent::save($data))
-                        {
-                                $this->setError(JText::_('COM_HWDMS_SAVE_FAILED'));
-                                return false;  
-                        }
-		}
-
-                // Clear the component's cache
-		$this->cleanCache();
-
-		return true;
-	}
         
-        /**
-	 * Method to assign user to a single record
+	/**
+	 * Method to delete one or more records. Overload to check if 
+         * gallery still contains media with the extension being deleted.
 	 *
-	 * @param	integer	The id of the primary key.
+	 * @param   array  $pks  An array of record primary keys.
 	 *
-	 * @return	mixed	Object on success, false on failure.
+	 * @return  boolean  True if successful, false if an error occurs.
 	 */
-	public function delete(&$pks)
+	public function delete($pks)
 	{
-		$db =& JFactory::getDBO();
+		$db = JFactory::getDBO();
                 $pks = (array) $pks;
                 
 		// Iterate the items to check for contents
 		foreach ($pks as $i => $pk)
 		{
-                        $query = $db->getQuery(true);
-                        $query->select('COUNT(*)');
-                        $query->from('#__hwdms_media AS a');
-                        $query->where('a.ext_id = '.$pk);
+                        $query = $db->getQuery(true)
+                                ->select('COUNT(*)')
+                                ->from('#__hwdms_media')
+                                ->where('ext_id = ' . $db->quote($pk));
                         $db->setQuery($query);
-                        $count = $db->loadResult();      
+                        try
+                        {
+                                $count = $db->loadResult();
+                        }
+                        catch (RuntimeException $e)
+                        {
+                                $this->setError($e->getMessage());
+                                return false;                            
+                        }
+     
                         if ($count > 0)
                         {
                                 $this->setError(JText::_('COM_HWDMS_ERROR_CAN_NOT_REMOVE_EXT_EXISTING_MEDIA_IN_GALLERY'));
