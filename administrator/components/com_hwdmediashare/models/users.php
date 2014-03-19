6f13bcb4d9e83d9c7cb4007dfb41781baff1e938
Dave Horsfall <dave@horsfall.eu>
2014-03-19 15:52:25 +0000

    Updated users model.

--- administrator/components/com_hwdmediashare/models/users.php
+++ administrator/components/com_hwdmediashare/models/users.php
@@ -1,63 +1,123 @@
 <?php
 /**
- * @version    SVN $Id: users.php 277 2012-03-28 10:03:31Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2011 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      15-Apr-2011 10:13:15
+ * @package     Joomla.administrator
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
 
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
 
-// import Joomla modelform library
-jimport('joomla.application.component.modellist');
-
-/**
- * hwdMediaShare Model
- */
 class hwdMediaShareModelUsers extends JModelList
 {
-	var $elementType = 5;
-        /**
-         * Method to build an SQL query to load the list data.
-         *
-         * @return      string  An SQL query
-         */
+    	/**
+	 * The element type to use with model methods.
+	 * @var    integer
+	 */    
+	public $elementType = 5;
+
+    	/**
+	 * Constructor override, defines a white list of column filters.
+	 *
+	 * @param   array  $config  An optional associative array of configuration settings.
+	 */
+	public function __construct($config = array())
+	{
+		if (empty($config['filter_fields']))
+		{
+			$config['filter_fields'] = array(
+				'id', 'a.id',
+				'alias', 'a.alias',
+				'private', 'a.private',
+				'likes', 'a.likes',
+				'dislikes', 'a.dislikes',
+				'status', 'a.status',   
+				'published', 'a.published', 
+				'featured', 'a.featured',
+				'checked_out', 'a.checked_out',
+				'checked_out_time', 'a.checked_out_time',
+				'access', 'a.access', 'access_level',
+				'ordering', 'a.ordering', 'map.ordering',
+				'created_user_id', 'a.created_user_id', 'created_user_id_alias', 'a.created_user_id_alias', 'author',
+				'created', 'a.created',
+				'publish_up', 'a.publish_up',
+				'publish_down', 'a.publish_down',
+				'modified_user_id', 'a.modified_user_id',
+				'modified', 'a.modified',
+				'hits', 'a.hits',
+				'language', 'a.language',
+                                /** Filter fields for additional joins **/                            
+				'report_count', 'a.report_count',
+                                'title',
+			);
+		}
+
+		parent::__construct($config);
+	}
+        
+	/**
+	 * Method to get a list of items.
+	 *
+	 * @return  mixed  An array of data items on success, false on failure.
+	 */
+	public function getItems()
+	{
+		$items = parent::getItems();
+
+                for ($x = 0, $count = count($items); $x < $count; $x++)
+                {
+                }
+
+		return $items;
+	}
+        
+	/**
+	 * Method to get the database query.
+	 *
+	 * @return  JDatabaseQuery  database query
+	 */
         protected function getListQuery()
         {
                 // Create a new query object.
                 $db = JFactory::getDBO();
                 $query = $db->getQuery(true);
 
+                // Get HWD config.
+                $hwdms = hwdMediaShareFactory::getInstance();
+                $config = $hwdms->getConfig();
+                
 		// Select the required fields from the table.
 		$query->select(
 			$this->getState(
 				'list.select',
-				'u.id, u.name, u.username, u.block, u.activation'
+				'a.id, a.description, a.alias, a.checked_out, a.checked_out_time,' .
+				'a.created_user_id, a.hits, a.published, a.featured,' .
+				'a.status, a.publish_up, a.publish_down, a.ordering, a.created, a.created_user_id_alias, a.access,'.
+				'a.language'
 			)
 		);
 
-                // From the Joomla user table
-                $query->from('#__users AS u');
+                // From the user channel table.
+                $query->from('#__hwdms_users AS a');
 
-                $query->select('u.id, u.name AS editor');
-
-                // Join over the hwdMediaShare user table
-                $query->select('a.description, a.alias, a.checked_out, a.checked_out_time,' .
-			       'a.hits, a.published, a.featured,' .
-			       'a.status, a.publish_up, a.publish_down, a.created, a.access,'.
-			       'a.language');
-
-		$query->join('LEFT', '`#__hwdms_users` AS a ON a.id = u.id');
-
-                $query->select('IF(a.id > 0, true, false) AS created');
-
-		// Join over the language
+		// Join over the language.
 		$query->select('l.title AS language_title');
 		$query->join('LEFT', '`#__languages` AS l ON l.lang_code = a.language');
 
+		// Join over the users for the checked out user.
+		$query->select('uc.name AS editor');
+		$query->join('LEFT', '#__users AS uc ON uc.id=a.checked_out');
+
+                // Join over the users.
+		$query->select('u.name, u.username, u.block, u.activation');
+		$query->join('LEFT', '#__users AS u ON u.id=a.id');
+
+                // Join over the users for the author, with value based on configuration.
+                $config->get('author') == 0 ? $query->select('ua.name AS title') : $query->select('ua.username AS title');
+		$query->join('LEFT', '#__users AS ua ON ua.id=a.id');
+                                
 		// Join over the asset groups.
 		$query->select('ag.title AS access_level');
 		$query->join('LEFT', '#__viewlevels AS ag ON ag.id = a.access');
@@ -68,18 +128,18 @@ class hwdMediaShareModelUsers extends JModelList
 			$query->where('a.access = '.(int) $access);
 		}
 
-		// Filter by published state
-		$published = $this->getState('filter.published');              
+		// Filter by published state.
+		$published = $this->getState('filter.published');
 		if (is_numeric($published))
                 {
 			$query->where('a.published = '.(int) $published);
 		}
                 else if ($published === '')
                 {
-			$query->where('(a.published IN (0, 1) OR a.published IS NULL)');
+			$query->where('(a.published IN (0, 1))');
 		}
-                
-                // Filter by status state
+
+                // Filter by status state.
 		$status = $this->getState('filter.status');
                 if (is_numeric($status)) 
                 {
@@ -93,22 +153,28 @@ class hwdMediaShareModelUsers extends JModelList
                         else
                         {
                                 $query->where('a.status = '.(int) $status);
-                        } 
-		}
-                
-		// Filter by search in title
+                        }                                
+                }
+
+		// Filter by search in title.
 		$search = $this->getState('filter.search');
 		if (!empty($search))
                 {
 			if (stripos($search, 'id:') === 0)
                         {
-				$query->where('u.id = '.(int) substr($search, 3));
+				$query->where('a.id = '.(int) substr($search, 3));
 			}
                         else
                         {
 				$search = $db->Quote('%'.$db->escape($search, true).'%');
-				$query->where('(u.name LIKE '.$search.' OR u.username LIKE '.$search.')');
+				$query->where('(u.username LIKE '.$search.' OR u.name LIKE '.$search.')');
 			}
+		}
+
+		// Filter on the language.
+		if ($language = $this->getState('filter.language'))
+                {
+			$query->where('a.language = ' . $db->quote($language));
 		}
 
 		// Add the list ordering clause.
@@ -117,15 +183,34 @@ class hwdMediaShareModelUsers extends JModelList
 
 		$query->order($db->escape($listOrder.' '.$listDirn));
 
+                // Additional filters
+                $groupId = $this->getState('filter.group_id');
+                if ($groupId > 0)
+                {
+                        // Join over the language
+                        $query->select('map.id AS mapid, map.group_id, IF(map.group_id = '.$groupId.', true, false) AS connection');
+                        $query->join('LEFT', '`#__hwdms_group_members` AS map ON map.member_id = a.id AND map.group_id = '.$groupId);
+
+                        $viewAll = $this->getState('filter.add_to_group') ? true : false;
+                        if (!$viewAll)
+                        {
+                                $query->where('map.group_id = ' . $db->quote($groupId));
+                        }
+                }
+                
 		//echo nl2br(str_replace('#__','jos_',$query));
 		return $query;
         }
+        
 	/**
 	 * Method to auto-populate the model state.
 	 *
 	 * Note. Calling getState in this method will result in recursion.
 	 *
-	 * @since	0.1
+	 * @param   string  $ordering   An optional ordering field.
+	 * @param   string  $direction  An optional direction (asc|desc).
+	 *
+	 * @return  void
 	 */
 	protected function populateState($ordering = null, $direction = null)
 	{
@@ -144,21 +229,15 @@ class hwdMediaShareModelUsers extends JModelList
 		
                 $status = $this->getUserStateFromRequest($this->context.'.filter.status', 'filter_status', '', 'string');
 		$this->setState('filter.status', $status);
-                
+
 		$language = $this->getUserStateFromRequest($this->context.'.filter.language', 'filter_language', '');
 		$this->setState('filter.language', $language);
-
-                $listOrder = $this->getUserStateFromRequest($this->context.'.filter_order', 'filter_order', 'u.name');
-                $this->setState('list.ordering', $listOrder);
-
-                $listDirn  = $this->getUserStateFromRequest($this->context.'.filter_order_Dir', 'filter_order_Dir', 'ASC');
-                $this->setState('list.direction', $listDirn);
 
 		// Load the parameters.
 		$params = JComponentHelper::getParams('com_hwdmediashare');
 		$this->setState('params', $params);
 
 		// List state information.
-		parent::populateState($listOrder, $listDirn);
+		parent::populateState('a.created', 'desc');
 	}
 }
