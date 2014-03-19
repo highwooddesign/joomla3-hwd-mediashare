2c32f2515c587a3a8aab6a34b82f244cda654ba2
Dave Horsfall <dave@horsfall.eu>
2014-03-19 10:14:03 +0000

    Updated subscriptions model.

--- administrator/components/com_hwdmediashare/models/subscriptions.php
+++ administrator/components/com_hwdmediashare/models/subscriptions.php
@@ -1,61 +1,96 @@
 <?php
 /**
- * @version    SVN $Id: subscriptions.php 425 2012-06-28 07:48:57Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2011 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      07-Nov-2011 11:40:26
+ * @package     Joomla.administrator
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
 
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
 
-// import Joomla modelform library
-jimport('joomla.application.component.modellist');
-
-/**
- * hwdMediaShare Model
- */
 class hwdMediaShareModelSubscriptions extends JModelList
 {
-        /**
-         * Method to build an SQL query to load the list data.
-         *
-         * @return      string  An SQL query
-         */
+    	/**
+	 * Constructor override, defines a white list of column filters.
+	 *
+	 * @param   array  $config  An optional associative array of configuration settings.
+	 */
+	public function __construct($config = array())
+	{
+		if (empty($config['filter_fields']))
+		{
+			$config['filter_fields'] = array(
+				'id', 'a.id',
+				'title', 'a.title',
+				'user_id', 'a.user_id',
+				'created', 'a.created',
+			);
+		}
+
+		parent::__construct($config);
+	}
+        
+	/**
+	 * Method to get a list of items.
+	 *
+	 * @return  mixed  An array of data items on success, false on failure.
+	 */
+	public function getItems()
+	{
+		$items = parent::getItems();
+
+                for ($x = 0, $count = count($items); $x < $count; $x++)
+                {
+                }
+
+		return $items;
+	}
+        
+	/**
+	 * Method to get the database query.
+	 *
+	 * @return  JDatabaseQuery  database query
+	 */
         protected function getListQuery()
         {
                 // Create a new query object.
                 $db = JFactory::getDBO();
                 $query = $db->getQuery(true);
-
+                
 		// Select the required fields from the table.
 		$query->select(
 			$this->getState(
 				'list.select',
-				'a.id, a.element_type, a.element_id, a.user_id'
+				'a.id, a.element_type, a.element_id, a.created, a.user_id'
 			)
 		);
-                // From the hello table
+                
+                // From the subscriptions table.
                 $query->from('#__hwdms_subscriptions AS a');
 
-                // From the Joomla user table
-		$query->select('u.name, u.username');
-		$query->join('LEFT', '`#__users` AS u ON a.user_id = u.id');
+                // Join over the users for the author, with value based on configuration.
+                $query->select('ua.name, ua.username');
+		$query->join('LEFT', '#__users AS ua ON ua.id=a.user_id');
 
-                // Filter by published state
+		// Filter by published state.
 		$element_type = $this->getState('filter.element_type');
-                if ($element_type !== 0) {
-			//$query->where('element_type = "'.$element_type.'"');
+		if (is_numeric($element_type) && $element_type)
+                {
+			$query->where('a.element_type = '.(int) $element_type);
 		}
 
-		// Filter by search in title
+		// Filter by search in title.
 		$search = $this->getState('filter.search');
-		if (!empty($search)) {
-			if (stripos($search, 'id:') === 0) {
+		if (!empty($search))
+                {
+			if (stripos($search, 'id:') === 0)
+                        {
 				$query->where('a.id = '.(int) substr($search, 3));
-			} else {
+			}
+                        else
+                        {
 				$search = $db->Quote('%'.$db->escape($search, true).'%');
 				$query->where('(u.name LIKE '.$search.' OR u.username LIKE '.$search.')');
 			}
@@ -70,12 +105,16 @@ class hwdMediaShareModelSubscriptions extends JModelList
 		//echo nl2br(str_replace('#__','jos_',$query));
 		return $query;
         }
+        
 	/**
 	 * Method to auto-populate the model state.
 	 *
 	 * Note. Calling getState in this method will result in recursion.
 	 *
-	 * @since	0.1
+	 * @param   string  $ordering   An optional ordering field.
+	 * @param   string  $direction  An optional direction (asc|desc).
+	 *
+	 * @return  void
 	 */
 	protected function populateState($ordering = null, $direction = null)
 	{
@@ -89,17 +128,11 @@ class hwdMediaShareModelSubscriptions extends JModelList
 		$elementType = $this->getUserStateFromRequest($this->context.'.filter.element_type', 'filter_element_type', '0', 'int');
 		$this->setState('filter.element_type', $elementType);
 
-                $listOrder = $this->getUserStateFromRequest($this->context.'.filter_order', 'filter_order', 'a.id');
-                $this->setState('list.ordering', $listOrder);
-
-                $listDirn  = $this->getUserStateFromRequest($this->context.'.filter_order_Dir', 'filter_order_Dir', 'ASC');
-                $this->setState('list.direction', $listDirn);
-
 		// Load the parameters.
 		$params = JComponentHelper::getParams('com_hwdmediashare');
 		$this->setState('params', $params);
 
 		// List state information.
-		parent::populateState($listOrder, $listDirn);
+		parent::populateState('a.created', 'desc');
 	}
 }
