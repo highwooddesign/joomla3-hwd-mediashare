67e5a85fee5de39d3a7a5f0a1d6b9b712a72f61a
Dave Horsfall <dave@horsfall.eu>
2014-03-19 10:42:03 +0000

    Updated extensions model.

--- administrator/components/com_hwdmediashare/models/extensions.php
+++ administrator/components/com_hwdmediashare/models/extensions.php
@@ -1,52 +1,87 @@
 <?php
 /**
- * @version    SVN $Id: extensions.php 277 2012-03-28 10:03:31Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2011 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      15-Apr-2011 10:13:15
+ * @package     Joomla.administrator
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
 
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
 
-// import Joomla modelform library
-jimport('joomla.application.component.modellist');
-
-/**
- * hwdMediaShare Model
- */
 class hwdMediaShareModelExtensions extends JModelList
 {
-	/**
-	 * Build an SQL query to load the list data.
+    	/**
+	 * Constructor override, defines a white list of column filters.
 	 *
-	 * @return	JDatabaseQuery
-	 * @since	0.1
+	 * @param   array  $config  An optional associative array of configuration settings.
+	 */
+	public function __construct($config = array())
+	{
+		if (empty($config['filter_fields']))
+		{
+			$config['filter_fields'] = array(
+				'id', 'a.id',
+				'ext', 'a.ext',
+				'media_type', 'a.media_type',
+				'published', 'a.published', 
+				'checked_out', 'a.checked_out',
+				'checked_out_time', 'a.checked_out_time',
+				'access', 'a.access', 'access_level',
+				'created_user_id', 'a.created_user_id', 'author',
+				'created', 'a.created',
+				'publish_up', 'a.publish_up',
+				'publish_down', 'a.publish_down',
+				'modified_user_id', 'a.modified_user_id',
+				'modified', 'a.modified',
+			);
+		}
+
+		parent::__construct($config);
+	}
+        
+	/**
+	 * Method to get a list of items.
+	 *
+	 * @return  mixed  An array of data items on success, false on failure.
+	 */
+	public function getItems()
+	{
+		$items = parent::getItems();
+
+                for ($x = 0, $count = count($items); $x < $count; $x++)
+                {
+                }
+
+		return $items;
+	}
+        
+	/**
+	 * Method to get the database query.
+	 *
+	 * @return  JDatabaseQuery  database query
 	 */
 	protected function getListQuery()
 	{
-		// Create a new query object.
-		$db		= $this->getDbo();
-		$query	= $db->getQuery(true);
-
+                // Create a new query object.
+                $db = JFactory::getDBO();
+                $query = $db->getQuery(true);
+                
 		// Select the required fields from the table.
 		$query->select(
 			$this->getState(
 				'list.select',
-				'a.id,a.ext,a.media_type,a.published,' .
-				'a.created_user_id,a.publish_up,a.checked_out,a.checked_out_time,' .
-				'a.publish_down,a.access'
+				'a.id, a.ext, a.media_type, a.published,' .
+				'a.created_user_id, a.publish_up, a.checked_out, a.checked_out_time,' .
+				'a.publish_down, a.access'
 			)
 		);
 
-                // Select the required fields from the table.
-		$query->select('a.id,a.ext,a.media_type,a.published,a.created_user_id,a.publish_up,a.publish_down,a.access');
-
+                // From the extensions table.
 		$query->from('#__hwdms_ext AS a');
 
-                // Join over the users for the checked out user.
+		// Join over the users for the checked out user.
 		$query->select('uc.name AS editor');
 		$query->join('LEFT', '#__users AS uc ON uc.id=a.checked_out');
 
@@ -54,7 +89,7 @@ class hwdMediaShareModelExtensions extends JModelList
 		$query->select('ag.title AS access_level');
 		$query->join('LEFT', '#__viewlevels AS ag ON ag.id = a.access');
 
-                // Filter by published state
+                // Filter by published state.
 		$published = $this->getState('filter.published');
 		if (is_numeric($published))
                 {
@@ -71,13 +106,14 @@ class hwdMediaShareModelExtensions extends JModelList
 			$query->where('a.access = '.(int) $access);
 		}
 
-		// Filter by media type
-		$mediaType = $this->getState('filter.mediaType');
-		if ($mediaType !== '')
+		// Filter by media type.
+		$mediaType = $this->getState('filter.media_type');
+		if (is_numeric($mediaType))
                 {
-			$query->where('media_type = "'.$mediaType.'"');
-		}
-		// Filter by search in title
+			$query->where('a.media_type = '.(int) $mediaType);
+		}                
+
+		// Filter by search in title.
 		$search = $this->getState('filter.search');
 		if (!empty($search))
                 {
@@ -86,7 +122,7 @@ class hwdMediaShareModelExtensions extends JModelList
 				$query->where('id = '.(int) substr($search, 3));
 			}
                         else
-                            {
+                        {
 				$search = $db->Quote('%'.$db->escape($search, true).'%');
 				$query->where('ext LIKE '.$search);
 			}
@@ -123,8 +159,8 @@ class hwdMediaShareModelExtensions extends JModelList
 		$published = $this->getUserStateFromRequest($this->context.'.filter.published', 'filter_published', '', 'string');
                 $this->setState('filter.published', $published);
 
-		$mediaType = $this->getUserStateFromRequest($this->context.'.filter.mediaType', 'filter_mediaType', '', 'string');
-		$this->setState('filter.mediaType', $mediaType);
+		$mediaType = $this->getUserStateFromRequest($this->context.'.filter.media_type', 'filter_media_type', '', 'string');
+		$this->setState('filter.media_type', $mediaType);
 
                 $listOrder = JRequest::getCmd('filter_order', 'ext');
                 $this->setState('list.ordering', $listOrder);
@@ -139,26 +175,4 @@ class hwdMediaShareModelExtensions extends JModelList
 		// List state information.
 		parent::populateState($listOrder, $listDirn);
 	}
-
-	/**
-	 * Method to get a store id based on model configuration state.
-	 *
-	 * This is necessary because the model is used by the component and
-	 * different modules that might need different sets of data or different
-	 * ordering requirements.
-	 *
-	 * @param	string		$id	A prefix for the store id.
-	 * @return	string		A store id.
-	 * @since	0.1
-	 */
-	protected function getStoreId($id = '')
-	{
-		// Compile the store id.
-		$id.= ':' . $this->getState('filter.search');
-		$id.= ':' . $this->getState('filter.media');
-
-		return parent::getStoreId($id);
-	}
-
-
 }
