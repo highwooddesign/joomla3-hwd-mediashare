092a5732fe45aa076d4e360b6f09beb7295186b1
Dave Horsfall <dave@horsfall.eu>
2014-03-19 10:52:13 +0000

    Updated customfields model.

--- administrator/components/com_hwdmediashare/models/customfields.php
+++ administrator/components/com_hwdmediashare/models/customfields.php
@@ -1,111 +1,138 @@
 <?php
 /**
- * @version    SVN $Id: customfields.php 1135 2013-02-21 11:04:43Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2011 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      15-Apr-2011 10:13:15
+ * @package     Joomla.administrator
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
 
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
 
-// import Joomla modelform library
-jimport('joomla.application.component.modellist');
-
-/**
- * hwdMediaShare Model
- */
 class hwdMediaShareModelCustomFields extends JModelList
 {
-	/**
-	 * Constructor.
+    	/**
+	 * Constructor override, defines a white list of column filters.
 	 *
-	 * @param	array	An optional associative array of configuration settings.
-	 * @see		JController
-	 * @since	0.1
+	 * @param   array  $config  An optional associative array of configuration settings.
 	 */
 	public function __construct($config = array())
 	{
+		if (empty($config['filter_fields']))
+		{
+			$config['filter_fields'] = array(
+				'id', 'a.id',
+				'element_type', 'a.element_type',
+				'type', 'a.type',
+ 				'ordering', 'a.ordering', 'map.ordering',
+				'published', 'a.published', 
+				'name', 'a.name',
+				'visible', 'a.visible',
+				'required', 'a.required',
+				'searchable', 'a.searchable',
+				'fieldcode', 'a.fieldcode',
+			);
+		}
+
 		parent::__construct($config);
 	}
-        /**
-         * Method to build an SQL query to load the list data.
-         *
-         * @return      string  An SQL query
-         */
+        
+	/**
+	 * Method to get a list of items.
+	 *
+	 * @return  mixed  An array of data items on success, false on failure.
+	 */
+	public function getItems()
+	{
+		$items = parent::getItems();
+
+                for ($x = 0, $count = count($items); $x < $count; $x++)
+                {
+                }
+
+		return $items;
+	}
+        
+	/**
+	 * Method to get the database query.
+	 *
+	 * @return  JDatabaseQuery  database query
+	 */
         protected function getListQuery()
         {
                 // Create a new query object.
                 $db = JFactory::getDBO();
                 $query = $db->getQuery(true);
-                // Select some fields
+
+                // Get HWD config.
+                $hwdms = hwdMediaShareFactory::getInstance();
+                $config = $hwdms->getConfig();
+                
 		// Select the required fields from the table.
 		$query->select(
 			$this->getState(
 				'list.select',
-				'a.id, a.element_type, a.type, a.name, a.ordering, a.published, a.visible, a.required, a.searchable, a.fieldcode, a.tooltip'
+				'a.id, a.element_type, a.type, a.name, a.ordering, a.published,' .
+				'a.visible, a.required, a.searchable, a.fieldcode, a.tooltip'
 			)
 		);
 
-                // From the hello table
+                // From the fields table
                 $query->from('#__hwdms_fields AS a');
 
-                // Filter by published state
-		$element_type = $this->getState('filter.element_type');
-		if ($element_type !== '') {
-			$query->where('element_type = "'.$element_type.'"');
-		}
-
-		// Filter by search in title
-		$search = $this->getState('filter.search');
-		if (!empty($search)) {
-			if (stripos($search, 'id:') === 0) {
-				$query->where('id = '.(int) substr($search, 3));
-			} else {
-				$search = $db->Quote('%'.$db->escape($search, true).'%');
-				$query->where('(name LIKE '.$search.' OR tooltip LIKE '.$search.')');
-			}
-		}
-
-		// Filter by published state
+		// Filter by published state.
 		$published = $this->getState('filter.published');
 		if (is_numeric($published))
                 {
 			$query->where('a.published = '.(int) $published);
-		} 
+		}
                 else if ($published === '')
                 {
 			$query->where('(a.published IN (0, 1))');
 		}
+                
+		// Filter by element type.
+		$elementType = $this->getState('filter.element_type');
+		if (is_numeric($elementType))
+                {
+			$query->where('a.element_type = '.(int) $elementType);
+		}   
+                
+		// Filter by search in title.
+		$search = $this->getState('filter.search');
+		if (!empty($search))
+                {
+			if (stripos($search, 'id:') === 0)
+                        {
+				$query->where('a.id = '.(int) substr($search, 3));
+			}
+                        else
+                        {
+				$search = $db->Quote('%'.$db->escape($search, true).'%');
+				$query->where('(a.name LIKE '.$search.' OR a.tooltip LIKE '.$search.')');
+			}
+		}
 
 		// Add the list ordering clause.
-                $orderCol = $this->state->get('list.ordering');
-                if (empty($orderCol))
-                {
-                        $orderCol = JRequest::getCmd('filter_order', 'ordering');
-                        $this->setState('list.ordering', $orderCol);
-                }
+                $listOrder = $this->state->get('list.ordering');
+                $listDirn = $this->state->get('list.direction');
 
-                $orderDirn = $this->state->get('list.direction');
-                if (empty($orderDirn))
-                {
-                        $orderDirn  = JRequest::getCmd('filter_order_Dir', 'ASC');
-                        $this->setState('list.direction', $listOrder);
-                }
-
-                $query->order($db->escape($orderCol.' '.$orderDirn));
+		$query->order($db->escape($listOrder.' '.$listDirn));
 
 		//echo nl2br(str_replace('#__','jos_',$query));
 		return $query;
         }
+        
 	/**
 	 * Method to auto-populate the model state.
 	 *
 	 * Note. Calling getState in this method will result in recursion.
 	 *
-	 * @since	0.1
+	 * @param   string  $ordering   An optional ordering field.
+	 * @param   string  $direction  An optional direction (asc|desc).
+	 *
+	 * @return  void
 	 */
 	protected function populateState($ordering = null, $direction = null)
 	{
@@ -119,44 +146,14 @@ class hwdMediaShareModelCustomFields extends JModelList
 		$published = $this->getUserStateFromRequest($this->context.'.filter.published', 'filter_published', '', 'string');
                 $this->setState('filter.published', $published);
                 
-		$element_type = $this->getUserStateFromRequest($this->context.'.filter.element_type', 'filter_element_type', '1', 'string');
-		$this->setState('filter.element_type', $element_type);
-
-                $listOrder = JRequest::getCmd('filter_order', 'ordering');
-                $this->setState('list.ordering', $listOrder);
-
-                $listDirn  = JRequest::getCmd('filter_order_Dir', 'ASC');
-                $this->setState('list.direction', $listDirn);
+		$elementType = $this->getUserStateFromRequest($this->context.'.filter.element_type', 'filter_element_type', '1', 'string');
+		$this->setState('filter.element_type', $elementType);
 
 		// Load the parameters.
 		$params = JComponentHelper::getParams('com_hwdmediashare');
 		$this->setState('params', $params);
 
 		// List state information.
-		parent::populateState($listOrder, $listDirn);
-	}
-	/**
-	 * Method to
-	 */
-	public function getProfileTypes()
-	{
-		static $types = false;
-
-		if( !$types )
-		{
-                        $path = JPATH_ROOT . '/components/com_hwdmediashare/libraries/fields/customfields.xml';
-                        $parser =& JFactory::getXML($path);                                        
-                        $fields	= $parser->fields;
-                        
-			$data	= array();
-
-			foreach( $fields->children() as $field )
-			{
-                                $data["$field->type"] = $field->name;
-			}
-			$types = $data;
-		}
-               
-		return $types;
+		parent::populateState('a.created', 'desc');
 	}
 }
