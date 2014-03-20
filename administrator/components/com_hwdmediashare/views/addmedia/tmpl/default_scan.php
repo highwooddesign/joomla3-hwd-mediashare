6d08f82c8bce99e792f9ea8364e85b0a432c0b96
Dave Horsfall <dave@horsfall.eu>
2014-03-20 14:31:54 +0000

    Updated the addmedia view default_scan template.

--- administrator/components/com_hwdmediashare/views/addmedia/tmpl/default_scan.php
+++ administrator/components/com_hwdmediashare/views/addmedia/tmpl/default_scan.php
@@ -1,48 +1,40 @@
 <?php
 /**
- * @version    SVN $Id: default_scan.php 496 2012-08-29 13:26:32Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2011 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      15-Apr-2011 10:13:15
+ * @package     Joomla.administrator
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
 
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
 
-// load tooltip behavior
-JHtml::_('behavior.tooltip');
-JHtml::_('behavior.framework');
-
-$user = & JFactory::getUser();
+JHtml::_('behavior.formvalidation');
 ?>
-<form action="<?php echo JRoute::_('index.php?option=com_hwdmediashare&task=addmedia.import'); ?>" method="post" name="adminForm" id="adminForm" class="form-validate">
-        <fieldset class="adminform" >
-                <ul class="panelform">
-                        <li>
-                                <label title="" class="hasTip" for="jform_embed_code" id="jform_embed_code-lbl">Directory</label>
-                                <input type="text" size="40" class="readonly" value="/media/<?php echo $this->folder; ?>" id="jform_secret">
-                        </li>
-                        <li>
-                                <label title="" class="hasTip" for="jform_embed_code" id="jform_embed_code-lbl">Media discovered</label>
-                                <input type="text" size="40" class="readonly" value="<?php echo $this->count; ?> media" id="jform_secret">
-                        </li>
-                        <?php if ($this->count > 0): ?>
-                        <li>
-                                <label></label>
-                                <button type="button" onclick="Joomla.submitbutton('addmedia.import')">
-                                        <?php echo JText::_('COM_HWDMS_IMPORT') ?>
-                                </button>
-                        </li>
-                        <?php endif; ?>
-                </ul>
-        </fieldset>
-        <div class="clr"> </div>
+<form action="<?php echo JRoute::_('index.php?option=com_hwdmediashare&view=addmedia&tmpl=component'); ?>" method="post" name="adminForm" id="adminForm" class="form-validate">
+
+        <div class="row-fluid">
+                <div class="span12">
+                        <div class="well well-small">
+                                <h4><?php echo JText::_('COM_HWDMS_DIRECTORY_SCANNING_IN', $this->folder); ?></h4>
+                                <p><code>/media/<?php echo $this->folder; ?></code></p>
+                                <div class="label<?php echo ($this->count > 0 ? ' label-success' : ''); ?>"><?php echo JText::sprintf('COM_HWDMS_FOUND_N_MEDIA', (int) $this->count); ?></div>
+                        </div>
+                </div>
+        </div>
+
+        <?php if ($this->count > 0): ?>
         <div>
-		<input type="hidden" name="task" value="" />
-                <input type="hidden" name="tmpl" value="component" />
-                <input type="hidden" name="folder" value="<?php echo $this->folder; ?>" />
-		<?php echo JHtml::_('form.token'); ?>
-	</div>
+                <div class="btn-group">
+                        <a class="btn" onclick="Joomla.submitbutton('addmedia.import')" href="javascript:void(0);"><?php echo JText::_('COM_HWDMS_IMPORT'); ?></a>
+                </div> 
+        </div>  
+        <?php endif; ?>
+
+        <input type="hidden" name="tmpl" value="component" />
+        <input type="hidden" name="folder" value="<?php echo $this->folder; ?>" />   
+
+        <input type="hidden" name="task" value="" />
+        <?php echo JHtml::_('form.token'); ?>
 </form>
