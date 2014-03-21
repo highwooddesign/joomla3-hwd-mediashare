1d5f10a2f30e9652e814b1fdd683610bd7d1a30e
Dave Horsfall <dave@horsfall.eu>
2014-03-21 12:10:20 +0000

    Updated categoryform model.

--- components/com_hwdmediashare/controllers/categoryform.php
+++ components/com_hwdmediashare/controllers/categoryform.php
@@ -1,48 +1,111 @@
 <?php
 /**
- * @version    SVN $Id: categoryform.php 1024 2013-02-01 10:37:53Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2011 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      26-Nov-2011 12:01:40
+ * @package     Joomla.administrator
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
 
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
 
-// Import Joomla controllerform library
-jimport('joomla.application.component.controllerform');
-
-/**
- * hwdMediaShare Controller
- */
 class hwdMediaShareControllerCategoryForm extends JControllerForm
 {
-        /**
-	 * @since	0.1
-	 */
-        public $elementType = 6;
-        
-        /**
-	 * @since	0.1
+	/**
+	 * The URL view item variable.
+	 *
+	 * @var    string
 	 */
 	protected $view_item = 'categoryform';
 
 	/**
-	 * @since	0.1
+	 * The URL view list variable.
+	 *
+	 * @var    string
 	 */
 	protected $view_list = 'categories';
 
 	/**
+	 * The URL edit variable.
+	 *
+	 * @var    string
+	 */
+	protected $urlVar = 'id';
+
+	/**
+	 * Method to add a new record.
+	 *
+	 * @return  mixed  True if the record can be added, a error object if not.
+	 */
+	public function add()
+	{
+		if (!parent::add())
+		{
+			// Redirect to the return page.
+			$this->setRedirect($this->getReturnPage());
+		}
+	}
+
+	/**
+	 * Method override to check if you can edit an existing record.
+	 *
+	 * @param   array   $data  An array of input data.
+	 * @param   string  $key   The name of the key for the primary key; default is id.
+	 *
+	 * @return  boolean
+	 */
+	protected function allowEdit($data = array(), $key = 'id')
+	{
+		$recordId = (int) isset($data[$key]) ? $data[$key] : 0;
+		$user     = JFactory::getUser();
+		$userId   = $user->get('id');
+		$asset    = 'com_hwdmediashare.category.' . $recordId;
+
+		// Check general edit permission first.
+		if ($user->authorise('core.edit', $asset))
+		{
+			return true;
+		}
+
+		// Fallback on edit.own.
+		// First test if the permission is available.
+		if ($user->authorise('core.edit.own', $asset))
+		{
+			// Now test the owner is the user.
+			$ownerId = (int) isset($data['created_user_id']) ? $data['created_user_id'] : 0;
+			if (empty($ownerId) && $recordId)
+			{
+				// Need to do a lookup from the model.
+				$record = $this->getModel()->getItem($recordId);
+
+				if (empty($record))
+				{
+					return false;
+				}
+
+				$ownerId = $record->created_user_id;
+			}
+
+			// If the owner matches 'me' then do the test.
+			if ($ownerId == $userId)
+			{
+				return true;
+			}
+		}
+
+		// Since there is no asset tracking, revert to the component permissions.
+		return parent::allowEdit($data, $key);
+	}
+
+	/**
 	 * Method to cancel an edit.
 	 *
-	 * @param	string	$key	The name of the primary key of the URL variable.
+	 * @param   string  $key  The name of the primary key of the URL variable.
 	 *
-	 * @return	Boolean	True if access level checks pass, false otherwise.
-	 * @since	0.1
+	 * @return  boolean  True if access level checks pass, false otherwise.
 	 */
-	public function cancel($key = 'id')
+	public function cancel($key = 'a_id')
 	{
 		parent::cancel($key);
 
@@ -51,16 +114,33 @@ class hwdMediaShareControllerCategoryForm extends JControllerForm
 	}
 
 	/**
+	 * Method to edit an existing record.
+	 *
+	 * @param   string  $key     The name of the primary key of the URL variable.
+	 * @param   string  $urlVar  The name of the URL variable if different from the primary key
+	 * (sometimes required to avoid router collisions).
+	 *
+	 * @return  boolean  True if access level check and checkout passes, false otherwise.
+	 *
+	 * @since   1.6
+	 */
+	public function edit($key = null, $urlVar = 'id')
+	{
+		$result = parent::edit($key, $urlVar);
+
+		return $result;
+	}
+
+	/**
 	 * Method to get a model object, loading it if required.
 	 *
-	 * @param	string	$name	The model name. Optional.
-	 * @param	string	$prefix	The class prefix. Optional.
-	 * @param	array	$config	Configuration array for model. Optional.
+	 * @param   string  $name    The model name. Optional.
+	 * @param   string  $prefix  The class prefix. Optional.
+	 * @param   array   $config  Configuration array for model. Optional.
 	 *
-	 * @return	object	The model.
-	 * @since	1.5
+	 * @return  object  The model.
 	 */
-	public function &getModel($name = 'categoryForm', $prefix = 'hwdMediaShareModel', $config = array('ignore_request' => true))
+	public function getModel($name = 'categoryForm', $prefix = 'hwdMediaShareModel', $config = array('ignore_request' => true))
 	{
 		$model = parent::getModel($name, $prefix, $config);
 
@@ -70,43 +150,37 @@ class hwdMediaShareControllerCategoryForm extends JControllerForm
 	/**
 	 * Gets the URL arguments to append to an item redirect.
 	 *
-	 * @param	int		$recordId	The primary key id for the item.
-	 * @param	string	$urlVar		The name of the URL variable for the id.
+	 * @param   integer  $recordId  The primary key id for the item.
+	 * @param   string   $urlVar    The name of the URL variable for the id.
 	 *
-	 * @return	string	The arguments to append to the redirect URL.
-	 * @since	0.1
+	 * @return  string	The arguments to append to the redirect URL.
 	 */
-	protected function getRedirectToItemAppend($recordId = null, $urlVar = 'a_id')
+	protected function getRedirectToItemAppend($recordId = null, $urlVar = 'id')
 	{
-		// Need to override the parent method completely.
-		$tmpl		= JRequest::getCmd('tmpl');
-		$layout		= JRequest::getCmd('layout', 'edit');
-		$append		= '';
+		$itemId	= $this->input->getInt('Itemid');
+		$return	= $this->getReturnPage();
+		$tmpl   = $this->input->get('tmpl');
 
-		// Setup redirect info.
-		if ($tmpl) {
-			$append .= '&tmpl='.$tmpl;
-		}
-
-		// TODO This is a bandaid, not a long term solution.
-//		if ($layout) {
-//			$append .= '&layout='.$layout;
-//		}
-		$append .= '&layout=edit';
-
-		if ($recordId) {
+		$append = '&layout=edit';
+                
+		if ($recordId)
+		{
 			$append .= '&'.$urlVar.'='.$recordId;
 		}
-
-		$itemId	= JRequest::getInt('Itemid');
-		$return	= $this->getReturnPage();
-
-		if ($itemId) {
+                
+		if ($itemId)
+		{
 			$append .= '&Itemid='.$itemId;
 		}
 
-		if ($return) {
+		if ($return)
+		{
 			$append .= '&return='.base64_encode($return);
+		}
+                
+		if ($tmpl)
+		{
+			$append .= '&tmpl='.$tmpl;
 		}
 
 		return $append;
@@ -117,17 +191,18 @@ class hwdMediaShareControllerCategoryForm extends JControllerForm
 	 *
 	 * If a "return" variable has been passed in the request
 	 *
-	 * @return	string	The return URL.
-	 * @since	0.1
+	 * @return  string	The return URL.
 	 */
 	protected function getReturnPage()
 	{
-		$return = JRequest::getVar('return', null, 'default', 'base64');
+		$return = $this->input->get('return', null, 'base64');
 
-		if (empty($return) || !JUri::isInternal(base64_decode($return))) {
-			return JURI::base();
+		if (empty($return) || !JUri::isInternal(base64_decode($return)))
+		{
+			return JUri::base();
 		}
-		else {
+		else
+		{
 			return base64_decode($return);
 		}
 	}
@@ -135,49 +210,61 @@ class hwdMediaShareControllerCategoryForm extends JControllerForm
 	/**
 	 * Function that allows child controller access to model data after the data has been saved.
 	 *
-	 * @param	JModel	$model		The data model object.
-	 * @param	array	$validData	The validated data.
+	 * @param   JModelLegacy  $model  The data model object.
+	 * @param   array         $validData   The validated data.
 	 *
-	 * @return	void
-	 * @since	0.1
+	 * @return  void
 	 */
-	protected function postSaveHook(JModelLegacy &$model, $validData)
+	protected function postSaveHook(JModelLegacy $model, $validData = array())
 	{
-		$task = $this->getTask();
-        }
+		return;
+	}
 
 	/**
 	 * Method to save a record.
 	 *
-	 * @param	string	$key	The name of the primary key of the URL variable.
-	 * @param	string	$urlVar	The name of the URL variable if different from the primary key (sometimes required to avoid router collisions).
+	 * @param   string  $key     The name of the primary key of the URL variable.
+	 * @param   string  $urlVar  The name of the URL variable if different from the primary key (sometimes required to avoid router collisions).
 	 *
-	 * @return	Boolean	True if successful, false otherwise.
-	 * @since	0.1
+	 * @return  boolean  True if successful, false otherwise.
 	 */
-	public function save()
+	public function save($key = null, $urlVar = 'id')
 	{
-                $form = parent::save();
-   
+		$result = parent::save($key, $urlVar);
+
 		// If ok, redirect to the return page.
-		if ($form) {
+		if ($result)
+		{
 			$this->setRedirect($this->getReturnPage());
 		}
 
-		return $form;
+		return $result;
 	}
-        
+
 	/**
-	 * Method to view the report form
-	 *
-	 * @param	int		Optional primary key of the article to increment.
-	 *
-	 * @return	boolean	True if successful; false otherwise and internal error set.
+	 * Method to display the report view
+	 * @return	void
 	 */
 	public function report()
 	{
-                $view = $this->getView('categoryForm','html');
-                $view->setModel( $this->getModel( 'categoryForm' ), true );
-                $view->report();
-	}   
+		// Get the document object.
+		$document	= JFactory::getDocument();
+		$vName		= 'categoryForm';
+		$vFormat	= 'html';
+
+		// Get and render the view.
+		if ($view = $this->getView($vName, $vFormat))
+		{
+			// Get the model for the view.
+			$model = $this->getModel($vName);
+
+			// Push the model into the view (as default).
+			$view->setModel($model, true);
+
+			// Push document object into the view.
+			$view->document = $document;
+
+			$view->report();
+		}
+	}
 }
