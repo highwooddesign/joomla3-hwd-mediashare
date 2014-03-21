03b20f507a4579599b67acb60f6f00e5b796b837
Dave Horsfall <dave@horsfall.eu>
2014-03-21 17:14:42 +0000

    Updated frontend discover view.

--- components/com_hwdmediashare/views/discover/view.html.php
+++ components/com_hwdmediashare/views/discover/view.html.php
@@ -1,64 +1,69 @@
 <?php
 /**
- * @version    SVN $Id: view.html.php 350 2012-05-01 11:13:13Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2011 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      14-Nov-2011 21:02:42
+ * @package     Joomla.administrator
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
 
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
 
-// Import Joomla view library
-jimport('joomla.application.component.view');
-
-/**
- * HTML View class for the hwdMediaShare Component
- */
-class hwdMediaShareViewDiscover extends JViewLegacy {
-	// Overwriting JView display method
+class hwdMediaShareViewDiscover extends JViewLegacy
+{
+	public $state;
+        
+	/**
+	 * Display the view
+	 *
+	 * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
+	 *
+	 * @return  void
+	 */
 	function display($tpl = null)
 	{
-                // Get data from the model
-		$state	= $this->get('State');
+                // Get data from the model.
+		$this->state = $this->get('State');
+		$this->params = $this->state->params;
 
-                // Download links
+                // Load libraries.
                 JLoader::register('hwdMediaShareHelperModule', JPATH_COMPONENT . '/helpers/module.php');
 
-		// Check for errors.
-		if (count($errors = $this->get('Errors')))
+		$this->pageclass_sfx = htmlspecialchars($this->params->get('pageclass_sfx'));
+                $this->return = base64_encode(JFactory::getURI()->toString());
+
+                // Check for errors.
+                if (count($errors = $this->get('Errors')))
                 {
-			JError::raiseWarning(500, implode("\n", $errors));
-			return false;
-		}
-
-                $params = &$state->params;
-
-		//Escape strings for HTML output
-		$this->pageclass_sfx = htmlspecialchars($params->get('pageclass_sfx'));
-
-                $this->assign('return',                 base64_encode(JFactory::getURI()->toString()));
-
-                $this->assignRef('state',		$state);
-                $this->assignRef('params',		$params);
+                        JError::raiseError(500, implode('<br />', $errors));
+                        return false;
+                }
 
 		$this->_prepareDocument();
-
+                
+		// Display the template.
 		parent::display($tpl);
 	}
+
+        
 	/**
 	 * Prepares the document
+	 *
+	 * @return  void
 	 */
 	protected function _prepareDocument()
 	{
-		$app	= JFactory::getApplication();
-                $menus	= $app->getMenu();
-		$title	= null;
+		$app = JFactory::getApplication();
+		$menus = $app->getMenu();
+		$pathway = $app->getPathway();
+		$title = null;
 
+                // Add page assets.
                 $this->document->addStyleSheet(JURI::base( true ).'/media/com_hwdmediashare/assets/css/hwd.css');
-                if ($this->state->params->get('load_joomla_css') != 0) $this->document->addStyleSheet(JURI::base( true ).'/media/com_hwdmediashare/assets/css/joomla.css');
+                if ($this->params->get('load_joomla_css') != 0) $this->document->addStyleSheet(JURI::base( true ).'/media/com_hwdmediashare/assets/css/joomla.css');
+                if ($this->params->get('list_thumbnail_aspect') != 0) $this->document->addStyleSheet(JURI::base( true ).'/media/com_hwdmediashare/assets/css/aspect.css');
+                if ($this->params->get('list_thumbnail_aspect') != 0) $this->document->addScript(JURI::base( true ).'/media/com_hwdmediashare/assets/javascript/aspect.js');
 
 		// Because the application sets a default page title,
 		// we need to get it from the menu item itself
@@ -67,14 +72,33 @@ class hwdMediaShareViewDiscover extends JViewLegacy {
 		{
 			$this->params->def('page_heading', $this->params->get('page_title', $menu->title));
 		}
-                else
-                {
+		else
+		{
 			$this->params->def('page_heading', JText::_('COM_HWDMS_DISCOVER'));
 		}
+
 		$title = $this->params->get('page_title', '');
+
+		// If the menu item does not concern this item
+		if ($menu && ($menu->query['option'] != 'com_hwdmediashare' || $menu->query['view'] != 'discover'))
+		{       
+			// If this is not a single item menu item, set the page title to the item title
+			$title = JText::_('COM_HWDMS_DISCOVER');    
+                        
+                        // Breadcrumb support
+			$path = array(array('title' => JText::_('COM_HWDMS_DISCOVER'), 'link' => ''));
+                                                
+			$path = array_reverse($path);
+			foreach($path as $item)
+			{
+				$pathway->addItem($item['title'], $item['link']);
+			}                    
+		}
+                
+		// Check for empty title and add site name if param is set
 		if (empty($title))
                 {
-			$title = JText::_('COM_HWDMS_DISCOVER');
+			$title = $app->getCfg('sitename');
 		}
 		elseif ($app->getCfg('sitename_pagetitles', 0) == 1)
                 {
@@ -84,26 +108,22 @@ class hwdMediaShareViewDiscover extends JViewLegacy {
                 {
 			$title = JText::sprintf('JPAGETITLE', $title, $app->getCfg('sitename'));
 		}
-                $this->document->setTitle($title);
+                
+		$this->document->setTitle($title);
 
-		if ($this->params->get('meta_desc'))
-		{
+                if ($this->params->get('meta_desc'))
+                {
 			$this->document->setDescription($this->params->get('meta_desc'));
-		}
+                }              
 
 		if ($this->params->get('meta_keys'))
-		{
+                {
 			$this->document->setMetadata('keywords', $this->params->get('meta_keys'));
-		}
+                }
 
 		if ($this->params->get('meta_rights'))
-		{
+                {
 			$this->document->setMetadata('copyright', $this->params->get('meta_rights'));
-		}
-         	
-                if ($this->params->get('meta_author'))
-		{
-			//$this->document->setMetadata('author', $this->params->get('meta_author'));
-		}       
+                }   
 	}
 }
