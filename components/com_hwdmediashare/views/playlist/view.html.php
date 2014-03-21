c9570d45b7039982b8885a22d1d35ad0aaf19744
Dave Horsfall <dave@horsfall.eu>
2014-03-21 14:47:57 +0000

    Updated frontend playlist view class

--- components/com_hwdmediashare/views/playlist/view.html.php
+++ components/com_hwdmediashare/views/playlist/view.html.php
@@ -1,96 +1,89 @@
 <?php
 /**
- * @version    SVN $Id: view.html.php 1218 2013-03-05 13:31:48Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2011 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      15-Apr-2011 10:13:15
+ * @package     Joomla.administrator
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
 
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
 
-// Import Joomla view library
-jimport('joomla.application.component.view');
+class hwdMediaShareViewPlaylist extends JViewLegacy
+{
+	public $playlist;
 
-/**
- * HTML View class for the hwdMediaShare Component
- */
-class hwdMediaShareViewPlaylist extends JViewLegacy {
-	// Overwriting JView display method
+	public $items;
+        
+	public $state;
+        
+	public $params;
+        
+	/**
+	 * Display the view
+	 *
+	 * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
+	 *
+	 * @return  void
+	 */
 	function display($tpl = null)
 	{
-                $app = & JFactory::getApplication();
+                // Get data from the model.
+                $this->playlist = $this->get('Playlist');
+                $this->items = $this->get('Items');
+                $this->pagination = $this->get('Pagination');
+		$this->state = $this->get('State');
+		$this->params = $this->state->params;
 
-                // Get the playlist data from the model
-                $playlist = $this->get('Playlist');
-
-                // Get data from the model
-                $items = $this->get('Items');
-                $pagination = $this->get('Pagination');
-		$state	= $this->get('State');
-
-                $playlist->nummedia = $this->get('NumMedia');
-                                
-                // Download links
-                hwdMediaShareFactory::load('downloads');
+                // Load libraries.
+                JLoader::register('JHtmlHwdIcon', JPATH_COMPONENT . '/helpers/icon.php');
+                JLoader::register('JHtmlHwdDropdown', JPATH_COMPONENT . '/helpers/dropdown.php');
+                JLoader::register('JHtmlString', JPATH_LIBRARIES.'/joomla/html/html/string.php');
                 hwdMediaShareFactory::load('files');
+                hwdMediaShareFactory::load('downloads');
                 hwdMediaShareFactory::load('media');
 		hwdMediaShareFactory::load('utilities');
-                JLoader::register('JHtmlHwdIcon', JPATH_COMPONENT . '/helpers/icon.php');
-                JLoader::register('JHtmlString', JPATH_LIBRARIES.'/joomla/html/html/string.php');
-
-		// Check for errors.
-		if (count($errors = $this->get('Errors')))
-		{
-			JError::raiseError(500, implode('<br />', $errors));
-			return false;
-		}
                 
-                if (count($items) == 0)
+                $this->utilities = hwdMediaShareUtilities::getInstance();
+		$this->pageclass_sfx = htmlspecialchars($this->params->get('pageclass_sfx'));
+                $this->columns = $this->params->get('list_columns', 3);
+                $this->return = base64_encode(JFactory::getURI()->toString());
+                $this->display = $this->state->get('media.display', 'details');
+
+                // Check for errors.
+                if (count($errors = $this->get('Errors')))
                 {
-                        JFactory::getApplication()->enqueueMessage( JText::_('COM_HWDMS_NOTICE_NO_MEDIA_IN_PLAYLIST') );
+                        JError::raiseError(500, implode('<br />', $errors));
+                        return false;
                 }
-                
-		$params = &$state->params;
-
-		// Escape strings for HTML output
-		$this->pageclass_sfx = htmlspecialchars($params->get('pageclass_sfx'));
-
-                $this->assign('return',                 base64_encode(JFactory::getURI()->toString()));
-                
-                $this->assignRef('params',		$params);
-		$this->assignRef('playlist',		$playlist);
-                $this->assignRef('parent',		$parent);
-		$this->assignRef('items',		$items);
-                $this->assignRef('state',		$state);
-                $this->assignRef('pagination',		$pagination);
-                $this->assignRef('display',		JRequest::getWord( 'display', 'grid' ));
-                $this->assignRef('utilities',		hwdMediaShareUtilities::getInstance());
 
                 $model = $this->getModel();
 		$model->hit();
-                
-		$this->_prepareDocument();
 
-                // Display the view
-                parent::display($tpl);
+		$this->_prepareDocument();
+                
+		// Display the template.
+		parent::display($tpl);
 	}
+        
 	/**
 	 * Prepares the document
+	 *
+	 * @return  void
 	 */
 	protected function _prepareDocument()
 	{
-		$app	= JFactory::getApplication();
-		$menus	= $app->getMenu();
+		$app = JFactory::getApplication();
+		$menus = $app->getMenu();
 		$pathway = $app->getPathway();
-		$title	= null;
+		$title = null;
 
+                // Add page assets.
                 $this->document->addStyleSheet(JURI::base( true ).'/media/com_hwdmediashare/assets/css/hwd.css');
-                if ($this->state->params->get('load_joomla_css') != 0) $this->document->addStyleSheet(JURI::base( true ).'/media/com_hwdmediashare/assets/css/joomla.css');
-                if ($this->state->params->get('list_thumbnail_aspect') != 0) $this->document->addStyleSheet(JURI::base( true ).'/media/com_hwdmediashare/assets/css/aspect.css');
-                if ($this->state->params->get('list_thumbnail_aspect') != 0) $this->document->addScript(JURI::base( true ).'/media/com_hwdmediashare/assets/javascript/aspect.js');
+                if ($this->params->get('load_joomla_css') != 0) $this->document->addStyleSheet(JURI::base( true ).'/media/com_hwdmediashare/assets/css/joomla.css');
+                if ($this->params->get('list_thumbnail_aspect') != 0) $this->document->addStyleSheet(JURI::base( true ).'/media/com_hwdmediashare/assets/css/aspect.css');
+                if ($this->params->get('list_thumbnail_aspect') != 0) $this->document->addScript(JURI::base( true ).'/media/com_hwdmediashare/assets/javascript/aspect.js');
 
 		// Because the application sets a default page title,
 		// we need to get it from the menu item itself
@@ -107,7 +100,7 @@ class hwdMediaShareViewPlaylist extends JViewLegacy {
 		$title = $this->params->get('page_title', '');
 
 		$id = (int) @$menu->query['id'];
-
+                
 		// If the menu item does not concern this item
 		if ($menu && ($menu->query['option'] != 'com_hwdmediashare' || $menu->query['view'] != 'playlist' || $id != $this->playlist->id))
 		{
@@ -128,17 +121,22 @@ class hwdMediaShareViewPlaylist extends JViewLegacy {
 		}
 
 		// Check for empty title and add site name if param is set
-		if (empty($title)) {
+		if (empty($title))
+                {
 			$title = $app->getCfg('sitename');
 		}
-		elseif ($app->getCfg('sitename_pagetitles', 0) == 1) {
+		elseif ($app->getCfg('sitename_pagetitles', 0) == 1)
+                {
 			$title = JText::sprintf('JPAGETITLE', $app->getCfg('sitename'), $title);
 		}
-		elseif ($app->getCfg('sitename_pagetitles', 0) == 2) {
+		elseif ($app->getCfg('sitename_pagetitles', 0) == 2)
+                {
 			$title = JText::sprintf('JPAGETITLE', $title, $app->getCfg('sitename'));
 		}
-		if (empty($title)) {
-			$title = $this->playlist->title;
+                
+		if (empty($title))
+		{
+			$title = $this->item->title;
 		}
 		$this->document->setTitle($title);
 
@@ -146,13 +144,13 @@ class hwdMediaShareViewPlaylist extends JViewLegacy {
 		{
 			$this->document->setDescription($this->playlist->params->get('meta_desc'));
 		}
+                elseif ($this->playlist->description)
+                {                        
+			$this->document->setDescription($this->escape(JHtmlString::truncate($this->playlist->description, 160, true, false)));   
+                }                 
                 elseif ($this->params->get('meta_desc'))
                 {
 			$this->document->setDescription($this->params->get('meta_desc'));
-                }
-                else
-                {                        
-			$this->document->setDescription($this->escape(JHtmlString::truncate($this->playlist->description, $this->params->get('list_desc_truncate'), true, false)));   
                 }                
 
 		if ($this->playlist->params->get('meta_keys'))
@@ -180,6 +178,6 @@ class hwdMediaShareViewPlaylist extends JViewLegacy {
                 elseif ($this->params->get('meta_author') == 1)
                 {
 			$this->document->setMetadata('author', $this->playlist->author);
-                }   
-	}
+                }
+        }
 }
