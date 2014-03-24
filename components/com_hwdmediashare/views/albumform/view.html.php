16e63c614b8b93c59b21b7edada7c4a84938c2cd
Dave Horsfall <dave@horsfall.eu>
2014-03-24 18:01:40 +0000

    Updated frontend albumform view.

--- components/com_hwdmediashare/views/albumform/view.html.php
+++ components/com_hwdmediashare/views/albumform/view.html.php
@@ -57,7 +57,7 @@ class hwdMediaShareViewAlbumForm extends JViewLegacy
 		}
 		else
 		{
-			$authorised = $this->item->params->get('access-edit');
+			$authorised = $this->item->access->get('access-edit');
 		}
 		if ($authorised !== true)
 		{
