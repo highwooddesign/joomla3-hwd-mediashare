a4b0f32c1445f1e60176292ad96ad862f1794ba8
Dave Horsfall <dave@horsfall.eu>
2014-03-24 18:00:49 +0000

    Updated frontend playlistform view.

--- components/com_hwdmediashare/views/playlistform/view.html.php
+++ components/com_hwdmediashare/views/playlistform/view.html.php
@@ -57,7 +57,7 @@ class hwdMediaShareViewPlaylistForm extends JViewLegacy
 		}
 		else
 		{
-			$authorised = $this->item->params->get('access-edit');
+			$authorised = $this->item->access->get('access-edit');
 		}
 		if ($authorised !== true)
 		{
