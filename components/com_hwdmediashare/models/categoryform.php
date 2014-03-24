ea52e6c1699bb79c25bf2a8ae3ed5ab1390ae3b6
Dave Horsfall <dave@horsfall.eu>
2014-03-24 17:47:19 +0000

    Updated frontend categoryform model.

--- components/com_hwdmediashare/models/categoryform.php
+++ components/com_hwdmediashare/models/categoryform.php
@@ -1,66 +1,53 @@
 <?php
 /**
- * @version    SVN $Id: categoryform.php 425 2012-06-28 07:48:57Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2011 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      26-Nov-2011 12:02:06
+ * @package     Joomla.administrator
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
 
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
 
 // Base this model on the backend version.
 require_once JPATH_ADMINISTRATOR.'/components/com_categories/models/category.php';
 
-// Import Joomla modelitem library
-jimport('joomla.application.component.modelform');
-
-/**
- * hwdMediaShare Model
- */
 class hwdMediaShareModelCategoryForm extends CategoriesModelCategory
 {
-        /**
-	 * Returns a reference to the a Table object, always creating it.
-	 *
-	 * @param	type	The table type to instantiate
-	 * @param	string	A prefix for the table class name. Optional.
-	 * @param	array	Configuration array for model. Optional.
-	 * @return	JTable	A database object
-	 * @since	0.1
-	 */
-	public function getTable($type = 'Category', $prefix = 'hwdMediaShareTable', $config = array())
-	{
-                JTable::addIncludePath(JPATH_ADMINISTRATOR.'/components/com_hwdmediashare/tables');
-                return JTable::getInstance($type, $prefix, $config);
-	}
-        /**
+	/**
 	 * Method to auto-populate the model state.
 	 *
 	 * Note. Calling getState in this method will result in recursion.
 	 *
-	 * @since	0.1
+	 * @param   string  $ordering   An optional ordering field.
+	 * @param   string  $direction  An optional direction (asc|desc).
+	 *
+	 * @return  void
 	 */
 	protected function populateState()
 	{
+		// Initialise variables.
 		$app = JFactory::getApplication();
-
-		// Load state from the request.
-		$id = JRequest::getInt('id');
-		$this->setState('category.id', $id);
-
-		$return = JRequest::getVar('return', null, 'default', 'base64');
-		$this->setState('return_page', base64_decode($return));
-
+                $user = JFactory::getUser();
+                
 		// Load the parameters.
                 $hwdms = hwdMediaShareFactory::getInstance();
                 $config = $hwdms->getConfig();
                 $this->setState('params', $config);
 
-		parent::populateState();
+		// Load state from the request.
+		$id = $app->input->getInt('id');
+		$this->setState('category.id', $id);
+
+		$return = $app->input->get('return', null, 'base64');
+		$this->setState('return_page', base64_decode($return));
+
+		$this->setState('layout', $app->input->getString('layout'));                
+
+		parent::populateState();               
 	}
+
 	/**
 	 * Method to get article data.
 	 *
@@ -80,7 +67,8 @@ class hwdMediaShareModelCategoryForm extends CategoriesModelCategory
 		$return = $table->load($itemId);
 
 		// Check for a table object error.
-		if ($return === false && $table->getError()) {
+		if ($return === false && $table->getError())
+                {
 			$this->setError($table->getError());
 			return false;
 		}
@@ -88,9 +76,16 @@ class hwdMediaShareModelCategoryForm extends CategoriesModelCategory
 		$properties = $table->getProperties(1);
 		$value = JArrayHelper::toObject($properties, 'JObject');
 
-		// Convert attrib field to Registry.
-		$value->params = new JRegistry;
-                $value->params->loadString(@$value->params);
+		// Convert params field to registry.
+		if (property_exists($value, 'params'))
+		{
+			$registry = new JRegistry;
+			$registry->loadString($value->params);
+			$value->params = $registry->toArray();
+		}
+                
+		// Define access registry.
+ 		$value->access = new JRegistry;
 
 		// Compute selected asset permissions.
 		$user	= JFactory::getUser();
@@ -98,62 +93,75 @@ class hwdMediaShareModelCategoryForm extends CategoriesModelCategory
 		$asset	= 'com_hwdmediashare.category.'.$value->id;
 
 		// Check general edit permission first.
-		if ($user->authorise('core.edit', $asset)) 
-                {
-			$value->params->set('access-edit', true);
+		if ($user->authorise('core.edit', $asset))
+		{
+			$value->access->set('access-edit', true);
 		}
+
 		// Now check if edit.own is available.
-		else if (!empty($userId) && $user->authorise('core.edit.own', $asset)) 
-                {
+		elseif (!empty($userId) && $user->authorise('core.edit.own', $asset))
+		{
 			// Check for a valid user and that they are the owner.
 			if ($userId == $value->created_user_id)
-                        {
-				$value->params->set('access-edit', true);
+			{
+				$value->access->set('access-edit', true);
 			}
 		}
 
 		// Check edit state permission.
-		if ($itemId) 
-                {
+		if ($itemId)
+		{
 			// Existing item
-			$value->params->set('access-change', $user->authorise('core.edit.state', $asset));
+			$value->access->set('access-change', $user->authorise('core.edit.state', $asset));
 		}
-		else 
-                {
-                        // New item    
-			$value->params->set('access-change', $user->authorise('core.edit.state', 'com_hwdmediashare'));
+		else
+		{
+			// New item.
+			$value->access->set('access-change', $user->authorise('core.edit.state', 'com_hwdmediashare'));
+		}
+
+		if ($itemId)
+		{
+			//$value->tags = new JHelperTags;
+			//$value->tags->getTagIds($value->id, 'com_content.article');
+                        hwdMediaShareFactory::load('customfields');
+                        $cf = hwdMediaShareCustomFields::getInstance();
+                        $cf->elementType = 2;
+                        $item->customfields = $cf->get($item);
+                        $value->thumbnail = $this->getThumbnail($value);
 		}
 
 		return $value;
 	}
+
 	/**
 	 * Get the return URL.
 	 *
-	 * @return	string	The return URL.
-	 * @since	0.1
+	 * @return  string  The return URL.
 	 */
 	public function getReturnPage()
 	{
 		return base64_encode($this->getState('return_page'));
 	}
         
-       /**
-	 * Method to get the record form.
+	/**
+	 * Method for getting the report form.
 	 *
-	 * @param	array	$data		Data for the form.
-	 * @param	boolean	$loadData	True if the form is to load its own data (default case), false if not.
-	 * @return	mixed	A JForm object on success, false on failure
-	 * @since	0.1
+	 * @param   array    $data      Data for the form.
+	 * @param   boolean  $loadData  True if the form is to load its own data (default case), false if not.
+	 *
+	 * @return  mixed  A JForm object on success, false on failure
 	 */
-	public function getReportForm($data = array(), $loadData = true)
+	public function getReportForm()
 	{
-                $form = & JForm::getInstance('report', JPATH_SITE.'/components/com_hwdmediashare/models/forms/report.xml');
+		// Get the form.
+		$form = JForm::getInstance('report', JPATH_SITE.'/components/com_hwdmediashare/models/forms/report.xml');
 
-                if (empty($form))
+		if (empty($form))
 		{
-			$this->setError(JText::_('COM_HWDMS_ERROR_FAILED_TO_LOAD_FORM'));
-                        return false;
+			return false;
 		}
+
 		return $form;
-	}        
+	}
 }
