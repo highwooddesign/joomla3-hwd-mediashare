9f2d62aa5479cabb945e40e589875e385e9d547d
Dave Horsfall <dave@horsfall.eu>
2014-03-27 11:55:30 +0000

    Cleanup getItem method in playlist model to get the Joomla tags (using Joomla API), and other data.

--- administrator/components/com_hwdmediashare/models/playlist.php
+++ administrator/components/com_hwdmediashare/models/playlist.php
@@ -29,11 +29,20 @@ class hwdMediaShareModelPlaylist extends JModelAdmin
 	{
                 if ($item = parent::getItem($pk))
                 {
+                        // Add the tags.
+                        $item->tags = new JHelperTags;
+                        $item->tags->getTagIds($item->id, 'com_hwdmediashare.playlist');
+                        
+                        // Add the custom fields.
                         hwdMediaShareFactory::load('customfields');
                         $cf = hwdMediaShareCustomFields::getInstance();
                         $cf->elementType = 4;
                         $item->customfields = $cf->get($item);
+                        
+                        // Add the number of media in the playlist.
                         $item->nummedia = $this->getMediaCount($item);
+                        
+                        // Add the playlist thumbnail.
                         $item->thumbnail = $this->getThumbnail($item);
                 }
 
