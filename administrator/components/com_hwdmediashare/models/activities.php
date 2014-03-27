cbea55f7fb483b938cc70ad9b226c6306fdeb6fc
Dave Horsfall <dave@horsfall.eu>
2014-03-27 11:47:55 +0000

    Updated activities model.

--- administrator/components/com_hwdmediashare/models/activities.php
+++ administrator/components/com_hwdmediashare/models/activities.php
@@ -1,59 +1,65 @@
 <?php
 /**
- * @version    SVN $Id: activities.php 1398 2013-04-30 09:30:46Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2011 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      26-Oct-2011 10:22:53
+ * @package     Joomla.administrator
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
 
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
 
-// import Joomla modelform library
-jimport('joomla.application.component.modellist');
-
-/**
- * hwdMediaShare Model
- */
 class hwdMediaShareModelActivities extends JModelList
 {
-    
-        /**
-	 * Method to get a single record.
+    	/**
+	 * Constructor override, defines a white list of column filters.
 	 *
-	 * @param	integer	The id of the primary key.
-	 *
-	 * @return	mixed	Object on success, false on failure.
+	 * @param   array  $config  An optional associative array of configuration settings.
 	 */
-	public function getItems($pk = null)
+	public function __construct($config = array())
 	{
-                if ($items = parent::getItems($pk))
-                {
-                        for ($i=0, $n=count($items); $i < $n; $i++)
-                        {
-                                if (empty($items[$i]->author))
-                                {
-                                        $items[$i]->author = JText::_('COM_HWDMS_GUEST');
-                                }
-                        }
-                }
-                return $items;
+		if (empty($config['filter_fields']))
+		{
+			$config['filter_fields'] = array(
+				'activity_type', 'a.activity_type',
+				'created', 'a.created',
+				'id', 'a.id',
+			);
+		}
+
+		parent::__construct($config);
 	}
         
-        /**
-         * Method to build an SQL query to load the list data.
-         *
-         * @return      string  An SQL query
-         */
+	/**
+	 * Method to get a list of items.
+	 *
+	 * @return  mixed  An array of data items on success, false on failure.
+	 */
+	public function getItems()
+	{
+		$items = parent::getItems();
+
+                for ($x = 0, $count = count($items); $x < $count; $x++)
+                {
+                        if (empty($items[$x]->author)) $items[$x]->author = JText::_('COM_HWDMS_GUEST');
+                }
+
+		return $items;
+	}
+        
+	/**
+	 * Method to get the database query.
+	 *
+	 * @return  JDatabaseQuery  database query
+	 */
         protected function getListQuery()
         {
                 // Create a new query object.
                 $db = JFactory::getDBO();
                 $query = $db->getQuery(true);
 
-                // Get hwdMediaShare config
+                // Get HWD config.
                 $hwdms = hwdMediaShareFactory::getInstance();
                 $config = $hwdms->getConfig();
                 
@@ -61,95 +67,18 @@ class hwdMediaShareModelActivities extends JModelList
 		$query->select(
 			$this->getState(
 				'list.select',
-				'a.id, a.activity_type, a.element_type, a.element_id, a.element_type, a.reply_id,' .
-                                'a.title, a.description, a.alias, a.checked_out, a.checked_out_time,' .
-				'a.created_user_id, a.hits, a.likes, a.dislikes, a.published, a.featured,' .
-				'a.status, a.publish_up, a.publish_down, a.ordering, a.created, a.access,'.
-				'a.language, a.created_user_id_alias'
+				'a.id, a.activity_type, a.element_type, a.element_id, a.reply_id,' .
+                                'a.target_element_id, a.target_element_id, a.description, a.created_user_id, a.created'
 			)
 		);
 
-                // From the albums table
-                $query->from('#__hwdms_activities AS a');
+                // From the activities table.
+		$query->from($db->quoteName('#__hwdms_activities') . ' AS a');
 
-		// Join over the language
-		$query->select('l.title AS language_title');
-		$query->join('LEFT', '`#__languages` AS l ON l.lang_code = a.language');
-
-		// Join over the users for the checked out user.
-		$query->select('uc.name AS editor');
-		$query->join('LEFT', '#__users AS uc ON uc.id=a.checked_out');
-		
-                if ($config->get('author') == 0)
-                {
-                    $query->select("CASE WHEN a.created_user_id_alias > ' ' THEN a.created_user_id_alias ELSE ua.username END AS author");
-                }
-                else
-                {
-                    $query->select("CASE WHEN a.created_user_id_alias > ' ' THEN a.created_user_id_alias ELSE ua.name END AS author");
-                }
-                $query->join('LEFT', '#__users AS ua ON ua.id = a.created_user_id');
-		$query->join('LEFT', '#__users AS uam ON uam.id = a.modified_user_id');
-                
-		// Join over the asset groups.
-		$query->select('ag.title AS access_level');
-		$query->join('LEFT', '#__viewlevels AS ag ON ag.id = a.access');
-
-		// Filter by access level.
-		if ($access = $this->getState('filter.access'))
-                {
-			$query->where('a.access = '.(int) $access);
-		}
-
-		// Filter by published state
-		$published = $this->getState('filter.published');
-		if (is_numeric($published))
-                {
-			$query->where('a.published = '.(int) $published);
-		}
-                else if ($published === '')
-                {
-			$query->where('(a.published IN (0, 1))');
-		}
-
-                // Filter by status state
-		$status = $this->getState('filter.status');
-                if (is_numeric($status)) 
-                {
-			if ($status == 3)
-                        {
-                                $query->select('COUNT(report.element_id) AS report_count');
-                                $query->join('LEFT', '`#__hwdms_reports` AS report ON report.element_id = a.id AND report.element_type = 7');
-                                $query->where('a.id = report.element_id');
-                                $query->group('report.element_id');
-                        }
-                        else
-                        {
-                                $query->where('a.status = '.(int) $status);
-                        } 
-		}
-                
-		// Filter by search in title
-		$search = $this->getState('filter.search');
-		if (!empty($search))
-                {
-			if (stripos($search, 'id:') === 0)
-                        {
-				$query->where('a.id = '.(int) substr($search, 3));
-			}
-                        else
-                        {
-				$search = $db->Quote('%'.$db->escape($search, true).'%');
-				$query->where('(a.title LIKE '.$search.' OR a.alias LIKE '.$search.')');
-			}
-		}
-
-		// Filter on the language.
-		if ($language = $this->getState('filter.language'))
-                {
-			$query->where('a.language = ' . $db->quote($language));
-		}
-
+                // Join over the users for the author, with value based on configuration.
+                $config->get('author') == 0 ? $query->select('ua.name AS author') : $query->select('ua.username AS author');
+		$query->join('LEFT', '#__users AS ua ON ua.id=a.created_user_id');
+                                
 		// Add the list ordering clause.
                 $listOrder = $this->state->get('list.ordering');
                 $listDirn = $this->state->get('list.direction');
@@ -159,50 +88,20 @@ class hwdMediaShareModelActivities extends JModelList
 		//echo nl2br(str_replace('#__','jos_',$query));
 		return $query;
         }
+        
 	/**
 	 * Method to auto-populate the model state.
 	 *
 	 * Note. Calling getState in this method will result in recursion.
 	 *
-	 * @since	0.1
+	 * @param   string  $ordering   An optional ordering field.
+	 * @param   string  $direction  An optional direction (asc|desc).
+	 *
+	 * @return  void
 	 */
 	protected function populateState($ordering = null, $direction = null)
 	{
-		// Initialise variables.
-		$app = JFactory::getApplication('administrator');
-
-		// Load the filter state.
-		$search = $this->getUserStateFromRequest($this->context.'.filter.search', 'filter_search');
-		$this->setState('filter.search', $search);
-
-		$accessId = $this->getUserStateFromRequest($this->context.'.filter.access', 'filter_access', null, 'int');
-		$this->setState('filter.access', $accessId);
-
-		$published = $this->getUserStateFromRequest($this->context.'.filter.published', 'filter_published', '', 'string');
-                $this->setState('filter.published', $published);
-                
-                $status = $this->getUserStateFromRequest($this->context.'.filter.status', 'filter_status', '', 'string');
-		$this->setState('filter.status', $status);
-                
-		$categoryId = $this->getUserStateFromRequest($this->context.'.filter.category_id', 'filter_category_id', '');
-		$this->setState('filter.category_id', $categoryId);
-
-		$language = $this->getUserStateFromRequest($this->context.'.filter.language', 'filter_language', '');
-		$this->setState('filter.language', $language);
-
-                // Passing additional parameters to prevent resetting the page
-                $listOrder = $this->getUserStateFromRequest($this->context.'.filter_order', 'filter_order', 'a.created', null, false);
-                $this->setState('list.ordering', $listOrder);
-
-                // Passing additional parameters to prevent resetting the page
-                $listDirn  = $this->getUserStateFromRequest($this->context.'.filter_order_Dir', 'filter_order_Dir', 'DESC', null, false);
-                $this->setState('list.direction', $listDirn);
-
-		// Load the parameters.
-		$params = JComponentHelper::getParams('com_hwdmediashare');
-		$this->setState('params', $params);
-
 		// List state information.
-		parent::populateState($listOrder, $listDirn);
+		parent::populateState('a.created', 'desc');
 	}
 }
