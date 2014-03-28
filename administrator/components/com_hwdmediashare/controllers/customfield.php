a27641e39600ff1c6032c0b23e29a9e98118cedb
Dave Horsfall <dave@horsfall.eu>
2014-03-28 14:44:40 +0000

    Added batch method to the customfield controller.

--- administrator/components/com_hwdmediashare/controllers/customfield.php
+++ administrator/components/com_hwdmediashare/controllers/customfield.php
@@ -12,4 +12,29 @@ defined('_JEXEC') or die;
 
 class hwdMediaShareControllerCustomField extends JControllerForm
 {
+	/**
+	 * The URL view list variable.
+	 * @var    string
+	 */
+    	protected $view_list = "customfields";
+
+	/**
+	 * Method to run batch operations.
+	 *
+	 * @param   object      $model  The model.
+	 *
+	 * @return  boolean     True if successful, false otherwise and internal error is set.
+	 */    
+	public function batch($model = null)
+	{
+		JSession::checkToken() or jexit(JText::_('JINVALID_TOKEN'));
+
+		// Set the model
+		$model = $this->getModel('CustomField', '', array());
+
+		// Preset the redirect
+		$this->setRedirect(JRoute::_('index.php?option=com_hwdmediashare&view=' . $this->view_list . $this->getRedirectToListAppend(), false));
+
+		return parent::batch($model);
+	}     
 }
