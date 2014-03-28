7fd7baf743195ba714aca86562b619fe0350c702
Dave Horsfall <dave@horsfall.eu>
2014-03-28 14:43:06 +0000

    Added batch method to the album controller.

--- administrator/components/com_hwdmediashare/controllers/album.php
+++ administrator/components/com_hwdmediashare/controllers/album.php
@@ -17,10 +17,24 @@ class hwdMediaShareControllerAlbum extends JControllerForm
 	 * @var    string
 	 */
     	protected $view_list = "albums";
-        
+
 	/**
-	 * The ID of this element type.
-	 * @var    integer
-	 */
-    	protected $elementType = 2;
+	 * Method to run batch operations.
+	 *
+	 * @param   object      $model  The model.
+	 *
+	 * @return  boolean     True if successful, false otherwise and internal error is set.
+	 */    
+	public function batch($model = null)
+	{
+		JSession::checkToken() or jexit(JText::_('JINVALID_TOKEN'));
+
+		// Set the model
+		$model = $this->getModel('Album', '', array());
+
+		// Preset the redirect
+		$this->setRedirect(JRoute::_('index.php?option=com_hwdmediashare&view=' . $this->view_list . $this->getRedirectToListAppend(), false));
+
+		return parent::batch($model);
+	}  
 }
