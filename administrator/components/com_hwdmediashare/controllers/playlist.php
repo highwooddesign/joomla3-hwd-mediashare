321c88a0487d2085f10491be6225e6b1fca59adf
Dave Horsfall <dave@horsfall.eu>
2014-03-28 15:41:35 +0000

    Added batch method to playlist controller.

--- administrator/components/com_hwdmediashare/controllers/playlist.php
+++ administrator/components/com_hwdmediashare/controllers/playlist.php
@@ -17,10 +17,24 @@ class hwdMediaShareControllerPlaylist extends JControllerForm
 	 * @var    string
 	 */
     	protected $view_list = "playlists";
-        
+
 	/**
-	 * The ID of this element type.
-	 * @var    string
-	 */
-    	protected $elementType = 4;
+	 * Method to run batch operations.
+	 *
+	 * @param   object      $model  The model.
+	 *
+	 * @return  boolean     True if successful, false otherwise and internal error is set.
+	 */    
+	public function batch($model = null)
+	{
+		JSession::checkToken() or jexit(JText::_('JINVALID_TOKEN'));
+
+		// Set the model
+		$model = $this->getModel('Playlist', '', array());
+
+		// Preset the redirect
+		$this->setRedirect(JRoute::_('index.php?option=com_hwdmediashare&view=' . $this->view_list . $this->getRedirectToListAppend(), false));
+
+		return parent::batch($model);
+	}     
 }
