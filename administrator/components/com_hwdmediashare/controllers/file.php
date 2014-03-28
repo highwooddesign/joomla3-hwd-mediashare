fb417475e7d514a19732d35a269c3cbd9ae3b7ac
Dave Horsfall <dave@horsfall.eu>
2014-03-28 15:34:46 +0000

    Added new file controller

---
+++ administrator/components/com_hwdmediashare/controllers/file.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @package     Joomla.administrator
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
+ */
+
+defined('_JEXEC') or die;
+
+class hwdMediaShareControllerFile extends JControllerForm
+{
+	/**
+	 * The URL view list variable.
+	 * @var    string
+	 */
+    	protected $view_list = "files";
+
+	/**
+	 * Method to run batch operations.
+	 *
+	 * @param   object      $model  The model.
+	 *
+	 * @return  boolean     True if successful, false otherwise and internal error is set.
+	 */    
+	public function batch($model = null)
+	{
+		JSession::checkToken() or jexit(JText::_('JINVALID_TOKEN'));
+
+		// Set the model
+		$model = $this->getModel('File', '', array());
+
+		// Preset the redirect
+		$this->setRedirect(JRoute::_('index.php?option=com_hwdmediashare&view=' . $this->view_list . $this->getRedirectToListAppend(), false));
+
+		return parent::batch($model);
+	}     
+}
