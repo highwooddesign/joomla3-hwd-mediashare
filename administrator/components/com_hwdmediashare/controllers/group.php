33c4c8b3bc99f84cf6f78a9e221805f5e92bb46b
Dave Horsfall <dave@horsfall.eu>
2014-03-28 15:40:03 +0000

    Added batch method to group controller.

--- administrator/components/com_hwdmediashare/controllers/group.php
+++ administrator/components/com_hwdmediashare/controllers/group.php
@@ -17,10 +17,24 @@ class hwdMediaShareControllerGroup extends JControllerForm
 	 * @var    string
 	 */
     	protected $view_list = "groups";
-        
+
 	/**
-	 * The ID of this element type.
-	 * @var    string
-	 */
-    	protected $elementType = 3;
+	 * Method to run batch operations.
+	 *
+	 * @param   object      $model  The model.
+	 *
+	 * @return  boolean     True if successful, false otherwise and internal error is set.
+	 */    
+	public function batch($model = null)
+	{
+		JSession::checkToken() or jexit(JText::_('JINVALID_TOKEN'));
+
+		// Set the model
+		$model = $this->getModel('Group', '', array());
+
+		// Preset the redirect
+		$this->setRedirect(JRoute::_('index.php?option=com_hwdmediashare&view=' . $this->view_list . $this->getRedirectToListAppend(), false));
+
+		return parent::batch($model);
+	}     
 }
