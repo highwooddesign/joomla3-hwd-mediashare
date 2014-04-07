0638ee43b201771719a4b6018fb9b2a741efca56
Dave Horsfall <dave@horsfall.eu>
2014-04-07 17:00:27 +0100

    Updated hwdeditor field type.

---
+++ administrator/components/com_hwdmediashare/models/fields/hwdeditor.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @package     Joomla.administrator
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
+ */
+
+defined('_JEXEC') or die;
+
+JFormHelper::loadFieldClass('editor');
+
+class JFormFieldHwdEditor extends JFormFieldEditor
+{
+	/**
+	 * Method to get a JEditor object based on the form field.
+	 *
+	 * @return  JEditor  The JEditor object.
+	 */
+	protected function getEditor()
+	{
+                // Get HWD config
+                $hwdms = hwdMediaShareFactory::getInstance();
+                $config = $hwdms->getConfig();
+                
+		$this->editorType = array($config->get('editor'));
+                
+                return parent::getEditor();
+	}      
+}
