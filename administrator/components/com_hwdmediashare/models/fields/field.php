a7e20b2a64dce3bb5d9a1abe2f13842f6e5d02a3
Dave Horsfall <dave@horsfall.eu>
2014-04-07 16:36:33 +0100

    Updated field field type.

--- administrator/components/com_hwdmediashare/models/fields/field.php
+++ administrator/components/com_hwdmediashare/models/fields/field.php
@@ -1,53 +1,49 @@
 <?php
 /**
- * @version    SVN $Id: field.php 1133 2013-02-21 11:00:22Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2012 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      18-Feb-2012 16:16:04
+ * @package     Joomla.administrator
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
 
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
 
-// import the list field type
-jimport('joomla.form.helper');
 JFormHelper::loadFieldClass('list');
 
-/**
- * hwdMediaShare Form Field class for the hwdMediaShare component
- */
 class JFormFieldField extends JFormFieldList
 {
 	/**
-	 * The field type.
+	 * The form field type.
 	 *
-	 * @var		string
+	 * @var  string
 	 */
 	protected $type = 'Field';
 
 	/**
-	 * Method to get a list of options for a list input.
+	 * Method to get the field options.
 	 *
-	 * @return	array		An array of JHtml options.
+	 * @return  array  The field option objects.
 	 */
 	protected function getOptions()
 	{
-                // Get name of field type
+                // Get list of field types
                 $path = JPATH_ROOT . '/components/com_hwdmediashare/libraries/fields/customfields.xml';
-                $parser =& JFactory::getXML($path);                                        
+                $parser = JFactory::getXML($path);                                        
                 $fields	= $parser->fields;
                 
                 // Initialise variables.
-		$options   = array();
-                $options[] = JHtml::_('select.option', 'group', JText::_('COM_HWDMS_GROUP'));
-
-                foreach( $fields->children() as $field )
+		$options = array();
+                
+                foreach($fields->children() as $field)
                 {
                         $options[] = JHtml::_('select.option', $field->type, JText::_($field->name));
                 }
 
-                return $options;
+                // Merge any additional options in the XML definition.
+		$options = array_merge(parent::getOptions(), $options);
+
+		return $options;
 	}
 }
