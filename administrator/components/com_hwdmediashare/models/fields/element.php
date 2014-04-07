c3b222c35c69ae96c1d90f3a72dcc3ea5377e94d
Dave Horsfall <dave@horsfall.eu>
2014-04-07 16:28:19 +0100

    Updated element field class.

--- administrator/components/com_hwdmediashare/models/fields/element.php
+++ administrator/components/com_hwdmediashare/models/fields/element.php
@@ -1,48 +1,48 @@
 <?php
 /**
- * @version    SVN $Id: element.php 219 2012-02-29 14:22:40Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2011 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      16-Oct-2011 17:54:20
+ * @package     Joomla.administrator
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
 
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
 
-// Import the list field type
-jimport('joomla.form.helper');
 JFormHelper::loadFieldClass('list');
 
- /**
-  * Element field class
-  */
 class JFormFieldElement extends JFormFieldList
 {
 	/**
-	 * The field type.
+	 * The form field type.
 	 *
-	 * @var		string
+	 * @var  string
 	 */
 	protected $type = 'Element';
 
-        /**
-	 * Method to get a list of options for a list input.
+	/**
+	 * Method to get the field options.
 	 *
-	 * @return	array		An array of JHtml options.
+	 * @return  array  The field option objects.
 	 */
-	public function getOptions()
+	protected function getOptions()
 	{
-		// Initialise variables.
-		$options	= array();
-
-                $options[] = JHtml::_('select.option', '', JText::_('COM_HWDMS_LIST_SELECT_TYPE'));
+                // Get HWD config
+                $hwdms = hwdMediaShareFactory::getInstance();
+                $config = $hwdms->getConfig();
+                
+                // Initialise variables.
+		$options = array();
                 $options[] = JHtml::_('select.option', '1', JText::_('COM_HWDMS_MEDIA'));
-                $options[] = JHtml::_('select.option', '2', JText::_('COM_HWDMS_ALBUM'));
-                $options[] = JHtml::_('select.option', '3', JText::_('COM_HWDMS_GROUP'));
-                $options[] = JHtml::_('select.option', '4', JText::_('COM_HWDMS_PLAYLIST'));
-                $options[] = JHtml::_('select.option', '5', JText::_('COM_HWDMS_USER_CHANNEL'));
+                
+                if ($config->get('enable_albums'))          $options[] = JHtml::_('select.option', '2', JText::_('COM_HWDMS_ALBUM'));
+                if ($config->get('enable_groups'))          $options[] = JHtml::_('select.option', '3', JText::_('COM_HWDMS_GROUP'));
+                if ($config->get('enable_playlists'))       $options[] = JHtml::_('select.option', '4', JText::_('COM_HWDMS_PLAYLIST'));
+                if ($config->get('enable_user_channels'))   $options[] = JHtml::_('select.option', '5', JText::_('COM_HWDMS_USER_CHANNEL'));
+
+                // Merge any additional options in the XML definition.
+		$options = array_merge(parent::getOptions(), $options);
 
 		return $options;
 	}
