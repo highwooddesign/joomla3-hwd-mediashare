2a261fc070d421926dff0cb0c6c0d65274fa7f53
Dave Horsfall <dave@horsfall.eu>
2014-04-07 16:31:05 +0100

    Updated extension field class.

---
+++ administrator/components/com_hwdmediashare/models/fields/extension.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * @package     Joomla.administrator
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
+ */
+
+defined('_JEXEC') or die;
+
+JFormHelper::loadFieldClass('list');
+
+class JFormFieldExtension extends JFormFieldList
+{
+	/**
+	 * The form field type.
+	 *
+	 * @var  string
+	 */
+	protected $type = 'Extension';
+
+	/**
+	 * Method to get the field options.
+	 *
+	 * @return  array  The field option objects.
+	 */
+	protected function getOptions()
+	{
+                $db = JFactory::getDbo();
+                $query = $db->getQuery(true)
+                        ->select('*')
+                        ->from('#__hwdms_ext')
+                        ->where('published = ' . $db->quote(1));
+                $db->setQuery($query);
+                try
+                {
+                        $db->query(); 
+                        $rows = $db->loadObjectList();                        
+                }
+                catch (RuntimeException $e)
+                {
+                        $this->setError($e->getMessage());
+                        return false;                            
+                }
+
+                // Loop all plugins and check if a cdn plugin
+		for($i = 0; $i < count($rows); $i++)
+		{
+                        $options[] = JHtml::_('select.option', $rows[$i]->id, $row->ext);     
+		}
+
+                // Merge any additional options in the XML definition.
+		$options = array_merge(parent::getOptions(), $options);
+
+		return $options;
+	}
+}
