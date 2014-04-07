6a2fb8250c73a6d29c743d6cdef4531b02a3fcc6
Dave Horsfall <dave@horsfall.eu>
2014-04-07 16:22:16 +0100

    Updated commenting field class.

--- administrator/components/com_hwdmediashare/models/fields/commenting.php
+++ administrator/components/com_hwdmediashare/models/fields/commenting.php
@@ -1,68 +1,72 @@
 <?php
 /**
- * @version    SVN $Id: commenting.php 425 2012-06-28 07:48:57Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2012 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      14-Mar-2012 13:26:54
+ * @package     Joomla.administrator
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
 
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
 
-// Import the list field type
-jimport('joomla.form.helper');
 JFormHelper::loadFieldClass('list');
 
- /**
-  * Process field class
-  */
 class JFormFieldCommenting extends JFormFieldList
 {
 	/**
-	 * The field type.
+	 * The form field type.
 	 *
-	 * @var		string
+	 * @var  string
 	 */
-	protected $type = 'Commenting';
+	protected $type = 'CDN';
 
 	/**
-	 * Method to get a list of options for a list input.
+	 * Method to get the field options.
 	 *
-	 * @return	array		An array of JHtml options.
+	 * @return  array  The field option objects.
 	 */
 	protected function getOptions()
 	{
-                // Initialise variables.
-		$options	= array();    
-                $options[]      = JHtml::_('select.option', '', JText::_('COM_HWDMS_LIST_SELECT_COMMENTING'));
-                $options[]      = JHtml::_('select.option', '1', JText::_('COM_HWDMS_ACTIVITY_STREAM_COMMENTING'));
-
-                // Load all hwdMediaShare plugins
-                $db =& JFactory::getDBO();
-                $query = $db->getQuery(true);
-                $query->select('*');
-                $query->from('#__extensions AS a');
-                $query->where('a.type = '.$db->quote('plugin'));
-                $query->where('a.folder = '.$db->quote('hwdmediashare'));
+                $db = JFactory::getDbo();
+                $query = $db->getQuery(true)
+                        ->select('*')
+                        ->from('#__extensions')
+                        ->where('type = ' . $db->quote('plugin'))
+                        ->where('folder = ' . $db->quote('hwdmediashare'));
                 $db->setQuery($query);
-                $rows = $db->loadObjectList();            
+                try
+                {
+                        $db->query(); 
+                        $rows = $db->loadObjectList();                        
+                }
+                catch (RuntimeException $e)
+                {
+                        $this->setError($e->getMessage());
+                        return false;                            
+                }
 
-                // Loop all plugins and check if a commenting plugin
-		for( $i = 0; $i < count($rows); $i++ )
+                // Loop all plugins and check if a cdn plugin
+		for($i = 0; $i < count($rows); $i++)
 		{
 			$row = $rows[$i];
 
-                        if( substr($row->element, 0, 9) == 'comments_' )
+                        if(substr($row->element, 0, 9) == 'comments_')
 			{
-                                // Load the HWDMediaShare language file
-                                $lang =& JFactory::getLanguage();
-                                $lang->load($row->name, JPATH_SITE.'/administrator', $lang->getTag());
+                                // Load the plugin language file
+                                $lang = JFactory::getLanguage();
+                                $lang->load($row->name, JPATH_SITE . '/administrator', $lang->getTag());
 
-                                $options[] = JHtml::_('select.option', $row->element, JText::_($row->name));                                
+                                // Add option
+                                if (file_exists(JPATH_ROOT.'/plugins/hwdmediashare/' . $row->element . '/' . $row->element . '.php'))
+                                {
+                                        $options[] = JHtml::_('select.option', $row->element, JText::_($row->name));  
+                                }
 			}
 		}
+
+                // Merge any additional options in the XML definition.
+		$options = array_merge(parent::getOptions(), $options);
 
 		return $options;
 	}
