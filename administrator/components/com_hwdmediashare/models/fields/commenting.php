2d2cfdacb53a8b84e3b6eccae08707864fe50ca4
Dave Horsfall <dave@horsfall.eu>
2014-04-07 16:28:03 +0100

    Updated commenting field class.

--- administrator/components/com_hwdmediashare/models/fields/commenting.php
+++ administrator/components/com_hwdmediashare/models/fields/commenting.php
@@ -19,7 +19,7 @@ class JFormFieldCommenting extends JFormFieldList
 	 *
 	 * @var  string
 	 */
-	protected $type = 'CDN';
+	protected $type = 'Commenting';
 
 	/**
 	 * Method to get the field options.
