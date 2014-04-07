d417816d5e80274c36bb17aeefe9ce58482a3bbf
Dave Horsfall <dave@horsfall.eu>
2014-04-07 17:16:40 +0100

    Updated playlist field class.

--- administrator/components/com_hwdmediashare/models/fields/playlist.php
+++ administrator/components/com_hwdmediashare/models/fields/playlist.php
@@ -1,24 +1,23 @@
 <?php
 /**
- * @version    SVN $Id: playlist.php 425 2012-06-28 07:48:57Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2011 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      06-Dec-2011 09:54:14
+ * @package     Joomla.administrator
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
 
-// No direct access
- defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
 
-// Import Joomla formfield library
-jimport('joomla.form.formfield');
-
- /**
-  * Playlist field class
-  */
- class JFormFieldPlaylist extends JFormField
- {
+/**
+ * Supports a modal playlist picker.
+ *
+ * @package     Joomla.Administrator
+ * @subpackage  com_hwdmediashare
+ */
+class JFormFieldPlaylist extends JFormField
+{
         /**
  	 * Field type
  	 * @var string
@@ -37,78 +36,118 @@ jimport('joomla.form.formfield');
  	 */
  	protected $id = 'playlist';
 
-        /**
-         * Method to get the field input markup
-         */
+	/**
+	 * Method to get the field input markup.
+	 *
+	 * @return  string	The field input markup.
+	 */
         public function getInput()
         {
-              // Load modal behavior
-              JHtml::_('behavior.modal', 'a.modal');
+		$allowClear		= ((string) $this->element['clear'] != 'false') ? true : false;
 
-              // Build the script
-              $script = array();
-              $script[] = '    function jSelectPlaylist_'.$this->id.'(id, title, object) {';
-              $script[] = '        document.id("'.$this->id.'_id").value = id;';
-              $script[] = '        document.id("'.$this->id.'_name").value = title;';
-              $script[] = '        SqueezeBox.close();';
-              $script[] = '    }';
+		// Load language
+		JFactory::getLanguage()->load('com_hwdmediashare', JPATH_ADMINISTRATOR);
 
-              // Add to document head
-              JFactory::getDocument()->addScriptDeclaration(implode("\n", $script));
+		// Load the modal behavior script.
+		JHtml::_('behavior.modal', 'a.modal');
 
-              // Setup variables for display
-              $html = array();
-              $link = 'index.php?option=com_hwdmediashare&amp;view=playlists&amp;layout=modal&amp;tmpl=component&amp;function=jSelectPlaylist_'.$this->id;
+		// Build the script.
+		$script = array();
 
-              // The active item id field
-              if (0 == (int)$this->value) 
-              {
-                $title = JText::_('COM_HWDMS_SELECT_PLAYLIST');
-              } 
-              else 
-              {
-                $db = JFactory::getDBO();
-                $db->setQuery(
+		// Select button script
+		$script[] = '	function jSelectPlaylist_'.$this->id.'(id, title, catid, object) {';
+		$script[] = '		document.getElementById("'.$this->id.'_id").value = id;';
+		$script[] = '		document.getElementById("'.$this->id.'_name").value = title;';
+
+		if ($allowClear)
+		{
+			$script[] = '		jQuery("#'.$this->id.'_clear").removeClass("hidden");';
+		}
+
+		$script[] = '		SqueezeBox.close();';
+		$script[] = '	}';
+
+		// Clear button script
+		static $scriptClear;
+
+		if ($allowClear && !$scriptClear)
+		{
+			$scriptClear = true;
+
+			$script[] = '	function jClearPlaylist(id) {';
+			$script[] = '		document.getElementById(id + "_id").value = "";';
+			$script[] = '		document.getElementById(id + "_name").value = "'.htmlspecialchars(JText::_('COM_HWDMS_SELECT_PLAYLIST', true), ENT_COMPAT, 'UTF-8').'";';
+			$script[] = '		jQuery("#"+id + "_clear").addClass("hidden");';
+			$script[] = '		return false;';
+			$script[] = '	}';
+		}
+
+		// Add the script to the document head.
+		JFactory::getDocument()->addScriptDeclaration(implode("\n", $script));
+
+		// Setup variables for display.
+		$html	= array();
+                $link   = 'index.php?option=com_hwdmediashare&amp;view=playlists&amp;layout=modal&amp;tmpl=component&amp;function=jSelectPlaylist_'.$this->id;
+
+		if (isset($this->element['language']))
+		{
+			$link .= '&amp;forcedLanguage='.$this->element['language'];
+		}
+
+		$db	= JFactory::getDbo();
+		$db->setQuery(
                   'SELECT title' .
                   ' FROM #__hwdms_playlists' .
                   ' WHERE id = '.(int) $this->value
-                );
-                $title = $db->loadResult();
-              }              
-              $title = htmlspecialchars($title, ENT_QUOTES, 'UTF-8');
+		);
 
-              // The current item input field
-              $html[] = '<div class="fltlft">';
-              $html[] = '  <input type="text" id="'.$this->id.'_name" value="'.$title.'" disabled="disabled" />';
-              $html[] = '</div>';
+		try
+		{
+			$title = $db->loadResult();
+		}
+		catch (RuntimeException $e)
+		{
+			JError::raiseWarning(500, $e->getMessage());
+		}
 
-              // The item select button
-              $html[] = '<div class="button2-left">';
-              $html[] = '  <div class="blank">';
-              $html[] = '    <a class="modal" title="'.JText::_('COM_HWDMS_SELECT_PLAYLIST').'" href="'.$link.
-                             '" rel="{handler: \'iframe\', size: {x:800, y:450}}">'.
-                             JText::_('COM_HWDMS_SELECT_PLAYLIST').'</a>';
-              $html[] = '  </div>';
-              $html[] = '</div>';
+		if (empty($title))
+		{
+			$title = JText::_('COM_HWDMS_SELECT_PLAYLIST');
+		}
+		$title = htmlspecialchars($title, ENT_QUOTES, 'UTF-8');
 
-              // The active item id field
-              if (0 == (int)$this->value) 
-              {
-                      $value = '';
-              } 
-              else
-              {
-                      $value = (int)$this->value;
-              }
+		// The active id field.
+		if (0 == (int) $this->value)
+		{
+			$value = '';
+		}
+		else
+		{
+			$value = (int) $this->value;
+		}
 
-              // Set class='required' for client side validation
-              $class = '';
-              if ($this->required) {
-                      $class = ' class="required modal-value"';
-              }
+		// The current display field.
+		$html[] = '<span class="input-append">';
+		$html[] = '<input type="text" class="input-medium" id="'.$this->id.'_name" value="'.$title.'" disabled="disabled" size="35" />';
+		$html[] = '<a class="modal btn hasTooltip" title="'.JHtml::tooltipText('COM_HWDMS_CHANGE_PLAYLIST').'"  href="'.$link.'&amp;'.JSession::getFormToken().'=1" rel="{handler: \'iframe\', size: {x: 800, y: 450}}"><i class="icon-file"></i> '.JText::_('JSELECT').'</a>';
 
-              $html[] = '<input type="hidden" id="'.$this->id.'_id"'.$class.' name="'.$this->name.'" value="'.$value.'" />';
+		// Clear button
+		if ($allowClear)
+		{
+			$html[] = '<button id="'.$this->id.'_clear" class="btn'.($value ? '' : ' hidden').'" onclick="return jClearPlaylist(\''.$this->id.'\')"><span class="icon-remove"></span> ' . JText::_('JCLEAR') . '</button>';
+		}
 
-              return implode("\n", $html);
+		$html[] = '</span>';
+
+		// class='required' for client side validation
+		$class = '';
+		if ($this->required)
+		{
+			$class = ' class="required modal-value"';
+		}
+
+		$html[] = '<input type="hidden" id="'.$this->id.'_id"'.$class.' name="'.$this->name.'" value="'.$value.'" />';
+
+		return implode("\n", $html);
         }
 }
