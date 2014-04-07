768d8bbe8760137f57d98f243349b262a8cc9dcb
Dave Horsfall <dave@horsfall.eu>
2014-04-07 15:32:32 +0100

    Updated editmedia controller

--- administrator/components/com_hwdmediashare/controllers/editmedia.php
+++ administrator/components/com_hwdmediashare/controllers/editmedia.php
@@ -7,9 +7,7 @@
  * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
  * @author      Dave Horsfall
  */
-/**
- * UNFINISHED
- */
+
 defined('_JEXEC') or die;
 
 class hwdMediaShareControllerEditMedia extends JControllerForm
@@ -19,74 +17,24 @@ class hwdMediaShareControllerEditMedia extends JControllerForm
 	 * @var    string
 	 */
     	protected $view_list = "media";
-        
+
 	/**
-	 * The ID of this element type.
-	 * @var    string
-	 */
-    	protected $elementType = 1;
-                
-        /**
-	 * Method to syncronise data from the local gallery to a remote CDN.
-	 * @return	void
-	 */
-	public function syncToCdn()
+	 * Method to run batch operations.
+	 *
+	 * @param   object      $model  The model.
+	 *
+	 * @return  boolean     True if successful, false otherwise and internal error is set.
+	 */    
+	public function batch($model = null)
 	{
-		$this->task = 'apply';
-                if (parent::save())
-                {                      
-                        JTable::addIncludePath(JPATH_ADMINISTRATOR.'/components/com_hwdmediashare/tables');
-                        $table =& JTable::getInstance('Media', 'hwdMediaShareTable');
-                        $table->load(JRequest::getVar('id'));
-                        $properties = $table->getProperties(1);
-                        $item = JArrayHelper::toObject($properties, 'JObject');
+		JSession::checkToken() or jexit(JText::_('JINVALID_TOKEN'));
 
-                        if ($item->type == 6)
-                        {
-                                $pluginClass = 'plgHwdmediashare'.$item->storage;
-                                $pluginPath = JPATH_ROOT.'/plugins/hwdmediashare/'.$item->storage.'/'.$item->storage.'.php';
+		// Set the model
+		$model = $this->getModel('EditMedia', '', array());
 
-                                // Import hwdMediaShare plugins
-                                if (file_exists($pluginPath))
-                                {
-                                        JLoader::register($pluginClass, $pluginPath);
-                                        $platform = call_user_func(array($pluginClass, 'getInstance'));
-                                        $platform->syncToCdn();
-                                }
-                        }
-			return true;
-		}
-	}
-        
-        /**
-	 * Method to syncronise data from a remote CDN to the local gallery.
-	 * @return	void
-	 */
-	public function syncFromCdn()
-	{
-		$this->task = 'apply';
-                if (parent::save())
-                {                      
-                        JTable::addIncludePath(JPATH_ADMINISTRATOR.'/components/com_hwdmediashare/tables');
-                        $table =& JTable::getInstance('Media', 'hwdMediaShareTable');
-                        $table->load(JRequest::getVar('id'));
-                        $properties = $table->getProperties(1);
-                        $item = JArrayHelper::toObject($properties, 'JObject');
+		// Preset the redirect
+		$this->setRedirect(JRoute::_('index.php?option=com_hwdmediashare&view=' . $this->view_list . $this->getRedirectToListAppend(), false));
 
-                        if ($item->type == 6)
-                        {
-                                $pluginClass = 'plgHwdmediashare'.$item->storage;
-                                $pluginPath = JPATH_ROOT.'/plugins/hwdmediashare/'.$item->storage.'/'.$item->storage.'.php';
-
-                                // Import hwdMediaShare plugins
-                                if (file_exists($pluginPath))
-                                {
-                                        JLoader::register($pluginClass, $pluginPath);
-                                        $platform = call_user_func(array($pluginClass, 'getInstance'));
-                                        $platform->syncFromCdn();
-                                }
-                        }
-			return true;
-		}
-	}
+		return parent::batch($model);
+	}     
 }
