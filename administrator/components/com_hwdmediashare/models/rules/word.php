aac04b2d02c340061f6a9b4383b46ca4496268b7
Dave Horsfall <dave@horsfall.eu>
2014-04-09 13:49:36 +0100

    Added 'word' type validation rule.

---
+++ administrator/components/com_hwdmediashare/models/rules/word.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @package     Joomla.administrator
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
+ */
+
+defined('_JEXEC') or die;
+
+// Import formrule library
+jimport('joomla.form.formrule');
+
+class JFormRuleWord extends JFormRule
+{
+	/**
+	 * The regular expression.
+	 *
+	 * @access	protected
+	 * @var		string
+	 */
+	protected $regex = '^[a-zA-Z]+$';
+}
