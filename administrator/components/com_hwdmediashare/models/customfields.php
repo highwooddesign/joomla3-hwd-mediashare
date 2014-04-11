3a030a3d7a4614ee794d268029a484b96838f364
Dave Horsfall <dave@horsfall.eu>
2014-04-11 11:39:03 +0100

    Grouped the customfields query over the ID to prevent duplicates after joining with other tables.

--- administrator/components/com_hwdmediashare/models/customfields.php
+++ administrator/components/com_hwdmediashare/models/customfields.php
@@ -114,6 +114,9 @@ class hwdMediaShareModelCustomFields extends JModelList
 
 		$query->order($db->escape($listOrder.' '.$listDirn));
 
+   		// Group over the ID to prevent duplicates.
+                $query->group('a.id');
+                
 		//echo nl2br(str_replace('#__','jos_',$query));
 		return $query;
         }
