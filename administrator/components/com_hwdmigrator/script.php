f99a336f8fe591de48c2485874d73ac57adc2957
Dave Horsfall <dave@horsfall.eu>
2014-04-25 17:20:11 +0100

    Updated hwdMigrator administrator.

--- administrator/components/com_hwdmigrator/script.php
+++ administrator/components/com_hwdmigrator/script.php
@@ -1,28 +1,19 @@
 <?php
 /**
- * @version    SVN $Id: script.php 425 2012-06-28 07:48:57Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2011 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      15-Apr-2011 10:13:15
+ * @package     Joomla.administrator
+ * @subpackage  Component.hwdmigrator
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
 
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
 
-/**
- * Script file of hwdMediaShare component
- */
 class com_hwdMigratorInstallerScript
 {
         /**
-	 * Stored error
-	 */
-        protected $error;
-        
-        /**
-	 * Method to install the component
+	 * Method to install the component.
 	 *
 	 * @return void
 	 */
@@ -32,7 +23,7 @@ class com_hwdMigratorInstallerScript
 	}
 
 	/**
-	 * Method to uninstall the component
+	 * Method to uninstall the component.
 	 *
 	 * @return void
 	 */
@@ -42,7 +33,7 @@ class com_hwdMigratorInstallerScript
 	}
 
 	/**
-	 * Method to update the component
+	 * Method to update the component.
 	 *
 	 * @return void
 	 */
@@ -52,7 +43,7 @@ class com_hwdMigratorInstallerScript
 	}
 
 	/**
-	 * Method to run before an install/update/uninstall method
+	 * Method to run before an install/update/uninstall method.
 	 *
 	 * @return void
 	 */
@@ -62,7 +53,7 @@ class com_hwdMigratorInstallerScript
 	}
 
 	/**
-	 * Method to run after an install/update/uninstall method
+	 * Method to run after an install/update/uninstall method.
 	 *
 	 * @return void
 	 */
