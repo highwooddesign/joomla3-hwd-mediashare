6ecb6eba315bffaa065a443d38302c772521b8ab
Dave Horsfall <dave@horsfall.eu>
2014-04-25 17:33:53 +0100

    Updated hwdMigrator administrator.

--- administrator/components/com_hwdmigrator/views/dashboard/view.html.php
+++ administrator/components/com_hwdmigrator/views/dashboard/view.html.php
@@ -1,38 +1,35 @@
 <?php
 /**
- * @version    SVN $Id: view.html.php 425 2012-06-28 07:48:57Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2011 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      15-Apr-2011 10:13:15
+ * @package     Joomla.administrator
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
 
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
 
-// import Joomla view library
-jimport('joomla.application.component.view');
-
-/**
- * hwdMigrator View
- */
-class hwdMigratorViewDashboard extends JViewLegacy {
+class hwdMigratorViewDashboard extends JViewLegacy
+{
 	/**
-	 * display method of Hello view
-	 * @return void
+	 * Display the view
+	 *
+	 * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
+	 *
+	 * @return  void
 	 */
-	public function display($tpl = null)
+	function display($tpl = null)
 	{
-                // Get data from the model
-                $video_items = $this->get('VideoItems');
-                $video_categories = $this->get('VideoCategories');
-                $video_groups = $this->get('VideoGroups');
-                $video_playlists = $this->get('VideoPlaylists');
-                $photo_items = $this->get('PhotoItems');
-                $photo_categories = $this->get('PhotoCategories');
-                $photo_groups = $this->get('PhotoGroups');
-                $photo_albums = $this->get('PhotoAlbums');
+                // Get data from the model.
+                $this->video_items = $this->get('VideoItems');
+                $this->video_categories = $this->get('VideoCategories');
+                $this->video_groups = $this->get('VideoGroups');
+                $this->video_playlists = $this->get('VideoPlaylists');
+                $this->photo_items = $this->get('PhotoItems');
+                $this->photo_categories = $this->get('PhotoCategories');
+                $this->photo_groups = $this->get('PhotoGroups');
+                $this->photo_albums = $this->get('PhotoAlbums');
                 
                 // Check for errors.
                 if (count($errors = $this->get('Errors')))
@@ -40,57 +37,34 @@ class hwdMigratorViewDashboard extends JViewLegacy {
                         JError::raiseError(500, implode('<br />', $errors));
                         return false;
                 }
-
-                // Assign data to the view
-                $this->video_items = $video_items;
-                $this->video_categories = $video_categories;
-                $this->video_groups = $video_groups;
-                $this->video_playlists = $video_playlists;
-                $this->photo_items = $photo_items;
-                $this->photo_categories = $photo_categories;
-                $this->photo_groups = $photo_groups;
-                $this->photo_albums = $photo_albums;
                 
-		// Check for errors.
-		if (count($errors = $this->get('Errors')))
+		// We don't need toolbar in the modal window.
+		if ($this->getLayout() !== 'modal')
 		{
-			JError::raiseError(500, implode('<br />', $errors));
-			return false;
+			$this->addToolbar();
 		}
-
-		// Set the toolbar
-		$this->addToolBar();
-
-		// Display the template
+                
+		// Display the template.
 		parent::display($tpl);
-
-		// Set the document
-		$this->setDocument();
+                
+		$document = JFactory::getDocument();
+		$document->addStyleSheet(JURI::root() . "media/com_hwdmigrator/assets/css/administrator.css");                
+                $document->addScript(JURI::root() . "/administrator/components/com_hwdmigrator/views/dashboard/submitbutton.js");
 	}
 
 	/**
-	 * Setting the toolbar
+	 * Add the page title and toolbar.
+	 *
+	 * @return  void
 	 */
 	protected function addToolBar()
 	{
-		//JRequest::setVar('hidemainmenu', true);
+		JToolBarHelper::title(JText::_('COM_HWDMIGRATOR_MIGRATOR'), 'home');
 
-		JToolBarHelper::title(JText::_('COM_HWDMIGRATOR_MIGRATOR'), 'hwdmigrator');
-
-                JToolBarHelper::custom('maintenace.refresh', 'refresh.png', 'refresh_f2.png', JText::_('COM_HWDMIGRATOR_REFRESH'), false);
-                JToolBarHelper::custom('maintenace.run', 'apply.png', 'apply.png', JText::_('COM_HWDMIGRATOR_RUN'), false);
-		JToolBarHelper::divider();
-                JToolBarHelper::custom('help', 'help.png', 'help.png', 'JHELP', false);
-	}
-	/**
-	 * Method to set up the document properties
-	 *
-	 * @return void
-	 */
-	protected function setDocument()
-	{
-		$document = JFactory::getDocument();
-                $document->addScript(JURI::root() . "/administrator/components/com_hwdmigrator/views/dashboard/submitbutton.js");
-		$document->addStyleSheet(JURI::root() . "media/com_hwdmigrator/assets/css/administrator.css");
+		JToolBarHelper::custom('maintenance.refresh', 'refresh', 'refresh', JText::_('COM_HWDMIGRATOR_REFRESH'), false);
+		JToolbarHelper::divider();
+		JToolBarHelper::custom('maintenance.run', 'apply', 'apply', JText::_('COM_HWDMIGRATOR_RUN'), false);
+		JToolbarHelper::divider();
+		JToolbarHelper::help('HWD', false, 'http://hwdmediashare.co.uk/learn/docs/59-getting-started/migration/115-migrating-from-hwdvideoshare-to-hwdmediashare');
 	}
 }
