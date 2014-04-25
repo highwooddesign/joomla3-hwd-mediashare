6d04fdfa24443142a5a20b43f011ff09e8566be4
Dave Horsfall <dave@horsfall.eu>
2014-04-25 16:54:29 +0100

    Updated getItems overload method to prevent empty object.

--- administrator/components/com_hwdmediashare/models/activities.php
+++ administrator/components/com_hwdmediashare/models/activities.php
@@ -38,11 +38,12 @@ class hwdMediaShareModelActivities extends JModelList
 	 */
 	public function getItems()
 	{
-		$items = parent::getItems();
-
-                for ($x = 0, $count = count($items); $x < $count; $x++)
-                {
-                        if (empty($items[$x]->author)) $items[$x]->author = JText::_('COM_HWDMS_GUEST');
+		if ($items = parent::getItems())
+		{            
+                        for ($x = 0, $count = count($items); $x < $count; $x++)
+                        {
+                                if (empty($items[$x]->author)) $items[$x]->author = JText::_('COM_HWDMS_GUEST');
+                        }
                 }
 
 		return $items;
