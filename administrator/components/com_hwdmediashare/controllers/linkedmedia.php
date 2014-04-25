e98d8b41aa3b588388fe0179474ba19dafd99935
Dave Horsfall <dave@horsfall.eu>
2014-04-25 13:27:21 +0100

    Strict standards fix.

--- administrator/components/com_hwdmediashare/controllers/linkedmedia.php
+++ administrator/components/com_hwdmediashare/controllers/linkedmedia.php
@@ -22,7 +22,7 @@ class hwdMediaShareControllerLinkedMedia extends JControllerAdmin
 	 * Proxy for getModel.
 	 * @return	void
 	 */
-	public function getModel($name = 'LinkedMedia', $prefix = 'hwdMediaShareModel')
+	public function getModel($name = 'LinkedMedia', $prefix = 'hwdMediaShareModel', $config = array())
 	{
                 $model = parent::getModel($name, $prefix, array('ignore_request' => true));
                 return $model;
