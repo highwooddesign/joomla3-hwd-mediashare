7c1944196c4c96ad0508726a120c295fa153543c
Dave Horsfall <dave@horsfall.eu>
2014-04-25 17:25:18 +0100

    Updated hwdMigrator administrator.

--- administrator/components/com_hwdmigrator/hwdmigrator.php
+++ administrator/components/com_hwdmigrator/hwdmigrator.php
@@ -1,15 +1,14 @@
 <?php
 /**
- * @version    SVN $Id: hwdmigrator.php 1688 2013-10-16 15:09:12Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2011 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      15-Apr-2011 10:13:15
+ * @package     Joomla.administrator
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
 
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
 
 // Access check.
 if (!JFactory::getUser()->authorise('core.manage', 'com_hwdmigrator'))
@@ -17,16 +16,13 @@ if (!JFactory::getUser()->authorise('core.manage', 'com_hwdmigrator'))
 	return JError::raiseWarning(404, JText::_('JERROR_ALERTNOAUTHOR'));
 }
 
-// Require helper file
+// Require helper file.
 JLoader::register('hwdMigratorHelper', dirname(__FILE__).'/helpers/hwdmigrator.php');
 
-// Import joomla controller library
-jimport('joomla.application.component.controller');
-
-// Get an instance of the controller prefixed by hwdMigrator
+// Get an instance of the controller prefixed by hwdMediaShare.
 $controller = JControllerLegacy::getInstance('hwdMigrator');
 
-// Perform the Request task
+// Perform the task.
 $controller->execute(JFactory::getApplication()->input->get('task'));
 
 // Redirect if set by the controller
