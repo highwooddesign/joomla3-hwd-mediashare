40c83c2c109b2df548aa6c140ab9769a56d65f35
Dave Horsfall <dave@horsfall.eu>
2014-04-25 13:30:45 +0100

    Strict standards fix.

--- administrator/components/com_hwdmediashare/sql/updates/mysql/1.1.1.sql
+++ administrator/components/com_hwdmediashare/sql/updates/mysql/1.1.1.sql
@@ -1,7 +1,5 @@
--- $Id: 1.1.1.sql 924 2013-01-16 11:14:38Z dhorsfall $
-
 #
-# Database updates for 1.1.0 to 1.1.1
+# Database updates for 1.1.1
 #
 
 ALTER TABLE `#__hwdms_config` ADD COLUMN `id` int(11) NOT NULL AUTO_INCREMENT FIRST, ADD PRIMARY KEY (id);
