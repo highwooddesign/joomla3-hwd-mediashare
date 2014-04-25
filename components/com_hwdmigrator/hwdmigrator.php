a341993a1ed921ff8f75af1bab24108d200ed61c
Dave Horsfall <dave@horsfall.eu>
2014-04-25 17:12:21 +0100

    Updated hwdMigrator frontend directory.

--- components/com_hwdmigrator/hwdmigrator.php
+++ components/com_hwdmigrator/hwdmigrator.php
@@ -1,12 +1,11 @@
 <?php
 /**
- * @version    SVN $Id: hwdmigrator.php 425 2012-06-28 07:48:57Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2011 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      15-Apr-2011 10:13:15
+ * @package     Joomla.site
+ * @subpackage  Component.hwdmigrator
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
 
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
