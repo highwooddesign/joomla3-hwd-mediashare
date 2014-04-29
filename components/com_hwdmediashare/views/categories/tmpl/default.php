47a149f658ccce8ff3899b1af45eddd31aa9aeec
Dave Horsfall <dave@horsfall.eu>
2014-04-29 16:11:47 +0100

    Updated categories default.php template.

--- components/com_hwdmediashare/views/categories/tmpl/default.php
+++ components/com_hwdmediashare/views/categories/tmpl/default.php
@@ -1,16 +1,16 @@
 <?php
 /**
- * @version    SVN $Id: default.php 457 2012-08-02 15:09:12Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2011 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      15-Apr-2011 10:13:15
+ * @package     Joomla.site
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
 
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
 
+jimport('joomla.html.html.bootstrap');
 ?>
 <form action="<?php echo htmlspecialchars(JFactory::getURI()->toString()); ?>" method="post" name="adminForm" id="adminForm">
   <div id="hwd-container"> <a name="top" id="top"></a>
@@ -19,23 +19,31 @@ defined('_JEXEC') or die('Restricted access');
     <!-- Media Header -->
     <div class="media-header">
       <h2 class="media-category-title"><?php echo JText::_('COM_HWDMS_CATEGORIES'); ?></h2>
-      <ul class="media-category-ls">
-        <?php if ($this->params->get('list_details_button') != 'hide') :?><li><a href="<?php echo JRoute::_(hwdMediaShareHelperRoute::getSelfRoute('details')); ?>" class="ls-detail" title="<?php echo JText::_('COM_HWDMS_DETAILS'); ?>"><?php echo JText::_('COM_HWDMS_DETAILS'); ?></a></li><?php endif; ?>
-        <?php if ($this->params->get('list_tree_button') != 'hide') :?><li><a href="<?php echo JRoute::_(hwdMediaShareHelperRoute::getSelfRoute('tree')); ?>" class="ls-tree" title="<?php echo JText::_('COM_HWDMS_TREE'); ?>"><?php echo JText::_('COM_HWDMS_TREE'); ?></a></li><?php endif; ?>
-      </ul>
+      <!-- Buttons -->
+      <div class="btn-group pull-right">  
+        <?php if ($this->params->get('list_tree_button') != 'hide') : ?>
+          <a title="<?php echo JText::_('COM_HWDMS_TREE'); ?>" href="<?php echo JRoute::_(hwdMediaShareHelperRoute::getSelfRoute('tree')); ?>" class="btn"><i class="icon-tree-2"></i></a>
+        <?php endif; ?>
+        <?php if ($this->params->get('list_details_button') != 'hide') : ?>
+          <a title="<?php echo JText::_('COM_HWDMS_DETAILS'); ?>" href="<?php echo JRoute::_(hwdMediaShareHelperRoute::getSelfRoute('details')); ?>" class="btn"><i class="icon-image"></i></a>
+        <?php endif; ?>
+      </div>    
       <div class="clear"></div>
       <?php if ($this->params->get('category_list_quick_view') != 'hide' && count($this->items[$this->parent->id]) != 0) :?>
-        <?php  echo JHtml::_('sliders.start', 'media-category-slider', array('startOffset' => 1)); ?>
-        <?php  echo JHtml::_('sliders.panel',JText::_('COM_HWDMS_CATEGORY_QUICK_VIEW'), 'quick-view'); ?>
+	<?php echo JHtml::_('bootstrap.startAccordion', 'slide-quickview', array('active' => '')); ?>
+	<?php echo JHtml::_('bootstrap.addSlide', 'slide-quickview', JText::_('COM_HWDMS_CATEGORY_QUICK_VIEW'), 'details'); ?>
         <div class="media-categories-lists">
           <?php echo $this->loadTemplate('list'); ?>
           <div class="clear"></div>
         </div>
-        <?php echo JHtml::_('sliders.end'); ?>
+        <?php echo JHtml::_('bootstrap.endSlide'); ?>
+        <?php echo JHtml::_('bootstrap.endAccordion'); ?>
         <div class="clear"></div>
       <?php endif; ?>
       <div class="clear"></div>
     </div>
-    <?php echo $this->loadTemplate($this->display); ?>
+    <div class="media-<?php echo $this->display; ?>-view">    
+      <?php echo $this->loadTemplate($this->display); ?>
+    </div>
   </div>
 </form>
