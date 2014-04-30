612f87280153af53f081728aea2f8baf718f3bfb
Dave Horsfall <dave@horsfall.eu>
2014-04-30 09:58:47 +0100

    Updated events library.

--- components/com_hwdmediashare/libraries/events.php
+++ components/com_hwdmediashare/libraries/events.php
@@ -1,30 +1,21 @@
 <?php
 /**
- * @version    SVN $Id: events.php 1176 2013-02-25 15:16:38Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2011 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      15-Apr-2011 10:13:15
- */
-
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
-
-/**
- * hwdMediaShare framework events class
+ * @package     Joomla.site
+ * @subpackage  Component.hwdmediashare
  *
- * @package hwdMediaShare
- * @since   0.1
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
+
+defined('_JEXEC') or die;
+
 class hwdMediaShareEvents
 {
-	/**
-	 * Class constructor.
+    	/**
+	 * Constructor override, defines a white list of column filters.
 	 *
-	 * @param   array  $config  A configuration array including optional elements.
-	 *
-	 * @since   0.1
+	 * @param   array  $config  An optional associative array of configuration settings.
 	 */
 	public function __construct($config = array())
 	{
@@ -34,8 +25,7 @@ class hwdMediaShareEvents
 	 * Returns the hwdMediaShareEvents object, only creating it if it
 	 * doesn't already exist.
 	 *
-	 * @return  hwdMediaShareEvents A hwdMediaShareEvents object.
-	 * @since   0.1
+	 * @return  hwdMediaShareEvents Object.
 	 */
 	public static function getInstance()
 	{
@@ -51,188 +41,148 @@ class hwdMediaShareEvents
 	}
         
 	/**
-	 * Method to trigger a Joomla event
-         * 
-         * @since   0.1
-	 **/
-	public function triggerEvent( $event , $params = null , $needOrdering = false )
+	 * Method to trigger a Joomla event.
+         * @return	void
+	 */
+	public function triggerEvent($event, $action, $target = null)
 	{
-		$dispatcher =& JDispatcher::getInstance();
-                $act = new StdClass;
+                // Initialise variables.              
+		$dispatcher = JDispatcher::getInstance();
+                
+                // Load HWD activities
+                hwdMediaShareFactory::load('activities');
+                $HWDactivities = hwdMediaShareActivities::getInstance();
+                
+                // New activity.
+                $activity = new StdClass;
                 
 		// Avoid problem with php 5.3
-		if(is_null($params))
-                {
-			$params = array();
-		}
+		if(is_null($action)) $action = new StdClass;
 
-		switch( $event )
+		switch($event)
 		{
                         case 'onAfterMediaAdd':
-                                $act->activityType = 2;
-                                $act->elementType = 1;
-                                $act->elementId = $params->id;
-                                hwdMediaShareFactory::load('activities');
-                                hwdMediaShareActivities::save($act);                                
+                                $activity->action = $action->id; // The object linked to the action itself.
+                                $activity->target = 0;           // The object to which the activity was performed.
+                                $activity->verb = 2;             // The verb phrase that identifies the action of the activity.    
 			break;
                         case 'onAfterAlbumAdd':
-                                $act->activityType = 3;
-                                $act->elementType = 2;
-                                $act->elementId = $params->id;
-                                hwdMediaShareFactory::load('activities');
-                                hwdMediaShareActivities::save($act);
+                                $activity->action = $action->id;
+                                $activity->target = 0;
+                                $activity->verb = 3;
 			break;
                         case 'onAfterGroupAdd':
-                                $act->activityType = 4;
-                                $act->elementType = 3;
-                                $act->elementId = $params->id;
-                                hwdMediaShareFactory::load('activities');
-                                hwdMediaShareActivities::save($act);
+                                $activity->action = $action->id;
+                                $activity->target = 0;
+                                $activity->verb = 4;
 			break;
                         case 'onAfterPlaylistAdd':
-                                $act->activityType = 5;
-                                $act->elementType = 4;
-                                $act->elementId = $params->id;
-                                hwdMediaShareFactory::load('activities');
-                                hwdMediaShareActivities::save($act);
+                                $activity->action = $action->id;
+                                $activity->target = 0;
+                                $activity->verb = 5;
 			break;
                         case 'onAfterChannelAdd':
-                                $act->activityType = 6;
-                                $act->elementType = 5;
-                                $act->elementId = $params->id;
-                                hwdMediaShareFactory::load('activities');
-                                hwdMediaShareActivities::save($act);
+                                $activity->action = $action->id;
+                                $activity->target = 0;
+                                $activity->verb = 6;
 			break;
                         case 'onAfterJoinGroup':
-                                $act->activityType = 7;
-                                $act->elementType = 3;
-                                $act->elementId = $params->id;
-                                hwdMediaShareFactory::load('activities');
-                                hwdMediaShareActivities::save($act);
+                                $activity->action = $action->id;
+                                $activity->target = 0;
+                                $activity->verb = 7;
 			break;                    
                         case 'onAfterLeaveGroup':
-                                $act->activityType = 8;
-                                $act->elementType = 3;
-                                $act->elementId = $params->id;
-                                hwdMediaShareFactory::load('activities');
-                                hwdMediaShareActivities::save($act);
+                                $activity->action = $action->id;
+                                $activity->target = 0;
+                                $activity->verb = 8;
 			break;                     
                         case 'onAfterShareMediaWithGroup':
-                                $act->activityType = 9;
-                                $act->elementType = 1;
-                                $act->elementId = $params->media_id;
-                                $act->title = $params->group_id;
-                                hwdMediaShareFactory::load('activities');
-                                hwdMediaShareActivities::save($act);
+                                $activity->action = $action->id;
+                                $activity->target = $target->id;
+                                $activity->verb = 9;
 			break;
                         case 'onAfterMediaLike':
-                                $act->activityType = 10;
-                                $act->elementType = 1;
-                                $act->elementId = $params->media_id;
-                                hwdMediaShareFactory::load('activities');
-                                hwdMediaShareActivities::save($act);
+                                $activity->action = $action->id;
+                                $activity->target = 0;
+                                $activity->verb = 10;
 			break;
                         case 'onAfterMediaDislike':
-                                $act->activityType = 11;
-                                $act->elementType = 1;
-                                $act->elementId = $params->media_id;
-                                hwdMediaShareFactory::load('activities');
-                                hwdMediaShareActivities::save($act);
+                                $activity->action = $action->id;
+                                $activity->target = 0;
+                                $activity->verb = 11;
 			break;
                         case 'onAfterAlbumLike':
-                                $act->activityType = 12;
-                                $act->elementType = 2;
-                                $act->elementId = $params->id;
-                                hwdMediaShareFactory::load('activities');
-                                hwdMediaShareActivities::save($act);
+                                $activity->action = $action->id;
+                                $activity->target = 0;
+                                $activity->verb = 12;
 			break;
                         case 'onAfterAlbumDislike':
-                                $act->activityType = 13;
-                                $act->elementType = 2;
-                                $act->elementId = $params->id;
-                                hwdMediaShareFactory::load('activities');
-                                hwdMediaShareActivities::save($act);
+                                $activity->action = $action->id;
+                                $activity->target = 0;
+                                $activity->verb = 13;
 			break;
                         case 'onAfterGroupLike':
-                                $act->activityType = 14;
-                                $act->elementType = 3;
-                                $act->elementId = $params->id;
-                                hwdMediaShareFactory::load('activities');
-                                hwdMediaShareActivities::save($act);
+                                $activity->action = $action->id;
+                                $activity->target = 0;
+                                $activity->verb = 14;
 			break;
                         case 'onAfterGroupDislike':
-                                $act->activityType = 15;
-                                $act->elementType = 3;
-                                $act->elementId = $params->id;
-                                hwdMediaShareFactory::load('activities');
-                                hwdMediaShareActivities::save($act);
+                                $activity->action = $action->id;
+                                $activity->target = 0;
+                                $activity->verb = 15;
 			break;
                         case 'onAfterPlaylistLike':
-                                $act->activityType = 16;
-                                $act->elementType = 4;
-                                $act->elementId = $params->id;
-                                hwdMediaShareFactory::load('activities');
-                                hwdMediaShareActivities::save($act);
+                                $activity->action = $action->id;
+                                $activity->target = 0;
+                                $activity->verb = 16;
 			break;
                         case 'onAfterPlaylistDislike':
-                                $act->activityType = 17;
-                                $act->elementType = 4;
-                                $act->elementId = $params->id;
-                                hwdMediaShareFactory::load('activities');
-                                hwdMediaShareActivities::save($act);
+                                $activity->action = $action->id;
+                                $activity->target = 0;
+                                $activity->verb = 17;
 			break;
                         case 'onAfterChannelLike':
-                                $act->activityType = 18;
-                                $act->elementType = 5;
-                                $act->elementId = $params->id;
-                                hwdMediaShareFactory::load('activities');
-                                hwdMediaShareActivities::save($act);
+                                $activity->action = $action->id;
+                                $activity->target = 0;
+                                $activity->verb = 18;
 			break;
                         case 'onAfterChannelDislike':
-                                $act->activityType = 19;
-                                $act->elementType = 5;
-                                $act->elementId = $params->id;
-                                hwdMediaShareFactory::load('activities');
-                                hwdMediaShareActivities::save($act);
+                                $activity->action = $action->id;
+                                $activity->target = 0;
+                                $activity->verb = 19;
 			break;
                         case 'onAfterMediaFavourite':
-                                $act->activityType = 20;
-                                $act->elementType = 1;
-                                $act->elementId = $params->media_id;
-                                hwdMediaShareFactory::load('activities');
-                                hwdMediaShareActivities::save($act);
+                                $activity->action = $action->id;
+                                $activity->target = 0;
+                                $activity->verb = 20;
 			break;
                         case 'onAfterAddMediaToAlbum':
-                                $act->activityType = 21;
-                                $act->elementType = 1;
-                                $act->elementId = $params->media_id;
-                                $act->title = $params->album_id;
-                                hwdMediaShareFactory::load('activities');
-                                hwdMediaShareActivities::save($act);
+                                $activity->action = $action->id;
+                                $activity->target = $target->id;
+                                $activity->verb = 21;
 			break;
                         case 'onAfterAddMediaToPlaylist':
-                                $act->activityType = 22;
-                                $act->elementType = 1;
-                                $act->elementId = $params->media_id;
-                                $act->title = $params->playlist_id;
-                                hwdMediaShareFactory::load('activities');
-                                hwdMediaShareActivities::save($act);
+                                $activity->action = $action->id;
+                                $activity->target = $target->id;
+                                $activity->verb = 22;
 			break;
                         case 'onAfterAddMediaToCategory':
-                                $act->activityType = 23;
-                                $act->elementType = 1;
-                                $act->elementId = $params->media_id;
-                                $act->title = $params->category_id;
-                                hwdMediaShareFactory::load('activities');
-                                hwdMediaShareActivities::save($act);
+                                $activity->action = $action->id;
+                                $activity->target = $target->id;
+                                $activity->verb = 23;
 			break;
                 }
 
-                jimport( 'joomla.utilities.arrayhelper' );                
-                JPluginHelper::importPlugin( 'hwdmediashare' );
-                if (file_exists(JPATH_SITE.'/components/com_community/')) JPluginHelper::importPlugin( 'community' );
+                // Save the new activity.
+                $HWDactivities->save($activity);
+
+                // Load HWD plugins, and JomSocial plugins.
+                JPluginHelper::importPlugin('hwdmediashare');
+                if (file_exists(JPATH_SITE.'/components/com_community/')) JPluginHelper::importPlugin('community');
                 
-                $results = $dispatcher->trigger( $event, array('media' => JArrayHelper::fromObject($params)) );
+                // Trigger the event.
+                jimport('joomla.utilities.arrayhelper');                
+                $results = $dispatcher->trigger($event, $action, $target);
 
 		return $results;
 	}
