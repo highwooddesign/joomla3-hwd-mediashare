8b60223f7f3dcc0c561ab9a830aeed8cb80b6b03
Dave Horsfall <dave@horsfall.eu>
2014-04-30 08:52:13 +0100

    Major update to fileextension table class.

--- administrator/components/com_hwdmediashare/tables/fileextension.php
+++ administrator/components/com_hwdmediashare/tables/fileextension.php
@@ -23,4 +23,76 @@ class hwdMediaShareTableFileExtension extends JTable
 	{
 		parent::__construct('#__hwdms_ext', 'id', $db);
 	}
+        
+        
+	/**
+	 * Overload store method
+	 *
+	 * @param   boolean   $updateNulls   Toggle whether null values should be updated.
+         * 
+	 * @return  boolean   True on success, false on failure.
+	 */
+	public function store($updateNulls = false)
+	{
+		$date	= JFactory::getDate();
+		$user	= JFactory::getUser();
+
+		if ($this->id)
+		{
+			// Existing item
+			$this->modified		= $date->toSql();
+			$this->modified_user_id	= $user->get('id'); 
+		}
+		else
+		{
+			// New record. The created and created_by fields can be set by the user,
+			// so we don't touch either of these if they are set.
+			if (!(int) $this->created)
+			{
+				$this->created = $date->toSql();
+			}
+			if (empty($this->created_by))
+			{
+				$this->created_user_id = $user->get('id');
+			}  
+		}
+
+		// Set publish_up to null date if not set
+		if (!$this->publish_up)
+		{
+			$this->publish_up = $this->_db->getNullDate();
+		}
+
+		// Set publish_down to null date if not set
+		if (!$this->publish_down)
+		{
+			$this->publish_down = $this->_db->getNullDate();
+		}
+
+		return parent::store($updateNulls);
+	}
+        
+	/**
+	 * Overloaded check method to ensure data integrity.
+	 *
+	 * @return  boolean  True on success.
+	 */
+	public function check()
+	{
+		// Check for valid name
+		if (trim($this->ext) == '')
+		{
+			$this->setError(JText::_('COM_HWDMS_ERROR_SAVE_NO_EXT'));
+			return false;
+		}
+                
+		// Check the publish down date is not earlier than publish up.
+		if ($this->publish_down > $this->_db->getNullDate() && $this->publish_down < $this->publish_up)
+		{
+			$this->setError(JText::_('JGLOBAL_START_PUBLISH_AFTER_FINISH'));
+			return false;
+		}
+
+		return true;
+	}        
 }
