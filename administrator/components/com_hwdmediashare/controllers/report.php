2b9f6deac36fbc17de744fe9b8dbe1e889f3d9e2
Dave Horsfall <dave@horsfall.eu>
2014-05-07 11:23:13 +0100

    Removed reported controller, added new report controller to process tasks such as the ability to dismiss reports, and delete the reported element.

---
+++ administrator/components/com_hwdmediashare/controllers/report.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * @package     Joomla.administrator
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
+ */
+
+defined('_JEXEC') or die;
+
+class hwdMediaShareControllerReport extends JControllerAdmin
+{
+        /**
+	 * Proxy for getModel.
+	 * @return	void
+	 */
+	public function getModel($name = 'Report', $prefix = 'hwdMediaShareModel', $config = array())
+	{
+                $model = parent::getModel($name, $prefix, array('ignore_request' => true));
+                return $model;
+	}
+
+        /**
+	 * Method to dismiss a report.
+	 * @return	void
+	 */
+	public function dismiss()
+	{
+		// Check for request forgeries
+		JSession::checkToken('request') or die(JText::_('JINVALID_TOKEN'));
+
+		// Get items to remove from the request.
+		$cid = JFactory::getApplication()->input->get('cid', array(), 'array');
+
+		if (!is_array($cid) || count($cid) < 1)
+		{
+			JLog::add(JText::_($this->text_prefix . '_NO_ITEM_SELECTED'), JLog::WARNING, 'jerror');
+		}
+		else
+		{
+			// Get the model.
+			$model = $this->getModel();
+
+			// Make sure the item ids are integers
+			jimport('joomla.utilities.arrayhelper');
+			JArrayHelper::toInteger($cid);
+
+			// Remove the items.
+			if ($model->delete($cid))
+			{
+				$this->setMessage(JText::plural($this->text_prefix . '_N_ITEMS_DELETED', count($cid)));
+			}
+			else
+			{
+				$this->setMessage($model->getError());
+			}
+		}
+
+		// Redirect to the return page.
+		$this->setRedirect($this->getReturnPage());
+	}  
+        
+	/**
+	 * Get the return URL.
+	 *
+	 * If a "return" variable has been passed in the request
+	 *
+	 * @return  string	The return URL.
+	 */
+	protected function getReturnPage()
+	{
+		$return = $this->input->get('return', null, 'base64');
+
+		if (empty($return) || !JUri::isInternal(base64_decode($return)))
+		{
+			return JUri::base();
+		}
+		else
+		{
+			return base64_decode($return);
+		}
+	}        
+}
