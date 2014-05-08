cff79287740ffff6c4357fc62c05624fde4fac5d
Dave Horsfall <dave@horsfall.eu>
2014-05-08 11:23:44 +0100

    Removal of all js files in the forms directory.

--- administrator/components/com_hwdmediashare/models/forms/tag.js
+++
@@ -1,8 +0,0 @@
-/**
- * @version    SVN $Id: tag.js 277 2012-03-28 10:03:31Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) ${date?date?string("yyyy")} Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      ${date} ${time}
- */
