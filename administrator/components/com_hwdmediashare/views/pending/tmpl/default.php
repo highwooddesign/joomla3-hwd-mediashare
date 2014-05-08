186d6a0891790d246d43bac00bd00edf8ca7e24a
Dave Horsfall <dave@horsfall.eu>
2014-05-08 11:37:59 +0100

    Syntax change.

--- administrator/components/com_hwdmediashare/views/pending/tmpl/default.php
+++ administrator/components/com_hwdmediashare/views/pending/tmpl/default.php
@@ -13,7 +13,7 @@ defined('_JEXEC') or die;
 JHtml::_('behavior.framework', true);
 ?>
 <form action="<?php echo JRoute::_('index.php?option=com_hwdmediashare&view=pending'); ?>" method="post" name="adminForm" id="adminForm">
-<?php if (!empty( $this->sidebar)) : ?>
+<?php if (!empty($this->sidebar)) : ?>
 	<div id="j-sidebar-container" class="span2">
 		<?php echo $this->sidebar; ?>
 	</div>
