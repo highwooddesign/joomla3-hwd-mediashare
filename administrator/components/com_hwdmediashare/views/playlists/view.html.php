291f55d278f1e0a084665d8cfa5b4a18ec5e4c8b
Dave Horsfall <dave@horsfall.eu>
2014-05-08 11:26:26 +0100

    Syntax change.

--- administrator/components/com_hwdmediashare/views/playlists/view.html.php
+++ administrator/components/com_hwdmediashare/views/playlists/view.html.php
@@ -85,7 +85,7 @@ class hwdMediaShareViewPlaylists extends JViewLegacy
                         JToolBarHelper::deleteList('', 'playlists.delete', 'JTOOLBAR_EMPTY_TRASH');
                         JToolBarHelper::divider();
                 }
-		else if ($canDo->get('core.edit.state'))
+		elseif ($canDo->get('core.edit.state'))
                 {
 			JToolBarHelper::divider();
                         JToolBarHelper::trash('playlists.trash');
