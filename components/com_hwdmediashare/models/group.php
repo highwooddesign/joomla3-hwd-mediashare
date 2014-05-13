822cea48c77c54117c3b22397b4d0953cd0774ac
Dave Horsfall <dave@horsfall.eu>
2014-05-13 11:06:53 +0100

    Updated getItem method to allow users to view their own unapproved items.

--- components/com_hwdmediashare/models/group.php
+++ components/com_hwdmediashare/models/group.php
@@ -107,30 +107,30 @@ class hwdMediaShareModelGroup extends JModelList
                                 $this->setError(JText::_('COM_HWDMS_ERROR_ITEM_UNPUBLISHED'));
                                 return false;
                         }
-                        else if (is_int($published) && $table->published != $published) 
+                        elseif (is_int($published) && $table->published != $published) 
                         {
                                 $this->setError(JText::_('COM_HWDMS_ERROR_ITEM_UNPUBLISHED'));
                                 return false;
                         }
                 }
                                 
-                // Check approval status and access permissions.
+                // Check approval status and access permissions, but allow users to see own unapproved items.
+                $user = JFactory::getUser();
                 if ($status = $this->getState('filter.status'))
                 {
-                        if (is_array($status) && !in_array($table->status, $status)) 
+                        if (is_array($status) && !in_array($table->status, $status) && $table->created_user_id != $user->id) 
                         {
                                 $this->setError(JText::_('COM_HWDMS_ERROR_ITEM_UNAPPROVED'));
                                 return false;
                         }
-                        else if (is_int($status) && $table->status != $status) 
+                        elseif (is_int($status) && $table->status != $status && $table->created_user_id != $user->id) 
                         {
                                 $this->setError(JText::_('COM_HWDMS_ERROR_ITEM_UNAPPROVED'));
                                 return false;
                         }
                 }
-                
+
                 // Check group access level and access permissions.
-                $user = JFactory::getUser();
                 $groups = $user->getAuthorisedViewLevels();
                 if (!in_array($table->access, $groups)) 
                 {                                    
@@ -559,7 +559,7 @@ class hwdMediaShareModelGroup extends JModelList
                                         ->group('member_id');
                                 $db->setQuery($query);
                                 $db->query(); 
-                                $member = $db->getResult();
+                                $member = $db->loadResult();
                         }
                         catch (Exception $e)
                         {
