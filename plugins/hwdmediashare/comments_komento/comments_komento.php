66c2035eea1d5f36b97a5ea2a2b65ac9a9b96ef6
Dave Horsfall <dave@horsfall.eu>
2014-05-13 08:58:11 +0100

    Updated the HWD - Comments (Komento) plugin.

--- plugins/hwdmediashare/comments_komento/comments_komento.php
+++ plugins/hwdmediashare/comments_komento/comments_komento.php
@@ -1,45 +1,22 @@
 <?php
 /**
- * @version    $Id: comments_komento.php 892 2013-01-07 12:01:09Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2011 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      15-Apr-2011 10:13:15
- */
-
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
-
-// Import hwdMediaShare remote library
-hwdMediaShareFactory::load('remote');
-
-/**
- * hwdMediaShare framework files class
+ * @package     Joomla.site
+ * @subpackage  Plugin.hwdmediashare.comments_disqus
  *
- * @package hwdMediaShare
- * @since   0.1
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
-class plgHwdmediashareComments_komento
-{
-        /**
-	 * Constructor
-	 *
-	 * @access      protected
-	 * @param       object  $subject The object to observe
-	 * @param       array   $config  An array that holds the plugin configuration
-	 * @since       1.5
-	 */
-	public function __construct()
-	{
-	}
 
+defined('_JEXEC') or die;
+
+class plgHwdmediashareComments_komento extends JObject
+{
 	/**
-	 * Returns the hwdMediaShareFiles object, only creating it if it
+	 * Returns the plgHwdmediashareComments_komento object, only creating it if it
 	 * doesn't already exist.
 	 *
-	 * @return  hwdMediaShareFiles A hwdMediaShareFiles object.
-	 * @since   0.1
+	 * @return plgHwdmediashareComments_komento object.
 	 */
 	public static function getInstance()
 	{
@@ -55,75 +32,58 @@ class plgHwdmediashareComments_komento
 	}
 
         /**
-	 * Method to add a file to the database
-         *
-	 * @since   0.1
+	 * Method to insert the Komento commenting system.
+         * 
+	 * @return  void
 	 **/
-	public function getComments()
+	public function getComments($item, $elementType=1)
 	{
-                // Load hwdMediaShare config
+		// Initialise variables.
+                $app = JFactory::getApplication();
+                $doc = JFactory::getDocument();
+                
+                // Get HWD config.
                 $hwdms = hwdMediaShareFactory::getInstance();
                 $config = $hwdms->getConfig();
+                
+                // Load plugin.
+		$plugin = JPluginHelper::getPlugin('hwdmediashare', 'comments_komento');
+		
+                // Load the language file.
+                $lang = JFactory::getLanguage();
+                $lang->load('plg_hwdmediashare_comments_komento', JPATH_SITE . '/administrator');
 
-                $plugin =& JPluginHelper::getPlugin('hwdmediashare', 'comments_komento');
-
-                // Die if plugin not avaliable
-                if (isset($plugin->params))
+                if (!$plugin)
                 {
-                        $params = new JRegistry( $plugin->params );
-                }
-                else
-                {
-                        $params = new JRegistry();
-                }
-
-                // @task: load bootstrap
-                $bootstrap	= JPATH_ROOT . DS . 'components' . DS . 'com_komento' . DS . 'bootstrap.php';
-
-                jimport('joomla.filesystem.file');
-
-                if( !JFile::exists( $bootstrap ) )
-                {
-                        // Missing bootstrap
+                        $this->setError(JText::_('PLG_HWDMEDIASHARE_COMMENTS_KOMENTO_ERROR_NOT_PUBLISHED'));
                         return false;
                 }
 
-                $bootstrap = require_once( $bootstrap );
+                // Load parameters.
+                $params = new JRegistry($plugin->params);
 
-                // Get a row instance.
-                JTable::addIncludePath(JPATH_ADMINISTRATOR.'/components/com_hwdmediashare/tables');
-                $table = JTable::getInstance('Media', 'hwdMediaShareTable');
-
-                // temporary fix from Komento's team
-                $tempId = explode( ':', JRequest::getString( 'id' ) );
-                $tempId = $tempId[0];
-
-                // Attempt to load the row.
-                // if ($table->load(JRequest::getCmd( 'id' ))) // temporary fix from Komento's team
-                if ($table->load($tempId))
+                // Load Komento libraries.
+                $bootstrap = JPATH_ROOT . '/components/com_komento/bootstrap.php';
+                if(!JFile::exists($bootstrap))
                 {
-                        // Convert the JTable to a clean JObject.
-                        $properties = $table->getProperties(1);
-                        $item = JArrayHelper::toObject($properties, 'JObject');
-
-                        // Passing in the data
-                        $options		= array();
-                        $options['enable']	= true;
-                        //$options['trigger']	= $eventTrigger;
-                        //$options['context']	= $context;
-                        //$options['params']	= $params;
-                        //$options['page']	= $page;
-
-                        $extension = JRequest::getCmd( 'option' );
-
-                        // Ready to Commentify!
-                        return Komento::commentify( $extension, $item, $options );
-                }
-                else if ($error = $table->getError())
-                {
-                        $this->setError($error);
+                        $this->setError(JText::_('PLG_HWDMEDIASHARE_COMMENTS_KOMENTO_ERROR_COMPONENT_NOT_INSTALLED'));
+                        return false;                    
                 }
 
-                return false;
+                require_once($bootstrap);
+
+                // Define request parameters.
+                $extension = $app->input->get('option', '', 'word');
+                
+                // Passing in the data.
+                $options		= array();
+                $options['enable']	= true;
+                //$options['trigger']	= $eventTrigger;
+                //$options['context']	= $context;
+                //$options['params']	= $params;
+                //$options['page']	= $page;
+
+                // Ready to Commentify!
+                return Komento::commentify($extension, $item, $options);
         }
 }
