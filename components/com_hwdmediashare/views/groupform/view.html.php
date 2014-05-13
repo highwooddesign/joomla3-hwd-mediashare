d34c7a2a9e4a443eceb30e9091d5b499859ca48a
Dave Horsfall <dave@horsfall.eu>
2014-05-13 15:51:51 +0100

    Updated groupform view class.

--- components/com_hwdmediashare/views/groupform/view.html.php
+++ components/com_hwdmediashare/views/groupform/view.html.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @package     Joomla.administrator
+ * @package     Joomla.site
  * @subpackage  Component.hwdmediashare
  *
  * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
@@ -57,7 +57,7 @@ class hwdMediaShareViewGroupForm extends JViewLegacy
 		}
 		else
 		{
-			$authorised = $this->item->access->get('access-edit');
+			$authorised = $this->item->attributes->get('access-edit');
 		}
 		if ($authorised !== true)
 		{
