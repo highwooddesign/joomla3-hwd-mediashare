53c1eeb94b2e9c23d7594fab67230d8701007648
Dave Horsfall <dave@horsfall.eu>
2014-05-14 11:32:14 +0100

    Major update of media table class. Updated bin overload to check for password, and when empty replace with existing. Added new store overload, to provide same processes as other tables. Added new like method to increment like property in table. Added new view method to update view property in table.

--- administrator/components/com_hwdmediashare/tables/media.php
+++ administrator/components/com_hwdmediashare/tables/media.php
@@ -15,14 +15,11 @@ jimport('joomla.database.table');
 
 class hwdMediaShareTableMedia extends JTable
 {
-	var $id = null;
-	var $key = null;
-
 	/**
 	 * Constructor.
 	 * @return	void
 	 */
-	function __construct(&$db)
+	function __construct($db)
 	{             
 		parent::__construct('#__hwdms_media', 'id', $db);
                 JObserverMapper::addObserverClassToClass('JTableObserverTags', 'hwdMediaShareTableMedia', array('typeAlias' => 'com_hwdmediashare.media'));                
@@ -41,20 +38,32 @@ class hwdMediaShareTableMedia extends JTable
 		// Convert the params fields to a string.
                 if (isset($array['params']) && is_array($array['params']))
 		{
+                        // Get the password from the data array and unset
+                        $newPassword = $array['params']['password1'];
+                        unset($array['params']['password1']);
+
 			$registry = new JRegistry;
 			$registry->loadArray($array['params']);
-                        
-                        /** We don't want to loose the existing password! So we check if a new one has been passed, then retain the old one if empty **/
-                        $newPassword = $registry->get('password'); 
+
+                        // We don't want to loose the existing password! So we check if a new one has been passed, then retain the old one if empty
+                        JTable::addIncludePath(JPATH_ADMINISTRATOR.'/components/com_hwdmediashare/tables');
+                        $table = JTable::getInstance('Media', 'hwdMediaShareTable');
+                        $table->load($array['id']);
+                        $properties = $table->getProperties(1);
+                        $row = JArrayHelper::toObject($properties, 'JObject');
                         if (empty($newPassword))
                         {                                
-                                JTable::addIncludePath(JPATH_ADMINISTRATOR.'/components/com_hwdmediashare/tables');
-                                $table =& JTable::getInstance('Media', 'hwdMediaShareTable');
-                                $table->load($array['id']);
-                                $properties = $table->getProperties(1);
-                                $row = JArrayHelper::toObject($properties, 'JObject');
-                                $oldPassword = $row->params->get('password');                                
-                                if (!empty($oldPassword)) $parameter->set('password', $row->params->get('password'));
+                                $password = $row->params->get('password');                                
+                                if (!empty($password)) $registry->set('password', $password);
+                        }
+                        else
+                        {
+                                $password = md5($row->key . $newPassword);
+                                if (!empty($password)) 
+                                {
+                                        $registry->set('password', $password);
+                                        $registry->set('password', $password);
+                                }
                         }
                         
 			$array['params'] = (string) $registry;
@@ -94,6 +103,215 @@ class hwdMediaShareTableMedia extends JTable
 			return false;
 		}
 	}
+
+	/**
+	 * Overload store method
+	 *
+	 * @param   boolean   $updateNulls   Toggle whether null values should be updated.
+         * 
+	 * @return  boolean   True on success, false on failure.
+	 */
+	public function store($updateNulls = false)
+	{
+		// Initialise variables.
+                $app = JFactory::getApplication();
+                $date = JFactory::getDate();
+                $user = JFactory::getUser();
+                $isNew = false;
+
+                // Load HWD config
+                $hwdms = hwdMediaShareFactory::getInstance();
+                $config = $hwdms->getConfig();
+                
+		if ($this->id)
+		{
+			// Existing item, so set modified details.
+			$this->modified		= $date->toSql();
+			$this->modified_user_id	= $user->get('id');
+
+                        // Only allow users with permission to edit states.
+                        if (!$user->authorise('core.edit.state', 'com_hwdmediashare.album.'. (int) $this->id))
+                        {
+                                unset($this->published);
+                                unset($this->status);
+                                unset($this->featured);
+                                unset($this->access);
+                        }                          
+		}
+		else
+		{
+			// New item
+                        $isNew = true;
+                        
+                        // Set a unique key
+                        if (empty($this->key))
+                        {
+                                hwdMediaShareFactory::load('utilities');
+                                $this->key = hwdMediaShareUtilities::generateKey();
+                                if (hwdMediaShareUtilities::keyExists($this->key))
+                                {
+                                        $this->setError(JText::_('COM_HWDMS_KEY_EXISTS'));
+                                        return false;
+                                }
+                        }
+
+                        // Only allow users with permission to edit states.
+                        if (!$user->authorise('core.edit.state', 'com_hwdmediashare'))
+                        {
+                                $this->published = 1;
+                                $this->featured = 1;
+                                $this->access = 1;
+                        } 
+
+                        // Set approval status
+                        $this->status = (!$app->isAdmin() && $config->get('approve_new_playlists') == 1) ? 2 : 1;
+
+                        // The created and created_by fields can be set by the user,
+			// so we don't touch either of these if they are set.
+			if (!(int) $this->created)
+			{
+				$this->created = $date->toSql();
+			}
+			if (empty($this->created_user_id))
+			{
+				$this->created_user_id = $user->get('id');
+			}                      
+		}
+
+		// Set publish_up to null date if not set
+		if (!$this->publish_up)
+		{
+			$this->publish_up = $this->_db->getNullDate();
+		}
+
+		// Set publish_down to null date if not set
+		if (!$this->publish_down)
+		{
+			$this->publish_down = $this->_db->getNullDate();
+		}
+
+		// Verify that the alias is unique
+		$table = JTable::getInstance('Playlist', 'hwdMediaShareTable');
+
+		if ($table->load(array('alias' => $this->alias)) && ($table->id != $this->id || $this->id == 0))
+		{
+			$this->setError(JText::_('COM_HWDMS_ERROR_UNIQUE_ALIAS'));
+			return false;
+		}
+
+                $return = parent::store($updateNulls);
+		if ($return) 
+                {
+                        // Perform a few post-store tasks
+
+                        // Get the upload data
+                        hwdMediaShareFactory::load('upload');
+                        $data = hwdMediaShareUpload::getProcessedUploadData();                
+                        
+                        // Add category data
+                        if (isset($data['catid']))
+                        {
+                                hwdMediaShareFactory::load('category');
+                                $catid = (array) $data['catid'];
+                                // Make sure the item ids are integers
+                                jimport('joomla.utilities.arrayhelper');
+                                JArrayHelper::toInteger($catid);
+                                $object = new StdClass;
+                                $object->elementId = $this->id;
+                                $object->categoryId = $catid;
+                                $HWDcategory = hwdMediaShareCategory::getInstance();
+                                $HWDcategory->elementType = 1;
+                                $HWDcategory->save($object);
+                        }
+
+                        // Add custom field data
+                        hwdMediaShareFactory::load('customfields');                
+                        $object = new StdClass;
+                        $object->elementId = $this->id;
+                        $HWDcustomfields = hwdMediaShareCustomFields::getInstance();
+                        $HWDcustomfields->elementType = 1;
+                        $HWDcustomfields->save($object);
+
+                        // Add thumbnail
+                        hwdMediaShareFactory::load('upload');
+                        $object = new StdClass;
+                        $object->elementType = 1;
+                        $object->elementId = $this->id;
+                        $object->remove = (isset($data['remove_thumbnail']) ? true : false);
+                        $object->thumbnail_remote = (isset($data['thumbnail_remote']) ? $data['thumbnail_remote'] : null);
+                        $HWDupload = hwdMediaShareUpload::getInstance();
+                        $HWDupload->processThumbnail($object);
+                        
+                        // If new and approved, trigger event
+                        if ($isNew && $this->status == 1)
+                        {                            
+                                $properties = $this->getProperties(1);
+                                $row = JArrayHelper::toObject($properties, 'JObject');                                
+                                hwdMediaShareFactory::load('events');
+                                $events = hwdMediaShareEvents::getInstance();
+                                $events->triggerEvent('onAfterMediaAdd', $row);
+                                
+                                hwdMediaShareUpload::assignAssociations($row);
+                        }    
+
+                        // Send system notifications
+                        if ($isNew && $config->get('notify_new_media') == 1) 
+                        {
+                                if($row->status == 2){
+                                        ob_start();
+                                        require(JPATH_SITE . '/components/com_hwdmediashare/libraries/emails/newmedia_pending.php');
+                                        $body = ob_get_contents();
+                                        ob_end_clean();
+                                }
+                                else{
+                                        ob_start();
+                                        require(JPATH_SITE . '/components/com_hwdmediashare/libraries/emails/newmedia.php');
+                                        $body = ob_get_contents();
+                                        ob_end_clean();
+                                }
+                                hwdMediaShareFactory::load('utilities');
+                                $utilities = hwdMediaShareUtilities::getInstance();
+                                $utilities->sendSystemEmail(JText::_('COM_HWDMS_EMAIL_SUBJECT_NEW_MEDIA'), $body);
+                        }  
+                }        
+                
+		return $return;
+	}
+        
+	/**
+	 * Overloaded check method to ensure data integrity.
+	 *
+	 * @return  boolean  True on success.
+	 */
+	public function check()
+	{
+ 		// Check for valid name
+		if (trim($this->title) == '')
+		{
+			$this->setError(JText::_('COM_HWDMS_ERROR_SAVE_NO_TITLE'));
+			return false;
+		}
+                
+		// Check for valid alias
+		if (empty($this->alias))
+		{
+			$this->alias = $this->title;
+		}
+		$this->alias = JApplication::stringURLSafe($this->alias);
+		if (trim(str_replace('-', '', $this->alias)) == '')
+		{
+			$this->alias = JFactory::getDate()->format("Y-m-d-H-i-s");
+		}
+
+		// Check the publish down date is not earlier than publish up.
+		if ($this->publish_down > $this->_db->getNullDate() && $this->publish_down < $this->publish_up)
+		{
+			$this->setError(JText::_('JGLOBAL_START_PUBLISH_AFTER_FINISH'));
+			return false;
+		}
+
+		return true;
+	}
         
 	/**
 	 * Method to compute the default name of the asset.
@@ -123,10 +341,127 @@ class hwdMediaShareTableMedia extends JTable
 	 *
 	 * @return	int
 	 */
-	protected function _getAssetParentId($table = null, $id = null)
+	protected function _getAssetParentId(JTable $table = null, $id = null)
 	{
 		$asset = JTable::getInstance('Asset');
 		$asset->loadByName('com_hwdmediashare');
 		return $asset->id;
 	}
+        
+	/**
+	 * Method to increment the likes/dislikes for a row if the necessary property/field exists.
+	 *
+	 * @param   mixed    $pk     An optional primary key value to increment. If not set the instance property value is used.
+	 * @param   integer  $value  The value of the property to increment.
+         * 
+	 * @return  boolean  True on success.
+	 */
+	public function like($pk = null, $value = 1)
+	{
+		$values	= array(1 => 'likes', 0 => 'dislikes');
+		$property = JArrayHelper::getValue($values, $value, 'like', 'word');
+
+		// If there is no hits field, just return true.
+		if (!property_exists($this, $property))
+		{
+			return true;
+		}
+
+		if (is_null($pk))
+		{
+			$pk = array();
+
+			foreach ($this->_tbl_keys AS $key)
+			{
+				$pk[$key] = $this->$key;
+			}
+		}
+		elseif (!is_array($pk))
+		{
+			$pk = array($this->_tbl_key => $pk);
+		}
+
+		foreach ($this->_tbl_keys AS $key)
+		{
+			$pk[$key] = is_null($pk[$key]) ? $this->$key : $pk[$key];
+
+			if ($pk[$key] === null)
+			{
+				throw new UnexpectedValueException('Null primary key not allowed.');
+			}
+		}
+
+		// Check the row in by primary key.
+		$query = $this->_db->getQuery(true)
+			->update($this->_tbl)
+			->set($this->_db->quoteName($property) . ' = (' . $this->_db->quoteName($property) . ' + 1)');
+		$this->appendPrimaryKeys($query, $pk);
+		$this->_db->setQuery($query);
+		$this->_db->execute();
+
+		// Set table values in the object.
+		$this->hits++;
+
+		return true;
+	} 
+
+	/**
+	 * Method to update the viewed date for a row if the necessary property/field exists.
+	 *
+	 * @param   mixed    $pk     An optional primary key value to increment. If not set the instance property value is used.
+	 * @param   integer  $value  The value of the property to increment.
+         * 
+	 * @return  boolean  True on success.
+	 */
+	public function view($pk = null)
+	{
+                // Initialise variables.
+                $date = JFactory::getDate();
+
+		// The property we are updating.
+                $property = 'viewed';
+
+		// If there is no hits field, just return true.
+		if (!property_exists($this, $property))
+		{
+			return true;
+		}
+
+		if (is_null($pk))
+		{
+			$pk = array();
+
+			foreach ($this->_tbl_keys AS $key)
+			{
+				$pk[$key] = $this->$key;
+			}
+		}
+		elseif (!is_array($pk))
+		{
+			$pk = array($this->_tbl_key => $pk);
+		}
+
+		foreach ($this->_tbl_keys AS $key)
+		{
+			$pk[$key] = is_null($pk[$key]) ? $this->$key : $pk[$key];
+
+			if ($pk[$key] === null)
+			{
+				throw new UnexpectedValueException('Null primary key not allowed.');
+			}
+		}
+
+		// Check the row in by primary key.
+		$query = $this->_db->getQuery(true)
+			->update($this->_tbl)
+			->set($this->_db->quoteName($property) . ' = ' . $this->_db->quote($date->toSql()));
+		$this->appendPrimaryKeys($query, $pk);
+		$this->_db->setQuery($query);
+		$this->_db->execute();
+
+		// Set table values in the object.
+		$this->hits++;
+
+		return true;
+	}         
 }
