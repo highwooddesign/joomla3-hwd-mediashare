ef73c87f553d8e411ede63093fce0a5f856e4106
Dave Horsfall <dave@horsfall.eu>
2014-05-14 17:06:26 +0100

    Added new item models to frontend (linking to necessary backend) models.

---
+++ components/com_hwdmediashare/models/groupmediaitem.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package     Joomla.site
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
+ */
+
+defined('_JEXEC') or die;
+
+// Base this model on the backend version.
+require_once JPATH_ADMINISTRATOR.'/components/com_hwdmediashare/models/groupmediaitem.php';
