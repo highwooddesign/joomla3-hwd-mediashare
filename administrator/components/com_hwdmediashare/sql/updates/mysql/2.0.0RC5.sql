bd135173051db0f21737709d78a746da4543a615
Dave Horsfall <dave@horsfall.eu>
2014-05-29 11:30:37 +0100

    Added 2.0.0RC5 sql update file, which checks new activity table, and updates config table structure.

---
+++ administrator/components/com_hwdmediashare/sql/updates/mysql/2.0.0RC5.sql
@@ -0,0 +1,23 @@
+#
+# Database updates for 2.0.0RC5
+#
+
+CREATE TABLE IF NOT EXISTS `#__hwdms_activity` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `actor` int(11) unsigned NOT NULL,
+  `action` int(11) unsigned NOT NULL,
+  `target` int(11) unsigned NOT NULL,
+  `verb` int(11) unsigned NOT NULL,
+  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `access` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `params` text NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `actor` (`actor`),
+  KEY `action` (`action`),
+  KEY `target` (`target`),
+  KEY `verb` (`verb`),
+  KEY `created` (`created`),
+  KEY `access` (`access`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=30 ;
+
+ALTER TABLE `#__hwdms_config` ADD COLUMN `asset_id` int(11) unsigned NOT NULL DEFAULT '0' AFTER `id`;
