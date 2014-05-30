7d7a05fe08ae45e2cebad21276513a401f298b1e
Dave Horsfall <dave@horsfall.eu>
2014-05-30 13:33:57 +0100

    Updated mediaredirect system plugin.

--- plugins/system/mediaredirect/mediaredirect.php
+++ plugins/system/mediaredirect/mediaredirect.php
@@ -1,60 +1,78 @@
 <?php
 /**
- * @version    $Id: mediaredirect.php 1300 2013-03-19 11:41:46Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2011 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      15-Apr-2011 10:13:15
+ * @package     Joomla.site
+ * @subpackage  Plugin.system.mediaredirect
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
 
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
-
-// Import Joomla plugin library
-jimport('joomla.plugin.plugin');
+defined('_JEXEC') or die;
 
 class plgSystemMediaRedirect extends JPlugin
 {
+	/**
+	 * Check for a necessary redirection after Joomla initialises.
+	 *
+	 * @access      protected
+	 * @return      void
+	 */    
 	public function onAfterInitialise()
-	{
-                $db =& JFactory::getDBO();
-                $date =& JFactory::getDate();
+	{                
+                // Initialise variables.
+                $db = JFactory::getDBO();
+                $date = JFactory::getDate();
                 $user = JFactory::getUser();
-                $app = & JFactory::getApplication();
+                $app = JFactory::getApplication();
 
-                // Require hwdMediaShare factory
+		// No redirect for admin.
+		if ($app->isAdmin())
+		{
+			return false;
+		}
+
+                // Get entry point.
+                $option = $app->input->get('option');
+                $task = $app->input->get('task');
+                $id = $app->input->get('id', 0, 'integer');
+
+                // Only redirect com_hwdvideoshare option.
+                if ($option != 'com_hwdvideoshare')
+                {
+                        return false;
+                }
+                        
+                // Register HWD.
                 JLoader::register('hwdMediaShareFactory', JPATH_ROOT.'/components/com_hwdmediashare/libraries/factory.php');
 
-                $db->setQuery( 'SHOW TABLES' );
+                $db->setQuery('SHOW TABLES');
                 $tables = $db->loadColumn();
-
-                foreach ($tables as $table)
+                
+                if (in_array($app->getCfg( 'dbprefix' ).'hwdms_migrator', $tables)) 
                 {
-                        if ($table == $app->getCfg( 'dbprefix' ).'hwdms_migrator')
+                        if ($task == 'viewvideo' && $id)
                         {
-                                $option = JFactory::getApplication()->input->get('option');
-                                $task = JFactory::getApplication()->input->get('task');
-                                $id = JFactory::getApplication()->input->get('id');
-                                if ($option == 'com_hwdvideoshare' && $task == 'viewvideo' && $id)
+                                $query = $db->getQuery(true)
+                                        ->select('*')
+                                        ->from('#__hwdms_migrator')
+                                        ->where('element_id = ' . $db->quote($id))
+                                        ->where('element_type = ' . $db->quote(1));
+                                try
                                 {
-                                        $query = "
-                                            SELECT *
-                                            FROM ".$db->quoteName('#hwdms_migrator')."
-                                            WHERE `element_id` = ".$db->quoteName(intval($id))."
-                                            AND `element_type` = 1
-                                        ";
                                         $db->setQuery($query);
-                                        $redirect = $db->loadResult();
-
-                                        if ($redirect > 0) JFactory::getApplication()->redirect('index.php?option=com_hwdmediashare&view=mediaitem&id='.$redirect);
+                                        $redirect = $db->loadResult();                  
                                 }
-                                
-                                if ($option == 'com_hwdvideoshare')
+                                catch (RuntimeException $e)
                                 {
-                                        JFactory::getApplication()->redirect('index.php?option=com_hwdmediashare&view=media');
+                                        $this->setError($e->getMessage());
+                                        return false;                            
                                 }
+
+                                if ($redirect > 0) $app->redirect('index.php?option=com_hwdmediashare&view=mediaitem&id='.$redirect);
                         }
+
+                        $app->redirect('index.php?option=com_hwdmediashare&view=media');
                 }
 	}
 }
