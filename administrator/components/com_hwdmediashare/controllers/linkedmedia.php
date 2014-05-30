9dfe11dfbf5318c04703c1b9bda13f696ffc1c16
Dave Horsfall <dave@horsfall.eu>
2014-05-30 09:54:45 +0100

    Updated comment blocks.

--- administrator/components/com_hwdmediashare/controllers/linkedmedia.php
+++ administrator/components/com_hwdmediashare/controllers/linkedmedia.php
@@ -14,19 +14,25 @@ class hwdMediaShareControllerLinkedMedia extends JControllerAdmin
 {
 	/**
 	 * The prefix to use with controller messages.
-	 * @var    string
+         * 
+         * @access      protected
+	 * @var         string
 	 */
 	protected $text_prefix = 'COM_HWDMS';
                  
-    	/**
-	 * The URL view list variable.
-	 * @var    string
+	/**
+	 * The name of the listing view to use with this controller.
+         * 
+         * @access      protected
+	 * @var         string
 	 */
     	protected $view_list = "linkedmedia";
             
         /**
 	 * Proxy for getModel.
-	 * @return	void
+	 *
+	 * @access	public
+         * @return      object      The model.
 	 */
 	public function getModel($name = 'LinkedMedia', $prefix = 'hwdMediaShareModel', $config = array())
 	{
@@ -35,12 +41,14 @@ class hwdMediaShareControllerLinkedMedia extends JControllerAdmin
 	}
 
         /**
-	 * Method to unlink other media from a media item
-	 * @return	void
+	 * Method to unlink other media from a media item.
+	 *
+	 * @access	public
+         * @return      void
 	 */
 	public function unlink()
 	{
-		// Check for request forgeries
+		// Check for request forgeries.
 		JSession::checkToken() or die(JText::_('JINVALID_TOKEN'));
 
 		// Get items to remove from the request.
@@ -57,7 +65,7 @@ class hwdMediaShareControllerLinkedMedia extends JControllerAdmin
 			// Get the model.
 			$model = $this->getModel();
 
-			// Make sure the item ids are integers
+			// Make sure the item ids are integers.
 			jimport('joomla.utilities.arrayhelper');
 			JArrayHelper::toInteger($cid);
 
@@ -76,12 +84,14 @@ class hwdMediaShareControllerLinkedMedia extends JControllerAdmin
 	}
         
         /**
-	 * Method to link other media to a media item
-	 * @return	void
+	 * Method to link other media to a media item.
+	 *
+	 * @access	public
+         * @return      void
 	 */
 	public function link()
 	{
-		// Check for request forgeries
+		// Check for request forgeries.
 		JSession::checkToken() or die(JText::_('JINVALID_TOKEN'));
 
 		// Get items to remove from the request.
@@ -98,7 +108,7 @@ class hwdMediaShareControllerLinkedMedia extends JControllerAdmin
 			// Get the model.
 			$model = $this->getModel();
 
-			// Make sure the item ids are integers
+			// Make sure the item ids are integers.
 			jimport('joomla.utilities.arrayhelper');
 			JArrayHelper::toInteger($cid);
 
