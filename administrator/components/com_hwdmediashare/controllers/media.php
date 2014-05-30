07731d65ba3c976ad1edc448d0f89cff671e9295
Dave Horsfall <dave@horsfall.eu>
2014-05-30 10:25:17 +0100

    Updated comment blocks.

--- administrator/components/com_hwdmediashare/controllers/media.php
+++ administrator/components/com_hwdmediashare/controllers/media.php
@@ -9,422 +9,203 @@
  */
 /**
  * UNFINISHED
- */
+ */ 
 defined('_JEXEC') or die;
 
 class hwdMediaShareControllerMedia extends JControllerAdmin
 {
 	/**
-	 * Constructor.
-	 * @return	void
+	 * The prefix to use with controller messages.
+         * 
+         * @access      protected
+	 * @var         string
+	 */
+	protected $text_prefix = 'COM_HWDMS';
+        
+	/**
+	 * Class constructor.
+	 *
+	 * @access	public
+         * @return      void
 	 */
 	public function __construct($config = array())
 	{
 		parent::__construct($config);
+                
+		// Define standard task mappings.                
                 $this->registerTask('unfeature', 'feature');
                 $this->registerTask('unapprove', 'approve');
 	}
         
         /**
 	 * Proxy for getModel.
-	 * @return	void
+	 *
+	 * @access	public
+         * @return      object      The model.
 	 */
-	public function getModel($name = 'EditMedia', $prefix = 'hwdMediaShareModel')
+	public function getModel($name = 'EditMedia', $prefix = 'hwdMediaShareModel', $config = array())
 	{
                 $model = parent::getModel($name, $prefix, array('ignore_request' => true));
                 return $model;
-	}
-        
+	}        
+
 	/**
 	 * Method to toggle the status setting of a list of media.
-	 * @return	void
+	 *
+	 * @access	public
+         * @return      void
 	 */
 	public function approve()
 	{
-		// Check for request forgeries
-		JRequest::checkToken() or jexit(JText::_('JINVALID_TOKEN'));
+		// Check for request forgeries.
+		JSession::checkToken() or die(JText::_('JINVALID_TOKEN'));
+
+		// Get items to remove from the request.
+		$cid = $this->input->get('cid', array(), 'array');
 
 		// Initialise variables.
-		$user	= JFactory::getUser();
-		$ids	= JRequest::getVar('cid', array(), '', 'array');
 		$values	= array('approve' => 1, 'unapprove' => 0);
 		$task	= $this->getTask();
-                
 		$value	= JArrayHelper::getValue($values, $task, 0, 'int');
 
-		// Access checks.
-		foreach ($ids as $i => $id)
+		if (!is_array($cid) || count($cid) < 1)
 		{
-			if (!$user->authorise('core.edit.state', 'com_hwdmediashare.media.'.(int) $id)) {
-				// Prune items that you can't change.
-				unset($ids[$i]);
-				JError::raiseNotice(403, JText::_('JLIB_APPLICATION_ERROR_EDITSTATE_NOT_PERMITTED'));
-			}
-		}
-
-		if (empty($ids))
-                {
-			JError::raiseWarning(500, JText::_('JERROR_NO_ITEMS_SELECTED'));
+			JLog::add(JText::_($this->text_prefix . '_NO_ITEM_SELECTED'), JLog::WARNING, 'jerror');
 		}
 		else
-                {
+		{
 			// Get the model.
 			$model = $this->getModel();
 
-			// Publish the items.
-			if (!$model->approve($ids, $value))
-                        {
-				JError::raiseWarning(500, $model->getError());
+			// Make sure the item ids are integers.
+			jimport('joomla.utilities.arrayhelper');
+			JArrayHelper::toInteger($cid);
+
+			// Approve the items.
+			if ($model->approve($cid, $value))
+			{
+				$this->setMessage(JText::plural($this->text_prefix . '_N_ITEMS_'.strtoupper($task).'D', count($cid)));
+			}
+			else
+			{
+				$this->setMessage($model->getError());
 			}
 		}
-
-		$this->setRedirect('index.php?option=com_hwdmediashare&view=media');
+                
+		$this->setRedirect(JRoute::_('index.php?option=' . $this->option . '&view=' . $this->view_list, false));
 	}
         
 	/**
 	 * Method to toggle the featured setting of a list of media.
-	 * @return	void
+	 *
+	 * @access	public
+         * @return      void
 	 */
 	public function feature()
 	{
-		// Check for request forgeries
-		JRequest::checkToken() or jexit(JText::_('JINVALID_TOKEN'));
+		// Check for request forgeries.
+		JSession::checkToken() or die(JText::_('JINVALID_TOKEN'));
+
+		// Get items to remove from the request.
+		$cid = $this->input->get('cid', array(), 'array');
 
 		// Initialise variables.
-		$user	= JFactory::getUser();
-		$ids	= JRequest::getVar('cid', array(), '', 'array');
 		$values	= array('feature' => 1, 'unfeature' => 0);
 		$task	= $this->getTask();
-
 		$value	= JArrayHelper::getValue($values, $task, 0, 'int');
 
-		// Access checks.
-		foreach ($ids as $i => $id)
+		if (!is_array($cid) || count($cid) < 1)
 		{
-			if (!$user->authorise('core.edit.state', 'com_hwdmediashare.media.'.(int) $id)) 
-                        {
-				// Prune items that you can't change.
-				unset($ids[$i]);
-				JError::raiseNotice(403, JText::_('JLIB_APPLICATION_ERROR_EDITSTATE_NOT_PERMITTED'));
-			}
-		}
-
-		if (empty($ids))
-                {
-			JError::raiseWarning(500, JText::_('JERROR_NO_ITEMS_SELECTED'));
+			JLog::add(JText::_($this->text_prefix . '_NO_ITEM_SELECTED'), JLog::WARNING, 'jerror');
 		}
 		else
-                {
+		{
 			// Get the model.
 			$model = $this->getModel();
 
-			// Publish the items.
-			if (!$model->feature($ids, $value))
-                        {
-				JError::raiseWarning(500, $model->getError());
+			// Make sure the item ids are integers.
+			jimport('joomla.utilities.arrayhelper');
+			JArrayHelper::toInteger($cid);
+
+			// Feature/unfeature the items.
+			if ($model->feature($cid, $value))
+			{
+				$this->setMessage(JText::plural($this->text_prefix . '_N_ITEMS_'.strtoupper($task).'D', count($cid)));
+			}
+			else
+			{
+				$this->setMessage($model->getError());
 			}
 		}
-
-		$this->setRedirect('index.php?option=com_hwdmediashare&view=media');
+                
+		$this->setRedirect(JRoute::_('index.php?option=' . $this->option . '&view=' . $this->view_list, false));
 	}
 
-	/**
-	 * Method to set the values for multiple media.
-	 * @return	void
+       /**
+	 * Method to syncronise data from the local gallery to a remote CDN.
+	 *
+	 * @access	public
+         * @return      void
 	 */
-	public function batch()
+	public function syncToCdn()
 	{
-		// Check for request forgeries
-		JRequest::checkToken() or jexit(JText::_('JINVALID_TOKEN'));
+		$this->task = 'apply';
+                if (parent::save())
+                {                      
+                        JTable::addIncludePath(JPATH_ADMINISTRATOR.'/components/com_hwdmediashare/tables');
+                        $table =& JTable::getInstance('Media', 'hwdMediaShareTable');
+                        $table->load(JRequest::getVar('id'));
+                        $properties = $table->getProperties(1);
+                        $item = JArrayHelper::toObject($properties, 'JObject');
 
-		// Initialise variables.
-		$user	= JFactory::getUser();
-		$ids	= JRequest::getVar('cid', array(), '', 'array');
-		$task	= $this->getTask();
-
-		$value = array();
-                $value['user'] = JRequest::getInt('batch_user');
-                $value['access'] = JRequest::getInt('batch_access');
-                $value['language'] = JRequest::getVar('batch_language');
-
-		// Access checks.
-		foreach ($ids as $i => $id)
-		{
-			if (!$user->authorise('core.edit', 'com_hwdmediashare.activity.'.(int) $id)) 
+                        if ($item->type == 6)
                         {
-				// Prune items that you can't change.
-				unset($ids[$i]);
-				JError::raiseNotice(403, JText::_('JLIB_APPLICATION_ERROR_EDITSTATE_NOT_PERMITTED'));
-			}
-		}
+                                $pluginClass = 'plgHwdmediashare'.$item->storage;
+                                $pluginPath = JPATH_ROOT.'/plugins/hwdmediashare/'.$item->storage.'/'.$item->storage.'.php';
 
-		if (empty($ids))
-                {
-			JError::raiseWarning(500, JText::_('JERROR_NO_ITEMS_SELECTED'));
-		}
-		else
-                {
-			// Get the model.
-			$model = $this->getModel();
-
-			// Publish the items.
-			if (!$model->batch($ids, $value))
-                        {
-				JError::raiseWarning(500, $model->getError());
-			}
-                        else
-                        {
-                                JFactory::getApplication()->enqueueMessage( JText::_('COM_HWDMS_SUCCESSFULLY_PERFORMED_BATCH_OPERATION') );
+                                // Import hwdMediaShare plugins
+                                if (file_exists($pluginPath))
+                                {
+                                        JLoader::register($pluginClass, $pluginPath);
+                                        $platform = call_user_func(array($pluginClass, 'getInstance'));
+                                        $platform->syncToCdn();
+                                }
                         }
+			return true;
 		}
-
-		$this->setRedirect('index.php?option=com_hwdmediashare&view='.$this->view_list);
-	}
-
-	/**
-	 * Method to assign the category settings to a list of media.
-	 * @return	void
-	 */
-	public function assignCategory()
-	{
-		$app	        =& JFactory::getApplication();
-		$model		=& $this->getModel();
-		$id		= JRequest::getVar( 'cid' , '' , 'post' );
-		$errors		= false;
-		$message	= JText::_('COM_HWDMS_SUCCESSFULLY_ASSIGNED_CATEGORY');
-
-		for( $i = 0; $i < count($id); $i++ )
-		{
-			if( !$model->assignCategory( $id[ $i ] ) )
-			{
-				JError::raiseWarning(500, $model->getError());
-			}
-		}
-
-		$app->redirect( 'index.php?option=com_hwdmediashare&view=media' , $message );
 	}
         
-	/**
-	 * Method to remove the category settings from a list of items.
-	 * @return	void
+        /**
+	 * Method to syncronise data from a remote CDN to the local gallery.
+	 *
+	 * @access	public
+         * @return      void
 	 */
-	public function unassignCategory()
+	public function syncFromCdn()
 	{
-		$app	        =& JFactory::getApplication();
-		$model		=& $this->getModel();
-		$id		= JRequest::getVar( 'cid' , '' , 'post' );
-		$errors		= false;
-		$message	= JText::_('COM_HWDMS_SUCCESSFULLY_REMOVED_CATEGORY');
+		$this->task = 'apply';
+                if (parent::save())
+                {                      
+                        JTable::addIncludePath(JPATH_ADMINISTRATOR.'/components/com_hwdmediashare/tables');
+                        $table =& JTable::getInstance('Media', 'hwdMediaShareTable');
+                        $table->load(JRequest::getVar('id'));
+                        $properties = $table->getProperties(1);
+                        $item = JArrayHelper::toObject($properties, 'JObject');
 
-                if( empty($id) )
-		{
-			JError::raiseError( '500' , JText::_('COM_HWDMS_INVALID_ID') );
+                        if ($item->type == 6)
+                        {
+                                $pluginClass = 'plgHwdmediashare'.$item->storage;
+                                $pluginPath = JPATH_ROOT.'/plugins/hwdmediashare/'.$item->storage.'/'.$item->storage.'.php';
+
+                                // Import hwdMediaShare plugins
+                                if (file_exists($pluginPath))
+                                {
+                                        JLoader::register($pluginClass, $pluginPath);
+                                        $platform = call_user_func(array($pluginClass, 'getInstance'));
+                                        $platform->syncFromCdn();
+                                }
+                        }
+			return true;
 		}
-
-		for( $i = 0; $i < count($id); $i++ )
-		{
-			if( !$model->unassignCategory( $id[ $i ] ) )
-			{
-				JError::raiseWarning(500, $model->getError());
-			}
-		}
-
-		$app->redirect( 'index.php?option=com_hwdmediashare&view=media' , $message );
-	}
-        
-	/**
-	 * Method to unassign the album setting to a list of media.
-	 * @return	void
-	 */
-	public function assignAlbum()
-	{
-		$app	        =& JFactory::getApplication();
-		$model		=& $this->getModel();
-		$id		= JRequest::getVar( 'cid' , '' , 'post' );
-		$errors		= false;
-		$message	= JText::_('COM_HWDMS_SUCCESSFULLY_ASSIGNED_ALBUM');
-
-                if( empty($id) )
-		{
-			JError::raiseError( '500' , JText::_('COM_HWDMS_INVALID_ID') );
-		}
-
-		for( $i = 0; $i < count($id); $i++ )
-		{
-			if( !$model->assignAlbum( $id[ $i ] ) )
-			{
-				JError::raiseWarning(500, $model->getError());
-			}
-		}
-
-		$app->redirect( 'index.php?option=com_hwdmediashare&view=media' , $message );
-	}
-        
-	/**
-	 * Method to remove the album settings from a list of media.
-	 * @return	void
-	 */
-	public function unassignAlbum()
-	{
-		$app	        =& JFactory::getApplication();
-		$model		=& $this->getModel();
-		$id		= JRequest::getVar( 'cid' , '' , 'post' );
-		$errors		= false;
-		$message	= JText::_('COM_HWDMS_SUCCESSFULLY_REMOVED_ALBUM');
-
-                if( empty($id) )
-		{
-			JError::raiseError( '500' , JText::_('COM_HWDMS_INVALID_ID') );
-		}
-
-		for( $i = 0; $i < count($id); $i++ )
-		{
-			if( !$model->unassignAlbum( $id[ $i ] ) )
-			{
-				JError::raiseWarning(500, $model->getError());
-			}
-		}
-
-		$app->redirect( 'index.php?option=com_hwdmediashare&view=media' , $message );
-	}
-        
-	/**
-	 * Method to assign the playlist settings to a list of media.
-	 * @return	void
-	 */
-	public function assignPlaylist()
-	{
-		$app	        =& JFactory::getApplication();
-		$model		=& $this->getModel();
-		$id		= JRequest::getVar( 'cid' , '' , 'post' );
-		$errors		= false;
-		$message	= JText::_('COM_HWDMS_SUCCESSFULLY_ASSIGNED_PLAYLIST');
-
-                if( empty($id) )
-		{
-			JError::raiseError( '500' , JText::_('COM_HWDMS_INVALID_ID') );
-		}
-
-		for( $i = 0; $i < count($id); $i++ )
-		{
-			if( !$model->assignPlaylist( $id[ $i ] ) )
-			{
-				JError::raiseWarning(500, $model->getError());
-			}
-		}
-
-		$app->redirect( 'index.php?option=com_hwdmediashare&view=media' , $message );
-	}
-        
-	/**
-	 * Method to remove the playlist settings from a list of media.
-	 * @return	void
-	 */
-	public function unassignPlaylist()
-	{
-		$app	        =& JFactory::getApplication();
-		$model		=& $this->getModel();
-		$id		= JRequest::getVar( 'cid' , '' , 'post' );
-		$errors		= false;
-		$message	= JText::_('COM_HWDMS_SUCCESSFULLY_REMOVED_PLAYLIST');
-
-                if( empty($id) )
-		{
-			JError::raiseError( '500' , JText::_('COM_HWDMS_INVALID_ID') );
-		}
-
-		for( $i = 0; $i < count($id); $i++ )
-		{
-			if( !$model->unassignPlaylist( $id[ $i ] ) )
-			{
-				JError::raiseWarning(500, $model->getError());
-			}
-		}
-
-		$app->redirect( 'index.php?option=com_hwdmediashare&view=media' , $message );
-	}
-        
-	/**
-	 * Method to assign the group settings to a list of media.
-	 * @return	void
-	 */
-	public function assignGroup()
-	{
-		$app	        =& JFactory::getApplication();
-		$model		=& $this->getModel();
-		$id		= JRequest::getVar( 'cid' , '' , 'post' );
-		$errors		= false;
-		$message	= JText::_('COM_HWDMS_SUCCESSFULLY_ASSIGNED_GROUP');
-
-                if( empty($id) )
-		{
-			JError::raiseError( '500' , JText::_('COM_HWDMS_INVALID_ID') );
-		}
-
-		for( $i = 0; $i < count($id); $i++ )
-		{
-			if( !$model->assignGroup( $id[ $i ] ) )
-			{
-				JError::raiseWarning(500, $model->getError());
-			}
-		}
-
-		$app->redirect( 'index.php?option=com_hwdmediashare&view=media' , $message );
-	}
-        
-	/**
-	 * Method to remove the group settings from a list of media.
-	 * @return	void
-	 */
-	public function unassignGroup()
-	{
-		$app	        =& JFactory::getApplication();
-		$model		=& $this->getModel();
-		$id		= JRequest::getVar( 'cid' , '' , 'post' );
-		$errors		= false;
-		$message	= JText::_('COM_HWDMS_SUCCESSFULLY_REMOVED_GROUP');
-
-                if( empty($id) )
-		{
-			JError::raiseError( '500' , JText::_('COM_HWDMS_INVALID_ID') );
-		}
-
-		for( $i = 0; $i < count($id); $i++ )
-		{
-			if( !$model->unassignGroup( $id[ $i ] ) )
-			{
-				JError::raiseWarning(500, $model->getError());
-			}
-		}
-
-		$app->redirect( 'index.php?option=com_hwdmediashare&view=media' , $message );
-	}
-        
-	/**
-	 * Method to queue new processes to a list of media.
-	 * @return	void
-	 */
-	public function assignProcess()
-	{
-		$app	        =& JFactory::getApplication();
-		$model		=& $this->getModel();
-		$id		= JRequest::getVar( 'cid' , '' , 'post' );
-		$errors		= false;
-		$message	= JText::_('COM_HWDMS_SUCCESSFULLY_ASSIGNED_PROCESS');
-
-                if( empty($id) )
-		{
-			JError::raiseError( '500' , JText::_('COM_HWDMS_INVALID_ID') );
-		}
-
-		for( $i = 0; $i < count($id); $i++ )
-		{
-			if( !$model->assignProcess( $id[ $i ] ) )
-			{
-				JError::raiseWarning(500, $model->getError());
-			}
-		}
-
-		$app->redirect( 'index.php?option=com_hwdmediashare&view=media' , $message );
 	}
 }
