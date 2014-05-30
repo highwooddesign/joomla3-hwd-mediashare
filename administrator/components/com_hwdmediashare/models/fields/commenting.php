0f2269b71d1c49bfe6057d8d0dd6b8a99a2c454f
Dave Horsfall <dave@horsfall.eu>
2014-05-30 14:21:58 +0100

    Updated block comments.

--- administrator/components/com_hwdmediashare/models/fields/commenting.php
+++ administrator/components/com_hwdmediashare/models/fields/commenting.php
@@ -15,16 +15,18 @@ JFormHelper::loadFieldClass('list');
 class JFormFieldCommenting extends JFormFieldList
 {
 	/**
-	 * The form field type.
-	 *
-	 * @var  string
+	 * The name of the form field type.
+         * 
+         * @access      protected
+	 * @var         string
 	 */
 	protected $type = 'Commenting';
 
 	/**
 	 * Method to get the field options.
 	 *
-	 * @return  array  The field option objects.
+	 * @access	protected
+	 * @return      array       The field option objects.
 	 */
 	protected function getOptions()
 	{
@@ -46,7 +48,7 @@ class JFormFieldCommenting extends JFormFieldList
                         return false;                            
                 }
 
-                // Loop all plugins and check if a commenting plugin
+                // Loop all plugins and check if a commenting plugin.
 		for($i = 0; $i < count($rows); $i++)
 		{
 			$row = $rows[$i];
