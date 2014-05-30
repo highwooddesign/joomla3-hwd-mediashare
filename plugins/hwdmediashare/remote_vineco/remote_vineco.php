c313d79061d6c0de7d458b3f127e53d44529099b
Dave Horsfall <dave@horsfall.eu>
2014-05-30 11:36:51 +0100

    Updated Vine plugin.

--- plugins/hwdmediashare/remote_vineco/remote_vineco.php
+++ plugins/hwdmediashare/remote_vineco/remote_vineco.php
@@ -1,34 +1,29 @@
 <?php
 /**
- * @version    $Id: remote_viveco.php 1317 2013-03-20 10:23:09Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2011 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      15-Apr-2011 10:13:15
- */
-
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
-
-// Import hwdMediaShare remote library
-hwdMediaShareFactory::load('remote');
-
-/**
- * hwdMediaShare framework files class
+ * @package     Joomla.site
+ * @subpackage  Plugin.hwdmediashare.remote_youtubecom
  *
- * @package hwdMediaShare
- * @since   0.1
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
+
+defined('_JEXEC') or die;
+
 class plgHwdmediashareRemote_vineco extends JObject
 {
 	/**
-	 * Remote media type integer.
-	 *
-	 * @var		int
+	 * The remote media type integer: http://hwdmediashare.co.uk/learn/api/68-api-definitions
+         * 
+         * @access      public
+	 * @var         integer
 	 */
 	public $mediaType = 4;
-
+        
+	/**
+	 * Library data
+	 * @var strings
+	 */
         var $_url;
         var $_host;
         var $_buffer;
@@ -37,25 +32,24 @@ class plgHwdmediashareRemote_vineco extends JObject
         var $_source;
         var $_duration;
         var $_thumbnail;
-
-        /**
-	 * Constructor
+        
+	/**
+	 * Class constructor.
 	 *
-	 * @access      protected
-	 * @param       object  $subject The object to observe
-	 * @param       array   $config  An array that holds the plugin configuration
-	 * @since       1.5
+	 * @access	public
+         * @return      void
 	 */
 	public function __construct()
 	{
+		parent::__construct();
 	}
 
 	/**
-	 * Returns the hwdMediaShareFiles object, only creating it if it
+	 * Returns the plgHwdmediashareRemote_vineco object, only creating it if it
 	 * doesn't already exist.
 	 *
-	 * @return  hwdMediaShareFiles A hwdMediaShareFiles object.
-	 * @since   0.1
+	 * @access	public
+	 * @return      object      The plgHwdmediashareRemote_vineco object.
 	 */
 	public static function getInstance()
 	{
@@ -71,10 +65,11 @@ class plgHwdmediashareRemote_vineco extends JObject
 	}
 
         /**
-	 * Method to add a file to the database
-         *
-	 * @since   0.1
-	 **/
+	 * Get the title of the media.
+	 *
+	 * @access	public
+         * @return      void
+	 */
 	public function getTitle()
 	{
                 if( !$this->_title )
@@ -87,10 +82,11 @@ class plgHwdmediashareRemote_vineco extends JObject
         }
 
         /**
-	 * Method to add a file to the database
-         *
-	 * @since   0.1
-	 **/
+	 * Get the description of the media.
+	 *
+	 * @access	public
+         * @return      void
+	 */
 	public function getDescription()
 	{
                 if( !$this->_description )
@@ -103,10 +99,11 @@ class plgHwdmediashareRemote_vineco extends JObject
         }
 
         /**
-	 * Method to add a file to the database
-         *
-	 * @since   0.1
-	 **/
+	 * Get the source of the media.
+	 *
+	 * @access	public
+         * @return      void
+	 */
 	public function getSource()
 	{
                 if( !$this->_source )
@@ -119,10 +116,11 @@ class plgHwdmediashareRemote_vineco extends JObject
         }
 
         /**
-	 * Method to add a file to the database
-         *
-	 * @since   0.1
-	 **/
+	 * Get the duration of the media.
+	 *
+	 * @access	public
+         * @return      void
+	 */
 	public function getDuration()
 	{
                 if( !$this->_duration )
@@ -138,10 +136,11 @@ class plgHwdmediashareRemote_vineco extends JObject
         }
 
         /**
-	 * Method to add a file to the database
-         *
-	 * @since   0.1
-	 **/
+	 * Get the thumbnail location of the media.
+	 *
+	 * @access	public
+         * @return      void
+	 */
 	public function getThumbnail()
 	{
                 if( !$this->_thumbnail )
@@ -149,12 +148,12 @@ class plgHwdmediashareRemote_vineco extends JObject
                         jimport( 'joomla.filter.filterinput' );
                         JLoader::register('JHtmlString', JPATH_LIBRARIES.'/joomla/html/html/string.php');
 
-                        // We will apply the most strict filter to the variable
+                        // We will apply the most strict filter to the variable.
                         $noHtmlFilter = JFilterInput::getInstance();
 
                         $thumbnail = false;
 
-                        // Check Open Graph tag
+                        // Check OpenGraph tag.
                         preg_match('/<meta property="og:image" content="([^"]+)/', $this->_buffer, $match);
                         if (!empty($match[1]))
                         {
@@ -177,10 +176,11 @@ class plgHwdmediashareRemote_vineco extends JObject
         }
 
         /**
-	 * Method to add a file to the database
-         *
-	 * @since   0.1
-	 **/
+	 * Request the source, and set to buffer.
+	 *
+	 * @access	public
+         * @return      void
+	 */
 	public function getBuffer()
 	{
                 $this->getHost();
@@ -196,10 +196,11 @@ class plgHwdmediashareRemote_vineco extends JObject
 	}
 
         /**
-	 * Method to add a file to the database
-         *
-	 * @since   0.1
-	 **/
+	 * Get the host of the media source.
+	 *
+	 * @access	public
+         * @return      void
+	 */
 	public function getHost()
 	{
                 if (!$this->_host)
@@ -212,10 +213,11 @@ class plgHwdmediashareRemote_vineco extends JObject
 	}
 
         /**
-	 * Method to add a file to the database
-         *
-	 * @since   0.1
-	 **/
+	 * Get the url of the media source.
+	 *
+	 * @access	public
+         * @return      void
+	 */
 	public function getUrl()
 	{
                 if (!$this->_url)
@@ -228,42 +230,69 @@ class plgHwdmediashareRemote_vineco extends JObject
 	}
 
         /**
-	 * Method to add a file to the database
-         *
-	 * @since   0.1
-	 **/
+	 * Render the HTML to display the media.
+	 *
+	 * @access	public
+	 * @param       object      $item       The media item being displayed.
+         * @return      void
+	 */
 	public function display($item)
 	{
-		$plugin =& JPluginHelper::getPlugin('hwdmediashare', 'remote_vineco');
-		$params = new JRegistry( @$plugin->params );
+                // Initialise variables.
+                $app = JFactory::getApplication();
 
-                // Load hwdMediaShare config
+                // Load plugin.
+		$plugin = JPluginHelper::getPlugin('hwdmediashare', 'remote_vineco');
+		
+                // Load the language file.
+                $lang = JFactory::getLanguage();
+                $lang->load('plg_hwdmediashare_remote_vineco', JPATH_SITE . '/administrator');
+
+                if (!$plugin)
+                {
+                        $this->setError(JText::_('PLG_HWDMEDIASHARE_REMOTE_VINECO_ERROR_NOT_PUBLISHED'));
+                        return false;
+                }
+
+                // Load parameters.
+                $params = new JRegistry($plugin->params);
+
+                // Load HWD config.
                 $hwdms = hwdMediaShareFactory::getInstance();
                 $config = $hwdms->getConfig();
-
-                $autoplay = (JRequest::getInt('media_autoplay', $config->get('media_autoplay')) == 1 ? 'true' : 'false');
-
+                
+                // Load HWD utilities.
                 hwdMediaShareFactory::load('utilities');
                 $utilities = hwdMediaShareUtilities::getInstance();
-
-                $this->width = $utilities->getMediaWidth();
-                $this->height = (int) $config->get('mediaitem_height') ? $config->get('mediaitem_height') : $this->width*$config->get('video_aspect',0.75);
-                $this->height = (int) $this->width;
-
+                
+                // Get Vine ID
                 $id = plgHwdmediashareRemote_vineco::parse($item->source);
+                
+                $this->autoplay = $app->input->get('media_autoplay', $config->get('media_autoplay'), 'integer') == 1 ? '1' : '0';
+                $this->width = '100%';
+                $this->height = '100%';
                 ob_start();
                 ?>
-                <iframe class="vine-embed" src="https://vine.co/v/<?php echo $id ?>/embed/simple" width="<?php echo $this->width; ?>" height="<?php echo $this->height; ?>" frameborder="0"></iframe><script async src="//platform.vine.co/static/scripts/embed.js" charset="utf-8"></script>                <?php
+                <div class="media-respond" style="max-width:<?php echo $config->get('mediaitem_size', '500'); ?>px;">
+                  <div class="media-aspect" data-aspect="<?php echo $config->get('video_aspect', '0.75'); ?>"></div>
+                  <div class="media-content">
+                    <iframe class="vine-embed" src="https://vine.co/v/<?php echo $id ?>/embed/simple" width="<?php echo $this->width; ?>" height="<?php echo $this->height; ?>" frameborder="0"></iframe><script async src="//platform.vine.co/static/scripts/embed.js" charset="utf-8"></script>
+                  </div>
+                </div>
+                <?php
                 $html = ob_get_contents();
                 ob_end_clean();
-		return $html;
+                
+                return $html;
 	}
 
         /**
-	 * Method to add a file to the database
-         *
-	 * @since   0.1
-	 **/
+	 * Parse the source to extract the media.
+	 *
+	 * @access	public
+	 * @param       string      $url        The media url.
+         * @return      void
+	 */ 
         protected function parse($url)
         {
                 $pos_u = strpos($url, "v/");
