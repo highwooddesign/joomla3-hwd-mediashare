c86aba80b3699ab4acc598d3f5ed717e9b1ef318
Dave Horsfall <dave@horsfall.eu>
2014-05-30 09:58:07 +0100

    Updated comment blocks.

--- administrator/components/com_hwdmediashare/controllers/group.php
+++ administrator/components/com_hwdmediashare/controllers/group.php
@@ -12,27 +12,29 @@ defined('_JEXEC') or die;
 
 class hwdMediaShareControllerGroup extends JControllerForm
 {
-    	/**
-	 * The URL view list variable.
-	 * @var    string
+	/**
+	 * The name of the listing view to use with this controller.
+         * 
+         * @access      protected
+	 * @var         string
 	 */
     	protected $view_list = "groups";
 
 	/**
 	 * Method to run batch operations.
 	 *
-	 * @param   object      $model  The model.
-	 *
-	 * @return  boolean     True if successful, false otherwise and internal error is set.
-	 */    
+	 * @access	public
+	 * @param       object      $model      The model.
+	 * @return      boolean     True if successful, false otherwise and internal error is set.
+	 */     
 	public function batch($model = null)
 	{
 		JSession::checkToken() or jexit(JText::_('JINVALID_TOKEN'));
 
-		// Set the model
+		// Set the model.
 		$model = $this->getModel('Group', '', array());
 
-		// Preset the redirect
+		// Preset the redirect.
 		$this->setRedirect(JRoute::_('index.php?option=com_hwdmediashare&view=' . $this->view_list . $this->getRedirectToListAppend(), false));
 
 		return parent::batch($model);
