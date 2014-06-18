b89c2c752d1e939a5c4583184cfa7106c22a41f1
Dave Horsfall <dave@horsfall.eu>
2014-06-18 15:39:17 +0100

    Updated comment blocks in view class.

--- administrator/components/com_hwdmediashare/views/files/view.html.php
+++ administrator/components/com_hwdmediashare/views/files/view.html.php
@@ -12,14 +12,24 @@ defined('_JEXEC') or die;
 
 class hwdMediaShareViewFiles extends JViewLegacy 
 {
+	protected $items;
+
+	protected $pagination;
+
+	protected $state;
+        
+	public $filterForm;
+        
+	public $batchForm;
+        
 	/**
-	 * Display the view
+	 * Display the view.
 	 *
+	 * @access  public
 	 * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
-	 *
 	 * @return  void
 	 */
-	function display($tpl = null)
+	public function display($tpl = null)
 	{
                 // Get data from the model.
                 $this->items = $this->get('Items');
@@ -31,7 +41,7 @@ class hwdMediaShareViewFiles extends JViewLegacy
                 hwdMediaShareFactory::load('downloads');
                 hwdMediaShareFactory::load('files');
                 
-                // Check for errors
+                // Check for errors.
                 if (count($errors = $this->get('Errors')))
                 {
                         JError::raiseError(500, implode('<br />', $errors));
@@ -45,13 +55,14 @@ class hwdMediaShareViewFiles extends JViewLegacy
 			$this->sidebar = JHtmlSidebar::render();
 		}
                 
-		// Display the template
+		// Display the template.
 		parent::display($tpl);
 	}
 
 	/**
 	 * Add the page title and toolbar.
 	 *
+	 * @access  protected
 	 * @return  void
 	 */
 	protected function addToolBar()
@@ -59,30 +70,26 @@ class hwdMediaShareViewFiles extends JViewLegacy
 		$canDo = hwdMediaShareHelper::getActions();
 		$user  = JFactory::getUser();
                 
-		// Get the toolbar object instance
+		// Get the toolbar object instance.
 		$bar = JToolBar::getInstance('toolbar');
                 
 		JToolBarHelper::title(JText::_('COM_HWDMS_FILES'), 'file');
 
 		if ($this->state->get('filter.published') == -2 && $canDo->get('core.delete'))
                 {
-			JToolBarHelper::divider();
                         JToolBarHelper::deleteList('', 'files.delete', 'JTOOLBAR_EMPTY_TRASH');
-                        JToolBarHelper::divider();
                 }
 		elseif ($canDo->get('core.edit.state'))
                 {
-			JToolBarHelper::divider();
                         JToolBarHelper::trash('files.trash');
-                        JToolBarHelper::divider();
 		}
-		// Add a batch button
+		// Add a batch button.
 		if ($user->authorise('core.create', 'com_hwdmediashare') && $user->authorise('core.edit', 'com_hwdmediashare') && $user->authorise('core.edit.state', 'com_hwdmediashare'))
 		{
 			JHtml::_('bootstrap.modal', 'collapseModal');
 			$title = JText::_('JTOOLBAR_BATCH');
 
-			// Instantiate a new JLayoutFile instance and render the batch button
+			// Instantiate a new JLayoutFile instance and render the batch button.
 			$layout = new JLayoutFile('joomla.toolbar.batch');
 
 			$dhtml = $layout->render(array('title' => $title));
@@ -92,8 +99,10 @@ class hwdMediaShareViewFiles extends JViewLegacy
 	}
 
 	/**
-	 * Method to display a humna readbale file type.
-	 * @return  void
+	 * Method to display a human readbale file type.
+	 *
+	 * @access  public
+	 * @return  string  The file type.
 	 */
 	public function getFileType($item)
 	{
@@ -103,9 +112,11 @@ class hwdMediaShareViewFiles extends JViewLegacy
         
 	/**
 	 * Method to display the path of the file relative to the Joomla root directory.
-	 * @return  void
+	 *
+	 * @access  public
+	 * @return  string  The path.
 	 */
-	public function getPath( &$item )
+	public function getPath($item)
 	{
                 hwdMediaShareFactory::load('files');
                 hwdMediaShareFiles::getLocalStoragePath();
@@ -118,9 +129,11 @@ class hwdMediaShareViewFiles extends JViewLegacy
         
 	/**
 	 * Method to display the file extension.
-	 * @return  void
+	 *
+	 * @access  public
+	 * @return  string  The file extension.
 	 */
-	public function getExtension( &$item )
+	public function getExtension($item)
 	{
                 hwdMediaShareFactory::load('files');
                 return hwdMediaShareFiles::getExtension($item, $item->file_type);
