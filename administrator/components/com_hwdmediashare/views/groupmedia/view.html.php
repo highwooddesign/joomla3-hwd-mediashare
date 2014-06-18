0e36a8b60f7ec53b0f270954f00bda1c569ca71b
Dave Horsfall <dave@horsfall.eu>
2014-06-18 15:25:38 +0100

    Updated comment blocks in view class.

--- administrator/components/com_hwdmediashare/views/groupmedia/view.html.php
+++ administrator/components/com_hwdmediashare/views/groupmedia/view.html.php
@@ -12,14 +12,22 @@ defined('_JEXEC') or die;
 
 class hwdMediaShareViewGroupMedia extends JViewLegacy 
 {
+	protected $items;
+
+	protected $pagination;
+
+	protected $state;
+
+	protected $groupId;
+        
 	/**
-	 * Display the view
+	 * Display the view.
 	 *
+	 * @access  public
 	 * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
-	 *
 	 * @return  void
 	 */
-	function display($tpl = null)
+	public function display($tpl = null)
 	{
                 // Get data from the model.
                 $this->items = $this->get('Items');
@@ -36,7 +44,7 @@ class hwdMediaShareViewGroupMedia extends JViewLegacy
                         return false;
                 }
 
-		// Display the template
+		// Display the template.
 		parent::display($tpl);
 
 		$document = JFactory::getDocument();
@@ -45,7 +53,9 @@ class hwdMediaShareViewGroupMedia extends JViewLegacy
 
 	/**
 	 * Display appropriate button to either link or unlink the media from the group.
-	 * @return  void
+	 *
+	 * @access  public
+	 * @return  string  The HTML to render the button.
 	 */
 	public function getButton($row, $i)
 	{
@@ -53,7 +63,6 @@ class hwdMediaShareViewGroupMedia extends JViewLegacy
                 $class = $row->connection ? 'btn btn-danger' : 'btn';
                 $text = $row->connection ? 'COM_HWDMS_BTN_REMOVE_FROM_GROUP' : 'COM_HWDMS_BTN_ADD_TO_GROUP';
 
-                // Start output
                 ob_start();
                 ?>
                 <div class="btn-wrapper pull-right">
