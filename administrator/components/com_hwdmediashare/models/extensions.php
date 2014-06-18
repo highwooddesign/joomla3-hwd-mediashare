a1f085e469a894166acfb32a3303500e5f2926bb
Dave Horsfall <dave@horsfall.eu>
2014-06-18 10:09:17 +0100

    Updated comment blocks.

--- administrator/components/com_hwdmediashare/models/extensions.php
+++ administrator/components/com_hwdmediashare/models/extensions.php
@@ -12,11 +12,13 @@ defined('_JEXEC') or die;
 
 class hwdMediaShareModelExtensions extends JModelList
 {
-    	/**
-	 * Constructor override, defines a white list of column filters.
+	/**
+	 * Class constructor. Defines a white list of column filters.
 	 *
-	 * @param   array  $config  An optional associative array of configuration settings.
-	 */
+	 * @access	public
+	 * @param       array       $config     An optional associative array of configuration settings.
+         * @return      void
+	 */    
 	public function __construct($config = array())
 	{
 		if (empty($config['filter_fields']))
@@ -44,6 +46,7 @@ class hwdMediaShareModelExtensions extends JModelList
 	/**
 	 * Method to get the database query.
 	 *
+	 * @access  protected
 	 * @return  JDatabaseQuery  database query
 	 */
 	protected function getListQuery()
@@ -130,9 +133,9 @@ class hwdMediaShareModelExtensions extends JModelList
 	 *
 	 * Note. Calling getState in this method will result in recursion.
 	 *
+	 * @access  protected
 	 * @param   string  $ordering   An optional ordering field.
 	 * @param   string  $direction  An optional direction (asc|desc).
-	 *
 	 * @return  void
 	 */
 	protected function populateState($ordering = null, $direction = null)
