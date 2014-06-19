d9b243bba2bc27e6ba398477ddaeb267e95a5715
Dave Horsfall <dave@horsfall.eu>
2014-06-19 13:07:59 +0100

    View template cleanup.

--- administrator/components/com_hwdmediashare/views/linkedgroups/tmpl/default_manage.php
+++ administrator/components/com_hwdmediashare/views/linkedgroups/tmpl/default_manage.php
@@ -66,8 +66,6 @@ $listDirn  = $this->escape($this->state->get('list.direction'));
 	<div>
 		<input type="hidden" name="task" value="" />
 		<input type="hidden" name="boxchecked" value="0" />
-		<input type="hidden" name="filter_order" value="<?php echo $listOrder; ?>" />
-		<input type="hidden" name="filter_order_Dir" value="<?php echo $listDirn; ?>" />
 		<?php echo JHtml::_('form.token'); ?>
 	</div>
 </form>
