03bb20f2db7b4b8274c30518f867f00de2a83d13
Dave Horsfall <dave@horsfall.eu>
2014-06-19 13:05:08 +0100

    View template cleanup.

--- administrator/components/com_hwdmediashare/views/groupmembers/tmpl/default_manage.php
+++ administrator/components/com_hwdmediashare/views/groupmembers/tmpl/default_manage.php
@@ -73,8 +73,6 @@ if ($saveOrder)
 	<div>
 		<input type="hidden" name="task" value="" />
 		<input type="hidden" name="boxchecked" value="0" />
-		<input type="hidden" name="filter_order" value="<?php echo $listOrder; ?>" />
-		<input type="hidden" name="filter_order_Dir" value="<?php echo $listDirn; ?>" />
 		<?php echo JHtml::_('form.token'); ?>
 	</div>
 </form>
