d759850579df94644d4db15dd2650eadfe74281e
Dave Horsfall <dave@horsfall.eu>
2014-06-20 10:40:24 +0100

    Updated comment blocks.

--- administrator/components/com_hwdmediashare/controllers/maintenance.raw.php
+++ administrator/components/com_hwdmediashare/controllers/maintenance.raw.php
@@ -15,8 +15,11 @@ class HwdMediaShareControllerMaintenance extends JControllerLegacy
         /**
 	 * Proxy for getModel.
 	 *
-	 * @access	public
-         * @return      object      The model.
+	 * @access  public
+	 * @param   string  $name    The model name. Optional.
+	 * @param   string  $prefix  The class prefix. Optional.
+	 * @param   array   $config  Configuration array for model. Optional.          
+         * @return  object  The model.
 	 */               
 	public function getModel($name = 'Maintenance', $prefix = 'hwdMediaShareModel', $config = array())
 	{
