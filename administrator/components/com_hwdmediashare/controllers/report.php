fb1704c1d1c055e30b019c4e689cb99e35fdf129
Dave Horsfall <dave@horsfall.eu>
2014-06-20 10:39:25 +0100

    Updated comment blocks.

--- administrator/components/com_hwdmediashare/controllers/report.php
+++ administrator/components/com_hwdmediashare/controllers/report.php
@@ -23,8 +23,11 @@ class hwdMediaShareControllerReport extends JControllerAdmin
         /**
 	 * Proxy for getModel.
 	 *
-	 * @access	public
-         * @return      object      The model.
+	 * @access  public
+	 * @param   string  $name    The model name. Optional.
+	 * @param   string  $prefix  The class prefix. Optional.
+	 * @param   array   $config  Configuration array for model. Optional.          
+         * @return  object  The model.
 	 */
 	public function getModel($name = 'Report', $prefix = 'hwdMediaShareModel', $config = array())
 	{
