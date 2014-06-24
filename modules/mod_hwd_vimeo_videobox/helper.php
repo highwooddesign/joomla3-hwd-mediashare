b1ab4c1ab42700849493d14e99329efd6cea7a1e
Dave Horsfall <dave@horsfall.eu>
2014-06-24 15:14:22 +0100

    Standardise Vimeo videoBox.

---
+++ modules/mod_hwd_vimeo_videobox/helper.php
@@ -0,0 +1,206 @@
+<?php
+/**
+ * @package     Joomla.site
+ * @subpackage  Module.mod_hwd_vimeo_videobox
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
+ */
+
+defined('_JEXEC') or die;
+
+class modHwdVimeoVideoBoxHelper extends JObject
+{
+        /**
+	 * Class constructor.
+	 *
+	 * @access	public
+	 * @param       array       $module     The module object.
+	 * @param       array       $params     The module parameters object.
+         * @return      void
+	 */       
+	public function __construct($module, $params)
+	{
+                // Load caching.
+                $cache = JFactory::getCache();
+                $cache->setCaching(1);
+
+                JLoader::register('JHtmlString', JPATH_LIBRARIES.'/joomla/html/html/string.php');
+
+                // Get data.
+                $this->module = $module;                
+                $this->params = $params;                
+                $this->items = $cache->call(array($this, 'getItems'), $params);
+
+                // Add assets to the head tag.
+                $this->addHead();
+	}
+
+        /**
+	 * Method to add assets to the head.
+	 *
+	 * @access	public
+         * @return      void
+	 */         
+	public function addHead()
+	{
+                JHtml::_('bootstrap.tooltip');
+                $doc = JFactory::getDocument();
+                $doc->addScript(JURI::root() . 'modules/mod_hwd_vimeo_videobox/js/jquery.magnific-popup.js');
+                $doc->addScript(JURI::root() . 'modules/mod_hwd_vimeo_videobox/js/aspect.js');
+                $doc->addStylesheet(JURI::root() . 'modules/mod_hwd_vimeo_videobox/css/magnific-popup.css');
+                $doc->addStylesheet(JURI::root() . 'modules/mod_hwd_vimeo_videobox/css/strapped.3.hwd.css');
+                
+                // Load layout CSS file.
+                if ($layout = explode(":", $this->params->get('layout')))
+                {
+                        if (isset($layout[1]) && file_exists(dirname(__FILE__) . '/css/' . $layout[1] . '.css'))
+                        {
+                                $doc->addStylesheet(JURI::root() . 'modules/mod_hwd_vimeo_videobox/css/' . $layout[1] . '.css');                
+                        }
+                }
+
+                $doc->addScriptDeclaration("
+jQuery.noConflict();
+(function( $ ) {
+  $(function() {
+    $(document).ready(function() {
+      $('.popup-title-" . $this->module->id . "').magnificPopup({ 
+        type: 'iframe',
+        mainClass: 'hwd-vimeo-popup',
+        gallery: {
+          enabled: true
+        }
+      }); 
+      $('.popup-thumbnail-" . $this->module->id . "').magnificPopup({ 
+        type: 'iframe',
+        mainClass: 'hwd-vimeo-popup',
+        gallery: {
+          enabled: true
+        }
+      });       
+    });
+  });
+})(jQuery);");                 
+        }
+
+        /**
+	 * Method to get a list of media items.
+	 *
+	 * @access	public
+         * @return      object      A list of media items.
+	 */         
+	public function getItems()
+	{
+                JLoader::register('JHtmlString', JPATH_LIBRARIES.'/joomla/html/html/string.php');
+                $feed = $this->getFeed();
+
+                $media = 'http://search.yahoo.com/mrss/';
+                
+                $counter = 0;
+                $data = new DOMDocument();
+                if ($data->load($feed))
+                {       
+                        foreach ($data->getElementsByTagName('video') as $video)
+                        {
+                                $obj                    = new stdClass();
+
+                                $obj->title             = ($video->getElementsByTagName('title')->item(0) ? $video->getElementsByTagName('title')->item(0)->nodeValue : '');
+                                $obj->id                = ($video->getElementsByTagName('id')->item(0) ? $video->getElementsByTagName('id')->item(0)->nodeValue : '');
+                                if ($this->get('params')->get('thumbnail_width') <= '200')
+                                {
+                                    $obj->thumbnail     = ($video->getElementsByTagName('thumbnail_medium')->item(0) ? $video->getElementsByTagName('thumbnail_medium')->item(0)->nodeValue : '');
+                                }                                
+                                else if ($this->get('params')->get('thumbnail_width') > '200')
+                                {
+                                    $obj->thumbnail     = ($video->getElementsByTagName('thumbnail_large')->item(0) ? $video->getElementsByTagName('thumbnail_large')->item(0)->nodeValue : '');
+                                }                                                                
+                                $obj->description	= ($video->getElementsByTagName('description')->item(0) ? JHtmlString::truncate($video->getElementsByTagName('description')->item(0)->nodeValue, 300, true, false) : '');
+                                $obj->category          = ($video->getElementsByTagNameNS($media, 'category')->item(0) ? $video->getElementsByTagNameNS($media, 'category')->item(0)->nodeValue : '');
+                                $obj->uploadDate        = ($video->getElementsByTagName('upload_date')->item(0) ? $video->getElementsByTagName('upload_date')->item(0)->nodeValue : '');
+                                $obj->duration          = ($video->getElementsByTagName('duration')->item(0) ? $video->getElementsByTagName('duration')->item(0)->nodeValue : '');
+                                $obj->views             = ($video->getElementsByTagName('stats_number_of_plays')->item(0) ? $video->getElementsByTagName('stats_number_of_plays')->item(0)->nodeValue : '');
+                                $obj->tags              = ($video->getElementsByTagName('tags')->item(0) ? $video->getElementsByTagName('tags')->item(0)->nodeValue : '');
+                                $obj->userName          = ($video->getElementsByTagName('user_name')->item(0) ? $video->getElementsByTagName('user_name')->item(0)->nodeValue : '');
+                                $obj->userPic           = ($video->getElementsByTagName('user_portrait_small')->item(0) ? $video->getElementsByTagName('user_portrait_small')->item(0)->nodeValue : '');
+                                $obj->likes             = ($video->getElementsByTagName('stats_number_of_likes')->item(0) ? $video->getElementsByTagName('stats_number_of_likes')->item(0)->nodeValue : '');
+                                
+                                $items[] = $obj;
+                                $counter++;
+
+                                if ($counter >= $this->get('params')->get('count'))
+                                {
+                                    break;
+                                }
+                        }                         
+                }                        
+		return $items;                              
+	}
+        
+        /**
+	 * Method to get the feed URI.
+	 *
+	 * @access	public
+         * @return      string      The feed URI
+	 */          
+	public function getFeed()
+	{  
+                // Set a default feed.
+                $feed = 'http://vimeo.com/api/v2/channel/staffpicks/videos.xml';
+
+                switch ($this->get('params')->get('source'))
+                {
+                    case 'staff_picks':
+                        $feed = 'http://vimeo.com/api/v2/channel/staffpicks/videos.xml';
+                        break;
+                    case 'channel':
+                        $feed = 'http://vimeo.com/api/v2/channel/'.$this->get('params')->get('channel').'/videos.xml';
+                        break;
+                    case 'user':
+                        $feed = 'http://vimeo.com/api/v2/'.$this->get('params')->get('user').'/videos.xml';
+                        break;
+                    case 'group':
+                        $feed = 'http://vimeo.com/api/v2/group/'.$this->get('params')->get('group').'/videos.xml';
+                        break;
+                }
+  
+                return $feed;
+	}   
+        
+        /**
+	 * Method to convert an integer number of seconds into a timestamp.
+	 *
+	 * @access	public
+         * @param       integer     $seconds    The number of seconds.
+         * @return      void
+	 */         
+        public function secondsToTime($seconds)
+        {
+                // Extract hours.
+                $hours = floor($seconds / (60 * 60));
+
+                // Extract minutes.
+                $divisor_for_minutes = $seconds % (60 * 60);
+                $minutes = floor($divisor_for_minutes / 60);
+
+                // Extract the remaining seconds.
+                $divisor_for_seconds = $divisor_for_minutes % 60;
+                $seconds = ceil($divisor_for_seconds);
+
+                // Prepent seconds with zero if necessary.
+                if ($seconds < 10)
+                {
+                        $seconds = '0'.$seconds;
+                }
+
+                if ($hours > 0)
+                {
+                        return "$hours:$minutes:$seconds";
+                }
+                else
+                {
+                        return "$minutes:$seconds";
+                }
+        }        
+}
---
+++ modules/mod_hwd_vimeo_videobox/tmpl/grid.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @package     Joomla.site
+ * @subpackage  Module.mod_hwd_youtube_videobox
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
+ */
+
+defined('_JEXEC') or die;
+
+// Load additional <head> assets
+$doc = JFactory::getDocument();
+$doc->addStyleDeclaration('
+#photos {
+   -webkit-column-count: '.intval($helper->params->get('columns', 5)).';
+   -moz-column-count:    '.intval($helper->params->get('columns', 5)).';
+   column-count:         '.intval($helper->params->get('columns', 5)).';
+}');
+?>
+<div class="mod_hwd_vimeo_videobox_grid">
+<section id="photos">
+<?php foreach ($helper->items as $id => $item) : ?>
+<div class="block_grid"> 
+    <div class="media-item hasTooltip" title="<?php echo JHtml::tooltipText($item->title, JHtmlString::truncate($item->description, 300, true, false)); ?>">
+        <?php if ($item->duration > 0) :?>
+        <div class="media-duration">
+           <?php echo $helper->secondsToTime($item->duration); ?>
+        </div>
+        <?php endif; ?>
+        <a class="popup-thumbnail-<?php echo $module->id; ?>" href="http://player.vimeo.com/video/<?php echo $item->id; ?>?color=<?php echo $helper->params->get('color'); ?>&autoplay=<?php echo $helper->params->get('autoplay'); ?>&title=<?php echo $helper->params->get('title'); ?>" id="mb<?php echo $id; ?>" title="<?php echo htmlspecialchars($item->title); ?>">
+           <img src="<?php echo $item->thumbnail; ?>" border="0" alt="" title="" class="media-thumb"  />
+           <?php echo ($helper->params->get('icon', 1) ? '<span class="media-link-span"></span>' : ''); ?>
+        </a>
+    </div>
+</div>
+<?php endforeach; ?>
+</section>
+</div>
