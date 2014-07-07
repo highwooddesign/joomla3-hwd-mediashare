a8e67a0df0dde4f98ee9bef7108cd8010b86831d
Dave Horsfall <dave@horsfall.eu>
2014-07-07 10:37:59 +0100

    Updated mod_media_activities

--- modules/mod_media_activities/tmpl/default.php
+++ modules/mod_media_activities/tmpl/default.php
@@ -1,25 +1,15 @@
 <?php
 /**
- * @version    $Id: default.php 1331 2013-03-20 10:43:13Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2011 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      15-Apr-2011 10:13:15
+ * @package     Joomla.site
+ * @subpackage  Module.mod_media_activites
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
 
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
-
-$application = JFactory::getApplication();
-$user = JFactory::getUser();
-JHtml::_('behavior.framework', true);
-JHtml::_('behavior.tooltip');
-
-hwdMediaShareFactory::load('activities');
-$act = hwdMediaShareActivities::getInstance();
+defined('_JEXEC') or die;
 ?>
-<!-- Module Container -->
 <div class="hwd-module">
-    <div class="categories-list"> <?php echo $act->getActivities($items); ?> </div>            
+  <?php echo JLayoutHelper::render('activities_list', $helper, JPATH_ROOT.'/components/com_hwdmediashare/libraries/layouts'); ?>
 </div>
