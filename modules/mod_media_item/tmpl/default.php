d72fce2d6b3aca73c6c46a74d2ad4b75d3b548c7
Dave Horsfall <dave@horsfall.eu>
2014-07-07 12:19:40 +0100

    Updated mod_media_item

--- modules/mod_media_item/tmpl/default.php
+++ modules/mod_media_item/tmpl/default.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package     Joomla.site
- * @subpackage  Module.mod_media_media
+ * @subpackage  Module.mod_media_item
  *
  * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
  * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
@@ -9,17 +9,14 @@
  */
 
 defined('_JEXEC') or die;
-
-$app = JFactory::getApplication();
-$menu = $app->getMenu();
 ?>
 <div class="hwd-module">
   <div class="media-details-view">
-    <?php if (empty($helper->item)) : ?>
+    <?php if (empty($helper->item)): ?>
       <div class="alert alert-no-items">
         <?php echo JText::_('COM_HWDMS_NOTHING_TO_SHOW'); ?>
       </div>
-    <?php else : ?>
+    <?php else: ?>
       <?php echo JLayoutHelper::render('mediaitem_display', $helper, JPATH_ROOT.'/components/com_hwdmediashare/libraries/layouts'); ?>
     <?php endif; ?>
   </div> 
