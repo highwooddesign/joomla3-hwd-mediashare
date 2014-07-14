a4a44dd687c224c91ee02e56da6df9befb0e8904
Dave Horsfall <dave@horsfall.eu>
2014-07-14 16:24:39 +0100

    Commented up library.

--- components/com_hwdmediashare/libraries/category.php
+++ components/com_hwdmediashare/libraries/category.php
@@ -1,42 +1,46 @@
 <?php
 /**
- * @version    SVN $Id: category.php 1352 2013-04-09 13:05:49Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2011 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      15-Apr-2011 10:13:15
- */
-
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
-
-/**
- * hwdMediaShare framework categories class
+ * @package     Joomla.site
+ * @subpackage  Component.hwdmediashare
  *
- * @package hwdMediaShare
- * @since   0.1
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
-class hwdMediaShareCategory
+
+defined('_JEXEC') or die;
+
+class hwdMediaShareCategory extends JObject
 {
+	/**
+	 * The element type to use with this library.
+         * 
+         * @access      public
+	 * @var         string
+	 */
+	public $elementType = 1;
+        
 	/**
 	 * Class constructor.
 	 *
-	 * @param   array  $config  A configuration array including optional elements.
-	 *
-	 * @since   0.1
+	 * @access  public
+	 * @param   mixed  $properties  Either and associative array or another
+	 *                              object to set the initial properties of the object.
+         * @return  void
 	 */
-	public function __construct($config = array())
+	public function __construct($properties = null)
 	{
+		parent::__construct($properties);
 	}
 
 	/**
 	 * Returns the hwdMediaShareCategory object, only creating it if it
 	 * doesn't already exist.
 	 *
-	 * @return  hwdMediaShareCategory A hwdMediaShareCategory object.
-	 * @since   0.1
-	 */
+	 * @access  public
+         * @static
+	 * @return  hwdMediaShareCategory Object.
+	 */ 
 	public static function getInstance()
 	{
 		static $instance;
@@ -51,204 +55,254 @@ class hwdMediaShareCategory
 	}
         
 	/**
-	 * Method to save categories
-         * 
-         * @since   0.1
-	 **/
-        function save($params)
-        {
-                $db =& JFactory::getDBO();
-                $user = & JFactory::getUser();
-                $date =& JFactory::getDate();
-
-                JTable::addIncludePath(JPATH_ADMINISTRATOR.'/components/com_hwdmediashare/tables');
-                $row =& JTable::getInstance('CategoryMap', 'hwdMediaShareTable');
-
-                $query = "
-                      DELETE
-                        FROM ".$db->quoteName('#__hwdms_category_map')."
-                        WHERE ".$db->quoteName('element_id')." = ".$db->quote($params->elementId)."
-                        AND ".$db->quoteName('element_type')." = ".$db->quote($this->elementType)."
-                      ";
-
-                $db->setQuery($query);
-                if (!$db->query() && $config->getValue( 'debug' ))
+	 * Method to save categories.
+	 *
+	 * @access  public
+	 * @param   array    $pks    A list of the category keys to assign.
+	 * @param   integer  $value  The primary key of the element.
+	 * @return  hwdMediaShareCategory Object.
+	 */ 
+        public function save($pks, $value = 0)
+	{
+		// Initialiase variables.
+                $db = JFactory::getDBO();
+                $user = JFactory::getUser();
+                $date = JFactory::getDate();
+                
+		// Sanitize the ids.
+		$value = (int) $value;
+		$pks = (array) $pks;
+		JArrayHelper::toInteger($pks);
+                
+                if (!$value)
                 {
-                        $app->enqueueMessage(nl2br($db->getErrorMsg()),'error');
+			$this->setError(JText::_('COM_HWDMS_ERROR_NOAUTHORISED'));
+			return false;
                 }
 
-                // Loop over categories assigned to elementid
-                for ($i=0, $n=count($params->categoryId); $i < $n; $i++)
-                {
-                        $row =& JTable::getInstance('CategoryMap', 'hwdMediaShareTable');
+                // Remove the existing category map.
+                $query = $db->getQuery(true);
 
-                        // Create an object to bind to the database
+                $conditions = array(
+                    $db->quoteName('element_type') . ' = ' . $db->quote($this->elementType), 
+                    $db->quoteName('element_id') . ' = ' . $db->quote($pk), 
+                );
+                
+                $query->delete($db->quoteName('#__hwdms_category_map'));
+                $query->where($conditions);
+                try
+                {
+                        $db->setQuery($query);
+                        $result = $db->query();
+                }
+                catch (Exception $e)
+                {
+                        $this->setError($e->getMessage());
+                        return false;
+                }
+   
+		foreach ($pks as $i => $pk)
+		{
+                        if (empty($pk))
+                        {
+                                $this->setError(JText::_('COM_HWDMS_NO_ITEM_SELECTED'));
+                                return false;
+                        }
+
+                        JTable::addIncludePath(JPATH_ADMINISTRATOR.'/components/com_hwdmediashare/tables');
+                        $table = JTable::getInstance('CategoryMap', 'hwdMediaShareTable');    
+
+                        // Create an object to bind to the database.
                         $object = new StdClass;
-                        $object->id = null;
-                        $object->element_type = (int) $this->elementType;
-                        $object->element_id = (int) $params->elementId;
-                        $object->category_id = (int) $params->categoryId[$i];
-                        $object->created_user_id = (int) $user->id;
-                        $object->created = $date->format('Y-m-d H:i:s');
+                        $object->element_type = $this->elementType;
+                        $object->element_id = $value;
+                        $object->category_id = $pk;
+                        $object->created_user_id = $user->id;
+                        $object->created = $date->toSql();
 
-                        if (!$row->bind($object))
+                        // Attempt to save the details to the database.
+                        if (!$table->save($object))
                         {
-                                return JError::raiseWarning( 500, $row->getError() );
+                                $this->setError($table->getError());
+                                return false;
                         }
 
-                        if (!$row->store())
-                        {
-                                JError::raiseError(500, $row->getError() );
-                        }
-                }
-                return true;
+		}
+
+		return true;
         }
 	/**
-	 * Method to save an individual category to an item
-         * 
-         * @since   0.1
-	 **/
-        function saveIndividual($params)
+	 * Method to save an individual category to an item.
+	 *
+	 * @access  public
+	 * @param   integer  $pk     The primary category key to assign.
+	 * @param   integer  $value  The primary key of the element.
+	 * @return  hwdMediaShareCategory Object.
+	 */ 
+        public function saveIndividual($pk, $value = 0)
         {
-                $db =& JFactory::getDBO();
-                $user = & JFactory::getUser();
-                $date =& JFactory::getDate();
+		// Initialiase variables.
+                $db = JFactory::getDBO();
+                $user = JFactory::getUser();
+                $date = JFactory::getDate();
                 
-                JTable::addIncludePath(JPATH_ADMINISTRATOR.'/components/com_hwdmediashare/tables');
-                $row =& JTable::getInstance('CategoryMap', 'hwdMediaShareTable');
+		// Sanitize the ids.
+		$value = (int) $value;
+		$pk = (int) $pk;               
 
-                $query = "
-                      SELECT COUNT(*)
-                        FROM ".$db->quoteName('#__hwdms_category_map')."
-                        WHERE ".$db->quoteName('element_id')." = ".$db->quote($params->elementId)."
-                        AND ".$db->quoteName('element_type')." = ".$db->quote($params->elementType)."
-                        AND ".$db->quoteName('category_id')." = ".$db->quote($params->categoryId)."
-                      ";
-
-                $db->setQuery($query);
-                $result = $db->loadResult();
-
-                // Loop over categories assigned to elementid
-                if($result == 0)
+                if (!$value)
                 {
-                        $row =& JTable::getInstance('CategoryMap', 'hwdMediaShareTable');
+			$this->setError(JText::_('COM_HWDMS_ERROR_NOAUTHORISED'));
+			return false;
+                }
 
-                        // Create an object to bind to the database
+                $query = $db->getQuery(true)
+                        ->select('COUNT(*)')
+                        ->from('#__hwdms_category_map')
+                        ->where('element_type = ' . $db->quote($this->elementType))
+                        ->where('element_id = ' . $db->quote($value))
+                        ->where('category_id = ' . $db->quote($pk));
+                try
+                {                
+                        $db->setQuery($query);
+                        $result = $db->loadResult();
+                }
+                catch (Exception $e)
+                {
+                        $this->setError($e->getMessage());
+                        return false;
+                }
+
+                if(!$result)
+                {
+                        JTable::addIncludePath(JPATH_ADMINISTRATOR.'/components/com_hwdmediashare/tables');
+                        $table = JTable::getInstance('CategoryMap', 'hwdMediaShareTable');    
+
+                        // Create an object to bind to the database.
                         $object = new StdClass;
-                        $object->id = null;
-                        $object->element_type = $params->elementType;
-                        $object->element_id = $params->elementId;
-                        $object->category_id = $params->categoryId;
+                        $object->element_type = $this->elementType;
+                        $object->element_id = $value;
+                        $object->category_id = $pk;
                         $object->created_user_id = $user->id;
-                        $object->created = $date->format('Y-m-d H:i:s');
-                        
-                        if (!$row->bind($object))
-                        {
-                                return JError::raiseWarning( 500, $row->getError() );
-                        }
+                        $object->created = $date->toSql();
 
-                        if (!$row->store())
+                        // Attempt to save the details to the database.
+                        if (!$table->save($object))
                         {
-                                JError::raiseError(500, $row->getError() );
+                                $this->setError($table->getError());
+                                return false;
                         }
                 }
+
                 return true;
         }
         
 	/**
-	 * Method to get categories of an item
-         * 
-         * @since   0.1
-	 **/
-        function get($item)
+	 * Method to get categories of an item.
+	 *
+	 * @access  public
+	 * @param   integer $item   The item to check.
+	 * @return  array   An array of categories assigned to the item.
+	 */ 
+        public function load($item)
         {
-                // Create a new query object.
+		// Initialiase variables.
                 $db = JFactory::getDBO();
-                $query = $db->getQuery(true);
 		$user	= JFactory::getUser();
 		$groups	= implode(',', $user->getAuthorisedViewLevels());
 
-                // Get hwdMediaShare config
+                // Get HWD config.
                 $hwdms = hwdMediaShareFactory::getInstance();
                 $config = $hwdms->getConfig();
+
+                $query = $db->getQuery(true)
+                        ->select('c.id, c.title')
+                        ->from('#__hwdms_category_map AS cmap')
+                        ->join('LEFT', '#__categories AS c ON c.id = cmap.category_id')
+                        ->where($db->quoteName('cmap.element_id').' = '.$db->quote($item->id))
+                        ->where($db->quoteName('cmap.element_type').' = '.$db->quote($this->elementType))
+                        ->where($db->quoteName('c.id').' > 0'); // Make sure we don't get any invalid categories from a broken map
                 
-                $array = array();
-
-		// Select the required fields from the table.
-		$query->select('c.id, c.title');
-
-                // From the albums table
-                $query->from('#__hwdms_category_map AS cmap');
-
-		// Join over the categories.
-                $query->join('LEFT', '#__categories AS c ON c.id = cmap.category_id');
-
-                $query->where($db->quoteName('cmap.element_id').' = '.$db->quote($item->id));
-                $query->where($db->quoteName('cmap.element_type').' = '.$db->quote($this->elementType));
-                // Make sure we don't get any invalid categories
-                $query->where($db->quoteName('c.id').' > 0');
-
-                // Restrict based on access
-		if ($config->get('entice_mode') == 0)
+                // Restrict based on access.
+                if ($config->get('entice_mode') == 0)
                 {
                         $query->where('c.access IN ('.$groups.')');
-                }
-
-                //Filter by published category
+                }         
+                
+                // Filter by published category.
 		if ((!$user->authorise('core.edit.state', 'com_hwdmediashare')) && (!$user->authorise('core.edit', 'com_hwdmediashare')))
                 {
-			// Limit to published for people who can't edit or edit.state.
-			$published = 1;
+			$query->where('c.published IN (1)');
 		}
                 else
                 {
-			// Limit to published for people who can't edit or edit.state.
-			$published = array(0,1);
+                        $query->where('c.published IN (0,1)');
                 }
-		// Filter by state
-		if (is_array($published)) 
+
+                try
+                {                
+                        $db->setQuery($query);
+                        $result = $db->loadObjectList();
+                }
+                catch (Exception $e)
                 {
-			JArrayHelper::toInteger($published);
-			$published = implode(',', $published);
-			if ($published) 
-                        {
-                                $query->where('c.published IN ('.$published.')');
-			}
-		}
-                else if (is_numeric($published))
-                {
-			$query->where('c.published = '.(int) $published);
-		}
-                        
-                $db->setQuery($query);
-                $rows = $db->loadObjectList();
+                        $this->setError($e->getMessage());
+                        return false;
+                }
                 
-                $categories = JArrayHelper::toObject($rows);
-                $categories = $rows;
-                return $categories;
+                return $result;
         }
         
 	/**
-	 * Method to get categories of an item (in format for a form input)
-         * 
-         * @since   0.1
-	 **/
-        function getInput($item)
+	 * Method to get categories of an item (in format for a form input).
+	 *
+	 * @access  public
+	 * @param   integer $item   The item to check.
+	 * @return  array   An array of categories assigned to the item.
+	 */ 
+        public function getInputValue($item)
         {
-                // Get category array
-                $rows = hwdMediaShareCategory::get($item);
+                // Get categories.
+                $categories = $this->load($item);
 
                 $return = array();
-
-                // Loop over categories assigned to elementid
-                for ($i=0, $n=count($rows); $i < $n; $i++)
+                foreach ($categories as $category)
                 {
-                        if (!in_array(intval($rows[$i]->id), $return))
-                        {
-                                $return[] = intval($rows[$i]->id);
-                        }
+                        $return[] = (int) $category->id;
                 }
 
                 return $return;
         }
+        
+        /**
+	 * Method to render the HTML to display a list of categories.
+	 *
+	 * @access  public
+         * @static
+	 * @param   integer $item   The item.
+	 * @return  string  The HTML to display the categories.
+	 */ 
+	public static function renderCategories($item)
+	{            
+                if (!isset($item))
+                {
+                        return false;
+                }
+
+                $links = array();
+                
+                if (count($item->categories) > 0)
+                {
+                        foreach ($item->categories as $value)
+                        {
+                                $links[] = '<a href="'.JRoute::_(hwdMediaShareHelperRoute::getCategoryRoute($value->id)).'">' . $value->title . '</a>';
+                        }
+                }
+                else
+                {
+                        return false;
+                }             
+
+                return implode(", ", $links);
+	}        
 }
