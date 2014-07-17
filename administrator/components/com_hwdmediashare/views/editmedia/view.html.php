5f076a29824e658a1f60f5f994d4555ef8b34fcb
Dave Horsfall <dave@horsfall.eu>
2014-07-17 09:48:11 +0100

    Updated editmedia view class.

--- administrator/components/com_hwdmediashare/views/editmedia/view.html.php
+++ administrator/components/com_hwdmediashare/views/editmedia/view.html.php
@@ -19,10 +19,10 @@ class hwdMediaShareViewEditMedia extends JViewLegacy
 	protected $form;
         
 	/**
-	 * Display the view
+	 * Display the view.
 	 *
+	 * @access  public
 	 * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
-	 *
 	 * @return  void
 	 */
 	public function display($tpl = null)
@@ -32,11 +32,14 @@ class hwdMediaShareViewEditMedia extends JViewLegacy
 		$this->item	= $this->get('Item');
 		$this->form	= $this->get('Form');
 
+                // Import HWD libraries.
                 hwdMediaShareFactory::load('downloads');
                 hwdMediaShareFactory::load('files');
-                
+                hwdMediaShareFactory::load('media');
+
+                // Redirect new upload views to the upload page.
                 $isNew = $this->item->id == 0;
-                if ($isNew) JFactory::getApplication()->redirect(JRoute::_('index.php?option='.JRequest::getCmd('option').'&view=addmedia', false));
+                if ($isNew) JFactory::getApplication()->redirect(JRoute::_('index.php?option=com_hwdmediashare&view=addmedia', false));
 
                 // Check for errors.
                 if (count($errors = $this->get('Errors')))
@@ -48,25 +51,27 @@ class hwdMediaShareViewEditMedia extends JViewLegacy
 		$this->addToolbar();
 		$this->sidebar = JHtmlSidebar::render();
 
-		// Display the template
+		// Display the template.
 		parent::display($tpl);
+                
+		$document = JFactory::getDocument();
+		$document->addStyleSheet(JURI::root() . "media/com_hwdmediashare/assets/css/administrator.css");
 	}
 
 	/**
 	 * Add the page title and toolbar.
 	 *
+	 * @access  protected
 	 * @return  void
 	 */
 	protected function addToolBar()
 	{
 		JFactory::getApplication()->input->set('hidemainmenu', true);
 
+		$canDo		= hwdMediaShareHelper::getActions($this->item->id, 'media');
 		$user		= JFactory::getUser();
 		$isNew		= ($this->item->id == 0);
 		$checkedOut	= !($this->item->checked_out == 0 || $this->item->checked_out == $user->get('id'));
-
-		// Since we don't track these assets at the item level, use the category id.
-		$canDo		= hwdMediaShareHelper::getActions($this->item->id, 'media');
 
 		JToolBarHelper::title(JText::_('COM_HWDMS_EDIT_MEDIA'), 'video');
 
@@ -80,17 +85,26 @@ class hwdMediaShareViewEditMedia extends JViewLegacy
 		{
 			JToolbarHelper::save2new('editmedia.save2new');
 		}
-		// If CDN then show sync options.
+
+		// Show sync options for platform media.
 		if ($this->item->type == 6)
 		{
-                        // Load the HWDMediaShare language file
-                        $lang =& JFactory::getLanguage();
-                        $plugin = 'plg_hwdmediashare_'.$this->item->storage;
-                        $lang->load($plugin, JPATH_SITE.'/administrator', $lang->getTag());
+                        // Default language strings.
+                        $sync2platform = 'COM_HWDMS_SYNC_TO_PLATFORM';
+                        $sync2local = 'COM_HWDMS_SYNC_FROM_PLATFORM';
+                        
+                        $pluginPath = JPATH_ROOT.'/plugins/hwdmediashare/'.$this->item->storage.'/'.$this->item->storage.'.php';                                
+                        if (file_exists($pluginPath))
+                        {
+                                $lang = JFactory::getLanguage();
+                                $plugin = 'plg_hwdmediashare_'.$this->item->storage;
+                                $lang->load($plugin, JPATH_SITE.'/administrator', $lang->getTag());
+                                $sync2platform = 'COM_HWDMS_SYNC_TO_PLATFORM_'.$this->item->storage;
+                                $sync2local = 'COM_HWDMS_SYNC_FROM_PLATFORM_'.$this->item->storage;
+                        }
 
-                        JToolBarHelper::custom('editmedia.syncToCdn', 'database', 'database', 'COM_HWDMS_SYNC_TO_CDN', false);
-			JToolBarHelper::custom('editmedia.syncFromCdn', 'database', 'database', 'COM_HWDMS_SYNC_FROM_CDN', false);
-                        JToolBarHelper::divider();
+                        JToolBarHelper::custom('editmedia.sync2cdn', 'database', 'database', $sync2platform, false);
+			JToolBarHelper::custom('editmedia.sync2local', 'database', 'database', $sync2local, false);
 		}                
 		if (empty($this->item->id))
 		{
@@ -104,59 +118,21 @@ class hwdMediaShareViewEditMedia extends JViewLegacy
 		JToolbarHelper::divider();
 		JToolbarHelper::help('HWD', false, 'http://hwdmediashare.co.uk/learn/docs'); 
 	}
-
-	/**
-	 * Method to display the human readable file type.
-	 * @return  void
-	 */
-	public function getFileType($item)
-	{
-                hwdMediaShareFactory::load('files');
-                return hwdMediaShareFiles::getFileType($item);
-	}
-        
-	/**
-	 * Method to display the human readable media source (local, remote, etc).
-	 * @return  void
-	 */
-	public function getType($item)
-	{
-                hwdMediaShareFactory::load('media');
-                return hwdMediaShareMedia::getType($item);
-	}
-
-	/**
-	 * Method to display the human readable media type (audio, document, image, video).
-	 * @return  void
-	 */
-	public function getMediaType($item)
-	{
-                hwdMediaShareFactory::load('media');
-                return hwdMediaShareMedia::getMediaType($item);
-	}
         
 	/**
 	 * Method to display the relative path to a media file.
-	 * @return  void
+	 *
+	 * @access  public
+         * @param   object  $item   The media object
+	 * @return  string  The path.
 	 */
 	public function getPath($item)
 	{
-                hwdMediaShareFactory::load('files');
                 hwdMediaShareFiles::getLocalStoragePath();
                 $folders = hwdMediaShareFiles::getFolders($this->item->key);
                 $filename = hwdMediaShareFiles::getFilename($this->item->key,$item->file_type);
                 $ext = hwdMediaShareFiles::getExtension($this->item,$item->file_type);
                 $path = hwdMediaShareFiles::getPath($folders, $filename, $ext);
                 return str_replace(JPATH_SITE, '', $path);
-	}
-        
-	/**
-	 * Method to display the extension for a media file.
-	 * @return  void
-	 */
-	public function getExtension($item)
-	{
-                hwdMediaShareFactory::load('files');
-                return hwdMediaShareFiles::getExtension($this->item, $item->file_type);
 	}
 }
