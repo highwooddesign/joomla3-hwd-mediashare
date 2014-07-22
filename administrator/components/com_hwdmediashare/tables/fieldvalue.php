4ea309b2d23eb584d307717058acfada96ee054c
Dave Horsfall <dave@horsfall.eu>
2014-07-22 13:41:13 +0100

    Updated fieldvalue JTable class, and removed load method as this has been incorporated into the customfields library.

--- administrator/components/com_hwdmediashare/tables/fieldvalue.php
+++ administrator/components/com_hwdmediashare/tables/fieldvalue.php
@@ -10,50 +10,17 @@
 
 defined('_JEXEC') or die;
 
-// Import Joomla table library
-jimport('joomla.database.table');
-
 class hwdMediaShareTableFieldValue extends JTable
 {
-	var $id 		= null;
-	var $element_type	= null;
-	var $field_id	        = null;
-	var $value		= null;
-	var $access		= null;
-
 	/**
-	 * Constructor.
-	 * @return	void
-	 */
-	function __construct($db)
-	{
-                parent::__construct('#__hwdms_fields_values', 'id', $db);
-	}
-        
-	/**
-	 * Overloaded load function
+	 * Class constructor. Overridden to explicitly set the table and key fields.
 	 *
-	 * @param       int     $pk     primary key
-	 * @param       boolean $reset  reset data
-         * 
-	 * @return      boolean
-	 */
-	public function load($elementType, $elementId, $fieldId)
+	 * @access	public
+	 * @param       JDatabaseDriver  $db     JDatabaseDriver object.
+         * @return      void
+	 */ 
+	public function __construct($db)
 	{
-		$db = $this->getDBO();
-                
-		$query	= 'SELECT * FROM ' . $db->quoteName( '#__hwdms_fields_values' ) . ' '
-				. 'WHERE ' . $db->quoteName('field_id') . ' = ' . $db->Quote( $fieldId ) . ''
-                                . 'AND ' . $db->quoteName('element_type') . '=' . $db->Quote( $elementType ) . ''
-                                . 'AND ' . $db->quoteName('element_id') . '=' . $db->Quote( $elementId );
-		$db->setQuery($query);
-		if ($result = $db->loadObject())
-                {
-                        return $this->bind($result);
-                }
-                else
-                {
-                        return false;
-                }
+		parent::__construct('#__hwdms_fields_values', 'id', $db);
 	}
 }
