b981061c384e93e1dc33621989b63d9e2deda1bc
Dave Horsfall <dave@horsfall.eu>
2014-07-22 14:07:47 +0100

    Updated the customfield JTable overload.

--- administrator/components/com_hwdmediashare/tables/customfield.php
+++ administrator/components/com_hwdmediashare/tables/customfield.php
@@ -10,66 +10,46 @@
 
 defined('_JEXEC') or die;
 
-// Import Joomla table library
-jimport('joomla.database.table');
-
 class hwdMediaShareTableCustomField extends JTable
 {
 	/**
-	 * Constructor.
-	 * @return	void
-	 */
-	function __construct($db)
+	 * Class constructor. Overridden to explicitly set the table and key fields.
+	 *
+	 * @access	public
+	 * @param       JDatabaseDriver  $db     JDatabaseDriver object.
+         * @return      void
+	 */  
+	public function __construct($db)
 	{
 		parent::__construct('#__hwdms_fields', 'id', $db);
 	}
         
 	/**
-	 * Overloaded bind function
+	 * Method to bind an associative array or object to the JTable instance.
 	 *
-	 * @param   array  $array   Named array to bind
-	 * @param   mixed  $ignore  An optional array or space separated list of properties to ignore while binding.
-	 *
-	 * @return  mixed  Null if operation was satisfactory, otherwise returns an error
+         * @access  public
+	 * @param   mixed   $src     An associative array or object to bind to the JTable instance.
+	 * @param   mixed   $ignore  An optional array or space separated list of properties to ignore while binding.
+	 * @return  boolean True on success.
+	 * @link    http://docs.joomla.org/JTable/bind
+	 * @throws  InvalidArgumentException
 	 */
-	public function bind($array, $ignore = '')
-	{
-		$data = JFactory::getApplication()->input->get('params', array(), 'array');
+	public function bind($src, $ignore = '')
+	{        
+                // Initialise variables.
+                $app = JFactory::getApplication();
+                
+                // Load params from the request.
+		$data = $app->input->get('params', array(), 'array');
 
 		// Convert the params fields to a string.
                 if (isset($data) && is_array($data) && count($data) > 0)
 		{
 			$registry = new JRegistry;
 			$registry->loadArray($data);
-			$array['params'] = (string) $registry;
-		}               
-		return parent::bind($array, $ignore);
-	}
-
-	/**
-	 * Overloaded load function
-	 *
-	 * @param       int     $pk     primary key
-	 * @param       boolean $reset  reset data
-         * 
-	 * @return      boolean
-	 */
-	public function load($pk = null, $reset = true) 
-	{
-		if (parent::load($pk, $reset)) 
-		{
-                        // Convert the params string to an array.
-                        if (property_exists($this, 'params'))
-                        {
-                                $registry = new JRegistry;
-                                $registry->loadString($this->params);
-                                $this->params = $registry;
-                        }                    
-			return true;
-		}
-		else
-		{
-			return false;
-		}
+			$src['params'] = (string) $registry;
+		}       
+                
+		return parent::bind($src, $ignore);
 	}
 }
