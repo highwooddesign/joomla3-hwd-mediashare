8211c13bb947531cff65002a272e506e73ae7734
Dave Horsfall <dave@horsfall.eu>
2014-07-25 13:53:01 +0100

    Update view class.

--- components/com_hwdmediashare/views/users/view.html.php
+++ components/com_hwdmediashare/views/users/view.html.php
@@ -18,14 +18,16 @@ class hwdMediaShareViewUsers extends JViewLegacy
         
 	public $params;
         
+	public $filterForm;   
+        
 	/**
-	 * Display the view
+	 * Display the view.
 	 *
+	 * @access  public
 	 * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
-	 *
 	 * @return  void
 	 */
-	function display($tpl = null)
+	public function display($tpl = null)
 	{
                 // Get data from the model.
                 $this->items = $this->get('Items');
@@ -34,12 +36,15 @@ class hwdMediaShareViewUsers extends JViewLegacy
 		$this->params = $this->state->params;
                 $this->filterForm = $this->get('FilterForm');
 
-                // Load libraries.
+                // Register classes.
                 JLoader::register('JHtmlHwdIcon', JPATH_COMPONENT . '/helpers/icon.php');
                 JLoader::register('JHtmlHwdDropdown', JPATH_COMPONENT . '/helpers/dropdown.php');
                 JLoader::register('JHtmlString', JPATH_LIBRARIES.'/joomla/html/html/string.php');
+                
+                // Import HWD libraries.                
                 hwdMediaShareFactory::load('files');
                 hwdMediaShareFactory::load('downloads');
+                hwdMediaShareFactory::load('thumbnails');
                 hwdMediaShareFactory::load('media');
 		hwdMediaShareFactory::load('utilities');
                 
@@ -63,8 +68,9 @@ class hwdMediaShareViewUsers extends JViewLegacy
 	}
         
 	/**
-	 * Prepares the document
+	 * Prepares the document.
 	 *
+         * @access  protected
 	 * @return  void
 	 */
 	protected function _prepareDocument()
@@ -81,37 +87,36 @@ class hwdMediaShareViewUsers extends JViewLegacy
                 if ($this->params->get('list_thumbnail_aspect') != 0) $this->document->addStyleSheet(JURI::base( true ).'/media/com_hwdmediashare/assets/css/aspect.css');
                 if ($this->params->get('list_thumbnail_aspect') != 0) $this->document->addScript(JURI::base( true ).'/media/com_hwdmediashare/assets/javascript/aspect.js');
 
-		// Because the application sets a default page title,
-		// we need to get it from the menu item itself
+		// Define the page title and headings. 
 		$menu = $menus->getActive();
 		if ($menu)
 		{
-			$this->params->def('page_heading', $this->params->get('page_title', $menu->title));
+                        $title = $this->params->get('page_title');
+                        $heading = $this->params->get('page_heading', JText::_('COM_HWDMS_USER_CHANNELS'));
 		}
 		else
 		{
-			$this->params->def('page_heading', JText::_('COM_HWDMS_USER_CHANNELS'));
+                        $title = JText::_('COM_HWDMS_USER_CHANNELS');
+                        $heading = JText::_('COM_HWDMS_USER_CHANNELS');
 		}
 
-		$title = $this->params->get('page_title', '');
+                $this->params->set('page_title', $title);
+                $this->params->set('page_heading', $heading);
 
-		// If the menu item does not concern this item
+		// If the menu item does not concern this view then add a breadcrumb.
 		if ($menu && ($menu->query['option'] != 'com_hwdmediashare' || $menu->query['view'] != 'users'))
 		{       
-			// If this is not a single item menu item, set the page title to the item title
-			$title = JText::_('COM_HWDMS_USER_CHANNELS');    
-                        
-                        // Breadcrumb support
+                        // Breadcrumb support.
 			$path = array(array('title' => JText::_('COM_HWDMS_USER_CHANNELS'), 'link' => ''));
-                                                
+                                               
 			$path = array_reverse($path);
 			foreach($path as $item)
 			{
 				$pathway->addItem($item['title'], $item['link']);
-			}                    
+			}                   
 		}
                 
-		// Check for empty title and add site name if param is set
+		// Check for empty title and add site name when configured.
 		if (empty($title))
                 {
 			$title = $app->getCfg('sitename');
@@ -125,6 +130,7 @@ class hwdMediaShareViewUsers extends JViewLegacy
 			$title = JText::sprintf('JPAGETITLE', $title, $app->getCfg('sitename'));
 		}
                 
+                // Set metadata.
 		$this->document->setTitle($title);
 
                 if ($this->params->get('meta_desc'))
