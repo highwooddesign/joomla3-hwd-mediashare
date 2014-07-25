4694df4c9441b70b7bc05c9f915eede44cbe3f31
Dave Horsfall <dave@horsfall.eu>
2014-07-25 09:22:50 +0100

    Comments.

--- administrator/components/com_hwdmediashare/models/playlists.php
+++ administrator/components/com_hwdmediashare/models/playlists.php
@@ -179,7 +179,7 @@ class hwdMediaShareModelPlaylists extends JModelList
 
 		$query->order($db->escape($listOrder.' '.$listDirn));
 
-                // Group over the ID to prevent duplicates.
+                // Group over the key to prevent duplicates.
                 $query->group('a.id');
                 
                 // Additional filters
