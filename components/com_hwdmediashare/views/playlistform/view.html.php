48e9362dc6b2ede3537597f48e84b405a074fa39
Dave Horsfall <dave@horsfall.eu>
2014-07-25 14:25:26 +0100

    Update view class.

--- components/com_hwdmediashare/views/playlistform/view.html.php
+++ components/com_hwdmediashare/views/playlistform/view.html.php
@@ -19,14 +19,17 @@ class hwdMediaShareViewPlaylistForm extends JViewLegacy
 	protected $form;
 
 	/**
-	 * Display the view
+	 * Display the view.
 	 *
+	 * @access  public
 	 * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
-	 *
 	 * @return  void
-	 */       
+	 */
 	public function display($tpl = null)
 	{
+		// Initialise variables.
+		$user = JFactory::getUser();
+                
                 // Get data from the model.
 		$this->state = $this->get('State');
 		$this->item = $this->get('Item');
@@ -34,10 +37,12 @@ class hwdMediaShareViewPlaylistForm extends JViewLegacy
                 $this->return_page = $this->get('ReturnPage');
 		$this->params = $this->state->params;
 
-                // Load libraries.
+                // Register classes.
                 JLoader::register('JHtmlHwdIcon', JPATH_COMPONENT . '/helpers/icon.php');
                 JLoader::register('JHtmlHwdDropdown', JPATH_COMPONENT . '/helpers/dropdown.php');
                 JLoader::register('JHtmlString', JPATH_LIBRARIES.'/joomla/html/html/string.php');
+                
+                // Import HWD libraries.                
                 hwdMediaShareFactory::load('files');
                 hwdMediaShareFactory::load('downloads');
                 hwdMediaShareFactory::load('media');
@@ -46,10 +51,7 @@ class hwdMediaShareViewPlaylistForm extends JViewLegacy
                 $this->utilities = hwdMediaShareUtilities::getInstance();
 		$this->pageclass_sfx = htmlspecialchars($this->params->get('pageclass_sfx'));
                 $this->isNew = $this->item->id == 0;
-                
-		// Initialise variables.
-		$user = JFactory::getUser();
-                
+
                 // Check access.
 		if (empty($this->item->id))
 		{
@@ -79,17 +81,13 @@ class hwdMediaShareViewPlaylistForm extends JViewLegacy
 	}
         
 	/**
-	 * Prepares the document
+	 * Prepares the document.
 	 *
+         * @access  protected
 	 * @return  void
 	 */
 	protected function _prepareDocument()
 	{
-		$app = JFactory::getApplication();
-		$menus = $app->getMenu();
-		$pathway = $app->getPathway();
-		$title = null;
-
                 // Add page assets.
                 JHtml::_('bootstrap.framework');
                 $this->document->addStyleSheet(JURI::base( true ).'/media/com_hwdmediashare/assets/css/hwd.css');
@@ -97,6 +95,7 @@ class hwdMediaShareViewPlaylistForm extends JViewLegacy
                 if ($this->params->get('list_thumbnail_aspect') != 0) $this->document->addStyleSheet(JURI::base( true ).'/media/com_hwdmediashare/assets/css/aspect.css');
                 if ($this->params->get('list_thumbnail_aspect') != 0) $this->document->addScript(JURI::base( true ).'/media/com_hwdmediashare/assets/javascript/aspect.js');
                 
+		// Define the page title and headings. 
 		if ($this->isNew)
 		{
 			$this->params->set('page_heading', JText::_('COM_HWDMS_NEW_PLAYLIST'));
@@ -105,7 +104,6 @@ class hwdMediaShareViewPlaylistForm extends JViewLegacy
 		{
 			$this->params->set('page_heading', JText::sprintf('COM_HWDMS_EDIT_PLAYLISTX', $this->escape($this->item->title)));
 		}
-                
 		$this->document->setTitle($this->params->get('page_heading'));  
 	}
 
