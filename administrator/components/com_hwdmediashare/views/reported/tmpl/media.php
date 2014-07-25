52273fbef1539643755b6a9ecd064c008b9b8975
Dave Horsfall <dave@horsfall.eu>
2014-07-25 12:12:42 +0100

    Updated the media layout template for the reported view.

--- administrator/components/com_hwdmediashare/views/reported/tmpl/media.php
+++ administrator/components/com_hwdmediashare/views/reported/tmpl/media.php
@@ -1,105 +1,98 @@
 <?php
 /**
- * @version    SVN $Id: media.php 1249 2013-03-08 14:24:48Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2012 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      01-Feb-2012 08:50:16
+ * @package     Joomla.administrator
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
 
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
 
-// load tooltip behavior
-JHtml::_('behavior.tooltip');
-JHtml::_('script','system/multiselect.js', false, true);
+$app = JFactory::getApplication();
 
-$listOrder	= $this->escape($this->state->get('list.ordering'));
-$listDirn	= $this->escape($this->state->get('list.direction'));
-$id             = $this->escape($this->state->get('filter.element_id'));
-?>
-<form action="<?php echo JRoute::_('index.php?option=com_hwdmediashare'); ?>" method="post" name="adminForm" id="adminForm">
-        <fieldset>
-		<div class="fltrt">
-                        <button type="button" onclick="Joomla.submitform('reported.delete', this.form);">
-                                <?php echo JText::_('JTOOLBAR_DELETE');?></button>
- 			<button type="button" onclick="<?php echo JRequest::getBool('refresh', 0) ? 'window.parent.location.href=window.parent.location.href;' : '';?>  window.parent.SqueezeBox.close();">
-				<?php echo JText::_('JCANCEL');?></button>
-		</div>
-                <div class="configuration">
-                        <?php echo JText::_('COM_HWDMS_REPORTED_MEDIA');?>
-                </div>
-        </fieldset>
-        <fieldset id="filter-bar">
-                <div class="filter-search fltlft">
-			<label class="filter-search-lbl" for="filter_search"><?php echo JText::_('JSEARCH_FILTER_LABEL'); ?></label>
-			<input type="text" name="filter_search" id="filter_search" value="<?php echo $this->escape($this->state->get('filter.search')); ?>" title="<?php echo JText::_('COM_HWDMS_SEARCH_IN_TITLE'); ?>" />
-			<button type="submit"><?php echo JText::_('JSEARCH_FILTER_SUBMIT'); ?></button>
-			<button type="button" onclick="document.id('filter_search').value='';this.form.submit();"><?php echo JText::_('JSEARCH_FILTER_CLEAR'); ?></button>
-		</div>
-	</fieldset>
-	<div class="clr"> </div>
-        <table class="adminlist">
-                <thead>
-                        <tr>
-                                <th width="20">
-                                        <input type="checkbox" name="toggle" value="" onclick="Joomla.checkAll(this);" />
-                                </th>                            
-                                <th width="20%">
-                                        <?php echo JHtml::_('grid.sort',  JText::_('COM_HWDMS_REPORTS'), 'a.report_id', $listDirn, $listOrder); ?>
-                                </th>
-                                <th> 
-                                        <?php echo JHtml::_('grid.sort', JText::_('COM_HWDMS_COMMENT'), 'a.description', $listDirn, $listOrder); ?>
-                                </th>
-                                <th width="20%">
-                                        <?php echo JHtml::_('grid.sort', JText::_('JGRID_HEADING_CREATED_BY'), 'a.user_id', $listDirn, $listOrder); ?>
-                                </th>
-                                <th width="5%">
-                                        <?php echo JHtml::_('grid.sort', JText::_('JDATE'), 'a.created', $listDirn, $listOrder); ?>
-                                </th>
-                           </tr>
-                </thead>
-                <tbody>
-                        <?php foreach($this->items as $i => $item):
-                                $owner =& JFactory::getUser($item->user_id);
-                                ?>
-                                <tr class="row<?php echo $i % 2; ?>">
-                                        <td>
-                                                <?php echo JHtml::_('grid.id', $i, $item->id); ?>
-                                        </td>
-                                        <td class="center">
-                                                <?php echo $this->getReportType($item); ?>
-                                        </td>
-                                        <td>
-                                                <span class="editlinktip hasTip" title="<?php echo JText::_('COM_HWDMS_DESCRIPTION'); ?>::<?php echo $this->escape($item->description); ?>" >
-                                                <?php echo $this->escape($item->description); ?>
-                                                </span>
-                                        </td>
-                                        <td class="center">
-                                                <?php echo $this->escape($owner->username); ?>
-                                        </td>
-                                        <td class="center nowrap">
-                                                <?php echo JHtml::_('date',$item->created, JText::_('DATE_FORMAT_LC4')); ?>
-                                        </td>
-                                </tr>
-                        <?php endforeach; ?>
-                </tbody>
-                <tfoot>
-                        <tr>
-                                <td colspan="6"><?php echo $this->pagination->getListFooter(); ?></td>
-                        </tr>
-                </tfoot>
-        </table>
-        <div>
-                <input type="hidden" name="task" value="" />
-                <input type="hidden" name="view" value="reported" />
-                <input type="hidden" name="boxchecked" value="0" />
-                <input type="hidden" name="tmpl" value="component" />
-                <input type="hidden" name="layout" value="<?php echo JRequest::getWord('layout'); ?>" />
-                <input type="hidden" name="id" value="<?php echo $id; ?>" />
+if ($app->isSite())
+{
+	JSession::checkToken('get') or die(JText::_('JINVALID_TOKEN'));
+}
+
+JHtml::addIncludePath(JPATH_COMPONENT.'/helpers/html');
+JHtml::_('bootstrap.tooltip');
+JHtml::_('behavior.framework', true);
+
+$listOrder = $this->escape($this->state->get('list.ordering'));
+$listDirn  = $this->escape($this->state->get('list.direction'));
+?>              
+<form action="<?php echo JRoute::_('index.php?option=com_hwdmediashare&view=reported&layout=media&tmpl=component&id=' . $app->input->get('id', 0, 'integer'));?>" method="post" name="adminForm" id="adminForm" class="form-inline">
+<?php if (!empty($this->sidebar)) : ?>
+	<div id="j-sidebar-container" class="span2">
+		<?php echo $this->sidebar; ?>
+	</div>
+	<div id="j-main-container" class="span10">
+<?php else : ?>
+	<div id="j-main-container">
+<?php endif;?>
+        <table class="table table-striped table-condensed">
+		<thead>
+			<tr>
+				<th class="title">
+					<?php echo JHtml::_('grid.sort', 'COM_HWDMS_REPORT_DETAILS', 'a.report_id', $listDirn, $listOrder); ?>                                    
+				</th>
+                                <th width="15%" class="center nowrap">
+                                        <?php echo JHtml::_('grid.sort', 'COM_HWDMS_REPORTED_BY', 'a.user_id', $listDirn, $listOrder); ?>
+                                </th>                                
+				<th width="5%" class="center nowrap">
+					<?php echo JHtml::_('grid.sort', 'JDATE', 'a.created', $listDirn, $listOrder); ?>
+				</th>
+			</tr>
+		</thead>
+		<tfoot>
+			<tr>
+				<td colspan="3">
+					<?php echo $this->pagination->getListFooter(); ?>
+				</td>
+			</tr>
+		</tfoot>
+		<tbody>
+		<?php foreach ($this->items as $i => $item) : ?>
+			<tr class="row<?php echo $i % 2; ?>">
+				<td>  
+                                        <?php echo JHtml::_('HwdGrid.id', $i, $item->id, 'cb', false, 'cid', 'hide'); ?>
+                                        <div class="pull-right label"><?php echo $this->getReportType($item); ?></div>
+                                        <p><a href="<?php echo JRoute::_('index.php?option=com_hwdmediashare&task=editmedia.edit&id=' . $item->element_id); ?>" title="<?php echo JText::_('JACTION_EDIT'); ?>" target="_blank"><?php echo $this->escape($item->title); ?></a></p>
+                                        <div class="small">
+                                                <blockquote><?php echo $this->escape($item->description); ?></blockquote> 
+                                        </div>  
+                                        <div class="btn-group">
+                                                <a href="javascript:void(0);" onclick="return listItemTask('cb<?php echo $i; ?>', 'report.dismiss')" class="btn btn-mini"><i class="icon-trash"></i> Dismiss Report</a>                                            
+                                                <a href="javascript:void(0);" onclick="return listItemTask('cb<?php echo $i; ?>', 'report.remove')" class="btn btn-mini"><i class="icon-trash"></i> Remove Media</a>                                            
+                                                <a href="<?php echo JRoute::_('index.php?option=com_hwdmediashare&task=editmedia.edit&id=' . $item->element_id); ?>" target="_blank" class="btn btn-mini"><i class="icon-eye"></i> View Media</a>                                            
+                                        </div>  
+				</td>
+				<td class="center">
+					<?php echo $this->escape($item->author); ?>
+				</td>
+				<td class="center nowrap">
+					<?php echo JHtml::_('date.relative', $item->created); ?>
+				</td>
+			</tr>
+			<?php endforeach; ?>
+		</tbody>
+	</table>
+        <?php if (empty($this->items)) : ?>
+                <div class="alert alert-no-items">
+                        <?php echo JText::_('JGLOBAL_NO_MATCHING_RESULTS'); ?>
+                </div>                                 
+        <?php endif; ?>             
+	<div>
+		<input type="hidden" name="task" value="" />
+		<input type="hidden" name="boxchecked" value="0" />
 		<input type="hidden" name="filter_order" value="<?php echo $listOrder; ?>" />
 		<input type="hidden" name="filter_order_Dir" value="<?php echo $listDirn; ?>" />
-                <?php echo JHtml::_('form.token'); ?>
-        </div>
+		<input type="hidden" name="return" value="<?php echo $this->return; ?>" />
+		<?php echo JHtml::_('form.token'); ?>
+	</div>
+	</div>
 </form>
+
