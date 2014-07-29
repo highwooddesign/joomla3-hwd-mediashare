c89b3f1e2fec7921abd2684c96b5537b7d59c5ca
Dave Horsfall <dave@horsfall.eu>
2014-07-29 11:59:00 +0100

    Updated view.

--- components/com_hwdmediashare/views/playlist/tmpl/default.php
+++ components/com_hwdmediashare/views/playlist/tmpl/default.php
@@ -12,8 +12,8 @@ defined('_JEXEC') or die;
 
 // Include the component HTML helpers.
 JHtml::addIncludePath(JPATH_COMPONENT_ADMINISTRATOR . '/helpers/html');
-JHtml::_('HwdPopup.form');
-JHtml::_('HwdPopup.page');
+JHtml::_('HwdPopup.iframe', 'form');
+JHtml::_('HwdPopup.iframe', 'page');
 
 $user = JFactory::getUser();
 $canEdit = ($user->authorise('core.edit', 'com_hwdmediashare.playlist.'.$this->playlist->id) || ($user->authorise('core.edit.own', 'com_hwdmediashare.playlist.'.$this->playlist->id) && ($this->playlist->created_user_id == $user->id)));
@@ -23,21 +23,21 @@ $canAddMedia = (($user->authorise('hwdmediashare.upload','com_hwdmediashare') ||
 $canManageMedia = ($this->playlist->created_user_id == $user->id);
 ?>
 <form action="<?php echo htmlspecialchars(JFactory::getURI()->toString()); ?>" method="post" name="adminForm" id="adminForm">
-  <div id="hwd-container"> <a name="top" id="top"></a>
+  <div id="hwd-container" class="<?php echo $this->pageclass_sfx;?>"> <a name="top" id="top"></a>
     <!-- Media Navigation -->
     <?php echo hwdMediaShareHelperNavigation::getInternalNavigation(); ?>
     <!-- Media Header -->
     <div class="media-header">
-      <?php if ($this->params->get('item_meta_title') != 'hide') :?>
-        <h2 class="media-playlist-title"><?php echo $this->escape($this->playlist->title); ?></h2>
-      <?php endif; ?>
+      <?php if ($this->params->get('item_meta_title') != '0') :?>
+        <h2 class="media-playlist-title"><?php echo $this->escape($this->params->get('page_heading')); ?></h2>
+      <?php endif; ?>    
       <!-- Buttons -->
       <div class="btn-group pull-right">
         <?php if ($canAddMedia): ?>
           <a title="<?php echo JText::_('COM_HWDMS_ADD_MEDIA'); ?>" href="<?php echo JRoute::_('index.php?option=com_hwdmediashare&view=upload&tmpl=component&playlist_id='.(int)$this->playlist->id); ?>" class="btn"><i class="icon-plus"></i> <?php echo JText::_('COM_HWDMS_ADD_MEDIA'); ?></a>
         <?php endif; ?>
         <?php if ($canManageMedia): ?>
-          <a title="<?php echo JText::_('COM_HWDMS_MANAGE_MEDIA'); ?>" href="<?php echo JRoute::_('index.php?option=com_hwdmediashare&view=playlistmedia&tmpl=component&playlist_id=' . $this->playlist->id); ?>" class="btn media-popup-page" ><?php echo JText::_('COM_HWDMS_MANAGE_MEDIA'); ?></a>
+          <a title="<?php echo JText::_('COM_HWDMS_MANAGE_MEDIA'); ?>" href="<?php echo JRoute::_('index.php?option=com_hwdmediashare&view=playlistmedia&tmpl=component&playlist_id=' . $this->playlist->id); ?>" class="btn media-popup-iframe-page" ><?php echo JText::_('COM_HWDMS_MANAGE_MEDIA'); ?></a>
         <?php endif; ?>          
         <?php if ($this->playlist->featured) : ?>
           <div class="btn btn-info"><i class="icon-star"></i> <?php echo JText::_('COM_HWDMS_FEATURED'); ?></div>
@@ -50,7 +50,7 @@ $canManageMedia = ($this->playlist->created_user_id == $user->id);
         <?php endif; ?>           
         <a title="<?php echo JText::_('COM_HWDMS_PLAY_NOW'); ?>" href="<?php echo JRoute::_(hwdMediaShareHelperRoute::getSlideshowRoute(null, array('playlist_id' => $this->playlist->id), false)); ?>" class="btn"><i class="icon-play"></i> <?php echo JText::_('COM_HWDMS_PLAY_NOW'); ?></a>            
         <?php if ($this->params->get('item_meta_report') != 'hide'): ?>  
-          <a title="<?php echo JText::_('COM_HWDMS_REPORT'); ?>" href="<?php echo JRoute::_('index.php?option=com_hwdmediashare&task=playlistform.report&id=' . $this->playlist->id . '&return=' . $this->return . '&tmpl=component'); ?>" class="btn media-popup-form"><i class="icon-warning"></i> <?php echo JText::_('COM_HWDMS_REPORT'); ?></a>
+          <a title="<?php echo JText::_('COM_HWDMS_REPORT'); ?>" href="<?php echo JRoute::_('index.php?option=com_hwdmediashare&task=playlistform.report&id=' . $this->playlist->id . '&return=' . $this->return . '&tmpl=component'); ?>" class="btn media-popup-iframe-form"><i class="icon-flag"></i> <?php echo JText::_('COM_HWDMS_REPORT'); ?></a>
         <?php endif; ?>
         <?php if ($canEdit || $canDelete): ?>
         <?php
@@ -76,7 +76,13 @@ $canManageMedia = ($this->playlist->created_user_id == $user->id);
       <div class="clear"></div>
     </div>
     <div class="media-list-view">
-      <?php echo JLayoutHelper::render('media_list', $this, JPATH_ROOT.'/components/com_hwdmediashare/libraries/layouts'); ?>
+      <?php if (empty($this->items)) : ?>
+        <div class="alert alert-no-items">
+          <?php echo JText::_('COM_HWDMS_NOTHING_TO_SHOW'); ?>
+        </div>
+      <?php else : ?>
+        <?php echo JLayoutHelper::render('media_list', $this, JPATH_ROOT.'/components/com_hwdmediashare/libraries/layouts'); ?>
+      <?php endif; ?> 
     </div>           
     <!-- Pagination -->
     <div class="pagination"> <?php echo $this->pagination->getPagesLinks(); ?> </div>
@@ -89,23 +95,40 @@ $canManageMedia = ($this->playlist->created_user_id == $user->id);
       <?php if ($this->params->get('item_meta_author') != 'hide' || $this->params->get('item_meta_created') != 'hide' || $this->params->get('item_meta_hits') != 'hide' || $this->params->get('item_meta_likes') != 'hide' || $this->params->get('item_meta_report') != 'hide') : ?>
       <dl class="media-info">
         <dt class="media-info-term"><?php echo JText::_('COM_HWDMS_DETAILS'); ?> </dt>
-        <?php if ($this->params->get('list_meta_author') != 'hide' || $this->params->get('list_meta_created') != 'hide') : ?>
-          <dd class="media-info-createdby">
-            <?php if ($this->params->get('list_meta_author') != 'hide') : ?><?php echo JText::sprintf('COM_HWDMS_CREATED_BY', '<a href="'.JRoute::_(hwdMediaShareHelperRoute::getUserRoute($this->playlist->created_user_id)).'">'.htmlspecialchars($this->playlist->author, ENT_COMPAT, 'UTF-8').'</a>'); ?><?php endif; ?><?php if ($this->params->get('list_meta_created') != 'hide') : ?>, <?php echo JHtml::_('date.relative', $this->playlist->created); ?><?php endif; ?>
+        <?php if ($this->params->get('list_meta_likes') != '0') :?>
+          <dd class="media-info-likes">
+            <i class="icon-thumbs-up"></i> <span id="media-likes"><?php echo (int) $this->playlist->likes; ?></span>
+            <i class="icon-thumbs-down"></i> <span id="media-dislikes"><?php echo (int) $this->playlist->dislikes; ?></span>
           </dd>
-        <?php endif; ?>
-        <?php if ($this->params->get('item_meta_media_count') != 'hide') :?>
-          <dd class="media-info-count"> <?php echo JText::_('COM_HWDMS_MEDIA'); ?> (<?php echo (int) $this->playlist->nummedia; ?>)</dd>
+        <?php endif; ?>        
+        <?php if ($this->params->get('list_meta_author') != '0' || $this->params->get('list_meta_created') != '0') : ?>
+        <dd class="media-info-meta">
+          <?php if ($this->params->get('list_meta_author') != '0') : ?>
+            <span class="media-info-createdby">
+              <?php echo JText::sprintf('COM_HWDMS_BY_X_USER', '<a href="'.JRoute::_(hwdMediaShareHelperRoute::getUserRoute($this->playlist->created_user_id)).'">'.htmlspecialchars($this->playlist->author, ENT_COMPAT, 'UTF-8').'</a>'); ?>
+            </span>
+          <?php endif; ?>
+          <?php if ($this->params->get('list_meta_created') != '0') : ?>
+            <span class="media-info-created">
+              <?php echo JHtml::_('hwddate.relative', $this->playlist->created); ?>
+            </span>
+          <?php endif; ?>
+        </dd>
         <?php endif; ?>
         <?php if ($this->params->get('item_meta_hits') != 'hide') :?>
-          <dd class="media-info-hits"> <?php echo JText::_('COM_HWDMS_VIEWS'); ?> (<?php echo (int) $this->playlist->hits; ?>)</dd>
-        <?php endif; ?>   
+          <dd class="media-info-hits label"><?php echo JText::sprintf('COM_HWDMS_X_VIEWS', number_format((int) $this->playlist->hits)); ?></dd>
+        <?php endif; ?>      
+        <?php if ($this->params->get('item_meta_media_count') != 'hide') :?>
+          <dd class="media-info-count label"><?php echo JText::plural('COM_HWDMS_X_MEDIA_COUNT', (int) $this->playlist->nummedia); ?></dd>
+        <?php endif; ?>        
+        <div class="clearfix"></div>
         <?php if ($this->params->get('item_meta_likes') != 'hide') :?>
-          <dd class="media-info-like"> <a href="<?php echo JRoute::_('index.php?option=com_hwdmediashare&task=playlists.like&id=' . $this->playlist->id . '&return=' . $this->return . '&tmpl=component&'.JSession::getFormToken().'=1'); ?>"><?php echo JText::_('COM_HWDMS_LIKE'); ?></a> (<?php echo $this->escape($this->playlist->likes); ?>) <a href="<?php echo JRoute::_('index.php?option=com_hwdmediashare&task=playlists.dislike&id=' . $this->playlist->id . '&return=' . $this->return . '&tmpl=component&'.JSession::getFormToken().'=1'); ?>"><?php echo JText::_('COM_HWDMS_DISLIKE'); ?></a> (<?php echo $this->escape($this->playlist->dislikes); ?>) </dd>
-        <?php endif; ?>  
+          <dd class="media-info-like"><a href="<?php echo JRoute::_('index.php?option=com_hwdmediashare&task=playlists.like&id=' . $this->playlist->id . '&return=' . $this->return . '&tmpl=component&'.JSession::getFormToken().'=1'); ?>"><?php echo JText::_('COM_HWDMS_LIKE'); ?></a></dd>
+          <dd class="media-info-dislike"><a href="<?php echo JRoute::_('index.php?option=com_hwdmediashare&task=playlists.dislike&id=' . $this->playlist->id . '&return=' . $this->return . '&tmpl=component&'.JSession::getFormToken().'=1'); ?>"><?php echo JText::_('COM_HWDMS_DISLIKE'); ?></a></dd>
+        <?php endif; ?>   
         <?php if ($this->params->get('item_meta_report') != 'hide') :?>
-          <dd class="media-info-report"> <a title="<?php echo JText::_('COM_HWDMS_REPORT'); ?>" href="<?php echo JRoute::_('index.php?option=com_hwdmediashare&task=playlistform.report&id=' . $this->playlist->id . '&return=' . $this->return . '&tmpl=component'); ?>" class="media-popup-form"><?php echo JText::_('COM_HWDMS_REPORT'); ?> </a> </dd>
-        <?php endif; ?>             
+          <dd class="media-info-report"><a title="<?php echo JText::_('COM_HWDMS_REPORT'); ?>" href="<?php echo JRoute::_('index.php?option=com_hwdmediashare&task=playlistform.report&id=' . $this->playlist->id . '&return=' . $this->return . '&tmpl=component'); ?>" class="media-popup-iframe-form"><?php echo JText::_('COM_HWDMS_REPORT'); ?></a></dd>
+        <?php endif; ?>          
       </dl>
       <?php endif; ?>  
       <!-- Tags -->
@@ -114,19 +137,14 @@ $canManageMedia = ($this->playlist->created_user_id == $user->id);
 	<?php echo $this->playlist->tagLayout->render($this->playlist->tags->itemTags); ?>
       <?php endif; ?>
       <!-- Custom fields -->
-      <dl class="media-media-info">
-      <?php foreach ($this->playlist->customfields['fields'] as $group => $groupFields) : ?>
-        <dt class="media-media-info-term"><?php echo JText::_( $group ); ?></dt>
-        <?php foreach ($groupFields as $field) :
-        $field	= JArrayHelper::toObject ( $field );
-        $field->value = $this->escape( $field->value );
-        ?>
-          <dd class="media-createdby" title="" class="hasTip" for="jform_<?php echo $field->id;?>" id="jform_<?php echo $field->id;?>-lbl">
-            <?php echo JText::_( $field->name );?> <?php echo $this->escape($field->value); ?>
-          </dd>
+      <dl class="media-info">
+        <?php foreach($this->playlist->customfields->fields as $group => $fields) : ?>
+          <dt class="media-info-group"><?php echo JText::_($group); ?> </dt>
+          <?php foreach($fields as $field) : ?>
+            <dd class="media-info-<?php echo $field->fieldcode; ?>"><?php echo $field->name; ?>: <?php echo $this->playlist->customfields->display($field); ?></dd>
+          <?php endforeach; ?>
         <?php endforeach; ?>
-      <?php endforeach; ?>
-      </dl>  
+      </dl>
       <div class="clear"></div> 
     </div>
   </div>
