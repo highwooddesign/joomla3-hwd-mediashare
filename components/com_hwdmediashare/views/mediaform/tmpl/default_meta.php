a2ac4fa00424219538d29ddf6658406f98b7430b
Dave Horsfall <dave@horsfall.eu>
2014-07-31 14:23:39 +0100

    Updated the default_meta template.

--- components/com_hwdmediashare/views/mediaform/tmpl/default_meta.php
+++ components/com_hwdmediashare/views/mediaform/tmpl/default_meta.php
@@ -1,47 +1,34 @@
 <?php
 /**
- * @version    SVN $Id: default_meta.php 496 2012-08-29 13:26:32Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2012 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      16-Mar-2012 10:45:37
+ * @package     Joomla.site
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
 
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
-
-JHtml::_('behavior.framework');
-
-$counter=0;
+defined('_JEXEC') or die;
 ?>
 <form action="<?php echo htmlspecialchars(JFactory::getURI()->toString()); ?>" method="post" name="adminForm" id="adminForm">
-  <div id="hwd-container"> <a name="top" id="top"></a>
+  <div id="hwd-container" class="<?php echo $this->pageclass_sfx;?>"> <a name="top" id="top"></a>
     <div class="media-list-view">
-    <table class="category">
+      <table class="table">
         <tbody>
-        <?php if (!is_array($this->items) || count($this->items) == 0) : ?>
-        <tr class="cat-list-row<?php echo ($counter % 2);?>">
+        <?php if (!is_array($this->meta) || count($this->meta) == 0) : ?>
+          <tr class="cat-list-row<?php echo ($counter % 2);?>">
             <td><?php echo JText::_('COM_HWDMS_NO_DATA'); ?></td>
-        </tr>   
+          </tr>   
         <?php else : ?>
-        <?php foreach ($this->items as $id => &$item) : ?>
-        <tr class="cat-list-row<?php echo ($counter % 2);?>">
-            <td><?php echo $this->escape(ucwords($id)); ?></a></td>
-            <td><?php echo $this->escape(ucwords($item)); ?></td>
-        </tr>
-        <?php $counter++; ?>
-        <?php endforeach; ?>
-        <?php foreach ($this->items as $id => &$item) : ?>
-        <tr class="cat-list-row<?php echo ($counter % 2);?>">
-            <td><?php echo $this->escape(ucwords($id)); ?></a></td>
-            <td><?php echo $this->escape(ucwords($item)); ?></td>
-        </tr>
-        <?php $counter++; ?>
-        <?php endforeach; ?>
+          <?php foreach ($this->meta as $id => $item) : ?>
+            <tr>
+              <td><?php echo ucwords($id); ?></a></td>
+              <td><?php echo ucwords($item); ?></td>
+            </tr>
+          <?php endforeach; ?>
         <?php endif; ?>
         </tbody>
-    </table>
+      </table>
     </div>
   </div>   
 </form>
