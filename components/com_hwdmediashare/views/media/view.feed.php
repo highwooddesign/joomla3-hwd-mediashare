f35b358b972f96a8384ff28fd20db3dad28573e5
Dave Horsfall <dave@horsfall.eu>
2014-08-01 11:55:55 +0100

    Updated media feed view class.

--- components/com_hwdmediashare/views/media/view.feed.php
+++ components/com_hwdmediashare/views/media/view.feed.php
@@ -1,149 +1,135 @@
 <?php
 /**
- * @version    SVN $Id: view.feed.php 425 2012-06-28 07:48:57Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2012 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      06-Jan-2012 15:40:58
+ * @package     Joomla.site
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
 
-// No direct access
 defined('_JEXEC') or die;
 
-jimport('joomla.application.component.view');
-jimport('joomla.document.feed.feed');
-
-/**
- * HTML View class for the Content component
- *
- * @package		Joomla.Site
- * @subpackage	com_content
- * @since 1.5
- */
-class hwdMediaShareViewMedia extends JViewLegacy {
-	function display()
+class hwdMediaShareViewMedia extends JViewLegacy
+{
+	/**
+	 * Display the view.
+	 *
+	 * @access  public
+	 * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
+	 * @return  void
+	 */
+	public function display($tpl = null)
 	{
-                $app = JFactory::getApplication();
-		$doc	= JFactory::getDocument();
+                // Initialise variables
+		// $this->document->setTitle('');
+                // $this->document->setLink('');
+                // $this->document->setDescription('');
+                // $this->document->setLanguage('');
                 
+                // Get data from the model.
+                $this->items = $this->get('Items');
+		$this->state = $this->get('State');
+		$this->params = $this->state->params;
+                
+                // Register classes.
+                JLoader::register('JHtmlString', JPATH_LIBRARIES.'/joomla/html/html/string.php');
+
+                // Import HWD libraries.                
                 hwdMediaShareFactory::load('files');
                 hwdMediaShareFactory::load('downloads');
-                hwdMediaShareFactory::load('renderer.xml');
-                hwdMediaShareFactory::load('renderer.rssgeo');
-                hwdMediaShareFactory::load('renderer.xspf');
+                hwdMediaShareFactory::load('media');
+		hwdMediaShareFactory::load('utilities');
+                //hwdMediaShareFactory::load('renderer.xml');
+                //hwdMediaShareFactory::load('renderer.rssgeo');
+                //hwdMediaShareFactory::load('renderer.xspf');
+                
+                $this->utilities = hwdMediaShareUtilities::getInstance();
+                $this->return = base64_encode(JFactory::getURI()->toString());
 
-                // Initialise variables
-		$state		= $this->get('State');
-		$rows		= $this->get('Items');
-
-		foreach ($rows as $row)
+                // Process each item.
+		foreach ($this->items as $item)
 		{
-			// strip html from feed item title
-			$title = $this->escape($row->title);
+			// Prepare the feed title.
+			$title = $this->escape($item->title);
 			$title = html_entity_decode($title, ENT_COMPAT, 'UTF-8');
 
-			// Compute the article slug
-			$row->slug = $row->alias ? ($row->id . ':' . $row->alias) : $row->id;
+			// Compute the slug.
+			$item->slug = $item->alias ? ($item->id . ':' . $item->alias) : $item->id;
 
-                        // load individual item creator class
-			$item = new JFeedItem();
-			$item->title		= $title;
-			$item->link		= JRoute::_(hwdMediaShareHelperRoute::getMediaItemRoute($row->slug));
-			$item->description	= '<div class="feed-description">'.$row->description.'</div>';
-			$item->date		= ($row->created ? date('r', strtotime($row->created)) : '');
-			$item->author		= $row->created_by_alias ? $row->created_by_alias : $row->author;
-			$item->location		= $row->location;
-			$item->image		= JRoute::_(hwdMediaShareDownloads::thumbnail($row));
-                                
-                        // @TODO: Cleanup
-                        switch ($row->type) 
+                        // Load individual feed creator class.
+			$feed = new JFeedItem();
+                        
+                        // Define the feed elements (RSS).
+			$feed->title		= $title;
+			$feed->link		= JRoute::_(hwdMediaShareHelperRoute::getMediaItemRoute($item->slug));
+			$feed->description	= JHtmlString::truncate($item->description, $this->params->get('list_desc_truncate'), false, false);
+			$feed->date		= date('r', strtotime($item->created));
+			$feed->author		= $item->author;
+			$feed->image		= JRoute::_(hwdMediaShareDownloads::thumbnail($item));
+                            
+                        // Define additional elements (GEO-RSS).                        
+			$feed->location		= $item->location;
+
+                        // Define content specific elements.
+                        switch ($item->type) 
                         {
-                            case 1:  
-                            // Local
-                            switch ($row->media_type) 
-                            {
-                                case 1:
-                                    // Audio
-                                    hwdMediaShareFactory::load('audio');
-                                    if ($mp3 = hwdMediaShareAudio::getMp3($row))
-                                    {
-                                            $item->enclosure->url = $mp3;
-                                            $item->enclosure->length = "";
-                                            $item->enclosure->type = "audio/mpeg";
-                                    }
-                                    else if ($ogg = hwdMediaShareAudio::getOgg($row))
-                                    {
-                                            $item->enclosure->url = $ogg;
-                                            $item->enclosure->length = "";
-                                            $item->enclosure->type = "audio/ogg";
-                                    }
-                                    else
-                                    {
-                                            hwdMediaShareFiles::getLocalStoragePath();
-
-                                            $folders = hwdMediaShareFiles::getFolders($row->key);
-                                            $filename = hwdMediaShareFiles::getFilename($row->key, 1);
-                                            $ext = hwdMediaShareFiles::getExtension($row, 1);
-
-                                            $path = hwdMediaShareFiles::getPath($folders, $filename, $ext);
-                                            if (file_exists($path))
-                                            {
-                                                    $item->enclosure->url = hwdMediaShareFiles::getUrl($folders, $filename, $ext);
-                                                    $item->enclosure->length = "";
-                                                    $item->enclosure->type = "";
-                                            }
-                                    }
+                                case 1: // Local
+                                        switch ($item->media_type) 
+                                        {
+                                                case 1: // Audio
+                                                        hwdMediaShareFactory::load('audio');
+                                                        if ($mp3 = hwdMediaShareAudio::getMp3($item))
+                                                        {
+                                                                $enclosure = new JFeedEnclosure;
+                                                                $enclosure->url = $mp3->url;
+                                                                $enclosure->length = $mp3->size;
+                                                                $enclosure->type = $mp3->type;
+                                                                $feed->setEnclosure($enclosure);
+                                                        }
+                                                break;
+                                                case 3: // Image
+                                                        hwdMediaShareFactory::load('images');
+                                                        if ($jpg = hwdMediaShareImages::getJpg($item))
+                                                        {
+                                                                $enclosure = new JFeedEnclosure;
+                                                                $enclosure->url = $jpg->url;
+                                                                $enclosure->length = $jpg->size;
+                                                                $enclosure->type = $jpg->type;
+                                                                $feed->setEnclosure($enclosure);
+                                                        }
+                                                break;                                                
+                                                case 4: // Video
+                                                        hwdMediaShareFactory::load('videos');
+                                                        if ($mp4 = hwdMediaShareVideos::getMp4($item))
+                                                        {
+                                                                $feed->enclosure->url = $mp4;
+                                                                $feed->enclosure->length = "";
+                                                                $feed->enclosure->type = "video/mp4";
+                                                        }
+                                                        elseif ($flv = hwdMediaShareVideos::getFlv($item))
+                                                        {
+                                                                $feed->enclosure->url = $flv;
+                                                                $feed->enclosure->length = "";
+                                                                $feed->enclosure->type = "video/flv";
+                                                        }
+                                                break;
+                                        }
                                 break;
-                                case 4:
-                                    // Video
-                                    hwdMediaShareFactory::load('videos');
-                                    if ($mp4 = hwdMediaShareVideos::getMp4($row))
-                                    {
-                                            $item->enclosure->url = $mp4;
-                                            $item->enclosure->length = "";
-                                            $item->enclosure->type = "video/mp4";
-                                    }
-                                    else if ($flv = hwdMediaShareVideos::getFlv($row))
-                                    {
-                                            $item->enclosure->url = $flv;
-                                            $item->enclosure->length = "";
-                                            $item->enclosure->type = "video/flv";
-                                    }
-                                    else
-                                    {
-                                            hwdMediaShareFiles::getLocalStoragePath();
-
-                                            $folders = hwdMediaShareFiles::getFolders($row->key);
-                                            $filename = hwdMediaShareFiles::getFilename($row->key, 1);
-                                            $ext = hwdMediaShareFiles::getExtension($row, 1);
-
-                                            $path = hwdMediaShareFiles::getPath($folders, $filename, $ext);
-                                            if (file_exists($path))
-                                            {
-                                                    $item->enclosure->url = hwdMediaShareFiles::getUrl($folders, $filename, $ext);
-                                                    $item->enclosure->length = "";
-                                                    $item->enclosure->type = "";
-                                            }
-                                    }
-                            }
-                            case 7:                                
-                            // Remote
-                                $item->enclosure->url = $row->source;
-                                $item->enclosure->length = "";
-                                $item->enclosure->type = "video/mp4";                                
                         }
 
-                        // Add thumbnail to enclosure if it hasn't been defined yet
-			if ($item->enclosure == null)
+                        // If no enclosure set, then add original.
+			if ($feed->enclosure == null && $original = hwdMediaShareMedia::getOriginal($item))
 			{
-                                $item->enclosure->url = $item->image;
-                                $item->enclosure->length = "";
-                                $item->enclosure->type = "image/jpeg";  
+                                $enclosure = new JFeedEnclosure;
+                                $enclosure->url = $original->url;
+                                $enclosure->length = $original->size;
+                                $enclosure->type = $original->type;
+                                $feed->setEnclosure($enclosure);
 			}
                                 
-			// loads item info into rss array
-			$doc->addItem($item);
+			// Add feed item into rss array.
+			$this->document->addItem($feed);
 		}
 	}
 }
