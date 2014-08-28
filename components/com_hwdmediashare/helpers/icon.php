a82368b9a2645ff94342ce28b561ccbac6b1a8e6
Dave Horsfall <dave@horsfall.eu>
2014-08-28 10:50:00 +0100

    Syntax (minor)

--- components/com_hwdmediashare/helpers/icon.php
+++ components/com_hwdmediashare/helpers/icon.php
@@ -29,43 +29,44 @@ class JHtmlHwdIcon
                         {
                               case "zip":
                                     return JURI::root( true ).'/media/com_hwdmediashare/assets/images/icons/16/zip.png';
-                                    break;
+                              break;
                         }
                 }
 
                 if (!isset($type)) return;
 
-                switch ($type) {
+                switch ($type) 
+                {
                         case "1":
                                 return JURI::root( true ).'/media/com_hwdmediashare/assets/images/icons/24/media.png';
-                                break;
+                        break;
                         case "1-1":
                                 return JURI::root( true ).'/media/com_hwdmediashare/assets/images/icons/24/audio.png';
-                                break;
+                        break;
                         case "1-2":
                                 return JURI::root( true ).'/media/com_hwdmediashare/assets/images/icons/24/document.png';
-                                break;
+                        break;
                         case "1-3":
                                 return JURI::root( true ).'/media/com_hwdmediashare/assets/images/icons/24/image.png';
-                                break;
+                        break;
                         case "1-4":
                                 return JURI::root( true ).'/media/com_hwdmediashare/assets/images/icons/24/video.png';
-                                break;
+                        break;
                         case 2:
                                 return JURI::root( true ).'/media/com_hwdmediashare/assets/images/icons/24/album.png';
-                                break;
+                        break;
                         case 3:
                                 return JURI::root( true ).'/media/com_hwdmediashare/assets/images/icons/24/group.png';
-                                break;
+                        break;
                         case 4:
                                 return JURI::root( true ).'/media/com_hwdmediashare/assets/images/icons/24/playlist.png';
-                                break;
+                        break;
                         case 5:
                                 return JURI::root( true ).'/media/com_hwdmediashare/assets/images/icons/24/user.png';
-                                break;
+                        break;
                         case 6:
                                 return JURI::root( true ).'/media/com_hwdmediashare/assets/images/icons/24/category.png';
-                                break;
+                        break;
                 }
                 
                 // If no appropriate icon, assume a remote media.
