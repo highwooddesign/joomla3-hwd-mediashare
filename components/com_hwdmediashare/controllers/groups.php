5f9b32820b3e6290ed63dc8045277d57ea453771
Dave Horsfall <dave@horsfall.eu>
2014-09-04 09:01:46 +0100

    Updated language string definitions for frontend controller.

--- components/com_hwdmediashare/controllers/groups.php
+++ components/com_hwdmediashare/controllers/groups.php
@@ -105,10 +105,10 @@ class hwdMediaShareControllerGroups extends JControllerForm
                                 switch ($task)
                                 {
                                         case 'delete':
-                                                $this->setMessage(JText::plural($this->text_prefix . '_N_ITEMS_DELETED', count($cid)));
+                                                $this->setMessage(JText::plural($this->text_prefix . '_N_GROUPS_DELETED', count($cid)));
                                         break;
                                         default:
-                                                $this->setMessage(JText::plural($this->text_prefix . '_N_ITEMS_'.strtoupper($task).'ED', count($cid)));
+                                                $this->setMessage(JText::plural($this->text_prefix . '_N_GROUPS_'.strtoupper($task).'ED', count($cid)));
                                 }
 			}
 			else
