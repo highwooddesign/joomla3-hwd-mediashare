31aa35cfd021de5b51bc78d5c043de03b75b8eeb
Dave Horsfall <dave@horsfall.eu>
2014-09-04 10:17:18 +0100

    approve_new_user_channels

---
+++ administrator/components/com_hwdmediashare/views/channel/view.html.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * @package     Joomla.administrator
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
+ */
+
+defined('_JEXEC') or die;
+
+class hwdMediaShareViewChannel extends JViewLegacy 
+{
+	protected $state;
+
+	protected $item;
+
+	protected $form;
+        
+	/**
+	 * Display the view.
+	 *
+	 * @access  public
+	 * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
+	 * @return  void
+	 */
+	public function display($tpl = null)
+	{
+                // Get data from the model.
+		$this->state	= $this->get('State');
+		$this->item	= $this->get('Item');
+		$this->form	= $this->get('Form');
+
+                // Import HWD libraries.
+                hwdMediaShareFactory::load('downloads');
+                hwdMediaShareFactory::load('thumbnails');
+
+                // Check for errors.
+                if (count($errors = $this->get('Errors')))
+                {
+                        JError::raiseError(500, implode('<br />', $errors));
+                        return false;
+                }
+
+		$this->addToolbar();
+		$this->sidebar = JHtmlSidebar::render();
+
+		// Display the template.
+		parent::display($tpl);
+	}
+
+	/**
+	 * Add the page title and toolbar.
+	 *
+	 * @access  protected
+	 * @return  void
+	 */
+	protected function addToolBar()
+	{
+		JFactory::getApplication()->input->set('hidemainmenu', true);
+
+		$canDo		= hwdMediaShareHelper::getActions($this->item->id, 'user');
+		$user		= JFactory::getUser();
+		$isNew		= ($this->item->id == 0);
+		$checkedOut	= !($this->item->checked_out == 0 || $this->item->checked_out == $user->get('id'));
+
+		JToolBarHelper::title($isNew ? JText::_('COM_HWDMS_NEW_CHANNEL') : JText::_('COM_HWDMS_EDIT_CHANNEL'), 'user');
+
+		// If not checked out, can save the item.
+		if (!$checkedOut && ($canDo->get('core.edit')||(count($user->getAuthorisedCategories('com_hwdmediashare', 'core.create')))))
+		{
+			JToolbarHelper::apply('channel.apply');
+			JToolbarHelper::save('channel.save');
+		}
+		if (!$checkedOut && (count($user->getAuthorisedCategories('com_hwdmediashare', 'core.create'))))
+		{
+			JToolbarHelper::save2new('channel.save2new');
+		}
+		// If an existing item, can save to a copy.
+		if (!$isNew && (count($user->getAuthorisedCategories('com_hwdmediashare', 'core.create')) > 0))
+		{
+			JToolbarHelper::save2copy('channel.save2copy');
+		}
+		if (empty($this->item->id))
+		{
+			JToolbarHelper::cancel('channel.cancel');
+		}
+		else
+		{
+			JToolbarHelper::cancel('channel.cancel', 'JTOOLBAR_CLOSE');
+		}
+		JToolbarHelper::help('HWD', false, 'http://hwdmediashare.co.uk/learn/docs'); 
+	}
+}
+
+
+
