f981be217c44d6a258aca117e35446f1fdb419f1
Dave Horsfall <dave@horsfall.eu>
2014-09-05 14:56:16 +0100

    Rearranged the media tab of the configuration page.

--- administrator/components/com_hwdmediashare/views/configuration/tmpl/default_media.php
+++ administrator/components/com_hwdmediashare/views/configuration/tmpl/default_media.php
@@ -13,24 +13,31 @@ defined('_JEXEC') or die;
 <div class="row-fluid">
         <div class="span6">
                 <fieldset class="form-horizontal">
-                        <legend><?php echo JText::_('COM_HWDMS_MEDIA_DELIVERY'); ?></legend>
+                        <legend><?php echo JText::_('COM_HWDMS_MEDIA_DEFAULTS'); ?></legend>
                         <div class="control-group">
-                                <div class="control-label"><?php echo $this->form->getLabel('fallback'); ?></div>
-                                <div class="controls"><?php echo $this->form->getInput('fallback'); ?></div>
+                                <div class="control-label"><?php echo $this->form->getLabel('media_autoplay'); ?></div>
+                                <div class="controls"><?php echo $this->form->getInput('media_autoplay'); ?></div>
                         </div>
+                        <div class="control-group">
+                                <div class="control-label"><?php echo $this->form->getLabel('mediaitem_size'); ?></div>
+                                <div class="controls"><?php echo $this->form->getInput('mediaitem_size'); ?></div>
+                        </div>                           
+                </fieldset>            
+                <fieldset class="form-horizontal">
+                        <legend><?php echo JText::_('COM_HWDMS_MEDIA_DELIVERY'); ?></legend>
                         <div class="control-group">
                                 <div class="control-label"><?php echo $this->form->getLabel('media_player'); ?></div>
                                 <div class="controls"><?php echo $this->form->getInput('media_player'); ?></div>
+                        </div>                        
+                        <div class="control-group">
+                                <div class="control-label"><?php echo $this->form->getLabel('fallback'); ?></div>
+                                <div class="controls"><?php echo $this->form->getInput('fallback'); ?></div>
                         </div>
                 </fieldset>
         </div>
         <div class="span6">
                 <fieldset class="form-horizontal">
                         <legend><?php echo JText::_('COM_HWDMS_VIDEO_DEFAULTS'); ?></legend>
-                        <div class="control-group">
-                                <div class="control-label"><?php echo $this->form->getLabel('media_autoplay'); ?></div>
-                                <div class="controls"><?php echo $this->form->getInput('media_autoplay'); ?></div>
-                        </div>
                         <div class="control-group">
                                 <div class="control-label"><?php echo $this->form->getLabel('video_quality'); ?></div>
                                 <div class="controls"><?php echo $this->form->getInput('video_quality'); ?></div>
