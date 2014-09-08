3240d4575bf870494e9d2ff3d356fc24126eba6e
Dave Horsfall <dave@horsfall.eu>
2014-09-08 13:28:48 +0100

    Updated default_link template.

--- components/com_hwdmediashare/views/mediaform/tmpl/default_link.php
+++ components/com_hwdmediashare/views/mediaform/tmpl/default_link.php
@@ -1,22 +1,21 @@
 <?php
 /**
- * @version    SVN $Id: default_link.php 425 2012-06-28 07:48:57Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2011 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      18-Nov-2011 10:01:56
+ * @package     Joomla.site
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
 
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
+
 JHtml::_('behavior.modal');
 JHtml::_('behavior.framework', true);
 $user = JFactory::getUser();
-
 ?>
-<div class="edit">
-  <form action="<?php echo JRoute::_('index.php'); ?>" method="post" id="adminForm" class="formelm">
+<form action="<?php echo htmlspecialchars(JFactory::getURI()->toString()); ?>" method="post" name="adminForm" id="adminForm">
+  <div id="hwd-container" class="hwd-modal <?php echo $this->pageclass_sfx;?>"> <a name="top" id="top"></a>
     <fieldset>
       <legend><?php echo JText::_( 'COM_HWDMS_ADD_MEDIA_TO' ); ?></legend>
       <?php if ($this->params->get('enable_categories') && $this->item->created_user_id == $user->id): ?>
@@ -37,7 +36,7 @@ $user = JFactory::getUser();
         <?php echo $this->form->getInput('album_id'); ?>
       </div>
       <?php endif; ?>
-      <?php if ($this->params->get('enable_groups') && $this->item->created_user_id == $user->id): ?>
+      <?php if ($this->params->get('enable_groups')): ?>
       <div class="formelm">
         <?php echo $this->form->getLabel('group_id'); ?>
         <?php echo $this->form->getInput('group_id'); ?>
@@ -45,14 +44,9 @@ $user = JFactory::getUser();
       <?php endif; ?>
     </fieldset>
     <div class="formelm-buttons">
-      <button onclick="Joomla.submitbutton('mediaitem.link')" type="button" class="button"><?php echo JText::_( 'COM_HWDMS_ADD' ); ?></button>
-      <button onclick="window.parent.SqueezeBox.close();" type="button" class="button"><?php echo JText::_( 'COM_HWDMS_CANCEL' ); ?></button>
+      <button onclick="Joomla.submitbutton('media.link')" type="button" class="btn"><?php echo JText::_('COM_HWDMS_ADD'); ?></button>
     </div>
-    <div>
-      <input type="hidden" name="option" value="com_hwdmediashare" />       
-      <input type="hidden" name="task" value="mediaitem.link" />
-      <input type="hidden" name="id" value="<?php echo $this->item->id; ?>" />
-      <input type="hidden" name="tmpl" value="component" />
-      <?php echo JHtml::_('form.token'); ?> </div>
-  </form>
-</div>
+    <input type="hidden" name="task" value="" />
+    <?php echo JHtml::_('form.token'); ?>
+  </div>
+</form>
