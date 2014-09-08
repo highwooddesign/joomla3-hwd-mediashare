48163a54341d5a566909ea4ee48f9e4cecff0b0d
Dave Horsfall <dave@horsfall.eu>
2014-09-08 13:16:24 +0100

    Updated report methods.

--- components/com_hwdmediashare/views/channelform/view.html.php
+++ components/com_hwdmediashare/views/channelform/view.html.php
@@ -89,6 +89,9 @@ class hwdMediaShareViewChannelForm extends JViewLegacy
 	 */
 	protected function _prepareDocument()
 	{
+		// Initialise variables.
+                $app = JFactory::getApplication();
+
                 // Add page assets.
                 JHtml::_('bootstrap.framework');
                 $this->document->addStyleSheet(JURI::base( true ).'/media/com_hwdmediashare/assets/css/hwd.css');
@@ -96,9 +99,18 @@ class hwdMediaShareViewChannelForm extends JViewLegacy
                 if ($this->params->get('list_thumbnail_aspect') != 0) $this->document->addStyleSheet(JURI::base( true ).'/media/com_hwdmediashare/assets/css/aspect.css');
                 if ($this->params->get('list_thumbnail_aspect') != 0) $this->document->addScript(JURI::base( true ).'/media/com_hwdmediashare/assets/javascript/aspect.js');
                 
-		// Define the page title and headings. 
-		$this->params->set('page_heading', JText::sprintf('COM_HWDMS_EDIT_CHANNELX', $this->escape($this->item->title)));
-		$this->document->setTitle($this->params->get('page_heading')); 
+		// Define the page heading.                
+                if ($app->input->get('view', '', 'word') == 'channelform')
+                {
+                        $this->params->set('page_heading', JText::sprintf('COM_HWDMS_EDIT_CHANNELX', $this->escape($this->item->title)));
+                }
+                else
+                {
+                        $this->params->set('page_heading', JText::_('COM_HWDMS_CHANNEL'));
+                }
+                
+		// Define the page title.                
+                $this->document->setTitle($this->params->get('page_heading')); 
 	}
 
 	/**
@@ -113,23 +125,36 @@ class hwdMediaShareViewChannelForm extends JViewLegacy
 		// Initialise variables.
 		$user = JFactory::getUser();
 		$app = JFactory::getApplication();
-                           
+                
+                // Get data from the model.
+                $this->form = $this->get('ReportForm');
+                $this->item = $this->get('Item');
+		$this->state = $this->get('State');
+		$this->params = $this->state->params;
+                
                 // Import HWD libraries.                
 		hwdMediaShareFactory::load('utilities');
                 
                 $this->utilities = hwdMediaShareUtilities::getInstance();
-                
+		$this->pageclass_sfx = htmlspecialchars($this->params->get('pageclass_sfx'));
+                $this->return = base64_encode(JFactory::getURI()->toString());
+
                 if (!$user->authorise('hwdmediashare.report', 'com_hwdmediashare'))
                 {
                         $this->utilities->printModalNotice('COM_HWDMS_NOTICE_NO_FEATURE_ACCESS', 'COM_HWDMS_NOTICE_NO_FEATURE_ACCESS_DESC'); 
                         return;
                 }
                 
-                // Get data from the model.
-                $this->form = $this->get('ReportForm');
-                $this->id = $app->input->get('id', '', 'int');
+                // Check for errors.
+                if (count($errors = $this->get('Errors')))
+                {
+                        JError::raiseError(500, implode('<br />', $errors));
+                        return false;
+                }
 
+		$this->_prepareDocument();
+                
 		// Display the template.
-		parent::display('report');                
+		parent::display('report');               
 	}
 }
