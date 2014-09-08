8722d02af86f223e2e6940fd1f5713905f499d40
Dave Horsfall <dave@horsfall.eu>
2014-09-08 13:23:25 +0100

    Updated mediaform default_meta template.

--- components/com_hwdmediashare/views/mediaform/tmpl/default_meta.php
+++ components/com_hwdmediashare/views/mediaform/tmpl/default_meta.php
@@ -11,7 +11,7 @@
 defined('_JEXEC') or die;
 ?>
 <form action="<?php echo htmlspecialchars(JFactory::getURI()->toString()); ?>" method="post" name="adminForm" id="adminForm">
-  <div id="hwd-container" class="<?php echo $this->pageclass_sfx;?>"> <a name="top" id="top"></a>
+  <div id="hwd-container" class="hwd-modal <?php echo $this->pageclass_sfx;?>"> <a name="top" id="top"></a>
     <div class="media-list-view">
       <table class="table">
         <tbody>
