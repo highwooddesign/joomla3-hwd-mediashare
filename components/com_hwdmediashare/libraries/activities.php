b49e77681d52ceac2f2f26be3535387899aaf258
Dave Horsfall <dave@horsfall.eu>
2014-09-11 16:06:45 +0100

    Updated activities library.

--- components/com_hwdmediashare/libraries/activities.php
+++ components/com_hwdmediashare/libraries/activities.php
@@ -11,7 +11,7 @@
 
 defined('_JEXEC') or die;
 
-class hwdMediaShareActivities
+class hwdMediaShareActivities extends JObject
 {
 	/**
 	 * Class constructor.
@@ -100,29 +100,29 @@ class hwdMediaShareActivities
         {
                 switch ($activity->verb)
                 {
-                    case 2:
-                        return JText::_('COM_HWDMS_X_UPLOADED_A_NEW_MEDIA');
+                        case 2:
+                                return JText::_('COM_HWDMS_X_UPLOADED_A_NEW_MEDIA');
                         break;    
-                    case 3:
-                        return JText::_('COM_HWDMS_X_CREATED_A_NEW_ALBUM');
+                        case 3:
+                                return JText::_('COM_HWDMS_X_CREATED_A_NEW_ALBUM');
                         break;   
-                    case 4:
-                        return JText::_('COM_HWDMS_X_CREATED_A_NEW_GROUP');
+                        case 4:
+                                return JText::_('COM_HWDMS_X_CREATED_A_NEW_GROUP');
                         break; 
-                    case 5:
-                        return JText::_('COM_HWDMS_X_CREATED_A_NEW_PLAYLIST');
+                        case 5:
+                                return JText::_('COM_HWDMS_X_CREATED_A_NEW_PLAYLIST');
                         break;
-                    case 6:
-                        return JText::_('COM_HWDMS_X_CREATED_A_NEW_CHANNEL');
+                        case 6:
+                                return JText::_('COM_HWDMS_X_CREATED_A_NEW_CHANNEL');
                         break; 
-                    case 7:
-                        return JText::_('COM_HWDMS_X_JOINED_A_GROUP');
+                        case 7:
+                                return JText::_('COM_HWDMS_X_JOINED_A_GROUP');
                         break; 
-                    case 8:
-                        return JText::_('COM_HWDMS_X_LEFT_A_GROUP');
+                        case 8:
+                                return JText::_('COM_HWDMS_X_LEFT_A_GROUP');
                         break;  
-                    case 9:
-                        return JText::_('COM_HWDMS_X_SHARED_MEDIA_WITH_A_GROUP');
+                        case 9:
+                                return JText::_('COM_HWDMS_X_SHARED_MEDIA_WITH_A_GROUP');
                         break;                    
                 }
         }
@@ -152,107 +152,113 @@ class hwdMediaShareActivities
                         return;
                 }                
 
+                // Load HWD table path.
+                JTable::addIncludePath(JPATH_ADMINISTRATOR.'/components/com_hwdmediashare/tables');
+
                 switch ($activity->verb) 
                 {
-                    case 1: // Comment
-                        break;
-                    case 2: // New Media
-                        $table = JTable::getInstance('Media', 'hwdMediaShareTable');
-                        $table->load($activity->action);
-                        $properties = $table->getProperties(1);
-                        $row = JArrayHelper::toObject($properties, 'JObject');
-                        if (isset($row->title))
-                        {
-                                    $routeuser  = ($app->isAdmin() ? 'index.php?option=com_hwdmediashare&task=user.edit&id=' . $activity->actor : JRoute::_(hwdMediaShareHelperRoute::getUserRoute($activity->actor)));
-                                    $routemedia = ($app->isAdmin() ? 'index.php?option=com_hwdmediashare&task=editmedia.edit&id=' . $activity->action : JRoute::_(hwdMediaShareHelperRoute::getMediaItemRoute($activity->action)));
-                                    $return =  JText::sprintf(hwdMediaShareActivities::getActivityType($activity), 
-                                                              '<a href="'.$routeuser.'">'.$activity->author.'</a>', 
-                                                              '<a href="'.$routemedia.'">'.JHtmlString::truncate($row->title, $config->get('list_title_truncate'), false, false).'</a>');
-                                    return $return;
-                        }
-                        break;
-                    case 3: // New Album
-                        $table = JTable::getInstance('Album', 'hwdMediaShareTable');
-                        $table->load($activity->action);
-                        $properties = $table->getProperties(1);
-                        $row = JArrayHelper::toObject($properties, 'JObject');
-                        if (isset($row->title))
-                        {
-                                    $routeuser  = ($app->isAdmin() ? 'index.php?option=com_hwdmediashare&task=user.edit&id=' . $activity->actor : JRoute::_(hwdMediaShareHelperRoute::getUserRoute($activity->actor)));
-                                    $routemedia = ($app->isAdmin() ? 'index.php?option=com_hwdmediashare&task=album.edit&id=' . $activity->action : JRoute::_(hwdMediaShareHelperRoute::getAlbumRoute($activity->action)));
-                                    $return =  JText::sprintf(hwdMediaShareActivities::getActivityType($activity), 
-                                                              '<a href="'.$routeuser.'">'.$activity->author.'</a>', 
-                                                              '<a href="'.$routemedia.'">'.JHtmlString::truncate($row->title, $config->get('list_title_truncate'), false, false).'</a>');
-                                    return $return;
-                        }
-                        break;  
-                    case 4: // New Group
-                    case 7: // Joined Group
-                    case 8: // Left Group
-                        $table = JTable::getInstance('Group', 'hwdMediaShareTable');
-                        $table->load($activity->action);
-                        $properties = $table->getProperties(1);
-                        $row = JArrayHelper::toObject($properties, 'JObject');
-                        if (isset($row->title))
-                        {
-                                    $routeuser  = ($app->isAdmin() ? 'index.php?option=com_hwdmediashare&task=user.edit&id=' . $activity->actor : JRoute::_(hwdMediaShareHelperRoute::getUserRoute($activity->actor)));
-                                    $routemedia = ($app->isAdmin() ? 'index.php?option=com_hwdmediashare&task=album.edit&id=' . $activity->action : JRoute::_(hwdMediaShareHelperRoute::getAlbumRoute($activity->action)));
-                                    $return =  JText::sprintf(hwdMediaShareActivities::getActivityType($activity), 
-                                                              '<a href="'.$routeuser.'">'.$activity->author.'</a>', 
-                                                              '<a href="'.$routemedia.'">'.JHtmlString::truncate($row->title, $config->get('list_title_truncate'), false, false).'</a>');
-                                    return $return;
-                        }
-                        break; 
-                    case 5: // New Playlist
-                        $table = JTable::getInstance('Playlist', 'hwdMediaShareTable');
-                        $table->load($activity->action);
-                        $properties = $table->getProperties(1);
-                        $row = JArrayHelper::toObject($properties, 'JObject');
-                        if (isset($row->title))
-                        {
-                                    $routeuser  = ($app->isAdmin() ? 'index.php?option=com_hwdmediashare&task=user.edit&id=' . $activity->actor : JRoute::_(hwdMediaShareHelperRoute::getUserRoute($activity->actor)));
-                                    $routemedia = ($app->isAdmin() ? 'index.php?option=com_hwdmediashare&task=playlist.edit&id=' . $activity->action : JRoute::_(hwdMediaShareHelperRoute::getPlaylistRoute($activity->action)));
-                                    $return =  JText::sprintf(hwdMediaShareActivities::getActivityType($activity), 
-                                                              '<a href="'.$routeuser.'">'.$activity->author.'</a>', 
-                                                              '<a href="'.$routemedia.'">'.JHtmlString::truncate($row->title, $config->get('list_title_truncate'), false, false).'</a>');
-                                    return $return;
-                        }
-                        break; 
-                    case 6: 
-                        break;
-//                            case 7:
-//                                break;
-//                            case 8:
-//                                break;
-                    case 9:
-                        //Get row in table where ID's are equal
-                        jimport( 'joomla.application.component.model' );
-                        JModelLegacy::addIncludePath(JPATH_ROOT.'/components/com_hwdmediashare/models');
-                        $model = JModelLegacy::getInstance('MediaItem', 'hwdMediaShareModel', array('ignore_request' => true));
-                        $model->setState('media.id', $activity->action);
+                        case 2: // New Media
+                                $table = JTable::getInstance('Media', 'hwdMediaShareTable');
+                            
+                                // Attempt to load the table row.
+                                $return = $table->load($activity->action);
 
-                        $activity->element_name = 'Group Name';
-                        if ($media = $model->getItem())
-                        {
-                                    $activity->title       = '<a href="'.JRoute::_(hwdMediaShareHelperRoute::getMediaItemRoute($activity->action)).'">'.$media->title.'</a>'; 
-                                    $activity->description = $media->description;
-                                    $activity->thumbnail = '<div class="media-item">
-                                                            <div class="media-item-format-1-'.$media->media_type .'">
-                                                                 <img src="'.JHtml::_('hwdicon.overlay', '1-'.$media->media_type, $media).'" alt="'.JText::_('COM_HWDMS_MEDIA_TYPE').'" />
-                                                            </div>';
-                                                            if($media->duration > 0) :
-                                                            $activity->thumbnail.= '
-                                                                   <div class="media-duration">'
-                                                                       .hwdMediaShareMedia::secondsToTime($media->duration).
-                                                                   '</div>';
-                                                            endif;
-                                                            $activity->thumbnail.= '
-                                                                <a href="'.JRoute::_(hwdMediaShareHelperRoute::getMediaItemRoute($activity->action)).'">                                            
-                                                                  <img src="'. JRoute::_(hwdMediaShareDownloads::thumbnail($media)) .'" border="0" alt="'.$media->title.'" style="max-width:100%;" class="media-thumb" title="'.$media->title.'"/>
-                                                               </a>
-                                                           </div>';
-                        }
+                                // Check for a table object error.
+                                if ($return === false && $table->getError())
+                                {
+                                        $this->setError($table->getError());
+                                        return false;
+                                }
+                
+                                $properties = $table->getProperties(1);
+                                $item = JArrayHelper::toObject($properties, 'JObject');
+                                if (isset($item->title))
+                                {
+                                            $routeuser  = ($app->isAdmin() ? 'index.php?option=com_hwdmediashare&task=user.edit&id=' . $activity->actor : JRoute::_(hwdMediaShareHelperRoute::getUserRoute($activity->actor)));
+                                            $routemedia = ($app->isAdmin() ? 'index.php?option=com_hwdmediashare&task=editmedia.edit&id=' . $activity->action : JRoute::_(hwdMediaShareHelperRoute::getMediaItemRoute($activity->action)));
+                                            $return =  JText::sprintf(hwdMediaShareActivities::getActivityType($activity), 
+                                                                      '<a href="'.$routeuser.'">'.$activity->author.'</a>', 
+                                                                      '<a href="'.$routemedia.'">'.JHtmlString::truncate($item->title, $config->get('list_title_truncate'), false, false).'</a>');
+                                            return $return;
+                                }
                         break;
+                        case 3: // New Album
+                                $table = JTable::getInstance('Album', 'hwdMediaShareTable');
+                            
+                                // Attempt to load the table row.
+                                $return = $table->load($activity->action);
+
+                                // Check for a table object error.
+                                if ($return === false && $table->getError())
+                                {
+                                        $this->setError($table->getError());
+                                        return false;
+                                }
+                
+                                $properties = $table->getProperties(1);
+                                $item = JArrayHelper::toObject($properties, 'JObject');
+                                if (isset($item->title))
+                                {
+                                            $routeuser  = ($app->isAdmin() ? 'index.php?option=com_hwdmediashare&task=user.edit&id=' . $activity->actor : JRoute::_(hwdMediaShareHelperRoute::getUserRoute($activity->actor)));
+                                            $routemedia = ($app->isAdmin() ? 'index.php?option=com_hwdmediashare&task=album.edit&id=' . $activity->action : JRoute::_(hwdMediaShareHelperRoute::getAlbumRoute($activity->action)));
+                                            $return =  JText::sprintf(hwdMediaShareActivities::getActivityType($activity), 
+                                                                      '<a href="'.$routeuser.'">'.$activity->author.'</a>', 
+                                                                      '<a href="'.$routemedia.'">'.JHtmlString::truncate($item->title, $config->get('list_title_truncate'), false, false).'</a>');
+                                            return $return;
+                                }
+                        break;  
+                        case 4: // New Group
+                        case 7: // Joined Group
+                        case 8: // Left Group
+                                $table = JTable::getInstance('Group', 'hwdMediaShareTable');
+                            
+                                // Attempt to load the table row.
+                                $return = $table->load($activity->action);
+
+                                // Check for a table object error.
+                                if ($return === false && $table->getError())
+                                {
+                                        $this->setError($table->getError());
+                                        return false;
+                                }
+                
+                                $properties = $table->getProperties(1);
+                                $item = JArrayHelper::toObject($properties, 'JObject');
+                                if (isset($item->title))
+                                {
+                                            $routeuser  = ($app->isAdmin() ? 'index.php?option=com_hwdmediashare&task=user.edit&id=' . $activity->actor : JRoute::_(hwdMediaShareHelperRoute::getUserRoute($activity->actor)));
+                                            $routemedia = ($app->isAdmin() ? 'index.php?option=com_hwdmediashare&task=album.edit&id=' . $activity->action : JRoute::_(hwdMediaShareHelperRoute::getAlbumRoute($activity->action)));
+                                            $return =  JText::sprintf(hwdMediaShareActivities::getActivityType($activity), 
+                                                                      '<a href="'.$routeuser.'">'.$activity->author.'</a>', 
+                                                                      '<a href="'.$routemedia.'">'.JHtmlString::truncate($item->title, $config->get('list_title_truncate'), false, false).'</a>');
+                                            return $return;
+                                }
+                        break; 
+                        case 5: // New Playlist
+                                $table = JTable::getInstance('Playlist', 'hwdMediaShareTable');
+                            
+                                // Attempt to load the table row.
+                                $return = $table->load($activity->action);
+
+                                // Check for a table object error.
+                                if ($return === false && $table->getError())
+                                {
+                                        $this->setError($table->getError());
+                                        return false;
+                                }
+                
+                                $properties = $table->getProperties(1);
+                                $item = JArrayHelper::toObject($properties, 'JObject');
+                                if (isset($item->title))
+                                {
+                                            $routeuser  = ($app->isAdmin() ? 'index.php?option=com_hwdmediashare&task=user.edit&id=' . $activity->actor : JRoute::_(hwdMediaShareHelperRoute::getUserRoute($activity->actor)));
+                                            $routemedia = ($app->isAdmin() ? 'index.php?option=com_hwdmediashare&task=playlist.edit&id=' . $activity->action : JRoute::_(hwdMediaShareHelperRoute::getPlaylistRoute($activity->action)));
+                                            $return =  JText::sprintf(hwdMediaShareActivities::getActivityType($activity), 
+                                                                      '<a href="'.$routeuser.'">'.$activity->author.'</a>', 
+                                                                      '<a href="'.$routemedia.'">'.JHtmlString::truncate($item->title, $config->get('list_title_truncate'), false, false).'</a>');
+                                            return $return;
+                                }
+                        break; 
                 }
 
                 return false;
