3a9656aa5116d47072a6a3cd442f6a639e7c2e61
Dave Horsfall <dave@horsfall.eu>
2014-09-12 09:59:22 +0100

    Updated select ordering for query.

--- administrator/components/com_hwdmediashare/models/albums.php
+++ administrator/components/com_hwdmediashare/models/albums.php
@@ -92,10 +92,9 @@ class hwdMediaShareModelAlbums extends JModelList
 		$query->select(
 			$this->getState(
 				'list.select',
-				'a.id, a.title, a.description, a.alias, a.checked_out, a.checked_out_time,' .
-				'a.created_user_id, a.hits, a.published, a.featured,' .
-				'a.status, a.publish_up, a.publish_down, a.ordering, a.created, a.created_user_id_alias, a.access,'.
-				'a.language'
+				'a.id, a.title, a.alias, a.description, a.status, a.published, a.featured, a.checked_out,' .
+                                'a.checked_out_time, a.access, a.ordering, a.created_user_id, a.created_user_id_alias,' .
+                                'a.created, a.publish_up, a.publish_down, a.hits, a.language'
 			)
 		);
 
