ef5a14a70a9fbbb1f86359013314169e46dfcd9c
Dave Horsfall <dave@horsfall.eu>
2014-09-12 10:25:02 +0100

    Updated library loads in view class.

--- components/com_hwdmediashare/views/albumform/view.html.php
+++ components/com_hwdmediashare/views/albumform/view.html.php
@@ -43,9 +43,11 @@ class hwdMediaShareViewAlbumForm extends JViewLegacy
                 JLoader::register('JHtmlString', JPATH_LIBRARIES.'/joomla/html/html/string.php');
                 
                 // Import HWD libraries.                
-                hwdMediaShareFactory::load('files');
+                hwdMediaShareFactory::load('activities');
                 hwdMediaShareFactory::load('downloads');
+                hwdMediaShareFactory::load('files');
                 hwdMediaShareFactory::load('media');
+                hwdMediaShareFactory::load('thumbnails');
 		hwdMediaShareFactory::load('utilities');
                 
                 $this->utilities = hwdMediaShareUtilities::getInstance();
