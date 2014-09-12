2a4db72f5563d83db54f6bd2a3fabd9e9ba878d4
Dave Horsfall <dave@horsfall.eu>
2014-09-12 10:24:29 +0100

    Updated library loads in view class.

--- components/com_hwdmediashare/views/category/view.html.php
+++ components/com_hwdmediashare/views/category/view.html.php
@@ -51,9 +51,11 @@ class hwdMediaShareViewCategory extends JViewLegacy
                 JLoader::register('JHtmlString', JPATH_LIBRARIES.'/joomla/html/html/string.php');
                 
                 // Import HWD libraries.                
-                hwdMediaShareFactory::load('files');
+                hwdMediaShareFactory::load('activities');
                 hwdMediaShareFactory::load('downloads');
+                hwdMediaShareFactory::load('files');
                 hwdMediaShareFactory::load('media');
+                hwdMediaShareFactory::load('thumbnails');
 		hwdMediaShareFactory::load('utilities');
                 
                 $this->category->nummedia = $this->get('numMedia');
