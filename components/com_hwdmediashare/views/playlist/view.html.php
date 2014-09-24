937bad6bd77bd89f43cf07858cbe71c5527f088a
Dave Horsfall <dave@horsfall.eu>
2014-09-24 14:04:07 +0100

    Switch individual registration of JHtml helpers into directory includes. Converted asset load into JHtmlHead::core call.

--- components/com_hwdmediashare/views/playlist/view.html.php
+++ components/com_hwdmediashare/views/playlist/view.html.php
@@ -39,10 +39,9 @@ class hwdMediaShareViewPlaylist extends JViewLegacy
 		$this->params = $this->state->params;
                 $this->filterForm = $this->get('FilterForm');
 
-                // Register classes.
-                JLoader::register('JHtmlHwdIcon', JPATH_COMPONENT . '/helpers/icon.php');
-                JLoader::register('JHtmlHwdDropdown', JPATH_COMPONENT . '/helpers/dropdown.php');
-                JLoader::register('JHtmlString', JPATH_LIBRARIES.'/joomla/html/html/string.php');
+                // Include JHtml helpers.
+                JHtml::addIncludePath(JPATH_COMPONENT_ADMINISTRATOR . '/helpers/html');
+                JHtml::addIncludePath(JPATH_COMPONENT . '/helpers/html');
                 
                 // Import HWD libraries.                
                 hwdMediaShareFactory::load('activities');
@@ -89,11 +88,7 @@ class hwdMediaShareViewPlaylist extends JViewLegacy
 		$title = null;
 
                 // Add page assets.
-                JHtml::_('bootstrap.framework');
-                $this->document->addStyleSheet(JURI::base( true ).'/media/com_hwdmediashare/assets/css/hwd.css');
-                if ($this->params->get('load_joomla_css') != 0) $this->document->addStyleSheet(JURI::base( true ).'/media/com_hwdmediashare/assets/css/joomla.css');
-                if ($this->params->get('list_thumbnail_aspect') != 0) $this->document->addStyleSheet(JURI::base( true ).'/media/com_hwdmediashare/assets/css/aspect.css');
-                if ($this->params->get('list_thumbnail_aspect') != 0) $this->document->addScript(JURI::base( true ).'/media/com_hwdmediashare/assets/javascript/aspect.js');
+                JHtml::_('hwdhead.core', $this->params);
 
 		// Define the page title and headings. 
 		$menu = $menus->getActive();
@@ -156,7 +151,7 @@ class hwdMediaShareViewPlaylist extends JViewLegacy
 		}
                 elseif ($this->playlist->description)
                 {                        
-			$this->document->setDescription($this->escape(JHtmlString::truncate($this->playlist->description, 160, true, false)));   
+			$this->document->setDescription($this->escape(JHtml::_('string.truncate', $this->playlist->description, 160, true, false)));   
                 }
                 elseif ($menu && $menu->query['option'] == 'com_hwdmediashare' && $menu->query['view'] == 'playlist' && (int) @$menu->query['id'] == $this->playlist->id && $this->params->get('menu-meta_description'))
                 {
