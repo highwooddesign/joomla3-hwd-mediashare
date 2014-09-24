2d3da46cf16b04d0dc7ae8d51c1a50b1f373a5a8
Dave Horsfall <dave@horsfall.eu>
2014-09-24 12:54:29 +0100

    Moved all forntend JHtml helpers into a separate subdirectory which will be included with a single call.

---
+++ components/com_hwdmediashare/helpers/html/hwdicon.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * @package     Joomla.site
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
+ */
+
+defined('_JEXEC') or die;
+
+class JHtmlHwdIcon
+{
+	/**
+	 * Method to return the icon url showing the media type.
+         * 
+         * @access  public
+         * @param   string  $type   The type of icon to display.
+         * @param   object  $item   The item being dsplayed.
+         * @return  string  The url of the icon.
+	 */
+	public static function overlay($type, $item = null)
+	{
+                // Check for specific file types.
+                if (isset($item->ext))
+                {
+                        switch ($item->ext)
+                        {
+                              case "zip":
+                                    return JURI::root(true).'/media/com_hwdmediashare/assets/images/icons/16/zip.png';
+                              break;
+                        }
+                }
+
+                if (!isset($type)) return;
+
+                switch ($type) 
+                {
+                        case "1":
+                                return JURI::root(true).'/media/com_hwdmediashare/assets/images/icons/24/media.png';
+                        break;
+                        case "1-1":
+                                return JURI::root(true).'/media/com_hwdmediashare/assets/images/icons/24/audio.png';
+                        break;
+                        case "1-2":
+                                return JURI::root(true).'/media/com_hwdmediashare/assets/images/icons/24/document.png';
+                        break;
+                        case "1-3":
+                                return JURI::root(true).'/media/com_hwdmediashare/assets/images/icons/24/image.png';
+                        break;
+                        case "1-4":
+                                return JURI::root(true).'/media/com_hwdmediashare/assets/images/icons/24/video.png';
+                        break;
+                        case 2:
+                                return JURI::root(true).'/media/com_hwdmediashare/assets/images/icons/24/album.png';
+                        break;
+                        case 3:
+                                return JURI::root(true).'/media/com_hwdmediashare/assets/images/icons/24/group.png';
+                        break;
+                        case 4:
+                                return JURI::root(true).'/media/com_hwdmediashare/assets/images/icons/24/playlist.png';
+                        break;
+                        case 5:
+                                return JURI::root(true).'/media/com_hwdmediashare/assets/images/icons/24/user.png';
+                        break;
+                        case 6:
+                                return JURI::root(true).'/media/com_hwdmediashare/assets/images/icons/24/category.png';
+                        break;
+                }
+                
+                // If no appropriate icon, assume a remote media.
+                return JURI::root(true).'/media/com_hwdmediashare/assets/images/icons/24/globe.png';
+	}
+}
