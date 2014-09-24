bb500843d7c6bef8b1f2ef5232345a5e99e03a65
Dave Horsfall <dave@horsfall.eu>
2014-09-24 14:07:03 +0100

    Switch individual registration of JHtml helpers into directory includes. Converted asset load into JHtmlHead::core call.

--- components/com_hwdmediashare/views/categoryform/view.html.php
+++ components/com_hwdmediashare/views/categoryform/view.html.php
@@ -37,10 +37,9 @@ class hwdMediaShareViewCategoryForm extends JViewLegacy
                 $this->return_page = $this->get('ReturnPage');
 		$this->params = $this->state->params;
 
-                // Register classes.
-                JLoader::register('JHtmlHwdIcon', JPATH_COMPONENT . '/helpers/icon.php');
-                JLoader::register('JHtmlHwdDropdown', JPATH_COMPONENT . '/helpers/dropdown.php');
-                JLoader::register('JHtmlString', JPATH_LIBRARIES.'/joomla/html/html/string.php');
+                // Include JHtml helpers.
+                JHtml::addIncludePath(JPATH_COMPONENT_ADMINISTRATOR . '/helpers/html');
+                JHtml::addIncludePath(JPATH_COMPONENT . '/helpers/html');
                 
                 // Import HWD libraries.                
                 hwdMediaShareFactory::load('activities');
@@ -91,12 +90,8 @@ class hwdMediaShareViewCategoryForm extends JViewLegacy
 	protected function _prepareDocument()
 	{
                 // Add page assets.
-                JHtml::_('bootstrap.framework');
-                $this->document->addStyleSheet(JURI::base( true ).'/media/com_hwdmediashare/assets/css/hwd.css');
-                if ($this->params->get('load_joomla_css') != 0) $this->document->addStyleSheet(JURI::base( true ).'/media/com_hwdmediashare/assets/css/joomla.css');
-                if ($this->params->get('list_thumbnail_aspect') != 0) $this->document->addStyleSheet(JURI::base( true ).'/media/com_hwdmediashare/assets/css/aspect.css');
-                if ($this->params->get('list_thumbnail_aspect') != 0) $this->document->addScript(JURI::base( true ).'/media/com_hwdmediashare/assets/javascript/aspect.js');
-                
+                JHtml::_('hwdhead.core', $this->params);
+
 		// Define the page title and headings. 
 		$this->params->set('page_heading', JText::sprintf('COM_HWDMS_EDIT_CATEGORYX', $this->escape($this->item->title)));
 		$this->document->setTitle($this->params->get('page_heading'));  
