0a56c5ae2a173f387aa099694521efd47874c775
Dave Horsfall <dave@horsfall.eu>
2014-10-13 15:53:08 +0100

    Updated group module.

--- modules/mod_media_groups/helper.php
+++ modules/mod_media_groups/helper.php
@@ -10,36 +10,44 @@
 
 defined('_JEXEC') or die;
 
-// Base this helper on the component view model.
-require_once JPATH_SITE.'/components/com_hwdmediashare/views/groups/view.html.php';
-
-class modMediaGroupsHelper extends hwdMediaShareViewGroups
+class modMediaGroupsHelper extends JViewLegacy
 {
+	/**
+	 * Class constructor.
+	 *
+	 * @access  public
+	 * @param   array  $module  The module object.
+	 * @param   array  $params  The module parameters.
+         * @return  void
+	 */       
 	public function __construct($module, $params)
 	{
                 // Load HWD assets.
                 JLoader::register('hwdMediaShareFactory', JPATH_ROOT.'/components/com_hwdmediashare/libraries/factory.php');
                 JLoader::register('hwdMediaShareHelperRoute', JPATH_ROOT.'/components/com_hwdmediashare/helpers/route.php');
-                JLoader::register('JHtmlHwdIcon', JPATH_ROOT.'/components/com_hwdmediashare/helpers/icon.php');
-                JLoader::register('JHtmlString', JPATH_LIBRARIES.'/joomla/html/html/string.php');
-                JLoader::register('JHtmlHwdDropdown', JPATH_ROOT.'/components/com_hwdmediashare/helpers/dropdown.php');
+
+                // Include JHtml helpers.
+                JHtml::addIncludePath(JPATH_ROOT.'/administrator/components/com_hwdmediashare/helpers/html');
+                JHtml::addIncludePath(JPATH_ROOT.'/components/com_hwdmediashare/helpers/html');
                 
-                // Load and register libraries.
-                hwdMediaShareFactory::load('media');
+                // Import HWD libraries.                
+                hwdMediaShareFactory::load('activities');
                 hwdMediaShareFactory::load('downloads');
                 hwdMediaShareFactory::load('files');
-                hwdMediaShareFactory::load('utilities');
+                hwdMediaShareFactory::load('media');
+                hwdMediaShareFactory::load('thumbnails');
+		hwdMediaShareFactory::load('utilities');
 
-                // Load HWD config.
+                // Load HWD config (and force reset).
                 $hwdms = hwdMediaShareFactory::getInstance();
-                $config = $hwdms->getConfig();
+                $config = $hwdms->getConfig(null, true);
                 
-                // We need to reset this varaible to avoid issues where other modules set this value in earlier position.
-                $config->set('list_default_media_type', '');
-
                 // Merge with module parameters.
                 $config->merge($params);
 
+                // Load lite CSS.
+                $config->set('load_lite_css', 1);
+                
                 // Load the HWD language file.
                 $lang = JFactory::getLanguage();
                 $lang->load('com_hwdmediashare', JPATH_SITE, $lang->getTag(), true, false);
@@ -53,72 +61,63 @@ class modMediaGroupsHelper extends hwdMediaShareViewGroups
                 $this->return = base64_encode(JFactory::getURI()->toString());
 
                 // Add assets to the head tag.
-                $this->addHead();
+                JHtml::_('hwdhead.core', $config);
 	}
 
-	public function addHead()
-	{
-                JHtml::_('bootstrap.framework');
-		$doc = JFactory::getDocument();
-		$doc->addStyleSheet(JURI::base( true ).'/media/com_hwdmediashare/assets/css/lite.css');
-                if ($this->params->get('load_joomla_css') != 0) $doc->addStyleSheet(JURI::base( true ).'/media/com_hwdmediashare/assets/css/joomla.css');
-                if ($this->params->get('list_thumbnail_aspect') != 0) $doc->addStyleSheet(JURI::base( true ).'/media/com_hwdmediashare/assets/css/aspect.css');
-                if ($this->params->get('list_thumbnail_aspect') != 0) $doc->addScript(JURI::base( true ).'/media/com_hwdmediashare/assets/javascript/aspect.js');
-	}
-
+	/**
+	 * Method to get a list of items.
+	 *
+	 * @access  public
+	 * @return  mixed  An array of data items on success, false on failure.
+	 */        
 	public function getItems()
 	{
 		// Initialise variables.
 		$app = JFactory::getApplication();
-                $doc = JFactory::getDocument();
-                $cache = JFactory::getCache();
-                
-                // Force method caching.
-                $cache->setCaching(1);
                 
                 JModelLegacy::addIncludePath(JPATH_ROOT.'/components/com_hwdmediashare/models');
-                $this->_model = JModelLegacy::getInstance('Groups', 'hwdMediaShareModel', array('ignore_request' => true));               
+                $model = JModelLegacy::getInstance('Groups', 'hwdMediaShareModel', array('ignore_request' => true));               
 
                 // Populate state (and set the context).
-                $this->_model->context = 'mod_media_groups';
-		$this->_model->populateState();
+                $model->context = 'mod_media_groups';
+		$model->populateState();
 
 		// Set the start and limit states.
-		$this->_model->setState('list.start', 0);
-		$this->_model->setState('list.limit', (int) $this->params->get('count', 0));
+		$model->setState('list.start', 0);
+		$model->setState('list.limit', (int) $this->params->get('count', 0));
 
 		// Set the ordering states.
                 $ordering = $this->params->get('list_order_group', 'a.created DESC');
                 $orderingParts = explode(' ', $ordering); 
-                $this->_model->setState('list.ordering', $orderingParts[0]);
-                $this->_model->setState('list.direction', $orderingParts[1]);
+                $model->setState('list.ordering', $orderingParts[0]);
+                $model->setState('list.direction', $orderingParts[1]);
                 
                 // Apply author filter.
                 $authors = $this->params->get('created_by');                                        
                 if (is_array($authors)) $authors = array_filter($authors);
                 if ($authors)
                 {    
-                        $this->_model->setState('filter.author_id', $this->params->get('created_by', ''));
-                        $this->_model->setState('filter.author_id.include', $this->params->get('author_filtering_type', 1));
+                        $model->setState('filter.author_id', $this->params->get('created_by', ''));
+                        $model->setState('filter.author_id.include', $this->params->get('author_filtering_type', 1));
                 }      
 
                 // Apply date filter.
                 $date_filtering = $this->params->get('date_filtering', 'off');
                 if ($date_filtering !== 'off') 
                 {
-                        $this->_model->setState('filter.date_filtering', $date_filtering);
-                        $this->_model->setState('filter.date_field', $this->params->get('date_field', 'a.created'));
-                        $this->_model->setState('filter.start_date_range', $this->params->get('start_date_range', '1000-01-01 00:00:00'));
-                        $this->_model->setState('filter.end_date_range', $this->params->get('end_date_range', '9999-12-31 23:59:59'));
-                        $this->_model->setState('filter.relative_date', $this->params->get('relative_date', 30));
+                        $model->setState('filter.date_filtering', $date_filtering);
+                        $model->setState('filter.date_field', $this->params->get('date_field', 'a.created'));
+                        $model->setState('filter.start_date_range', $this->params->get('start_date_range', '1000-01-01 00:00:00'));
+                        $model->setState('filter.end_date_range', $this->params->get('end_date_range', '9999-12-31 23:59:59'));
+                        $model->setState('filter.relative_date', $this->params->get('relative_date', 30));
                 }
                                 
 		// Additional filters.
-                $this->_model->setState('filter.media_type', $this->params->get('list_default_media_type', ''));                
-		$this->_model->setState('filter.featured', $this->params->get('show_featured', 'show'));
+                $model->setState('filter.media_type', $this->params->get('list_default_media_type', ''));                
+		$model->setState('filter.featured', $this->params->get('show_featured', 'show'));
 
-                $this->items = $this->_model->getItems();
+                $this->items = $model->getItems();
 
-                return $this->items;                
+                return $this->items;                 
 	}
 }
--- modules/mod_media_groups/tmpl/default.php
+++ modules/mod_media_groups/tmpl/default.php
@@ -13,7 +13,7 @@ defined('_JEXEC') or die;
 $app = JFactory::getApplication();
 $menu = $app->getMenu();
 ?>
-<div class="hwd-module">
+<div class="hwd-container">
   <div class="media-details-view">
     <?php if (empty($helper->items)) : ?>
       <div class="alert alert-no-items">
@@ -23,5 +23,5 @@ $menu = $app->getMenu();
       <?php echo JLayoutHelper::render('groups_details', $helper, JPATH_ROOT.'/components/com_hwdmediashare/libraries/layouts'); ?>
     <?php endif; ?>      
   </div> 
-  <?php if ($params->get('show_more_link') != 'hide') :?><a href="<?php echo ((intval($params->get('show_more_link')) > 0) ? JRoute::_($menu->getItem($params->get('show_more_link'))->link.'&Itemid='.$params->get('show_more_link')) : JRoute::_(hwdMediaShareHelperRoute::getGroupsRoute())); ?>" class="btn"><?php echo JText::_($params->get('more_link_text', 'MOD_MEDIA_VIEW_MORE')); ?></a><?php endif; ?>  
+  <?php if ($params->get('show_more_link') != 'hide') :?><a href="<?php echo ((intval($params->get('show_more_link')) > 0) ? JRoute::_($menu->getItem($params->get('show_more_link'))->link.'&Itemid='.$params->get('show_more_link')) : JRoute::_(hwdMediaShareHelperRoute::getGroupsRoute())); ?>" class="btn"><?php echo JText::_($params->get('more_link_text', 'MOD_MEDIA_VIEW_MORE_GROUPS')); ?></a><?php endif; ?>  
 </div>
