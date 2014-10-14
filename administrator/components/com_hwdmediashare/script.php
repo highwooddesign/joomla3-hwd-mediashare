5aabbb550a106eef7552a4f9b8942b27485960a6
Dave Horsfall <dave@horsfall.eu>
2014-10-14 15:14:23 +0100

    Added additional files (from assets) to remove during upgrades.

--- administrator/components/com_hwdmediashare/script.php
+++ administrator/components/com_hwdmediashare/script.php
@@ -464,6 +464,22 @@ class com_hwdMediaShareInstallerScript
                 $folders[] = JPATH_SITE.'/components/com_hwdmediashare/views/userform';
                 $folders[] = JPATH_SITE.'/components/com_hwdmediashare/views/users';
 
+                // Media/com_hwdmediashare
+                $folders[] = JPATH_SITE.'/media/com_hwdmediashare/assets/images/admin';        
+                $files[]   = JPATH_SITE.'/media/com_hwdmediashare/assets/images/ajaxupload/failed.png';
+                $files[]   = JPATH_SITE.'/media/com_hwdmediashare/assets/images/ajaxupload/file.png';
+                $files[]   = JPATH_SITE.'/media/com_hwdmediashare/assets/images/ajaxupload/success.png';
+                $files[]   = JPATH_SITE.'/media/com_hwdmediashare/assets/images/ajaxupload/uploading.png';
+                $folders[] = JPATH_SITE.'/media/com_hwdmediashare/assets/images/icons/16';
+                $files[]   = JPATH_SITE.'/media/com_hwdmediashare/assets/images/icons/32/process-all.png';
+                $files[]   = JPATH_SITE.'/media/com_hwdmediashare/assets/images/icons/32/process.png';
+                $files[]   = JPATH_SITE.'/media/com_hwdmediashare/assets/images/icons/32/sample-data.png';
+                $files[]   = JPATH_SITE.'/media/com_hwdmediashare/assets/images/icons/32/social-icon-facebook.png';
+                $files[]   = JPATH_SITE.'/media/com_hwdmediashare/assets/images/icons/32/social-icon-googleplus.png';
+                $files[]   = JPATH_SITE.'/media/com_hwdmediashare/assets/images/icons/32/social-icon-pinterest.png';
+                $files[]   = JPATH_SITE.'/media/com_hwdmediashare/assets/images/icons/32/social-icon-twitter.png';                
+                $folders[] = JPATH_SITE.'/media/com_hwdmediashare/assets/images/icons/48';
+
 		foreach ($files as $file)
 		{
 			if (JFile::exists($file))
