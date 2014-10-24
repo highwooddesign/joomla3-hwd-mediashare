032d4c4ce07c0f070a0d4bd2680079615edc5ce7
Dave Horsfall <dave@horsfall.eu>
2014-10-24 15:34:43 +0100

    Updated use of language load.

--- modules/mod_media_categories/helper.php
+++ modules/mod_media_categories/helper.php
@@ -50,7 +50,7 @@ class modMediaCategoriesHelper extends JViewLegacy
                 
                 // Load the HWD language file.
                 $lang = JFactory::getLanguage();
-                $lang->load('com_hwdmediashare', JPATH_SITE, $lang->getTag(), true, false);
+                $lang->load('com_hwdmediashare', JPATH_SITE, $lang->getTag());
                 
                 // Get data.
                 $this->module = $module;                
