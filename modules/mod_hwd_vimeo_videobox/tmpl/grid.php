0e84f06f7a04f67cb1ae9a4719644bb449add7b5
Dave Horsfall <dave@horsfall.eu>
2014-10-31 15:00:50 +0000

    Update mod_hwd_vimeo_videobox

--- modules/mod_hwd_vimeo_videobox/tmpl/grid.php
+++ modules/mod_hwd_vimeo_videobox/tmpl/grid.php
@@ -14,9 +14,9 @@ defined('_JEXEC') or die;
 $doc = JFactory::getDocument();
 $doc->addStyleDeclaration('
 #photos {
-   -webkit-column-count: '.intval($helper->params->get('columns', 5)).';
-   -moz-column-count:    '.intval($helper->params->get('columns', 5)).';
-   column-count:         '.intval($helper->params->get('columns', 5)).';
+   -webkit-column-count: '.intval($params->get('columns', 5)).';
+   -moz-column-count:    '.intval($params->get('columns', 5)).';
+   column-count:         '.intval($params->get('columns', 5)).';
 }');
 ?>
 <div class="mod_hwd_vimeo_videobox_grid">
@@ -24,14 +24,14 @@ $doc->addStyleDeclaration('
 <?php foreach ($helper->items as $id => $item) : ?>
 <div class="block_grid"> 
     <div class="media-item hasTooltip" title="<?php echo JHtml::tooltipText($item->title, JHtml::_('string.truncate', $item->description, 300, true, false)); ?>">
-        <?php if ($item->duration > 0 && $helper->params->get('show_duration', 1)) :?>
+        <?php if ($item->duration > 0 && $params->get('show_duration', 1)) :?>
         <div class="media-duration">
            <?php echo $helper->secondsToTime($item->duration); ?>
         </div>
         <?php endif; ?>
-        <a class="popup-thumbnail-<?php echo $module->id; ?>" href="http://player.vimeo.com/video/<?php echo $item->id; ?>?color=<?php echo $helper->params->get('color'); ?>&autoplay=<?php echo $helper->params->get('autoplay'); ?>&title=<?php echo $helper->params->get('title'); ?>" id="mb<?php echo $id; ?>" title="<?php echo htmlspecialchars($item->title); ?>">
+        <a class="popup-thumbnail-<?php echo $module->id; ?>" href="http://player.vimeo.com/video/<?php echo $item->id; ?>?color=<?php echo $params->get('color'); ?>&autoplay=<?php echo $params->get('autoplay'); ?>&title=<?php echo $params->get('title'); ?>" id="mb<?php echo $id; ?>" title="<?php echo htmlspecialchars($item->title); ?>">
            <img src="<?php echo $item->thumbnail; ?>" border="0" alt="" title="" class="media-thumb"  />
-           <?php echo ($helper->params->get('icon', 1) ? '<span class="media-link-span"></span>' : ''); ?>
+           <?php echo ($params->get('icon', 1) ? '<span class="media-link-span"></span>' : ''); ?>
         </a>
     </div>
 </div>
