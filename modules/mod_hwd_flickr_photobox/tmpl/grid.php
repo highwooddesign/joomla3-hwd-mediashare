fea59d386fb8a8ea30b2f8b4e8d0547a9f334864
Dave Horsfall <dave@horsfall.eu>
2014-10-31 15:02:07 +0000

    Update mod_hwd_flickr_photobox

--- modules/mod_hwd_flickr_photobox/tmpl/grid.php
+++ modules/mod_hwd_flickr_photobox/tmpl/grid.php
@@ -14,9 +14,9 @@ defined('_JEXEC') or die;
 $doc = JFactory::getDocument();
 $doc->addStyleDeclaration('
 #photos {
-   -webkit-column-count: '.intval($helper->params->get('columns', 5)).';
-   -moz-column-count:    '.intval($helper->params->get('columns', 5)).';
-   column-count:         '.intval($helper->params->get('columns', 5)).';
+   -webkit-column-count: '.intval($params->get('columns', 5)).';
+   -moz-column-count:    '.intval($params->get('columns', 5)).';
+   column-count:         '.intval($params->get('columns', 5)).';
 }');
 ?>
 <div class="mod_hwd_flickr_photobox_grid">
@@ -26,7 +26,7 @@ $doc->addStyleDeclaration('
     <div class="media-item hasTooltip" title="<?php echo JHtml::tooltipText($item->title, JHtml::_('string.truncate', $item->description, 300, true, false)); ?>">
         <a class="popup-thumbnail-<?php echo $module->id; ?>" href="<?php echo $item->media; ?>" id="mb<?php echo $id; ?>" title="<?php echo htmlspecialchars($item->title); ?>">
            <img src="<?php echo $item->thumbnail; ?>" border="0" alt="" title="" class="media-thumb"  />
-           <?php echo ($helper->params->get('icon', 1) ? '<span class="media-link-span"></span>' : ''); ?>
+           <?php echo ($params->get('icon', 1) ? '<span class="media-link-span"></span>' : ''); ?>
         </a>
     </div>
 </div>
