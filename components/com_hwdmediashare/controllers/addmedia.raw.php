e66a3d5b94c455f4fa61138f16ebb23b152f8ba9
Dave Horsfall <dave@horsfall.eu>
2014-11-04 15:57:47 +0000

    Updated main addmedia raw controller.

--- components/com_hwdmediashare/controllers/addmedia.raw.php
+++ components/com_hwdmediashare/controllers/addmedia.raw.php
@@ -1,68 +1,55 @@
 <?php
 /**
- * @version    SVN $Id: addmedia.raw.php 690 2012-10-24 10:34:07Z dhorsfall $
- * @package    hwdMediaShare
- * @copyright  Copyright (C) 2011 Highwood Design Limited. All rights reserved.
- * @license    GNU General Public License http://www.gnu.org/copyleft/gpl.html
- * @author     Dave Horsfall
- * @since      15-Apr-2011 10:13:15
+ * @package     Joomla.site
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
  */
 
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
 
-// Import Joomla controllerform library
-jimport('joomla.application.component.controllerform');
-
-/**
- * hwdMediaShare Controller
- */
 class hwdMediaShareControllerAddMedia extends JControllerForm
 {
         /**
-	 * @since	0.1
+	 * Method to process a file upload using FancyUpload2.
+	 *
+	 * @access	public
+         * @return      void
 	 */
-        public $elementType = 1;
-        
-	/**
-	 * Method to process file upload using FancyUpload2
-	 * @since	0.1
-	 */
-        function upload()
+        public function upload()
         {
-                $app = & JFactory::getApplication();
+                // Initialise variables.
+                $app = JFactory::getApplication();
                 $user = JFactory::getUser();
+                $document = JFactory::getDocument();
 
-                // Cross check and restore session from Flash request
+                // Cross check and restore session from Flash request.
                 if( $user->id == 0 )
                 {
-                        $tokenId	= JRequest::getVar( 'token' , '' );
-                        $userId		= JRequest::getVar( 'uploaderid' , '' );
+                        $tokenId	= $app->input->get('token', '', 'alnum');
+                        $userId		= $app->input->get('uploaderid', '', 'int');
 
-                        $user		= hwdMediaShareFactory::getUserFromTokenId( $tokenId , $userId );
+                        $user		= hwdMediaShareFactory::getUserFromTokenId($tokenId , $userId);
 
-                        $session = & JFactory::getSession();
-                        $session->set('user',$user);
+                        $session = JFactory::getSession();
+                        $session->set('user', $user);
                 }
 
-                $data = array();
-                //$data['catid'] = JRequest::getVar('catid', array(), 'post', 'array');
-                $data['catid'] = JRequest::getInt('catid');
-                $data['album_id'] = JRequest::getInt('album_id');
-                $data['playlist_id'] = JRequest::getInt('playlist_id');
-                $data['group_id'] = JRequest::getInt('group_id');
-                JRequest::setVar('jform', $data);
-                
+		// Define input field to process from the request.
                 $upload = new stdClass();
-                $upload->input  = 'Filedata';
+                $upload->input = 'Filedata';
                 
+                // Load HWD library.
                 hwdMediaShareFactory::load('upload');
                 $model = hwdMediaShareUpload::getInstance();
                 
-                // Add embed code
+                // Process the upload.
                 if (!$model->process($upload))
                 {
-                        $result = array(
+                        // Need to output JSON for FancyUpload2.
+                        $return = array(
                                 'status' => '0',
                                 'error' => $model->getError(),
                                 'code' => 0
@@ -70,31 +57,37 @@ class hwdMediaShareControllerAddMedia extends JControllerForm
                 }
                 else
                 {
-                        $result = array(
+                        // Need to output JSON for FancyUpload2.
+                        $return = array(
                                 'status' => '1',
-                                'name' => $model->_title,
-                                'id' => $model->_id
+                                'name' => $model->_item->title,
+                                'id' => $model->_item->id
                         );
-                }                          
-                        
-                header('Content-type: application/json');
-                echo json_encode($result);
+                }    
+
+                // Set the MIME type for JSON output.
+                $document->setMimeEncoding('application/json');
+
+                // Output the JSON data.      
+                echo json_encode($return);
+                
+		JFactory::getApplication()->close();
         }
         
-	/**
-	 * Method to process file upload using FancyUpload2
-	 * @since	0.1
+        /**
+	 * Method to process a file upload using FancyUpload2.
+	 *
+	 * @access	public
+         * @return      void
 	 */
-        function addCdnUpload()
+        public function addCdnUpload()
         {
-                // Load hwdMediaShare config
+                // Load HWD config.
                 $hwdms = hwdMediaShareFactory::getInstance();
                 $config = $hwdms->getConfig();
 
-                $pluginClass = 'plgHwdmediashare'.$config->get('platform');
-                $pluginPath = JPATH_ROOT.'/plugins/hwdmediashare/'.$config->get('platform').'/'.$config->get('platform').'.php';
-
-                // Import hwdMediaShare plugins
+                $pluginClass = 'plgHwdmediashare' . $config->get('platform');
+                $pluginPath = JPATH_ROOT . '/plugins/hwdmediashare/' . $config->get('platform') . '/' . $config->get('platform') . '.php';
                 if (file_exists($pluginPath))
                 {
                         JLoader::register($pluginClass, $pluginPath);
