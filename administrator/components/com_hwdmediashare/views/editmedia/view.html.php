fb48a3f027582fb708a685e59896648cf0ec66dd
Dave Horsfall <dave@horsfall.eu>
2014-11-04 14:34:38 +0000

    Change to document load.

--- administrator/components/com_hwdmediashare/views/editmedia/view.html.php
+++ administrator/components/com_hwdmediashare/views/editmedia/view.html.php
@@ -55,8 +55,7 @@ class hwdMediaShareViewEditMedia extends JViewLegacy
 		// Display the template.
 		parent::display($tpl);
                 
-		$document = JFactory::getDocument();
-		$document->addStyleSheet(JURI::root() . "media/com_hwdmediashare/assets/css/administrator.css");
+		$this->document->addStyleSheet(JURI::root() . "media/com_hwdmediashare/assets/css/administrator.css");  
 	}
 
 	/**
