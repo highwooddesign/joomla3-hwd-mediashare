3f93e9582383aa72c51bf94ca55d702b29b17f3d
Dave Horsfall <dave@horsfall.eu>
2014-11-20 10:59:13 +0000

    Remote plugin.

--- plugins/hwdmediashare/remote_vevocom/remote_vevocom.php
+++ plugins/hwdmediashare/remote_vevocom/remote_vevocom.php
@@ -232,7 +232,7 @@ class plgHwdmediashareRemote_vevocom extends hwdMediaShareRemote
                         <div class="media-respond" style="max-width:<?php echo $config->get('mediaitem_size', '500'); ?>px;">
                           <div class="media-aspect" data-aspect="<?php echo $config->get('video_aspect', '0.75'); ?>"></div>
                           <div class="media-content">
-                            <iframe width="<?php echo $this->width; ?>" height="<?php echo $this->height; ?>" src="<?php echo $embedLookup; ?>" frameborder="0" allowfullscreen></iframe>
+                            <iframe width="<?php echo $this->width; ?>" height="<?php echo $this->height; ?>" src="<?php echo $embedLookup; ?>" scrolling="no" frameborder="0" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" allowtransparency="true"></iframe>
                           </div>
                         </div>
                         <?php
