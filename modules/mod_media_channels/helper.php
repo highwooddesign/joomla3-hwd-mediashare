dedc8c2c63681b54b69662fbb411f8c5e78a61fe
Dave Horsfall <dave@horsfall.eu>
2015-02-25 12:48:25 +0000

    Added channel module helper.

--- modules/mod_media_channels/helper.php
+++ modules/mod_media_channels/helper.php
@@ -1,100 +1,134 @@
 <?php
-// no direct access
+/**
+ * @package     Joomla.site
+ * @subpackage  Module.mod_media_channels
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
+ */
+
 defined('_JEXEC') or die;
 
-class modMediaChannelsHelper extends JObject {
-
-	public $params 		= null;
-	public $url		= null;
-	public $container	= null;
-
-	public function __construct($module, &$params)
+class modMediaChannelsHelper extends JViewLegacy 
+{
+	/**
+	 * Class constructor.
+	 *
+	 * @access  public
+	 * @param   array  $module  The module object.
+	 * @param   array  $params  The module parameters.
+         * @return  void
+	 */       
+	public function __construct($module, $params)
 	{
-                // Load config
-                $hwdms = hwdMediaShareFactory::getInstance();
-                $config = $hwdms->getConfig();
-                $config->merge( $params );
+                // Load HWD assets.
+                JLoader::register('hwdMediaShareFactory', JPATH_ROOT.'/components/com_hwdmediashare/libraries/factory.php');
+                JLoader::register('hwdMediaShareHelperRoute', JPATH_ROOT.'/components/com_hwdmediashare/helpers/route.php');
+
+                // Include JHtml helpers.
+                JHtml::addIncludePath(JPATH_ROOT.'/administrator/components/com_hwdmediashare/helpers/html');
+                JHtml::addIncludePath(JPATH_ROOT.'/components/com_hwdmediashare/helpers/html');
                 
-                // Download links
+                // Import HWD libraries.                
+                hwdMediaShareFactory::load('activities');
                 hwdMediaShareFactory::load('downloads');
                 hwdMediaShareFactory::load('files');
-                hwdMediaShareFactory::load('utilities');
-                JLoader::register('JHtmlHwdIcon', JPATH_ROOT.'/components/com_hwdmediashare/helpers/icon.php');
-                JLoader::register('JHtmlString', JPATH_LIBRARIES.'/joomla/html/html/string.php');
-    
-                // Load the HWDMediaShare language file
-                $lang =& JFactory::getLanguage();
-                $lang->load('com_hwdmediashare', JPATH_SITE.'/components/com_hwdmediashare', $lang->getTag());
-                $lang->load('com_hwdmediashare', JPATH_SITE, $lang->getTag(), true, false);
+                hwdMediaShareFactory::load('media');
+                hwdMediaShareFactory::load('thumbnails');
+		hwdMediaShareFactory::load('utilities');
 
-                $params = $config;
-                $this->set('utilities', hwdMediaShareUtilities::getInstance());
-                $this->set('params', $config);
-		$this->set('url', JURI::root().'modules/mod_media_channels/');
-		$this->set('container', 'modmediachannels_'.$module->id);
-                $this->set('return', base64_encode(JFactory::getURI()->toString()));
-                $this->set('columns', $params->get('list_columns', 3));
-                $this->set('elementType', 5);
+                // Load HWD config, merge with module parameters (and force reset).
+                $hwdms = hwdMediaShareFactory::getInstance();
+                $config = $hwdms->getConfig($params, true);
+
+                // Load lite CSS.
+                $config->set('load_lite_css', 1);
+                
+                // Load the HWD language file.
+                $lang = JFactory::getLanguage();
+                $lang->load('com_hwdmediashare', JPATH_SITE, $lang->getTag());
+                
+                // Get data.
+                $this->module = $module;                
+                $this->params = $config;                
+                $this->items = $this->getItems();
+                $this->utilities = hwdMediaShareUtilities::getInstance();
+                $this->columns = $params->get('list_columns', 3);
+                $this->return = base64_encode(JFactory::getURI()->toString());
+
+                // Add assets to the head tag.
+                $this->addHead();
 	}
 
+	/**
+	 * Method to add page assets to the <head> tags.
+	 *
+	 * @access  public
+         * @return  void
+	 */        
 	public function addHead()
 	{
+                // Initialise variables.
 		$doc = JFactory::getDocument();
-		$doc->addStyleSheet(JURI::base( true ).'/media/com_hwdmediashare/assets/css/lite.css');
-                if ($this->params->get('load_joomla_css') != 0) $doc->addStyleSheet(JURI::base( true ).'/media/com_hwdmediashare/assets/css/joomla.css');
+                
+                // Add page assets.
+                JHtml::_('hwdhead.core', $this->params);
+                
+                // Extract the layout.
+                list($template, $layout) = explode(':', $this->params->get('layout', '_:default'));
+                
+                // Check for layout stylesheet.
+                if (file_exists(__DIR__ . '/css/' . $layout . '.css'))
+                {
+                        $doc->addStyleSheet(JURI::base( true ) . '/modules/mod_media_channels/css/' . $layout . '.css');
+                }
 	}
 
+	/**
+	 * Method to get a list of items.
+	 *
+	 * @access  public
+	 * @return  mixed  An array of data items on success, false on failure.
+	 */              
 	public function getItems()
 	{
-                jimport( 'joomla.application.component.model' );
-                //JModel::addIncludePath(JPATH_ROOT.'/components/com_hwdmediashare/models');
-                //$model =& JModel::getInstance('Users', 'hwdMediaShareModel', array('ignore_request' => true));
-                $version = new JVersion();
-                ($version->RELEASE >= 3.0 ? JModelLegacy::addIncludePath(JPATH_ROOT.'/components/com_hwdmediashare/models') : JModel::addIncludePath(JPATH_ROOT.'/components/com_hwdmediashare/models'));
-                $model = ($version->RELEASE >= 3.0 ? JModelLegacy::getInstance('Users', 'hwdMediaShareModel', array('ignore_request' => true)) : JModel::getInstance('Users', 'hwdMediaShareModel', array('ignore_request' => true)));
-
-		// Set application parameters in model
+		// Initialise variables.
 		$app = JFactory::getApplication();
-		$appParams = $app->getParams();
-		$model->setState('params', $appParams);
+                
+                JModelLegacy::addIncludePath(JPATH_ROOT.'/components/com_hwdmediashare/models');
+                $model = JModelLegacy::getInstance('Channels', 'hwdMediaShareModel', array('ignore_request' => true));               
 
-		// Set the filters based on the module params
+                // Populate state (and set the context).
+                $model->context = 'mod_media_channels';
+		$model->populateState();
+
+		// Set the start and limit states.
 		$model->setState('list.start', 0);
 		$model->setState('list.limit', (int) $this->params->get('count', 0));
-		$model->setState('filter.published', 1);
 
-		// Ordering
-		$model->setState('com_hwdmediashare.users.list.ordering', $this->params->get('ordering', 'a.ordering'));
-		$model->setState('com_hwdmediashare.users.list.direction', $this->params->get('ordering_direction', 'ASC'));
+		// Set the ordering states.
+                $ordering = $this->params->get('list_order_channel', 'a.created DESC');
+                $orderingParts = explode(' ', $ordering); 
+                $model->setState('list.ordering', $orderingParts[0]);
+                $model->setState('list.direction', $orderingParts[1]);
 
-		// New Parameters
-		$model->setState('filter.featured', $this->params->get('show_featured', 'show'));
-		$excluded_articles = $this->params->get('excluded_articles', '');
-
-		if ($excluded_articles) 
+                // Apply date filter.
+                $date_filtering = $this->params->get('date_filtering', 'off');
+                if ($date_filtering !== 'off') 
                 {
-			$excluded_articles = explode("\r\n", $excluded_articles);
-			$model->setState('filter.article_id', $excluded_articles);
-			$model->setState('filter.article_id.include', false); // Exclude
-		}
-
-		$date_filtering = $this->params->get('date_filtering', 'off');
-		if ($date_filtering !== 'off') 
-                {
-			$model->setState('filter.date_filtering', $date_filtering);
-			$model->setState('filter.date_field', $this->params->get('date_field', 'a.created'));
-			$model->setState('filter.start_date_range', $this->params->get('start_date_range', '1000-01-01 00:00:00'));
-			$model->setState('filter.end_date_range', $this->params->get('end_date_range', '9999-12-31 23:59:59'));
-			$model->setState('filter.relative_date', $this->params->get('relative_date', 30));
-		}
-
-		// Filter by language
-		$model->setState('filter.language', $app->getLanguageFilter());
-
-                if ($items = $model->getItems())
-                {
+                        $model->setState('filter.date_filtering', $date_filtering);
+                        $model->setState('filter.date_field', $this->params->get('date_field', 'a.created'));
+                        $model->setState('filter.start_date_range', $this->params->get('start_date_range', '1000-01-01 00:00:00'));
+                        $model->setState('filter.end_date_range', $this->params->get('end_date_range', '9999-12-31 23:59:59'));
+                        $model->setState('filter.relative_date', $this->params->get('relative_date', 30));
                 }
+                                
+		// Additional filters.
+		$model->setState('filter.featured', $this->params->get('show_featured', 'show'));
 
-		return $items;                
+                $this->items = $model->getItems();
+
+                return $this->items;                
 	}
 }
