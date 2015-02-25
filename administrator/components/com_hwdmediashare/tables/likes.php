40eb286bb8f6edda1e6b96b51ee3080d9427c9f5
Dave Horsfall <dave@horsfall.eu>
2015-02-25 10:59:54 +0000

    Added new likes table class.

---
+++ administrator/components/com_hwdmediashare/tables/likes.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package     Joomla.administrator
+ * @subpackage  Component.hwdmediashare
+ *
+ * @copyright   Copyright (C) 2013 Highwood Design Limited. All rights reserved.
+ * @license     GNU General Public License http://www.gnu.org/copyleft/gpl.html
+ * @author      Dave Horsfall
+ */
+
+defined('_JEXEC') or die;
+
+class hwdMediaShareTableLikes extends JTable
+{
+	/**
+	 * Class constructor. Overridden to explicitly set the table and key fields.
+	 *
+	 * @access  public
+	 * @param   JDatabaseDriver  $db  JDatabaseDriver object.
+         * @return  void
+	 */ 
+	public function __construct($db)
+	{
+		parent::__construct('#__hwdms_likes', 'id', $db);
+	}
+}
