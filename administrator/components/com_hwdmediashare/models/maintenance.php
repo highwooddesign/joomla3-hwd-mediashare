6d2dd1c88749bab36c5d9de3f5392bf7dbdaecb1
Dave Horsfall <dave@horsfall.eu>
2015-02-25 10:54:48 +0000

    Added new maintenance functions to remote old legacy plugins; remote_videogooglecom, remote_ukextremecom, mod_media_tags, legacy "hwdMediaShare Addons" package Added new maintenance functions to optimise database indexes. We've added the generation of fulltext index on #__hwdms_media for full text relation searches. We've also added indexes on 'idx_element_id' and 'idx_element_type' columns in the #__hwdms_category_map table.

--- administrator/components/com_hwdmediashare/models/maintenance.php
+++ administrator/components/com_hwdmediashare/models/maintenance.php
@@ -178,6 +178,180 @@ class hwdMediaShareModelMaintenance extends JModelLegacy
                         }
 		}
                 
+                // Find remote_videogooglecom.
+		$row = JTable::getInstance('extension');
+		$eid = $row->find(array('type' => 'plugin', 'folder' => 'hwdmediashare', 'element' => 'remote_videogooglecom'));
+                if ($eid)
+		{
+                        $result = $installer->uninstall('plugin', $eid);               
+                        if ($result === false)
+                        {
+                                $this->setError($installer->getError());
+                                return false;                               
+                        }
+		}
+                
+                // Find remote_ukextremecom.
+		$row = JTable::getInstance('extension');
+		$eid = $row->find(array('type' => 'plugin', 'folder' => 'hwdmediashare', 'element' => 'remote_ukextremecom'));
+                if ($eid)
+		{
+                        $result = $installer->uninstall('plugin', $eid);               
+                        if ($result === false)
+                        {
+                                $this->setError($installer->getError());
+                                return false;                               
+                        }
+		}
+                
+                // Find mod_media_tags.
+		$row = JTable::getInstance('extension');
+		$eid = $row->find(array('type' => 'module', 'element' => 'mod_media_tags'));
+                if ($eid)
+		{
+                        $result = $installer->uninstall('module', $eid);               
+                        if ($result === false)
+                        {
+                                $this->setError($installer->getError());
+                                return false;                               
+                        }
+		}
+                
+                // Find legacy "hwdMediaShare Addons" package.
+		$row = JTable::getInstance('extension');
+		$eid = $row->find(array('type' => 'package', 'element' => 'pkg_hwdmediashare'));
+                if ($eid)
+		{
+                        $query = $db->getQuery(true);
+
+                        $conditions = array(
+                            $db->quoteName('type') . ' = ' . $db->quote('package'), 
+                            $db->quoteName('element') . ' = ' . $db->quote('pkg_hwdmediashare')
+                        );
+
+                        $query->delete($db->quoteName('#__extensions'));
+                        $query->where($conditions);
+
+                        try
+                        {
+                                $db->setQuery($query);
+                                $result = $db->execute();                 
+                        }
+                        catch (RuntimeException $e)
+                        {
+                                $this->setError($e->getMessage());
+                                return false;                            
+                        }
+
+                        if ($result)
+                        {
+                                $file = JPATH_SITE.'/administrator/manifests/packages/pkg_hwdmediashare.xml';
+                                if (!JFile::delete($file))
+                                {
+                                        $this->setError(JText::sprintf('FILES_JOOMLA_ERROR_FILE_FOLDER', $file));
+                                }
+                        }
+		}
+
                 return true;
+        }   
+        
+        /**
+	 * Method to optimise database indexes
+         * 
+         * @access  public
+	 * @return  boolean  True if successful, false if an error occurs.
+	 */
+	public function databaseIndexOptimisation()
+	{
+                // Initialise variables.
+                $app = JFactory::getApplication();
+                $db = JFactory::getDbo();
+
+                // Fulltext idx_searches #__hwdms_media
+                $query = 'SHOW INDEX FROM ' . $db->quoteName('#__hwdms_media') . ' WHERE ' . $db->quoteName('Key_name') . ' = ' . $db->quote('idx_searches');
+                try
+                {
+                        $db->setQuery($query);
+                        $result = $db->loadAssoc();                        
+                }
+                catch (RuntimeException $e)
+                {
+                        $app->enqueueMessage($e->getMessage());
+                        return false;                         
+                }
+
+                if (!isset($result['Key_name']) || $result['Key_name'] != 'idx_searches')
+                {
+                        $query = 'ALTER TABLE ' . $db->quoteName('#__hwdms_media') . ' ADD FULLTEXT idx_searches (' . $db->quoteName('title') . ', ' . $db->quoteName('description') . ')';
+                        try
+                        {
+                                $db->setQuery($query);
+                                $result = $db->execute();                        
+                        }
+                        catch (RuntimeException $e)
+                        {
+                                $app->enqueueMessage($e->getMessage());
+                                return false;                         
+                        }
+                }
+
+                // Index idx_element_id #__hwdms_category_map
+                $query = 'SHOW INDEX FROM ' . $db->quoteName('#__hwdms_category_map') . ' WHERE ' . $db->quoteName('Key_name') . ' = ' . $db->quote('idx_element_id');
+                try
+                {
+                        $db->setQuery($query);
+                        $result = $db->loadAssoc();                        
+                }
+                catch (RuntimeException $e)
+                {
+                        $app->enqueueMessage($e->getMessage());
+                        return false;                         
+                }
+
+                if (!isset($result['Key_name']) || $result['Key_name'] != 'idx_element_id')
+                {
+                        $query = 'ALTER TABLE ' . $db->quoteName('#__hwdms_category_map') . ' ADD KEY idx_element_id  (' . $db->quoteName('element_id') . ')';
+                        try
+                        {
+                                $db->setQuery($query);
+                                $result = $db->execute();                        
+                        }
+                        catch (RuntimeException $e)
+                        {
+                                $app->enqueueMessage($e->getMessage());
+                                return false;                         
+                        }
+                }
+                
+                // Index idx_element_type #__hwdms_category_map
+                $query = 'SHOW INDEX FROM ' . $db->quoteName('#__hwdms_category_map') . ' WHERE ' . $db->quoteName('Key_name') . ' = ' . $db->quote('idx_element_type');
+                try
+                {
+                        $db->setQuery($query);
+                        $result = $db->loadAssoc();                        
+                }
+                catch (RuntimeException $e)
+                {
+                        $app->enqueueMessage($e->getMessage());
+                        return false;                         
+                }
+
+                if (!isset($result['Key_name']) || $result['Key_name'] != 'idx_element_type')
+                {
+                        $query = 'ALTER TABLE ' . $db->quoteName('#__hwdms_category_map') . ' ADD KEY idx_element_type  (' . $db->quoteName('element_type') . ')';
+                        try
+                        {
+                                $db->setQuery($query);
+                                $result = $db->execute();                        
+                        }
+                        catch (RuntimeException $e)
+                        {
+                                $app->enqueueMessage($e->getMessage());
+                                return false;                         
+                        }
+                }
+                
+                return true;                
         }          
 }
