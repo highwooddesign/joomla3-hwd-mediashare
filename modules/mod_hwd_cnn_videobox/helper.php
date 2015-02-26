9edb4dba45b5c7abf14a8e98c3bdabe1401d2d4f
Dave Horsfall <dave@horsfall.eu>
2015-02-26 08:46:20 +0000

    Updated CNN videoBox helper.

--- modules/mod_hwd_cnn_videobox/helper.php
+++ modules/mod_hwd_cnn_videobox/helper.php
@@ -12,186 +12,164 @@ defined('_JEXEC') or die;
 
 class modHwdCnnVideoBoxHelper extends JObject
 {
-	/**
-	 * Modal data
-	 * @var array
-	 */    
-	public $params;
-	public $module;
-	public $url;
-	public $container;
-        
+        /**
+	 * Class constructor.
+	 *
+	 * @access  public
+	 * @param   array   $module  The module object.
+	 * @param   array   $params  The module parameters object.
+         * @return  void
+	 */       
 	public function __construct($module, $params)
-	{
+	{                
                 // Load caching.
-                $cache = JFactory::getCache();
+                $cache = JFactory::getCache('mod_hwd_cnn_videobox');
                 $cache->setCaching(1);
 
-                JLoader::register('JHtmlString', JPATH_LIBRARIES.'/joomla/html/html/string.php');
-
-                // Get data.
+                // Get data.              
                 $this->module = $module;                
                 $this->params = $params;                
-                $this->items = $items = $cache->call(array($this, 'getItems'));
-		$this->url = JURI::root().'modules/mod_hwd_cnn_videobox/';
-		$this->container = 'mod_hwd_cnn_videobox'.$module->id;
+                $this->items = $cache->call(array($this, 'getItems'), $params);
 
                 // Add assets to the head tag.
-                $this->addHead();              
+                $this->addHead();  
 	}
 
+        /**
+	 * Method to add assets to the head.
+	 *
+	 * @access  public
+         * @return  void
+	 */         
 	public function addHead()
-	{
+	{           
                 JHtml::_('bootstrap.tooltip');
                 $doc = JFactory::getDocument();
-                $doc->addScript(JURI::root().'modules/mod_hwd_cnn_videobox/js/jquery.magnific-popup.js');
-                $doc->addScript(JURI::root().'modules/mod_hwd_cnn_videobox/js/aspect.js');
-                $doc->addStylesheet(JURI::root().'modules/mod_hwd_cnn_videobox/css/magnific-popup.css');
-                $doc->addStylesheet(JURI::root().'modules/mod_hwd_cnn_videobox/css/strapped.3.hwd.css');
+                $doc->addScript(JURI::root() . 'modules/mod_hwd_cnn_videobox/js/jquery.magnific-popup.js');
+                $doc->addScript(JURI::root() . 'modules/mod_hwd_cnn_videobox/js/aspect.js');
+                $doc->addStylesheet(JURI::root() . 'modules/mod_hwd_cnn_videobox/css/magnific-popup.css');
+                $doc->addStylesheet(JURI::root() . 'modules/mod_hwd_cnn_videobox/css/strapped.3.hwd.css');
+                
+                // Extract the layout.
+                list($template, $layout) = explode(':', $this->params->get('layout', '_:default'));
+                
+                // Check for layout stylesheet.
+                if (file_exists(__DIR__ . '/css/' . $layout . '.css'))
+                {
+                        $doc->addStyleSheet(JURI::base( true ) . '/modules/mod_hwd_cnn_videobox/css/' . $layout . '.css');
+                }
+
                 $doc->addScriptDeclaration("
-jQuery.noConflict();
-(function( $ ) {
-  $(function() {
-    $(document).ready(function() {
-      $('.popup-title-" . $this->module->id . "').magnificPopup({ 
-        type: 'iframe',
-        mainClass: 'hwd-cnn-popup',
-        gallery: {
-          enabled: true
+jQuery(document).ready(function() {
+  jQuery('.popup-title-" . $this->module->id . "').magnificPopup({ 
+    type: 'iframe',
+    mainClass: 'hwd-cnn-popup',
+    iframe: {     
+      patterns: {
+        generic: {
+          id: function(url) {        
+            return url;
+          },
+          src: '%id%'
         }
-      }); 
-      $('.popup-thumbnail-" . $this->module->id . "').magnificPopup({ 
-        type: 'iframe',
-        mainClass: 'hwd-cnn-popup',
-        gallery: {
-          enabled: true
+      }
+    },
+    gallery: {
+      enabled: true
+    }
+  }); 
+  jQuery('.popup-thumbnail-" . $this->module->id . "').magnificPopup({ 
+    type: 'iframe',
+    mainClass: 'hwd-cnn-popup',
+    iframe: {
+      patterns: {
+        cnn: {
+          id: function(url) {        
+            return url;
+          },
+          src: '%id%'
         }
-      });       
-    });
-  });
-})(jQuery);");                
+      }
+    },
+    gallery: {
+      enabled: true
+    }
+  });       
+});");                
 	}
 
+        /**
+	 * Method to get a list of media items.
+	 *
+	 * @access  public
+         * @return  object  A list of media items.
+	 */          
 	public function getItems()
 	{
                 $feed = $this->getFeed();
-
                 $counter = 0;
+                
                 $data = new DOMDocument();
                 if ($data->load($feed))
-                {
+                {       
                         foreach ($data->getElementsByTagName('item') as $video)
                         {
                                 $object                    = new stdClass();
-                                
+
                                 $object->title             = ($video->getElementsByTagName('title')->item(0) ? $video->getElementsByTagName('title')->item(0)->nodeValue : '');
-                                $object->description       = ($video->getElementsByTagName('description')->item(0) ? JHtmlString::truncate($video->getElementsByTagName('description')->item(0)->nodeValue, 300, true, false) : '');
-                                $object->category          = ($video->getElementsByTagName('category')->item(0) ? $video->getElementsByTagName('category')->item(0)->nodeValue : '');
-                                
-                                $object->publishedDate        = ($video->getElementsByTagName('pubDate')->item(0) ? $video->getElementsByTagName('pubDate')->item(0)->nodeValue : '');
-                                $object->publishedDate        = substr($object->publishedDate, 0, strlen($object->publishedDate)-4);
-                                    
                                 $object->id                = ($video->getElementsByTagName('guid')->item(0) ? $video->getElementsByTagName('guid')->item(0)->nodeValue : '');
-                                $pos = strpos($object->id, "#/video/");
-                                $pos_start = $pos + 8;
-                                $object->id = substr($object->id, $pos_start, strlen($object->id));
+                                $object->description	   = ($video->getElementsByTagName('description')->item(0) ? JHtml::_('string.truncate', $video->getElementsByTagName('description')->item(0)->nodeValue, 300, true, false) : '');
+ 
+                                // Extract ID.
+                                preg_match('#http://www.cnn.com/video/\#/video/(.*)#s', $object->id, $matches);
+                                if(isset($matches[1])) 
+                                {
+                                        $object->id = $matches[1];   
+                                        
+                                        // Define video details resource.
+                                        $url = 'http://edition.cnn.com/video/data/3.0/video/'.$object->id.'/index.xml';
+                                        $buffer = file_get_contents($url);
+ 
+                                        // Get thumbnail.
+                                        preg_match('#<image height="360" width="640" name="640x360">(.*)<\/image>#siU', $buffer, $matches);
+                                        if (!empty($matches[1]))
+                                        {
+                                                $object->thumbnail = $matches[1];   
+                                        }
+                
+                                        // Get duration.
+                                        preg_match('#<length>(.*)<\/length>#siU', $buffer, $matches);
+                                        if (!empty($matches[1]))
+                                        {
+                                                $object->duration = $matches[1];   
+                                        }   
+                                }
 
-                                // Get thumbnail & duration
-                                $urlXml = 'http://edition.cnn.com/video/data/3.0/video/'.$object->id.'/index.xml';
-                                $buffer = $this->getBuffer($urlXml);                              
-
-                                $pos_u = strpos($buffer, 'name="640x360');
-                                $pos_start = $pos_u + 15; 
-                                $pos_end = strpos($buffer, "</image>");
-                                $length = $pos_end - $pos_start;
-                                $object->thumbnail         = substr($buffer, $pos_start, $length);
-                                
-                                $pos_u = strpos($buffer, "<length>");
-                                $pos_start = $pos_u + 8;                                        
-                                $pos_end = strpos($buffer, "</length>");
-                                $length = $pos_end - $pos_start;                                    
-                                $object->duration          = substr($buffer, $pos_start, $length);     
-                                
                                 // Check data is valid
                                 if (!$object->duration) continue;
                                 if (!$object->thumbnail) continue;
 
                                 $items[] = $object;
+                                
                                 $counter++;
-
-                                if ($counter >= $this->get('params')->get('count', 6))
+                                if ($counter >= $this->params->get('count', 6))
                                 {
                                         break;
                                 }
                         }
                 }
-
-		return $items;
+        
+		return $items;     
 	}
 
-	public function getBuffer($url)
-	{
-                // A large number of CURL installations will not support SSL, so switch back to http
-                $url = str_replace("https", "http", $url);
-
-                if ($url)
-                {
-                        if (function_exists('curl_init'))
-                        {
-                                $useragent = "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1)";
-
-                                $curl_handle = curl_init();
-                                curl_setopt($curl_handle, CURLOPT_URL, $url);
-                                curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 30);
-                                curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
-                                curl_setopt($curl_handle, CURLOPT_REFERER, 'cnn.com');
-                                curl_setopt($curl_handle, CURLOPT_USERAGENT, $useragent);
-                                $buffer = curl_exec($curl_handle);
-                                curl_close($curl_handle);
-
-                                if (!empty($buffer))
-                                {
-                                        return $buffer;
-                                }
-                                else
-                                {
-                                        return false;
-                                }
-                        }
-                }
-
-		return false;
-	}
-
+        /**
+	 * Method to get the feed URI.
+	 *
+	 * @access  public
+         * @return  string  The feed URI
+	 */         
 	public function getFeed()
-	{
-                // Only one feed
-                $feed = 'http://rss.cnn.com/rss/cnn_freevideo.rss';
-                return $feed;
+	{  
+                return 'http://rss.cnn.com/rss/cnn_freevideo.rss';
 	}
-
-        public function getCategory($category)
-        {
-                switch ($category)
-                {
-                        case 'bestoftv':
-                            return 'Best of TV';
-                        case 'us':
-                            return 'U.S.';                    
-                        case 'world':
-                            return 'World';
-                        case 'crime':
-                            return 'Crime';
-                        case 'education':
-                            return 'Education';
-                        case 'international':
-                            return 'International';
-                        case 'sports':
-                            return 'Sports';
-                        case 'tech':
-                            return 'Tech';
-                        default: 
-                            return $category;
-                }           
-        }
 }
