f1647dbb44caaec31637f443aa2d282499103683
Dave Horsfall <dave@horsfall.eu>
2015-02-26 08:50:45 +0000

    Updated CNN videoBox layouts.

--- modules/mod_hwd_cnn_videobox/tmpl/default.php
+++ modules/mod_hwd_cnn_videobox/tmpl/default.php
@@ -9,41 +9,36 @@
  */
 
 defined('_JEXEC') or die;
-
-// Define Joomla document object, and load the template assets.
-$doc = JFactory::getDocument();
-$doc->addStylesheet($helper->get('url').'css/default.css');
 ?>
-<div class="row mod_hwd_cnn_videobox_default">
+<div class="row hwd-strapped3 hwd-cnn-videobox-default">
 <?php foreach ($helper->items as $id => $item) : ?>
   <!-- Clear the columns if their content doesn't match in height -->
-  <?php if ($id % 2 == 0) :?><div class="clearfix visible-xs"></div><?php endif; ?>
-  <?php if ($id % 3 == 0) :?><div class="clearfix visible-sm"></div><?php endif; ?>
-  <?php if ($id % $helper->params->get('columns', 3) == 0) :?><div class="clearfix visible-md visible-lg"></div><?php endif; ?>    
-  <div class="cell col-xs-6 col-sm-4 col-md-<?php echo intval(12/$helper->params->get('columns', 3)); ?>">
-    <div class="media-item hasTooltip" title="<?php echo JHtml::tooltipText($item->title, JHtmlString::truncate($item->description, 300, true, false)); ?>">
+  <?php if ($id % 1 == 0) :?><div class="clearfix visible-xs"></div><?php endif; ?>
+  <?php if ($id % 2 == 0) :?><div class="clearfix visible-sm"></div><?php endif; ?>
+  <?php if ($id % $params->get('columns', 3) == 0) :?><div class="clearfix visible-md visible-lg"></div><?php endif; ?>    
+  <div class="cell col-xs-12 col-sm-6 col-md-<?php echo intval(12/$params->get('columns', 3)); ?>">
+    <div class="media-item hasTooltip" title="<?php echo JHtml::tooltipText($item->title, JHtml::_('string.truncate', $item->description, 300, true, false)); ?>">
       <div class="media-aspect"></div>        
-      <?php if ($item->duration > 0) :?>
+      <?php if ($item->duration > 0 && $params->get('show_duration', 1)) :?>
       <div class="media-duration">
          <?php echo $item->duration; ?>
       </div>
       <?php endif; ?>
-      <a class="popup-thumbnail-<?php echo $module->id; ?>" href="http://i.cdn.turner.com/cnn/.element/apps/cvp/3.0/swf/cnn_416x234_embed.swf?context=embed_edition&videoId=<?php echo $item->id; ?>" id="mb<?php echo $id; ?>" title="<?php echo htmlspecialchars($item->title); ?>">
+      <a class="popup-thumbnail-<?php echo $module->id; ?>" href="http://edition.cnn.com/video/api/embed.html#/video/<?php echo $item->id; ?>" id="mb<?php echo $id; ?>" title="<?php echo htmlspecialchars($item->title); ?>">
          <img src="<?php echo $item->thumbnail; ?>" border="0" alt="" title="" class="media-thumb"  />
-         <?php echo ($helper->params->get('icon', 1) ? '<span class="media-link-span"></span></span>' : ''); ?>
+         <?php echo ($params->get('icon', 1) ? '<span class="media-link-span"></span></span>' : ''); ?>
       </a>
     </div>
     <div class="clear"></div>        
-    <?php if ($helper->params->get('show_title') != 'hide') :?>
-      <div class="hasTooltip title" title="<?php echo JHtml::tooltipText($item->title, JHtmlString::truncate($item->description, 300, true, false)); ?>">
-        <a class="popup-title-<?php echo $module->id; ?>" href="http://i.cdn.turner.com/cnn/.element/apps/cvp/3.0/swf/cnn_416x234_embed.swf?context=embed_edition&videoId=<?php echo $item->id; ?>" id="mb<?php echo $id; ?>" title="<?php echo htmlspecialchars($item->title); ?>">
-          <?php echo JHtmlString::truncate($item->title, 50); ?> 
+    <?php if ($params->get('show_title') != 'hide') :?>
+      <div class="hasTooltip title" title="<?php echo JHtml::tooltipText($item->title, JHtml::_('string.truncate', $item->description, 300, true, false)); ?>">
+        <a class="popup-title-<?php echo $module->id; ?>" href="http://edition.cnn.com/video/api/embed.html#/video/<?php echo $item->id; ?>" id="mb<?php echo $id; ?>" title="<?php echo htmlspecialchars($item->title); ?>">
+          <?php echo JHtml::_('string.truncate', $item->title, 50); ?> 
         </a>
       </div>
     <?php endif; ?>
     <div class="clear"></div>
-    <?php if ($helper->params->get('show_category') != 'hide'):?><span class="small badge pull-right"><?php echo $helper->getCategory($item->category); ?></span><?php endif; ?>
-    <?php if ($helper->params->get('show_date') != 'hide'):?><span class="small"><?php echo JHtml::_('date.relative', $item->publishedDate); ?></span><?php endif; ?>
+    <?php if ($params->get('show_description', 1)):?><p><?php echo $item->description; ?></p><?php endif; ?>
  </div>
  <?php endforeach; ?>
 </div>
