09fc1c7fe6e6b1f8326d24f72f4505b12c08de19
Dave Horsfall <dave@horsfall.eu>
2015-04-29 11:20:24 +0100

    Updated the maintenance submitbutton.js to use the jquery namespace.

--- administrator/components/com_hwdmediashare/views/maintenance/submitbutton.js
+++ administrator/components/com_hwdmediashare/views/maintenance/submitbutton.js
@@ -1,68 +1,64 @@
-(function($) {
-    $(document).ready(function () {
-        
-        Joomla.submitbutton = function(task)
-        {      
-            if (task == "maintenance.run")
+Joomla.submitbutton = function(task)
+{      
+    if (task == "maintenance.run")
+    {
+        jQuery("[id^=ajax-container]").html("")
+        post("cleancategorymap")
+        post("emptyuploadtokens")
+        post("purgeoldprocesses")
+        post("uninstalloldextensions")
+        post("databaseindexoptimisation")
+    }
+    else
+    {
+        Joomla.submitform(task);
+        return true;
+    }
+
+    return false;
+}
+
+function post(task) {
+    setTimeout(function() {
+        jQuery("#ajax-container-" + task).html("").addClass("ajax-loading");
+
+        var url = location.href;
+        var posting = jQuery.ajax({
+            async: false,
+            url: url,
+            data: 
             {
-                $("[id^=ajax-container]").html("")
-                post("cleancategorymap")
-                post("emptyuploadtokens")
-                post("purgeoldprocesses")
-                post("uninstalloldextensions")
+                    option: "com_hwdmediashare",
+                    task: "maintenance.run",
+                    format: "raw",
+                    maintenance: task
             }
-            else
-            {
-                Joomla.submitform(task);
-                return true;
+        });
+
+        posting.done(function(data) {
+            try {
+                var results = jQuery.parseJSON(data);
+                if (results.status == "success") {
+                    jQuery("#ajax-container-" + task).empty();
+                    jQuery("#ajax-container-" + task).removeClass('ajax-loading');
+                    jQuery("#ajax-container-" + task).html('<span class="label label-success">Success</span>'); 
+                } else {
+                    jQuery("#ajax-container-" + task).empty();
+                    jQuery("#ajax-container-" + task).removeClass('ajax-loading');
+                    jQuery("#ajax-container-" + task).html('<span class="label label-danger">Failed</span>');
+                }                
             }
+            catch(err) {
+                jQuery("#ajax-container-" + task).empty();
+                jQuery("#ajax-container-" + task).removeClass('ajax-loading');
+                jQuery("#ajax-container-" + task).html('<span class="label label-danger">Failed</span>');
+            }
+        });
 
-            return false;
-        }
-
-        function post(task) {
-            setTimeout(function() {
-                $("#ajax-container-" + task).html("").addClass("ajax-loading");
-
-                var url = location.href;
-                var posting = $.ajax({
-                    async: false,
-                    url: url,
-                    data: 
-                    {
-                            option: "com_hwdmediashare",
-                            task: "maintenance.run",
-                            format: "raw",
-                            maintenance: task
-                    }
-                });
-
-                posting.done(function(data) {
-                    try {
-                        var results = jQuery.parseJSON(data);
-                        if (results.status == "success") {
-                            $("#ajax-container-" + task).empty();
-                            $("#ajax-container-" + task).removeClass('ajax-loading');
-                            $("#ajax-container-" + task).html('<span class="label label-success">Success</span>'); 
-                        } else {
-                            $("#ajax-container-" + task).empty();
-                            $("#ajax-container-" + task).removeClass('ajax-loading');
-                            $("#ajax-container-" + task).html('<span class="label label-danger">Failed</span>');
-                        }                
-                    }
-                    catch(err) {
-                        $("#ajax-container-" + task).empty();
-                        $("#ajax-container-" + task).removeClass('ajax-loading');
-                        $("#ajax-container-" + task).html('<span class="label label-danger">Failed</span>');
-                    }
-                });
-
-                posting.fail(function(data) {
-                    $("#ajax-container-" + task).empty();
-                    $("#ajax-container-" + task).removeClass('ajax-loading');
-                    $("#ajax-container-" + task).html('<span class="label label-danger">Failed</span>');
-                });
-            }, 1);
-        }
-    });
-})(jQuery);
+        posting.fail(function(data) {
+            $("#ajax-container-" + task).empty();
+            $("#ajax-container-" + task).removeClass('ajax-loading');
+            $("#ajax-container-" + task).html('<span class="label label-danger">Failed</span>');
+        });
+    }, 1);
+}
