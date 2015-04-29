087dd911302fac516b265d2f60f48dafb4035861
Dave Horsfall <dave@horsfall.eu>
2015-04-29 10:15:19 +0100

    Removed the '#__hwdms_activities' query from the delete method for groups.

--- administrator/components/com_hwdmediashare/models/group.php
+++ administrator/components/com_hwdmediashare/models/group.php
@@ -312,12 +312,6 @@ class hwdMediaShareModelGroup extends JModelAdmin
 		// Loop through keys and generate queries to execute.
 		foreach ($pks as $i => $pk)
 		{
-                        // Delete records from activities.
-                        $queries[] = $db->getQuery(true)
-                                        ->delete('#__hwdms_activities')
-                                        ->where('element_type = ' . $db->quote(3))
-                                        ->where('element_id = ' . $db->quote($pk));
-
                         // Delete records from field values.
                         $queries[] = $db->getQuery(true)
                                         ->delete('#__hwdms_fields_values')
