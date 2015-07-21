a4b37663ee7e5c2289d3a86f1427ec5da31fe5b1
Dave Horsfall <dave@horsfall.eu>
2015-07-21 10:13:50 +0100

    Added new migrateLegacyTags method to maintenance model.

--- administrator/components/com_hwdmediashare/models/maintenance.php
+++ administrator/components/com_hwdmediashare/models/maintenance.php
@@ -353,5 +353,113 @@ class hwdMediaShareModelMaintenance extends JModelLegacy
                 }
                 
                 return true;                
-        }          
+        }   
+
+        /**
+	 * Method to migrate legacy tags to native Joomla tags.
+         * 
+         * @access  public
+	 * @return  boolean  True if successful, false if an error occurs.
+	 */
+	public function migrateLegacyTags()
+	{
+		// Initialiase variables.
+                $user = JFactory::getUser();
+                $db = JFactory::getDBO();
+                $date = JFactory::getDate();
+                $app = JFactory::getApplication();
+
+                $db->setQuery('SHOW TABLES');
+                $tables = $db->loadColumn();
+
+                // Check the legacy tag tables exist.
+                if (in_array($app->getCfg( 'dbprefix' ).'hwdms_tag_map', $tables) && in_array($app->getCfg( 'dbprefix' ).'hwdms_tags', $tables)) 
+                {
+                        // Load all media tags.
+                        $query = $db->getQuery(true)
+                                ->select('map.*, tags.tag')
+                                ->from('#__hwdms_tag_map AS map')
+                                ->join('LEFT', '#__hwdms_tags AS tags ON tags.id = map.tag_id')
+                                ->where('element_type = ' . $db->quote(1));
+                        try
+                        {                
+                                $db->setQuery($query);
+                                $tags = $db->loadObjectList();
+                        }
+                        catch (Exception $e)
+                        {
+                                $this->setError($e->getMessage());
+                                return false;
+                        }
+
+                        foreach ($tags as $i => $tag)
+                        {
+                                if (empty($tag->tag))
+                                {
+                                        continue;
+                                }
+                            
+                                if ($user->authorise('core.edit', 'com_hwdmediashare.media.'. (int) $tag->id))
+                                {
+                                        // Get a table instance.
+                                        JTable::addIncludePath(JPATH_ADMINISTRATOR.'/components/com_hwdmediashare/tables');
+                                        JObserverMapper::addObserverClassToClass('JTableObserverTags', 'hwdMediaShareTableMedia', array('typeAlias' => 'com_hwdmediashare.media'));                
+                                        $table = JTable::getInstance('Media', 'hwdMediaShareTable');
+
+                                        // Attempt to load the table row.
+                                        $return = $table->load($tag->element_id);
+
+                                        // Check for a table object error.
+                                        if ($return === false && $table->getError())
+                                        {
+                                                $this->setError($table->getError());
+                                                return false;
+                                        }
+
+                                        // Insert tag into native Joomla tagging system.
+                                        if ($table->id > 0)
+                                        {
+                                                $jtags = array($tag->tag);
+                                                array_walk($jtags, function(&$value, $key) { $value = '#new#' . $value; });
+                                                $tagsHelper = new JHelperTags;
+                                                $tagsHelper->createTagsFromField($jtags);                                
+                                                if (count($tagsHelper->tags))
+                                                {
+                                                        $tagsObserver = $table->getObserverOfClass('JTableObserverTags');   
+                                                        $result = $tagsObserver->setNewTags($tagsHelper->tags, false);
+                                                        
+                                                        if (!$result)
+                                                        {
+                                                                $this->setError($table->getError());
+                                                                return false;
+                                                        }
+                                                }
+                                        }
+
+                                        // Remove legacy tag.
+                                        $query = $db->getQuery(true);
+                                        $conditions = array(
+                                            $db->quoteName('element_type') . ' = ' . $db->quote(1),
+                                            $db->quoteName('element_id') . ' = ' . $db->quote($tag->element_id),
+                                            $db->quoteName('tag_id') . ' = ' . $db->quote($tag->tag_id)
+                                        );
+                                        $query->delete($db->quoteName('#__hwdms_tag_map'));
+                                        $query->where($conditions);
+
+                                        try
+                                        {
+                                                $db->setQuery($query);
+                                                $db->execute();                 
+                                        }
+                                        catch (RuntimeException $e)
+                                        {
+                                                $this->setError($e->getMessage());
+                                                return false;                            
+                                        }
+                                }
+                        }
+                }               
+
+                return true;
+        }   
 }
