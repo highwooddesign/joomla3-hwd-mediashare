952310488c921cdfaeea9c4a385aff0dded77da7
Dave Horsfall <dave@horsfall.eu>
2015-07-21 10:59:34 +0100

    Change to featured/status/published buttons to change class (to facilitate no pointer action) and remove icons. Updated meta data structure to separate likes/dislikes and put actions in a button group. Used JHtml::_('hwdimage.avatar') to create avatar image. Removed &tmpl=component from add new media link.

--- components/com_hwdmediashare/views/group/tmpl/default.php
+++ components/com_hwdmediashare/views/group/tmpl/default.php
@@ -19,12 +19,12 @@ $user = JFactory::getUser();
 $canEdit = ($user->authorise('core.edit', 'com_hwdmediashare.group.'.$this->group->id) || ($user->authorise('core.edit.own', 'com_hwdmediashare.group.'.$this->group->id) && ($this->group->created_user_id == $user->id)));
 $canEditState = $user->authorise('core.edit.state', 'com_hwdmediashare.group.'.$this->group->id);
 $canDelete = ($user->authorise('core.delete', 'com_hwdmediashare.group.'.$this->group->id) || ($user->authorise('core.edit.own', 'com_hwdmediashare.group.'.$this->group->id) && ($this->group->created_user_id == $user->id)));
-$canAddMedia = ((JFactory::getUser()->authorise('hwdmediashare.upload','com_hwdmediashare') || JFactory::getUser()->authorise('hwdmediashare.import','com_hwdmediashare')) && ($this->group->isMember));
+$canAddMedia = ((JFactory::getUser()->authorise('hwdmediashare.upload', 'com_hwdmediashare') || JFactory::getUser()->authorise('hwdmediashare.import', 'com_hwdmediashare')) && ($this->group->isMember));
 $canManageMedia = ($this->group->isAdmin);
 $canManageMembers = ($this->group->isAdmin);
 ?>
 <form action="<?php echo htmlspecialchars(JFactory::getURI()->toString()); ?>" method="post" name="adminForm" id="adminForm">
-  <div id="hwd-container"> <a name="top" id="top"></a>
+  <div id="hwd-container" class="<?php echo $this->pageclass_sfx;?>"> <a name="top" id="top"></a>
     <!-- Media Navigation -->
     <?php echo hwdMediaShareHelperNavigation::getInternalNavigation(); ?>
     <!-- Media Header -->
@@ -35,7 +35,7 @@ $canManageMembers = ($this->group->isAdmin);
       <!-- Buttons -->
       <div class="btn-group pull-right">
         <?php if ($canAddMedia): ?>
-          <a title="<?php echo JText::_('COM_HWDMS_ADD_MEDIA'); ?>" href="<?php echo JRoute::_('index.php?option=com_hwdmediashare&view=upload&tmpl=component&group_id='.(int)$this->group->id); ?>" class="btn"><i class="icon-plus"></i> <?php echo JText::_('COM_HWDMS_ADD_MEDIA'); ?></a>
+          <a title="<?php echo JText::_('COM_HWDMS_ADD_MEDIA'); ?>" href="<?php echo JRoute::_('index.php?option=com_hwdmediashare&view=upload&group_id='.(int)$this->group->id); ?>" class="btn"><i class="icon-plus"></i> <?php echo JText::_('COM_HWDMS_ADD_MEDIA'); ?></a>
         <?php endif; ?>
         <?php if ($canManageMedia): ?>
           <a title="<?php echo JText::_('COM_HWDMS_MANAGE_MEDIA'); ?>" href="<?php echo JRoute::_('index.php?option=com_hwdmediashare&view=groupmedia&tmpl=component&group_id=' . $this->group->id); ?>" class="btn media-popup-iframe-page" ><?php echo JText::_('COM_HWDMS_MANAGE_MEDIA'); ?></a>
@@ -43,14 +43,14 @@ $canManageMembers = ($this->group->isAdmin);
         <?php if ($canManageMembers): ?>
           <a title="<?php echo JText::_('COM_HWDMS_MANAGE_MEMBERS'); ?>" href="<?php echo JRoute::_('index.php?option=com_hwdmediashare&view=groupmembers&tmpl=component&group_id=' . $this->group->id); ?>" class="btn media-popup-iframe-page" ><?php echo JText::_('COM_HWDMS_MANAGE_MEMBERS'); ?></a>
         <?php endif; ?>          
-        <?php if ($this->group->featured) : ?>
-          <div class="btn btn-info"><i class="icon-star"></i> <?php echo JText::_('COM_HWDMS_FEATURED'); ?></div>
+        <?php if ($this->group->featured && $this->params->get('item_meta_featured_status') != '0') : ?>
+          <div class="btn btn-danger btn-noevent"><?php echo JText::_('COM_HWDMS_FEATURED'); ?></div>
         <?php endif; ?> 
         <?php if ($this->group->status != 1) : ?>
-          <div class="btn btn-danger"><i class="icon-notification"></i> <?php echo $this->utilities->getReadableStatus($this->group); ?></div>
+          <div class="btn btn-danger btn-noevent"><?php echo $this->utilities->getReadableStatus($this->group); ?></div>
         <?php endif; ?>   
         <?php if ($this->group->published != 1) : ?>
-          <div class="btn btn-danger"><i class="icon-unpublish"></i> <?php echo JText::_('COM_HWDMS_UNPUBLISHED'); ?></div>
+          <div class="btn btn-danger btn-noevent"><?php echo JText::_('COM_HWDMS_UNPUBLISHED'); ?></div>
         <?php endif; ?>           
         <?php if ($user->id && $this->group->isMember) : ?>
           <a title="<?php echo JText::_('COM_HWDMS_LEAVE_GROUP'); ?>" href="<?php echo JRoute::_('index.php?option=com_hwdmediashare&task=groups.leave&id=' . $this->group->id . '&return=' . $this->return . '&tmpl=component&'.JSession::getFormToken().'=1'); ?>" class="btn"><i class="icon-user"></i> <?php echo JText::_('COM_HWDMS_LEAVE_GROUP'); ?></a>
@@ -106,7 +106,8 @@ $canManageMembers = ($this->group->isAdmin);
             <?php echo JText::_('COM_HWDMS_NO_MEMBERS'); ?>
             <?php endif; ?>
             <?php foreach ($this->members as $id => $item) : ?>   
-              <a href="<?php echo JRoute::_(hwdMediaShareHelperRoute::getUserRoute($item->id)); ?>" class="pull-left"><img width="50" height="50" border="0" src="<?php echo JRoute::_($this->utilities->getAvatar($item)); ?>" class="hasTooltip" title="<?php echo JHtml::tooltipText($this->escape($item->name)); ?>" /></a>
+              <a href="<?php echo JRoute::_(hwdMediaShareHelperRoute::getUserRoute($item->id)); ?>" class="pull-left">
+                <?php echo JHtml::_('hwdimage.avatar', $item->id, 50); ?></a>
             <?php endforeach; ?>              
           </div>
           <div class="btn-toolbar row-fluid">
@@ -118,7 +119,8 @@ $canManageMembers = ($this->group->isAdmin);
           <ul class="media-activity-list">
             <?php foreach ($this->activities as $activity) : ?>
               <li class="media-activity-item">
-                <a href="<?php echo JRoute::_(hwdMediaShareHelperRoute::getUserRoute($activity->actor)); ?>" class="media-activity-avatar"><img width="50" height="50" border="0" src="<?php echo JRoute::_($this->utilities->getAvatar(JFactory::getUser($activity->actor))); ?>" /></a>
+                <a href="<?php echo JRoute::_(hwdMediaShareHelperRoute::getUserRoute($activity->actor)); ?>" class="media-activity-avatar">
+                  <?php echo JHtml::_('hwdimage.avatar', $activity->actor, 50); ?></a>
                 <p class="media-activity-desc"><?php echo hwdMediaShareActivities::renderActivityHtml($activity); ?></p>
                 <p class="media-activity-date"><?php echo JHtml::_('hwddate.relative', $activity->created); ?></p>
               </li>
@@ -136,23 +138,25 @@ $canManageMembers = ($this->group->isAdmin);
       <?php if ($this->params->get('item_meta_description') != '0') :?>
         <?php echo JHtml::_('content.prepare', $this->group->description); ?>
       <?php endif; ?> 
-      <?php if ($this->params->get('item_meta_author') != '0' || $this->params->get('item_meta_created') != '0' || $this->params->get('item_meta_hits') != '0' || $this->params->get('item_meta_likes') != '0' || $this->params->get('item_meta_report') != '0') : ?>
+      <?php if ($this->params->get('item_meta_likes') != '0' || $this->params->get('item_meta_dislikes') != '0') :?>
+      <div class="pull-right">
+        <div class="media-info-likes pull-right">      
+          <?php if ($this->params->get('item_meta_likes') != '0') :?><i class="icon-thumbs-up"></i> <span id="media-likes"><?php echo (int) $this->group->likes; ?></span><?php endif; ?>
+          <?php if ($this->params->get('item_meta_dislikes') != '0') :?><i class="icon-thumbs-down"></i> <span id="media-dislikes"><?php echo (int) $this->group->dislikes; ?></span><?php endif; ?>
+        </div>
+      </div>       
+      <?php endif; ?> 
+      <?php if ($this->params->get('item_meta_media_count') != '0' || $this->params->get('item_meta_author') != '0' || $this->params->get('item_meta_created') != '0' || $this->params->get('item_meta_hits') != '0') :?>
       <dl class="media-info">
-        <dt class="media-info-term"><?php echo JText::_('COM_HWDMS_DETAILS'); ?> </dt>
-        <?php if ($this->params->get('list_meta_likes') != '0') :?>
-          <dd class="media-info-likes">
-            <i class="icon-thumbs-up"></i> <span id="media-likes"><?php echo (int) $this->group->likes; ?></span>
-            <i class="icon-thumbs-down"></i> <span id="media-dislikes"><?php echo (int) $this->group->dislikes; ?></span>
-          </dd>
-        <?php endif; ?>        
-        <?php if ($this->params->get('list_meta_author') != '0' || $this->params->get('list_meta_created') != '0') : ?>
+        <dt class="media-info-term"><?php echo JText::_('COM_HWDMS_DETAILS'); ?> </dt>      
+        <?php if ($this->params->get('item_meta_author') != '0' || $this->params->get('item_meta_created') != '0') : ?>
         <dd class="media-info-meta">
-          <?php if ($this->params->get('list_meta_author') != '0') : ?>
+          <?php if ($this->params->get('item_meta_author') != '0') : ?>
             <span class="media-info-createdby">
               <?php echo JText::sprintf('COM_HWDMS_BY_X_USER', '<a href="'.JRoute::_(hwdMediaShareHelperRoute::getUserRoute($this->group->created_user_id)).'">'.htmlspecialchars($this->group->author, ENT_COMPAT, 'UTF-8').'</a>'); ?>
             </span>
           <?php endif; ?>
-          <?php if ($this->params->get('list_meta_created') != '0') : ?>
+          <?php if ($this->params->get('item_meta_created') != '0') : ?>
             <span class="media-info-created">
               <?php echo JHtml::_('hwddate.relative', $this->group->created); ?>
             </span>
@@ -166,16 +170,22 @@ $canManageMembers = ($this->group->isAdmin);
           <dd class="media-info-count label"><?php echo JText::plural('COM_HWDMS_X_MEDIA_COUNT', (int) $this->group->nummedia); ?></dd>
           <dd class="media-info-count label"><?php echo JText::plural('COM_HWDMS_X_MEMBER_COUNT', (int) $this->group->nummembers); ?></dd>
         <?php endif; ?>        
-        <div class="clearfix"></div>
-        <?php if ($this->params->get('item_meta_likes') != '0') :?>
-          <dd class="media-info-like"><a href="<?php echo JRoute::_('index.php?option=com_hwdmediashare&task=groups.like&id=' . $this->group->id . '&return=' . $this->return . '&tmpl=component&'.JSession::getFormToken().'=1'); ?>"><?php echo JText::_('COM_HWDMS_LIKE'); ?></a></dd>
-          <dd class="media-info-dislike"><a href="<?php echo JRoute::_('index.php?option=com_hwdmediashare&task=groups.dislike&id=' . $this->group->id . '&return=' . $this->return . '&tmpl=component&'.JSession::getFormToken().'=1'); ?>"><?php echo JText::_('COM_HWDMS_DISLIKE'); ?></a></dd>
-        <?php endif; ?>   
-        <?php if ($this->params->get('item_meta_report') != '0') :?>
-          <dd class="media-info-report"><a title="<?php echo JText::_('COM_HWDMS_REPORT'); ?>" href="<?php echo JRoute::_('index.php?option=com_hwdmediashare&task=groupform.report&id=' . $this->group->id . '&return=' . $this->return . '&tmpl=component'); ?>" class="media-popup-iframe-form"><?php echo JText::_('COM_HWDMS_REPORT'); ?></a></dd>
-        <?php endif; ?>          
+        <div class="clearfix"></div>         
       </dl>
       <?php endif; ?>  
+      <?php if ($this->params->get('item_meta_likes') != '0' || $this->params->get('item_meta_dislikes') != '0' || $this->params->get('item_meta_report') != '0') : ?>
+        <div class="btn-group">
+          <?php if ($this->params->get('item_meta_likes') != '0') : ?>
+            <a title="<?php echo JText::_('COM_HWDMS_LIKE'); ?>" href="<?php echo JRoute::_('index.php?option=com_hwdmediashare&task=groups.like&id=' . $this->group->id . '&return=' . $this->return . '&tmpl=component&'.JSession::getFormToken().'=1'); ?>" class="btn btn-mini" id="group-like-btn" data-media-task="like" data-media-id="<?php echo $this->group->id; ?>" data-media-return="<?php echo $this->return; ?>" data-media-token="<?php echo JSession::getFormToken(); ?>"><i class="icon-thumbs-up"></i> <?php echo JText::_('COM_HWDMS_LIKE'); ?></a>
+          <?php endif; ?>
+          <?php if ($this->params->get('item_meta_dislikes') != '0') : ?>
+            <a title="<?php echo JText::_('COM_HWDMS_DISLIKE'); ?>" href="<?php echo JRoute::_('index.php?option=com_hwdmediashare&task=groups.dislike&id=' . $this->group->id . '&return=' . $this->return . '&tmpl=component&'.JSession::getFormToken().'=1'); ?>" class="btn btn-mini" id="group-dislike-btn" data-media-task="dislike" data-media-id="<?php echo $this->group->id; ?>" data-media-return="<?php echo $this->return; ?>" data-media-token="<?php echo JSession::getFormToken(); ?>"><i class="icon-thumbs-down"></i> <?php echo JText::_('COM_HWDMS_DISLIKE'); ?></a>
+          <?php endif; ?>
+          <?php if ($this->params->get('item_meta_report') != '0') : ?>
+            <a title="<?php echo JText::_('COM_HWDMS_REPORT'); ?>" href="<?php echo JRoute::_('index.php?option=com_hwdmediashare&task=groupform.report&id=' . $this->group->id . '&return=' . $this->return . '&tmpl=component'); ?>" class="btn btn-mini media-popup-iframe-form" id="group-report-btn"><i class="icon-flag"></i> <?php echo JText::_('COM_HWDMS_REPORT'); ?></a>
+          <?php endif; ?>  
+        </div>          
+      <?php endif; ?>
       <!-- Tags -->
       <?php if ($this->params->get('show_tags', 1) && !empty($this->group->tags)) : ?>
 	<?php $this->group->tagLayout = new JLayoutFile('joomla.content.tags'); ?>
